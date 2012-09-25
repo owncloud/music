49d74b68e8daad8e95cb8b8d8ff0cd7071209cc9
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-25 02:06:22 +0200

    [tx-robot] updated from transifex

---
+++ l10n/es_AR.php
@@ -0,0 +1,14 @@
+<?php $TRANSLATIONS = array(
+"Music" => "Música",
+"Add album to playlist" => "Agregar álbum a la lista",
+"Play" => "Reproducir",
+"Pause" => "Pausa",
+"Previous" => "Anterior",
+"Next" => "Siguiente",
+"Mute" => "Silenciar",
+"Unmute" => "Quitar silencio",
+"Rescan Collection" => "Indexar colección nuevamente",
+"Artist" => "Artista",
+"Album" => "Álbum",
+"Title" => "Título"
+);

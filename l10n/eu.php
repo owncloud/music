9accfaf0e74a663fa4ff6c3bb5040de6007ede11
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-26 00:03:41 +0100

    [tx-robot] updated from transifex

--- l10n/eu.php
+++ l10n/eu.php
@@ -2,6 +2,7 @@
 "Music" => "Musika",
 "songs" => "abestiak",
 "albums" => "albumak",
+"Add to playlist" => "Gehitu erreprodukzio zerrendara",
 "Add album to playlist" => "Gehitu albuma erreprodukzio zerrendara",
 "Play" => "Erreproduzitu",
 "Pause" => "Pausarazi",

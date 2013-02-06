ac99cd837d7193107c763962dfe0494126a82fb5
Bernhard Posselt <nukeawhale@gmail.com>
2013-02-07 00:33:47 +0100

    test

--- l10n/ru.php
+++ l10n/ru.php
@@ -4,14 +4,16 @@
 "albums" => "альбомы",
 "Add to playlist" => "Добавить в плейлист",
 "Add album to playlist" => "Добавить альбом в плейлист",
+"Previous" => "Предыдущий",
 "Play" => "Проиграть",
 "Pause" => "Пауза",
-"Previous" => "Предыдущий",
 "Next" => "Следующий",
 "Mute" => "Отключить звук",
 "Unmute" => "Включить звук",
 "Rescan Collection" => "Пересканировать коллекцию",
 "Artist" => "Исполнитель",
 "Album" => "Альбом",
-"Title" => "Название"
+"Title" => "Название",
+"Media" => "Медиа",
+"Ampache address:" => "адрес Ampache:"
 );

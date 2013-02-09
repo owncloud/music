3dfc43a44f5801bb8b27f1b403fa3400c0bc446d
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-10 00:07:31 +0100

    [tx-robot] updated from transifex

--- l10n/vi.php
+++ l10n/vi.php
@@ -4,14 +4,16 @@
 "albums" => "album",
 "Add to playlist" => "Thêm vào danh sách",
 "Add album to playlist" => "Thêm album vào playlist",
+"Previous" => "Trang trước",
 "Play" => "Play",
 "Pause" => "Tạm dừng",
-"Previous" => "Trang trước",
 "Next" => "Tiếp theo",
 "Mute" => "Tắt",
 "Unmute" => "Bật",
 "Rescan Collection" => "Quét lại bộ sưu tập",
 "Artist" => "Nghệ sỹ",
 "Album" => "Album",
-"Title" => "Tiêu đề"
+"Title" => "Tiêu đề",
+"Media" => "Đa phương tiện",
+"Ampache address:" => "Địa chỉ Ampache:"
 );

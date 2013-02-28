f1faef792616fce4a94e3ce7ef7af33a50da36d3
Lukas Reschke <lukas@statuscode.ch>
2013-02-28 16:27:14 +0100

    Replace translation strings
    Grep: echo \$l\-\>t\(\'([^']*)\'\)
    Replace with:p(\$l\-\>t\(\'\1\'\)\)
    
    Grep: echo \$l\-\>t\(\"([^"]*)\"\)
    Replace with: p(\$l\-\>t\(\"\1\"\)\)

--- templates/settings.php
+++ templates/settings.php
@@ -1,7 +1,7 @@
 <form id="mediaform">
 	<fieldset class="personalblock">
-		<strong><?php echo $l->t('Media'); ?></strong><br />
-		<?php echo $l->t('Ampache address:'); ?>
+		<strong><?php p($l->t('Media')); ?></strong><br />
+		<?php p($l->t('Ampache address:')); ?>
 		<code><?php echo OCP\Util::linkToRemote('ampache'); ?></code><br />
 	</fieldset>
 </form>

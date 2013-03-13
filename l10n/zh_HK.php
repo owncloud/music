189c06276f644f90eae9e886d8bba15d6048e489
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-14 00:04:34 +0100

    [tx-robot] updated from transifex

---
+++ l10n/zh_HK.php
@@ -0,0 +1,19 @@
+<?php $TRANSLATIONS = array(
+"Music" => "音樂",
+"songs" => "音樂",
+"albums" => "唱片",
+"Add to playlist" => "加入播放列表",
+"Add album to playlist" => "把唱片加到播放列表",
+"Previous" => "上一首",
+"Play" => "播放",
+"Pause" => "暫停",
+"Next" => "下一首",
+"Mute" => "靜音",
+"Unmute" => "關閉靜音",
+"Rescan Collection" => "重新掃描",
+"Artist" => "演奏家",
+"Album" => "唱片",
+"Title" => "標題",
+"Media" => "媒體",
+"Ampache address:" => "Ampache 地址:"
+);

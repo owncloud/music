14811a80a8b71764960f77e3447f498db2498907
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-15 00:04:29 +0100

    [tx-robot] updated from transifex

--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -4,14 +4,16 @@
 "albums" => "专辑",
 "Add to playlist" => "添加到播放列表",
 "Add album to playlist" => "专辑加入到播放列表",
+"Previous" => "前一首",
 "Play" => "播放",
 "Pause" => "暂停",
-"Previous" => "前一首",
 "Next" => "后一首",
 "Mute" => "静音",
 "Unmute" => "取消静音",
 "Rescan Collection" => "重新扫描收藏",
 "Artist" => "艺术家",
 "Album" => "专辑",
-"Title" => "标题"
+"Title" => "标题",
+"Media" => "音乐",
+"Ampache address:" => "Ampache 地址："
 );

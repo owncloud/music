08da5df11ffff0645b4ca40dc16a32245a6a413c
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-03 01:00:23 +0200

    [tx-robot] updated from transifex

--- l10n/es.php
+++ l10n/es.php
@@ -3,7 +3,7 @@
 "songs" => "canciones",
 "albums" => "álbumes",
 "Add to playlist" => "Agregar a la lista de reproducción",
-"Add album to playlist" => "Añadir un Album a la lista.",
+"Add album to playlist" => "Añadir un álbum a la lista.",
 "Previous" => "Anterior",
 "Play" => "Reproducir",
 "Pause" => "Pausa",
@@ -12,7 +12,7 @@
 "Unmute" => "Quitar silencio",
 "Rescan Collection" => "Buscar canciones nuevas",
 "Settings" => "Ajustes",
-"Ampache address:" => "Direccion de Ampache:",
+"Ampache address:" => "Dirección de Ampache:",
 "Artist" => "Artista",
 "Album" => "Álbum",
 "Title" => "Título"

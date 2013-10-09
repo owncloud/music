da644411f2f8047d2e70a6e4c91075f533f93c40
Morris Jobke <morris.jobke@gmail.com>
2013-10-09 15:41:14 +0200

    fix english string

--- js/app/controllers/playercontroller.js
+++ js/app/controllers/playercontroller.js
@@ -68,7 +68,7 @@ angular.module('Music').controller('PlayerController',
 			if(mimeType === 'audio/ogg' && isChrome) {
 				// TODO inject this
 				OC.Notification.showHtml(gettext(
-					'Chrome is just able to playback MP3 files - see <a href="https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files">wiki</a>'
+					'Chrome is only able to playback MP3 files - see <a href="https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files">wiki</a>'
 				));
 			}
 			if(Audio.canPlayMIME(mimeType)) {

57fd9727942e91fcae01cd94a31bdcf535a9d18e
Morris Jobke <morris.jobke@gmail.com>
2013-10-16 17:58:16 +0200

    fix l10n script and add fake template for extraction

---
+++ templates/fake-template.php
@@ -0,0 +1,3 @@
+<!--  this template is just a fake template it's purpose is to get fetched
+while extraction, because this string is located in PHP code in
+appinfo/app.php --> <span translate>Music</span>

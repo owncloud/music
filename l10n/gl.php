55db0470670f6881c702e93b93cc67590c6cf16d
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-21 12:08:32 -0400

    [tx-robot] updated from transifex

--- l10n/gl.php
+++ l10n/gl.php
@@ -1,5 +1,17 @@
 <?php
 $TRANSLATIONS = array(
-"_Show all [[ trackcount ]] songs ..._::_Show all [[ trackcount ]] songs ..._" => array("","")
+"Music" => "Música",
+"Loading ..." => "Cargando ...",
+"Previous" => "Anterior",
+"Play" => "Reproducir",
+"Pause" => "Pausa",
+"Next" => "Seguinte",
+"Shuffle" => "Ao chou",
+"Repeat" => "Repetir",
+"Delete" => "Eliminar",
+"Nothing in here. Upload your music!" => "Aquí non hai nada. Envíe a súa música!",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Amosar todas as {{ trackcount }} cancións ...","Amosar todas as {{ trackcount }} cancións ..."),
+"Show less ..." => "Amosar menos ...",
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome só pode de reproducir ficheiros MP3 - vexa o <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

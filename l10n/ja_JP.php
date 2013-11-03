1003971ac4ed65ba650d49df38ddb79a17c7ea4e
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-11-03 12:25:45 -0500

    [tx-robot] updated from transifex

--- l10n/ja_JP.php
+++ l10n/ja_JP.php
@@ -11,6 +11,7 @@ $TRANSLATIONS = array(
 "Delete" => "削除",
 "Nothing in here. Upload your music!" => "ここには何もありません。ミュージックをアップロードしてください！",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("すべての {{ trackcount }} 曲を表示 ..."),
+"Show less ..." => "簡略表示 ...",
 "Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome は MP3 ファイルの再生のみ可能です - <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a> を参照してください。"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -1,5 +1,17 @@
 <?php
 $TRANSLATIONS = array(
-"_Show all [[ trackcount ]] songs ..._::_Show all [[ trackcount ]] songs ..._" => array("")
+"Music" => "音樂",
+"Loading ..." => "載入中…",
+"Previous" => "上一個",
+"Play" => "播放",
+"Pause" => "暫停",
+"Next" => "下一個",
+"Shuffle" => "隨機播放",
+"Repeat" => "重覆",
+"Delete" => "刪除",
+"Nothing in here. Upload your music!" => "這裡沒有東西，上傳你的音樂！",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("顯示全部 {{ trackcount }} 首歌曲"),
+"Show less ..." => "顯示更少",
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome 只能播放 MP3 檔案，請見 <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

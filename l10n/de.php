99c04a4fa0dde26f25773ccbcea9081796e62c00
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-11-04 04:03:53 -0500

    [tx-robot] updated from transifex

--- l10n/de.php
+++ l10n/de.php
@@ -1,5 +1,6 @@
 <?php
 $TRANSLATIONS = array(
+"Music" => "Musik",
 "Loading ..." => "Lade ...",
 "Previous" => "Zurück",
 "Play" => "Abspielen",
@@ -8,8 +9,9 @@ $TRANSLATIONS = array(
 "Shuffle" => "Zufallswiedergabe",
 "Repeat" => "Wiederholen",
 "Delete" => "Löschen",
-"Nothing in here. Upload your music!" => "Alles leer. Lade Deine Musik hoch!",
-"_Show all [[ trackcount ]] songs ..._::_Show all [[ trackcount ]] songs ..._" => array("[[ trackcount ]] Lied anzeigen  ...","Alle [[ trackcount ]] Lieder anzeigen  ..."),
-"Show less ..." => "Weniger anzeigen ..."
+"Nothing in here. Upload your music!" => "Alles leer. Laden Sie Ihre Musik hoch!",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("{{ trackcount }} Lied anzeigen  ...","Alle {{ trackcount }} Lieder anzeigen  ..."),
+"Show less ..." => "Weniger anzeigen ...",
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome ist nur in der Lage, MP3-Dateien wiederzugeben - siehe in das <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">Wiki</a>"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

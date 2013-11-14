0a0ec17a003499854dcdc6dc7002672be24091ef
Morris Jobke <morris.jobke@gmail.com>
2013-11-14 23:12:01 +0100

    typo

--- templates/ampache/error.php
+++ templates/ampache/error.php
@@ -1,5 +1,5 @@
 <?php
-echo "<?xml version="1.0" encoding="UTF-8" standalone="yes"?>";
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
 ?>
 <root>
 	<error code='<?php p($_['code']); ?>'><?php p($_['message']); ?></error>

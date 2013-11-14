3ca7139891e88d4c511df02213b9ed15c68af1c5
Morris Jobke <morris.jobke@gmail.com>
2013-11-14 23:06:52 +0100

    fix PHP shorttag

--- templates/ampache/error.php
+++ templates/ampache/error.php
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<?php
+echo "<?xml version="1.0" encoding="UTF-8" standalone="yes"?>";
+?>
 <root>
 	<error code='<?php p($_['code']); ?>'><?php p($_['message']); ?></error>
 </root>

bd7996e1a8f4999ad99c35e00291ce67cb2b060b
Mr. Jenkins <jenkins@owncloud.org>
2013-11-15 21:58:08 -0500

    [tx-robot] updated from transifex

--- l10n/sl.php
+++ l10n/sl.php
@@ -1,12 +1,17 @@
 <?php
 $TRANSLATIONS = array(
 "Music" => "Glasba",
+"Loading ..." => "Nalaganje ...",
 "Previous" => "Predhodna",
 "Play" => "Predvajaj",
 "Pause" => "Premor",
 "Next" => "Naslednja",
+"Shuffle" => "Premešaj",
 "Repeat" => "Ponovi",
 "Delete" => "Izbriši",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","","","")
+"Nothing in here. Upload your music!" => "V mapi ni glasbenih datotek. Dodajte skladbe!",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Pokaži {{ trackcount }} posnetek ...","Pokaži {{ trackcount }} posnetka ...","Pokaži {{ trackcount }} posnetke ...","Pokaži {{ trackcount }} posnetkov ..."),
+"Show less ..." => "Pokaži manj ...",
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Brskalnik Chrome lahko predvaja le datoteke MP3 - več podrobnosti je na straneh <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>."
 );
 $PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";

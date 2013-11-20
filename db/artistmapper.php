46eb470082274704cfc588693434d20a28d9129a
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-11-20 14:06:32 +0100

    add whitespace before FROM

--- db/artistmapper.php
+++ db/artistmapper.php
@@ -33,7 +33,7 @@ class ArtistMapper extends Mapper {
 	}
 
 	private function makeSelectQuery($condition=null){
-		return 'SELECT `artist`.`name`, `artist`.`image`, `artist`.`id`'.
+		return 'SELECT `artist`.`name`, `artist`.`image`, `artist`.`id` '.
 			'FROM `*PREFIX*music_artists` `artist` '.
 			'WHERE `artist`.`user_id` = ? ' . $condition;
 	}

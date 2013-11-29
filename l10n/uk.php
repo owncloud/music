c571915fa1262c51e6c1bf975592877874d614b6
Mr. Jenkins <jenkins@owncloud.org>
2013-11-29 13:15:59 -0500

    [tx-robot] updated from transifex

--- l10n/uk.php
+++ l10n/uk.php
@@ -1,13 +1,17 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome може програвати лише MP3 файли - дивись <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>",
 "Delete" => "Видалити",
 "Loading ..." => "Завантаження ...",
 "Music" => "Музика",
 "Next" => "Наступний",
+"Nothing in here. Upload your music!" => "Тут зараз нічого немає. Завантажте свою музику!",
 "Pause" => "Пауза",
 "Play" => "Грати",
 "Previous" => "Попередній",
 "Repeat" => "Повторювати",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","","")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Показати {{ trackcount }} пісню ...","Показати всі {{ trackcount }} пісні ...","Показати всі {{ trackcount }} пісні ..."),
+"Show less ..." => "Показати меньше ...",
+"Shuffle" => "Перемішати"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";

8433b67f2a2555751d425ef406f44817a87ab452
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-12-03 16:04:15 +0100

    start / stop implementation, filelist is playlist, no loop

--- js/public/musicFilePlayer.js
+++ js/public/musicFilePlayer.js
@@ -48,9 +48,79 @@ $(document).ready(function () {
 				}
 			});
 
-			var musicfiles = $('tr[data-mime^="audio/"]');
-			// add play button here
+			var playFile = function (filename) {
 				// trigger event for play/pause on click
-			
+				var filerow = $('#fileList').find('tr[data-file="'+filename+'"]');
+				var fileURL = filerow.find('a.name').attr('href');
+				var playAction = filerow.find('a[data-action="'+t('files', 'Play')+'"]');
+				var stopAction = filerow.find('a[data-action="'+t('files', 'Stop')+'"]');
+
+				soundManager.stopAll();
+				soundManager.destroySound('ownCloudSound');
+
+				soundManager.createSound({
+					id: 'ownCloudSound',
+					url: fileURL,
+					whileplaying: function() {
+						//$scope.setTime(this.position/1000, this.duration/1000);
+					},
+					onstop: function() {
+						stopAction.removeClass('permanent');
+						stopAction.hide();
+						playAction.show();
+					},
+					onplay: function() {
+						//hide allstop actions in fileList
+						$('#fileList').find('a[data-action="'+t('files', 'Stop')+'"]').hide();
+						playAction.hide();
+						stopAction.addClass('permanent');
+						stopAction.show();
+					},
+					onfinish: function() {
+						playAction.show();
+						var nextTrackId = false;
+						var playlist = $('tr[data-mime^="audio/"]');
+						jQuery.each (playlist, function(i,e) {
+							if ($(e).attr('data-file') === filename) {
+								nextTrackId = i+1;
+							}
+						});
+						if (nextTrackId && nextTrackId <= playlist.length) {
+							playFile($(playlist[nextTrackId]).attr('data-file'));
+						}
+					},
+					volume: 50
+				});
+				soundManager.play('ownCloudSound');
+			};
+
+			var stopPlayback = function (filename) {
+				var filerow = $('#fileList').find('tr[data-file="'+filename+'"]');
+				var playAction = filerow.find('a[data-action="'+t('files', 'Play')+'"]');
+				var stopAction = filerow.find('a[data-action="'+t('files', 'Stop')+'"]');
+				soundManager.togglePause('ownCloudSound');
+				stopAction.removeClass('permanent');
+				stopAction.hide();
+				playAction.show();
+			};
+
+			// add play button here
+			FileActions.register('audio', t('files', 'Play'), OC.PERMISSION_READ, function () {
+				return OC.imagePath('music', 'play-big');
+			}, playFile);
+
+			// add play button here
+			FileActions.register('audio', t('files', 'Stop'), OC.PERMISSION_READ, function () {
+				return OC.imagePath('music', 'pause-big');
+			}, stopPlayback);
+
+			var musicfiles = $('tr[data-mime^="audio/"]');
+			//redisplay file actions on music files
+			musicfiles.each(function () {
+				FileActions.display($(this).children('td.filename'));
+				//hide pause by default
+				$(this).find('a[data-action="'+t('files', 'Stop')+'"]').hide();
+			});
+
 		});
 });

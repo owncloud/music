7d50988873f3ccb7684393620fa738b098f5b3ea
Mr. Jenkins <jenkins@owncloud.org>
2013-12-17 05:49:13 -0500

    [tx-robot] updated from transifex

--- l10n/hu_HU.php
+++ l10n/hu_HU.php
@@ -1,12 +1,17 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Csak a Chrome képes lejátszani MP3 fileokat - nézd meg a <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki-t</a>",
 "Delete" => "Törlés",
+"Loading ..." => "Betöltés ...",
 "Music" => "Zene",
 "Next" => "Következő",
+"Nothing in here. Upload your music!" => "Nincs itt semmi. Töltsd fel a zenédet!",
 "Pause" => "Szünet",
 "Play" => "Lejátszás",
 "Previous" => "Előző",
 "Repeat" => "Ismétlés",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Mutasd mind a {{ trackcount }} zenét ...","Mutasd mind a {{ trackcount }} zenét ..."),
+"Show less ..." => "Mutass kevesebbet...",
+"Shuffle" => "Keverés"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

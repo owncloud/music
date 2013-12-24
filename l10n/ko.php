a6c5ad9e06434bc91ee8d2a57799bdc0d6218e8f
Mr. Jenkins <jenkins@owncloud.org>
2013-12-24 01:30:40 -0500

    [tx-robot] updated from transifex

--- l10n/ko.php
+++ l10n/ko.php
@@ -1,17 +1,17 @@
 <?php
 $TRANSLATIONS = array(
-"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "구글 크롬만이 MP3 파일을 재생할수 잇습니다 - <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>를 보세요",
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "구글 Chrome에서만 MP3 파일을 재생할 수 있습니다. 자세한 정보는<a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">위키</a>를 참고하십시오.",
 "Delete" => "삭제",
 "Loading ..." => "불러오는 중...",
 "Music" => "음악",
 "Next" => "다음",
-"Nothing in here. Upload your music!" => "아무것도 들어있지 않아요! 업로드 해주세요!",
+"Nothing in here. Upload your music!" => "아무 것도 없습니다. 음악을 업로드하십시오!",
 "Pause" => "일시 정지",
 "Play" => "재생",
 "Previous" => "이전",
 "Repeat" => "반복",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("전체 {{ trackcount }} 곡 보이기"),
-"Show less ..." => "그외 곡들 보여주기...",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("모든 {{ trackcount }} 곡 보기..."),
+"Show less ..." => "덜 보기...",
 "Shuffle" => "임의 재생"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

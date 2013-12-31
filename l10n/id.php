43c597afdb61330ca4eb58623284c0036aaca96a
Mr. Jenkins <jenkins@owncloud.org>
2013-12-31 01:30:45 -0500

    [tx-robot] updated from transifex

--- l10n/id.php
+++ l10n/id.php
@@ -1,12 +1,17 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome hanya dapat memutar berkas MP3 - lihat <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>",
 "Delete" => "Hapus",
+"Loading ..." => "Memuat ...",
 "Music" => "Musik",
 "Next" => "Selanjutnya",
+"Nothing in here. Upload your music!" => "Tidak ada apapun disini. Unggah musik anda!",
 "Pause" => "Jeda",
 "Play" => "Putar",
 "Previous" => "Sebelumnya",
 "Repeat" => "Ulangi",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Menampilkan semua {{ trackcount }} lagu ..."),
+"Show less ..." => "Menampilkan ringkas ...",
+"Shuffle" => "Acak"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

036bf63b8f1e91bd5b90ee7e6f22f893e0f17313
Mr. Jenkins <jenkins@owncloud.org>
2014-01-05 01:30:58 -0500

    [tx-robot] updated from transifex

--- l10n/sk.php
+++ l10n/sk.php
@@ -1,5 +1,7 @@
 <?php
 $TRANSLATIONS = array(
+"Delete" => "Odstrániť",
+"Repeat" => "Opakovať",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","","")
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";

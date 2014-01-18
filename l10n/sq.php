b7117c21e58119cb390f2ba14496a0dace9eaf49
Mr. Jenkins <jenkins@owncloud.org>
2014-01-18 01:30:39 -0500

    [tx-robot] updated from transifex

--- l10n/sq.php
+++ l10n/sq.php
@@ -1,12 +1,16 @@
 <?php
 $TRANSLATIONS = array(
 "Delete" => "Elimino",
+"Loading ..." => "Ngarkim...",
 "Music" => "Muzikë",
 "Next" => "Mëpasshëm",
+"Nothing in here. Upload your music!" => "Këtu nuk ka asgjë. Ngarkoni muziken tuaj!",
 "Pause" => "Pauzë",
 "Play" => "Luaj",
 "Previous" => "Mëparshëm",
 "Repeat" => "Përsëritet",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("",""),
+"Show less ..." => "Shfaq m'pak...",
+"Shuffle" => "Përziej"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

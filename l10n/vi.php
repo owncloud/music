d7bba58f17d64f44eea121e4dda9d2f5043a7c68
Mr. Jenkins <jenkins@owncloud.org>
2014-01-19 01:30:43 -0500

    [tx-robot] updated from transifex

--- l10n/vi.php
+++ l10n/vi.php
@@ -1,12 +1,17 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome chỉ có thể phát các tập tin MP3 - xem thêm <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>",
 "Delete" => "Xóa",
+"Loading ..." => "Đang nạp ...",
 "Music" => "Âm nhạc",
 "Next" => "Kế tiếp",
+"Nothing in here. Upload your music!" => "Không có gì ở đây. Hãy tải nhạc của bạn lên!",
 "Pause" => "Tạm dừng",
 "Play" => "Play",
 "Previous" => "Lùi lại",
 "Repeat" => "Lặp lại",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Hiển thị tất cả {{ trackcount }} bài hát ..."),
+"Show less ..." => "Hiển thị ít hơn ...",
+"Shuffle" => "Ngẫu nhiên"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

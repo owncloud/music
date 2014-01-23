720f1cb449681a5aee185165df9070b7fa70c15b
Mr. Jenkins <jenkins@owncloud.org>
2014-01-23 01:30:43 -0500

    [tx-robot] updated from transifex

--- l10n/es_AR.php
+++ l10n/es_AR.php
@@ -1,12 +1,19 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome sólo puede reproducir archivos MP3 - véase <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">la wiki</a>",
 "Delete" => "Borrar",
+"Loading ..." => "Cargando...",
 "Music" => "Música",
 "Next" => "Siguiente",
+"Nothing in here. Upload your music!" => "No hay nada aquí. ¡Suba su música!",
 "Pause" => "Pausar",
 "Play" => "Reproducir",
 "Previous" => "Previo",
 "Repeat" => "Repetir",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Mostrar la única canción...","Mostrar las {{ trackcount }} canciones..."),
+"Show less ..." => "Mostrar menos...",
+"Shuffle" => "Aleatorio",
+"Unknown album" => "Album desconocido",
+"Unknown artist" => "Artista desconocido"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

6bf2339b2f79a4425e64095e6ffd5ae39dce76b9
Mr. Jenkins <jenkins@owncloud.org>
2014-01-24 01:35:25 -0500

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -12,6 +12,8 @@ $TRANSLATIONS = array(
 "Repeat" => "Répéter",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Afficher le morceau {{ trackcount }}...","Afficher les {{ trackcount }} morceaux..."),
 "Show less ..." => "Afficher moins…",
-"Shuffle" => "Lecture aléatoire"
+"Shuffle" => "Lecture aléatoire",
+"Unknown album" => "Album inconnu",
+"Unknown artist" => "Artiste inconnu"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";

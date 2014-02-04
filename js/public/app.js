ed90f782290ed3aa6ddf8df28797bba344b2c23d
Pellaeon Lin <nfsmwlin@gmail.com>
2014-02-04 23:36:08 +0800

    Increase chunkSize

--- js/public/app.js
+++ js/public/app.js
@@ -287,7 +287,7 @@ angular.module('Music').controller('PlayerController',
 			});
 			$scope.player.play('ownCloudSound');*/
 			$scope.player=AV.Player.fromURL($scope.getPlayableFileURL($scope.currentTrack));
-			$scope.player.asset.source.chunkSize=5120;
+			$scope.player.asset.source.chunkSize=1048576;
 			$scope.setBuffering(true);
 			
 			$scope.player.play();

81b4cdbaa35510a167435d5f9eff3339b4f5d57e
Mr. Jenkins <jenkins@owncloud.org>
2014-02-15 06:24:04 -0500

    [tx-robot] updated from transifex

--- l10n/de_AT.php
+++ l10n/de_AT.php
@@ -1,5 +1,12 @@
 <?php
 $TRANSLATIONS = array(
+"Delete" => "Löschen",
+"Music" => "Musik",
+"Next" => "Nächstes",
+"Pause" => "Pause",
+"Play" => "Abspielen",
+"Previous" => "Vorheriges",
+"Repeat" => "Wiederholen",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

7c0bdbdfe4369f8dddcd012f46f06910fd08c971
Mr. Jenkins <jenkins@owncloud.org>
2014-02-16 01:32:01 -0500

    [tx-robot] updated from transifex

--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -1,12 +1,19 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome 只能播放 MP3 文件 - 见 <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">Wiki 文档</a>",
 "Delete" => "删除",
+"Loading ..." => "加载中...",
 "Music" => "音乐",
 "Next" => "下一个",
+"Nothing in here. Upload your music!" => "这里还什么都没有。上传你的音乐吧！",
 "Pause" => "暂停",
 "Play" => "播放",
 "Previous" => "前一首",
 "Repeat" => "重复",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("显示所有 {{ trackcount }} 首歌曲 ..."),
+"Show less ..." => "显示概要",
+"Shuffle" => "随机",
+"Unknown album" => "未知专辑",
+"Unknown artist" => "未知艺术家"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

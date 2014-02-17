0dec52e74367462b39f60651328665006d1ef6a1
Mr. Jenkins <jenkins@owncloud.org>
2014-02-17 03:16:23 -0500

    [tx-robot] updated from transifex

--- l10n/de_AT.php
+++ l10n/de_AT.php
@@ -1,12 +1,19 @@
 <?php
 $TRANSLATIONS = array(
+"Chrome is only able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>" => "Chrome ist nur in der Lage, MP3-Dateien wiederzugeben - siehe <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a>\nChrome is just able to playback MP3 files - see <a href=\"https://github.com/owncloud/music/wiki/Frequently-Asked-Questions#why-can-chromechromium-just-playback-mp3-files\">wiki</a> ",
 "Delete" => "Löschen",
+"Loading ..." => "Lade ...",
 "Music" => "Musik",
 "Next" => "Nächstes",
+"Nothing in here. Upload your music!" => "Alles leer. Lade deine Musik hoch!",
 "Pause" => "Pause",
 "Play" => "Abspielen",
 "Previous" => "Vorheriges",
 "Repeat" => "Wiederholen",
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("{{ trackcount }} Lied anzeigen ...","Alle {{ trackcount }} Lieder anzeigen ..."),
+"Show less ..." => "Weniger anzeigen ...",
+"Shuffle" => "Zufallswiedergabe",
+"Unknown album" => "Unbekanntes Album",
+"Unknown artist" => "Unbekannter Künstler"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

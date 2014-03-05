ef3e7de36747ec2f4ed87d4fcf56d17485dd1482
Armin <armin@webhippie.de>
2014-03-05 20:02:09 +0100

    'now playing' fixes and cleaner button css structure in artist-details when song is playing

--- templates/artist-detail.php
+++ templates/artist-detail.php
@@ -1,21 +1,32 @@
 <div>
   <div class="navbar navbar-default navbar-fixed-top interpret">
-    <div class="row">
-      <div class="col-xs-4">
-          <a class="btn btn-default navbar-btn btn-info" ng-click="showArtists()">
-            <img alt="{{'Previous' | translate }}" src="<?php p(OCP\image_path('music', 'new/angle_left.svg')) ?>" />
-            Artists
+    <!--div class="row">
+      <div class="col-xs-4"-->
+          <a class="btn btn-default navbar-btn btn-info left-btn" ng-click="showArtists()">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="btn-label">
+              Artists
+            </span>
           </a>
-      </div>
+      <!--/div>
       <div class="col-xs-4 col-xs-offset-4">
-        <a class="btn btn-default navbar-btn btn-info playing-btn right-block" ng-click="showPlayer()" ng-show="currentTrack">
+        <!--a class="btn btn-default navbar-btn btn-info playing-btn right-block" ng-click="showPlayer()" ng-show="currentTrack">
           <img alt="{{'Previous' | translate }}"
                 src="<?php p(OCP\image_path('music', 'new/angle_right.svg')) ?>" />
           <span> Now <br/> 
             Playing
           </span>  
+        </a-->
+
+
+        <a class="btn btn-default navbar-btn btn-info playing-btn right-btn" ng-click="showPlayer()" ng-show="currentTrack">
+          <span class="btn-label"> 
+            Now <br/> 
+            Playing
+          </span>
+          <span class="glyphicon glyphicon-chevron-right"></span>
         </a>
-    </div>
+    <!--/div-->
     </div>
   </div>
 

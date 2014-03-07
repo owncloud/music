1f3aba1dfd88a9bbc17a6ae803116150244de77a
Danyel Bayraktar <danyel@webhippie.de>
2014-03-07 06:20:01 +0100

    app.js updated

--- js/public/app.js
+++ js/public/app.js
@@ -360,7 +360,7 @@ angular.module('Music').controller('PlayerController',
 				url: $scope.getPlayableFileURL($scope.currentTrack),
 				whileplaying: function() {
 					console.log("pos:", this.position, "dur:", this.duration, "est:", this.durationEstimate, "bytesTotal:", this.bytesTotal, "bytesLoaded", this.bytesLoaded);
-					$scope.setTime(this.position, this.duration || 0);
+					$scope.setTime(this.position, this.durationEstimate || this.duration || 0);
 				},
 				onstop: function() {
 					$scope.setPlay(false);

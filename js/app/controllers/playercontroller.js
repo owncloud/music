58847235c547936cd1f4f6c62653af7006d1f66a
leizh <leizh@free.fr>
2014-03-18 18:35:11 +0100

    Fix the seek on Firefox

--- js/app/controllers/playercontroller.js
+++ js/app/controllers/playercontroller.js
@@ -368,9 +368,10 @@ angular.module('Music').controller('PlayerController',
 	};
 
 	$scope.seek = function($event) {
-		var sound = $scope.player.sounds.ownCloudSound;
-		sound.setPosition($event.offsetX * sound.durationEstimate / $event.currentTarget.clientWidth);
-	};
+		var sound = $scope.player.sounds.ownCloudSound,
+			offsetX = $event.offsetX || $event.originalEvent.layerX;
+		sound.setPosition(offsetX * sound.durationEstimate / $event.currentTarget.clientWidth);
+        };
 	
 	playlistService.subscribe('play', function(){
 		// fetch track and start playing

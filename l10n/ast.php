fa6192eb57c1db52b3ddeaf118de180d6a08552f
Mr. Jenkins <jenkins@owncloud.org>
2014-04-08 01:31:36 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ast.php
@@ -0,0 +1,8 @@
+<?php
+$TRANSLATIONS = array(
+"Delete" => "Desaniciar",
+"Description" => "Descripción",
+"Repeat" => "Repitir",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/jv.php
@@ -0,0 +1,9 @@
+<?php
+$TRANSLATIONS = array(
+"Music" => "Gamelan",
+"Next" => "Sak bare",
+"Play" => "Puter",
+"Previous" => "Sak durunge",
+"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

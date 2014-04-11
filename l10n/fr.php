f2480a61edde7323615833fd4cdb1f0c8e1de503
Mr. Jenkins <jenkins@owncloud.org>
2014-04-11 01:30:14 -0400

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -4,7 +4,11 @@ $TRANSLATIONS = array(
 "Artists" => "Artistes",
 "Delete" => "Supprimer",
 "Description" => "Description",
+"Description (e.g. App name)" => "Description (ex. nom de l'application)",
+"Generate API password" => "Générer un mot de passe de l'API",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Ici, vous pouvez générer des mots de passe à utiliser avec l'API Ampache, parce qu'ils ne peuvent être stockés d'une manière très sécurisée en raison de la conception de l'API d'Ampache. Vous pouvez générer autant de mots de passe que vous voulez et vous pouvez les révoquer à tout instant.",
 "Invalid path" => "Chemin invalide",
+"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Gardez en mémoire que l'API Ampache est une avant-première et n'est pas encore stable. N'hésitez pas à donner un retour d'expérience de cette fonctionnalité dans l'<a href=\"https://github.com/owncloud/music/issues/60\">item</a> dédié. On aimerait également obtenir une liste des clients avec lesquels tester. Merci.",
 "Loading ..." => "Chargement…",
 "Music" => "Musique",
 "Next" => "Suivant",
@@ -14,11 +18,15 @@ $TRANSLATIONS = array(
 "Play" => "Lire",
 "Previous" => "Précédent",
 "Repeat" => "Répéter",
+"Revoke API password" => "Révoquer le mot de passe de l'API",
+"Scanning ..." => "Analyse...",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Afficher le morceau {{ trackcount }}...","Afficher les {{ trackcount }} morceaux..."),
 "Show less ..." => "Afficher moins…",
 "Shuffle" => "Lecture aléatoire",
+"This setting restricts the shown music in the web interface of the music app." => "Ce paramètre restreint les musiques affichées dans l'interface web de l'application de musique.",
 "Tracks" => "Pistes",
 "Unknown album" => "Album inconnu",
-"Unknown artist" => "Artiste inconnu"
+"Unknown artist" => "Artiste inconnu",
+"Use your username and following password to connect to this Ampache instance:" => "Utilisez votre nom d'utilisateur et le mot de passe suivant pour vous connecter à cette instance d'Ampache : "
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
--- l10n/it.php
+++ l10n/it.php
@@ -6,9 +6,9 @@ $TRANSLATIONS = array(
 "Description" => "Descrizione",
 "Description (e.g. App name)" => "Descrizione (ad es. Nome applicazione)",
 "Generate API password" => "Genera una password API",
-"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Qui puoi creare le password da usare con l'API di Ampache, perché esse non possono essere memorizzare in maniera sicura a causa della forma dell'API di Ampache. Puoi generare tutte le password che vuoi e puoi riprenderle quando vuoi.",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Qui puoi generare le password da utilizzare con l'API di Ampache, perché esse non possono essere memorizzate in maniera sicura a causa della forma dell'API di Ampache. Puoi generare tutte le password che vuoi e revocarle quando vuoi.",
 "Invalid path" => "Percorso non valido",
-"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Ricorda, l'API di Ampache è solo un'anteprima e non è stabile. Sentiti libero nel riportare la tua esperienza con questa funzione nel corrispondente <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. Preferirei anche avere una lista di clienti da testare. Grazie.",
+"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Ricorda, l'API di Ampache è solo un'anteprima e non è stabile. Sentiti libero di segnalare la tua esperienza con questa funzionalità nel corrispondente <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. Preferirei inoltre avere un elenco di client da provare. Grazie.",
 "Loading ..." => "Caricamento in corso...",
 "Music" => "Musica",
 "Next" => "Successivo",
@@ -18,7 +18,7 @@ $TRANSLATIONS = array(
 "Play" => "Riproduci",
 "Previous" => "Precedente",
 "Repeat" => "Ripeti",
-"Revoke API password" => "Riprendi la password API",
+"Revoke API password" => "Revoca la password API",
 "Scanning ..." => "Scansione in corso...",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Mostra {{ trackcount }} brano...","Mostra tutti i {{ trackcount }} brani..."),
 "Show less ..." => "Mostra meno...",
@@ -27,6 +27,6 @@ $TRANSLATIONS = array(
 "Tracks" => "Tracce",
 "Unknown album" => "Album sconosciuto",
 "Unknown artist" => "Artista sconosciuto",
-"Use your username and following password to connect to this Ampache instance:" => "Usa il tuo username e la password per entrare in questo caso in Ampache:"
+"Use your username and following password to connect to this Ampache instance:" => "Utilizza il tuo nome utente e la password per collegarti a questa istanza di Ampache:"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

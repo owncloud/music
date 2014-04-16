676c5286110a2ba9125248cd9d6353f790be82bb
Mr. Jenkins <jenkins@owncloud.org>
2014-04-16 01:31:10 -0400

    [tx-robot] updated from transifex

--- l10n/ca.php
+++ l10n/ca.php
@@ -1,19 +1,32 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "Àlbums",
+"Artists" => "Artistes",
 "Delete" => "Esborra",
 "Description" => "Descripció",
+"Description (e.g. App name)" => "Descripció (per exemple nom de l'aplicació)",
+"Generate API password" => "Genera contrasenya API",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Aquí podeu generar contrasenyes per usar amb l'API d'Ampache,ja que no es poden desar de forma segura degut al diseny de l'API d'Ampache. Podeu generar tantes contrasenyes com volgueu i revocar-les en qualsevol moment.",
+"Invalid path" => "El camí no és vàlid",
+"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Recordeu que l'API d'Ampache és només una previsualització i que és inestable. Sou lliures d'informar de la vostra experiència amb aquesta característica en el <a href=\"https://github.com/owncloud/music/issues/60\">fil</a> corresponent. També voldríem tenir una llista de clients per fer proves. Gràcies.",
 "Loading ..." => "Carregant...",
 "Music" => "Música",
 "Next" => "Següent",
 "Nothing in here. Upload your music!" => "Res per aquí. Pugeu la vostra música!",
+"Path to your music collection" => "Camí a la col·lecció de música",
 "Pause" => "Pausa",
 "Play" => "Reprodueix",
 "Previous" => "Anterior",
 "Repeat" => "Repeteix",
+"Revoke API password" => "Revoca la cotrasenya de l'API",
+"Scanning ..." => "Escanejant ...",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Mostra totes les {{ trackcount }} peces...","Mostra totes les {{ trackcount }} peces..."),
 "Show less ..." => "Mostra'n menys...",
 "Shuffle" => "Aleatori",
+"This setting restricts the shown music in the web interface of the music app." => "Aquesta configuració restringeix la música que es mostra a la interfície web de l'aplicació de música.",
+"Tracks" => "Peces",
 "Unknown album" => "Àlbum desconegut",
-"Unknown artist" => "Artista desconegut"
+"Unknown artist" => "Artista desconegut",
+"Use your username and following password to connect to this Ampache instance:" => "Useu el vostre nom d'usuari i contrasenya per connectar amb la instància Ampache:"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

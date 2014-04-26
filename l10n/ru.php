21fe21de2559a57563256eb26b67721bb2e39669
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-26 01:30:01 -0400

    [tx-robot] updated from transifex

--- l10n/ru.php
+++ l10n/ru.php
@@ -1,18 +1,26 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "Альбомы",
+"Artists" => "Исполнители",
 "Delete" => "Удалить",
 "Description" => "Описание",
+"Generate API password" => "Генерация пароля для API",
+"Invalid path" => "Некорректный путь",
 "Loading ..." => "Загружается...",
 "Music" => "Музыка",
 "Next" => "Следующий",
 "Nothing in here. Upload your music!" => "Здесь ничего нет. Загрузите вашу музыку!",
+"Path to your music collection" => "Путь до вашей музыкальной коллекции",
 "Pause" => "Пауза",
 "Play" => "Проиграть",
 "Previous" => "Предыдущий",
 "Repeat" => "Повтор",
+"Revoke API password" => "Отозвать пароль для API",
+"Scanning ..." => "Поиск ...",
 "_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("Показать {{ trackcount }} песню...","Показать все {{ trackcount }} песни...","Показать все {{ trackcount }} песен..."),
 "Show less ..." => "Показать меньше...",
 "Shuffle" => "Перемешать",
+"Tracks" => "Композиции",
 "Unknown album" => "Неизвестный альбом",
 "Unknown artist" => "Неизвестный исполнитель"
 );

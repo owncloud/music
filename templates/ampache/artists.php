be37eb5f50bcf246b3fa6a8dad3d23a51c242f5f
Morris Jobke <morris.jobke@gmail.com>
2014-05-01 10:54:58 +0200

    fix unescaped print of artistName in ampache API

--- templates/ampache/artists.php
+++ templates/ampache/artists.php
@@ -4,7 +4,7 @@ print '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
 <root>
 	<?php foreach ($_['artists'] as $artist): ?>
 		<artist id='<?php p($artist->getId());?>'>
-			<name><?php print_unescaped($artist->getName());?></name>
+			<name><?php p($artist->getName());?></name>
 			<albums><?php p($artist->getAlbumCount());?></albums>
 			<songs><?php p($artist->getTrackCount());?></songs>
 			<rating>0</rating>

8277e2b29b42ec9fd4f6ed523c1b9c71f8089340
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-24 01:29:36 -0400

    [tx-robot] updated from transifex

--- l10n/ro.php
+++ l10n/ro.php
@@ -1,12 +1,20 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "Albume",
+"Artists" => "Artiști",
 "Delete" => "Șterge",
 "Description" => "Descriere",
+"Description (e.g. App name)" => "Descriere (ex. Numele aplicației)",
+"Generate API password" => "Generează parola API",
+"Invalid path" => "Cale invalidă",
 "Music" => "Muzică",
 "Next" => "Următor",
+"Path to your music collection" => "Calea spre colecția cu muzica dvs.",
 "Pause" => "Pauză",
 "Play" => "Redă",
 "Previous" => "Anterior",
-"Repeat" => "Repetă"
+"Repeat" => "Repetă",
+"Unknown album" => "Album necunoscut",
+"Unknown artist" => "Artist necunoscut"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1?0:(((n%100>19)||((n%100==0)&&(n!=0)))?2:1));";

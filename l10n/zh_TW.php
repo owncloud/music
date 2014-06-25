3adf75b08901a317b895691b2d15be789819186a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-25 06:32:13 -0400

    [tx-robot] updated from transifex

--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -1,7 +1,12 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "專輯",
+"Artists" => "歌手",
 "Delete" => "刪除",
 "Description" => "描述",
+"Description (e.g. App name)" => "描述 (例: App 名稱)",
+"Generate API password" => "產生 API 密碼",
+"Invalid path" => "無效的路徑",
 "Loading ..." => "載入中…",
 "Music" => "音樂",
 "Next" => "下一個",
@@ -9,6 +14,11 @@ $TRANSLATIONS = array(
 "Play" => "播放",
 "Previous" => "上一個",
 "Repeat" => "重覆",
-"Shuffle" => "隨機播放"
+"Revoke API password" => "撤銷 API 密碼",
+"Shuffle" => "隨機播放",
+"Some not playable tracks were skipped." => "部份無法播放的曲目已跳過。",
+"Tracks" => "曲目",
+"Unknown album" => "未知的專輯",
+"Unknown artist" => "未知的歌手"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

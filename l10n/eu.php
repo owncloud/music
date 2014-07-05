63531b3e70b6d6979c23fd01d3f6ba719d34a81c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-05 01:29:37 -0400

    [tx-robot] updated from transifex

--- l10n/eu.php
+++ l10n/eu.php
@@ -1,16 +1,27 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "Diskak",
+"Artists" => "Artistak",
 "Delete" => "Ezabatu",
 "Description" => "Deskribapena",
+"Description (e.g. App name)" => "Deskribapena (adb. App izena)",
+"Generate API password" => "Sortu API pasahitza",
+"Invalid path" => "Baliogabeko bidea",
 "Loading ..." => "Kargatzen...",
 "Music" => "Musika",
 "Next" => "Hurrengoa",
+"Path to your music collection" => "Musika bildumaren bidea",
 "Pause" => "Pausarazi",
 "Play" => "Erreproduzitu",
 "Previous" => "Aurrekoa",
 "Repeat" => "Errepikatu",
 "Shuffle" => "Nahastu",
+"Some not playable tracks were skipped." => "Erreproduzitu ezin ziren pista batzuk saltatu egin dira.",
+"This setting specifies the folder which will be scanned for music." => "Hemen musika bilatuko den karpetak zehazten dira.",
+"Tracks" => "Pistak",
 "Unknown album" => "Diska ezezaguna",
-"Unknown artist" => "Artista ezezaguna"
+"Unknown artist" => "Artista ezezaguna",
+"Use this address to browse your music collection from any Ampache compatible player." => "Erabili helbide hau zure musika bilduma Ampacherekin bateragarria den edozein erreproduktorekin arakatzeko.",
+"Use your username and following password to connect to this Ampache instance:" => "Erabili zure erabiltzaile izena eta hurrengo pasahitza Ampache honetara konektatzeko:"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

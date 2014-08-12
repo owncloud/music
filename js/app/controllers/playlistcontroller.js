3ecdbdec6852ae49cd31e81cf9a561024e0ef96c
Morris Jobke <hey@morrisjobke.de>
2014-08-12 09:09:38 +0200

    Refactoring for production use
    * removal of unused CSS classes
    * drop debug output
    * drop PlaylistFactory
    * renamings of playlist controller functions
    * don't reload playlist information after change(via REST-API) - just apply the change to the local object
    * better variable namings
    * coding style
    * pass in objects instead of just the id and then searching for the object again

--- js/app/controllers/playlistcontroller.js
+++ js/app/controllers/playlistcontroller.js
@@ -22,138 +22,96 @@
 
 
 angular.module('Music').controller('PlaylistController',
-	['$rootScope', '$scope', '$routeParams', 'PlaylistFactory', 'playlistService', 'gettextCatalog', 'Restangular', '$timeout',
-	function ($rootScope, $scope, $routeParams, PlaylistFactory, playlistService, gettextCatalog, Restangular , $timeout) {
+	['$rootScope', '$scope', '$routeParams', 'playlistService', 'gettextCatalog', 'Restangular', '$timeout',
+	function ($rootScope, $scope, $routeParams, playlistService, gettextCatalog, Restangular , $timeout) {
 
 		$scope.playlistSongs = [];
 		$scope.playlists = [];
 
-		$scope.newPlaylistForm = {
-			name: null,
-		};
+		$scope.newPlaylistName = null;
 
-		// Create playlist by providing the name
-		$scope.createPlaylist = function(playlist) {
-			var playlists = Restangular.all('playlists');
-			playlists.post(playlist).then(function(){
-				$scope.playlists = [];
-				console.log("new plist:" +arguments);
+		$scope.showCreateForm = false;
+		$scope.showEditForm = null;
 
-				$scope.getPlaylists();
-				$scope.newPlaylistForm = {
-					name: null,
-				};
+		// create playlist
+		$scope.create = function(playlist) {
+			Restangular.all('playlists').post({name: $scope.newPlaylistName}).then(function(playlist){
+				$scope.playlists.push(playlist);
+				$scope.newPlaylistName = null;
 			});
 
+			$scope.showCreateForm = false;
 		};
 
-		// Return all playlists
-		$scope.getPlaylists = function() {
-			Restangular.all('playlists').getList().then(function(getPlaylists){
-				var plist;
-				for(var i=0; i < getPlaylists.length; i++) {
-					plist = getPlaylists[i];
-					$scope.playlists.push(plist);
-				}
-				return $scope.playlists;
-
-				}, function error(reason) {
-					console.log("Cannot get playlists due to: " + reason);
+		// load all playlists
+		$scope.load = function() {
+			Restangular.all('playlists').getList().then(function(playlists){
+				$scope.playlists = playlists;
 			});
 		};
 
-		// Return playlist and its songs
+		// fetch playlist and its songs
 		$scope.getPlaylist = function(id) {
-			var playlist = Restangular.one('playlists', id).get().then(function(playlist){
-
-				console.log("==========HERE GOES THE LIST "+id+"=============");
-				console.log("name: "+playlist.name);
-				$scope.currentPlaylistName = playlist.name;
-				console.log("trackIds: "+playlist.trackIds);
-				$scope.currentPlaylistSongs = playlist.trackIds;
-				console.log("=======================");
-
+			Restangular.one('playlists', id).get().then(function(playlist){
+				$scope.currentPlaylist = playlist;
 			});
 		};
 
 		// Rename playlist
-		$scope.updatePlaylist = function(id, newName) {
-			Restangular.one('playlists', id).get().then(function(playlist){
-				var playlists = Restangular.one('playlists', id);
-				playlists.name = newName;
-				playlists.put();
-				$scope.playlists = [];
-				$scope.getPlaylists();
-				OC.Notification.show(t('music', '{oldName} has been renamed to {newPlaylistName}', {oldName: playlist.name, newPlaylistName: newName}));
-				$timeout(OC.Notification.hide, 5000);
-			});
+		$scope.update = function(playlist) {
+			// change of the attribute happens in form
+			playlist.put();
 
+			$scope.showEditForm = false;
 		};
 
 		// Remove playlist
-		$scope.removePlaylist = function(id) {
-			Restangular.one('playlists', id).get().then(function(playlist){
-				var pl = Restangular.one('playlists', id);
-				pl.remove().then(function(){
-					OC.Notification.show(t('music', 'Playlist {playlistName} removed', {playlistName: playlist.name}));
-					$timeout(OC.Notification.hide, 5000);
-					$scope.playlists = [];
-					$scope.getPlaylists();
-				});
-			});
+		$scope.remove = function(playlist) {
+			playlist.remove();
+
+			// remove the elemnt also from the AngularJS list
+			$scope.playlists.splice($scope.playlists.indexOf(playlist), 1);
 		};
 
-		$scope.currentPlaylist = $routeParams.playlistId;
-		console.log("Current Playlist: "+ $scope.currentPlaylist);
+		// Add track to the playlist
+		$scope.addTrack = function(playlist, song) {
 
-
-		// Add tracks to the playlist
-		$scope.addTracks = function(playlistId, songs) {
-
-			var message = Restangular.one('playlists', playlistId).all("add");
-			message.post({trackIds: songs}).then(function() {
-				Restangular.one('playlists', playlistId).get().then(function(playlist){
-					OC.Notification.show(t('music', 'Track {songs} was added to the playlist {playlistName}', {songs: songs, playlistName: playlist.name}));
-					$timeout(OC.Notification.hide, 3000);
-				});
-			}, function error(reason) {
-				console.log("error :(");
+			playlist.all("add").post({trackIds: song.id}).then(function() {
+				playlist.trackIds.push(song);
 			});
 		};
 
 		// Remove chosen track from the list
-		$scope.removeTrack = function(songs) {
-			var message = Restangular.one('playlists', $scope.currentPlaylist).all("remove");
-			message.post({trackIds: songs}).then(function() {
-				Restangular.one('playlists', $scope.currentPlaylist).get().then(function(playlist){
-					OC.Notification.show(t('music', 'Track {songs} was removed from the playlist {playlistName}', {songs: songs, playlistName: playlist.name}));
-					$timeout(OC.Notification.hide, 3000);
-					$scope.getPlaylist($scope.currentPlaylist);
-				});
-			}, function error(reason) {
-				console.log("error :(");
+		$scope.removeTrack = function(track) {
+			$scope.currentPlaylist.all("remove").post({trackIds: track.id}).then(function() {
+				// remove the element also from the JS array
+				$scope.currentPlaylist.trackIds.splice($scope.currentPlaylist.trackIds.indexOf(track), 1);
 			});
 		};
 
 		// Call playlistService to play all songs in the current playlist
 		$scope.playAll = function() {
-			playlistService.setPlaylist($scope.currentPlaylistSongs);
+			playlistService.setPlaylist($scope.currentPlaylist.trackIds);
 			playlistService.publish('play');
 		};
 
 		// Play only one song from the playlist
 		$scope.playTrack = function(track) {
-			var tracks = [];
-			tracks[0] = track;
-			playlistService.setPlaylist(tracks);
+			playlistService.setPlaylist([track]);
 			playlistService.publish('play');
 		};
 
-		// Emitted by MainController after dropping a  song on a playlist
-		$scope.$on('droppedSong', function(event, songId, playlistId) {
-			$scope.addTracks(playlistId, songId);
+		if($routeParams.playlistId) {
+			// load playlist in playlist route
+			$scope.getPlaylist($routeParams.playlistId);
+		}
+
+		// Emitted by MainController after dropping a song on a playlist
+		$scope.$on('droppedSong', function(event, song, playlist) {
+			$scope.addTrack(playlist, song);
 		});
 
-		$scope.getPlaylists();
+		// load all playlists in sidebar
+		$scope.load();
 
 }]);

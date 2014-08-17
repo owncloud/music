b5f8dc7b3dfee89a08146c52a18269b846191f8c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 01:29:34 -0400

    [tx-robot] updated from transifex

--- l10n/az.php
+++ l10n/az.php
@@ -1,5 +1,11 @@
 <?php
 $TRANSLATIONS = array(
-"_Show all {{ trackcount }} songs ..._::_Show all {{ trackcount }} songs ..._" => array("","")
+"Albums" => "Albomlar",
+"Artists" => "Müğənnilər",
+"Description" => "Açıqlanma",
+"Description (e.g. App name)" => "Açıqlanma(Misal üçün proqram adı)",
+"Generate API password" => "APİ şifrəsinin generasiyası",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Ampache APİ-nin istifadə edilməsi üçün burda siz şifrələr generasiya edə bilərsiniz ona görə ki, onlar yalnz təhlükəsiz saxlana bilər. Bu Ampache API-nin öz dizaynıdır. Siz istənilən zaman çoxlu şifrə yarada və onları silə bilərsiniz.",
+"Next" => "Növbəti"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

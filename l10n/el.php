fc6c956d504ca91df4ef52db521502c6fd7cbe89
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-18 01:29:40 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -1,28 +1,28 @@
 <?php
 $TRANSLATIONS = array(
-"Albums" => "Δίσκοι",
+"Albums" => "Άλμπουμ",
 "Artists" => "Καλλιτέχνες",
 "Description" => "Περιγραφή",
-"Description (e.g. App name)" => "Περιγραφή (π.χ. όνομα εφαρμογής)",
-"Generate API password" => "Δημιουργία συνθηματικού API",
-"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Εδώ μπορείτε να δημιουργήσετε συνθηματικά για χρήση με το API Ampache, γιατί δεν είναι δυνατό να αποθηκευτούν με πραγματικά ασφαλή τρόπο, λόγω της σχεδίασης του API Ampache. Μπορείτε να δημιουργήσετε όσα συνθηματικά θέλετε και να τα ανακαλέσετε οποτεδήποτε.",
+"Description (e.g. App name)" => "Περιγραφή (π.χ. όνομα Εφαρμογής)",
+"Generate API password" => "Δημιουργία κωδικού πρόσβασης API",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Εδώ μπορείτε να δημιουργήσετε κωδικούς πρόσβασης για χρήση με το API του Ampache, γιατί δεν είναι δυνατό να αποθηκευτούν με πραγματικά ασφαλή τρόπο λόγω της σχεδίασης του API του Ampache. Μπορείτε να δημιουργήσετε όσα συνθηματικά θέλετε και να τα ανακαλέσετε οποτεδήποτε.",
 "Invalid path" => "Άκυρη διαδρομή",
-"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Θυμηθείτε ότι το API Ampache είναι απλά μια επισκόπηση και είναι ασταθές. Παρακαλούμε αναφέρετε την εμπειρία σας με αυτή την λειτουργία στην αντίστοιχη <a href=\"https://github.com/owncloud/music/issues/60\">αναφορά</a>. Θα ήταν καλό να υπάρχει επίσης μια λίστα με εφαρμογές για δοκιμή. Ευχαριστώ!",
+"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Θυμηθείτε ότι το API του Ampache είναι απλά μια προτεπισκόπηση και είναι ασταθές. Παρακαλούμε αναφέρετε την εμπειρία σας με αυτή τη λειτουργία στην αντίστοιχη <a href=\"https://github.com/owncloud/music/issues/60\">αναφορά</a>. Θα ήταν καλό να υπάρχει επίσης μια λίστα με εφαρμογές προς δοκιμή. Ευχαριστούμε!",
 "Music" => "Μουσική",
 "Next" => "Επόμενο",
-"Path to your music collection" => "Διαδρομή για την μουσική σας συλλογή",
+"Path to your music collection" => "Διαδρομή για τη μουσική σας συλλογή",
 "Pause" => "Παύση",
 "Play" => "Αναπαραγωγή",
 "Previous" => "Προηγούμενο",
 "Repeat" => "Επανάληψη",
-"Revoke API password" => "Ανάκληση συνθηματικού API",
+"Revoke API password" => "Ανάκληση κωδικού πρόσβασης API",
 "Shuffle" => "Τυχαία αναπαραγωγή",
 "Some not playable tracks were skipped." => "Μερικά μη αναγνώσιμα τραγούδια έχουν παρακαμφθεί.",
-"This setting specifies the folder which will be scanned for music." => "Αυτή η ρύθμιση αναφέρεται στο φάκελο,στον οποίο θα γίνει σάρωση για να βρεθούν τραγούδια.",
+"This setting specifies the folder which will be scanned for music." => "Αυτή η ρύθμιση προσδιορίζει το φάκελο που θα σαρωθεί για μουσική.",
 "Tracks" => "Κομμάτια",
 "Unknown album" => "Άγνωστο άλμπουμ",
 "Unknown artist" => "Άγνωστος καλλιτέχνης",
-"Use this address to browse your music collection from any Ampache compatible player." => "Χρησιμοποιήστε αυτή τη διεύθυνση για να περιηγηθείτε στη μουσική σας συλλογή από οποιοδήποτε μέσο,συμβατό με το Ampache.",
-"Use your username and following password to connect to this Ampache instance:" => "Χρησιμοποιήστε το όνομα χρήστη σας και το παρακάτω συνθηματικό για να συνδεθείτε σε αυτή την εκτέλεση του Ampache:"
+"Use this address to browse your music collection from any Ampache compatible player." => "Χρησιμοποιήστε αυτή τη διεύθυνση για να περιηγηθείτε στη μουσική σας συλλογή από οποιοδήποτε εφαρμογή αναπαραγωγής συμβατή με το Ampache.",
+"Use your username and following password to connect to this Ampache instance:" => "Χρησιμοποιήστε το όνομα χρήστη σας και τον παρακάτω κωδικό πρόσβασης για να συνδεθείτε σε αυτή την εγκατάσταση του Ampache:"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

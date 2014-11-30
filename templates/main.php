ebc1aca513bd8f5fd9cf4c5f11244687d2582c82
Konrad Mosoń <mosonkonrad@gmail.com>
2014-11-30 11:29:44 +0100

    Added CSS for progress in webui

--- templates/main.php
+++ templates/main.php
@@ -74,8 +74,8 @@ if($version[0] === 6 || ($version[0] === 5 && $version[1] >= 80)) {
 					<span ng-show="loading" class="muted">Loading...</span>
 					<div class="progress">
 						<div class="seek-bar" ng-click="seek($event)">
-							<div style="width: {{ position.buffer }}%"></div>
-							<div ng-show="position.total" style="width: {{ position.current/position.total * 100 }}%"></div>
+							<div class="buffer-bar" style="width: {{ position.buffer }}%"></div>
+							<div class="play-bar" ng-show="position.total" style="width: {{ position.current/position.total * 100 }}%"></div>
 						</div>
 					</div>
 				</div>

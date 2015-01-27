1b9eab39c527afa92824ad4e6d9117fad99c8b6d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-27 01:29:34 -0500

    [tx-robot] updated from transifex

--- l10n/fi_FI.php
+++ l10n/fi_FI.php
@@ -1,6 +1,6 @@
 <?php
 $TRANSLATIONS = array(
-"Albums" => "Levyt",
+"Albums" => "Albumit",
 "Artists" => "Esittäjät",
 "Description" => "Kuvaus",
 "Description (e.g. App name)" => "Kuvaus (esim. sovelluksen nimi)",
@@ -13,12 +13,12 @@ $TRANSLATIONS = array(
 "Play" => "Toista",
 "Previous" => "Edellinen",
 "Repeat" => "Kertaa",
-"Revoke API password" => "Peru API-salasana",
+"Revoke API password" => "Kumoa API-salasana",
 "Shuffle" => "Sekoita",
 "Some not playable tracks were skipped." => "Ohitettiin joitain sellaisia kappaleita, joita ei voi toistaa.",
 "This setting specifies the folder which will be scanned for music." => "Tämä asetus määrittää kansion, josta musiikkia etsitään.",
 "Tracks" => "Kappaleet",
-"Unknown album" => "Tuntematon levy",
+"Unknown album" => "Tuntematon albumi",
 "Unknown artist" => "Tuntematon esittäjä",
 "Use this address to browse your music collection from any Ampache compatible player." => "Käytä tätä osoitetta selataksesi musiikkikokoelmaasi miltä tahansa Ampache-yhteensopivalta soittimelta.",
 "Use your username and following password to connect to this Ampache instance:" => "Käytä käyttäjätunnustasi ja seuraavaa salasanaa yhditäessäsi tähän Ampache-istuntoon:"

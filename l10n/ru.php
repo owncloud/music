31ea6265567ec83465f396911e9e5240053ad94c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-07 01:29:45 -0500

    [tx-robot] updated from transifex

--- l10n/ru.php
+++ l10n/ru.php
@@ -5,9 +5,9 @@ $TRANSLATIONS = array(
 "Description" => "Описание",
 "Description (e.g. App name)" => "Описание (например Название приложения)",
 "Generate API password" => "Генерация пароля для API",
-"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Здесь вы можете генерировать пароли для использования с API Ampache, в связи с тем что, они не могут быть сохранены в действительно безопасным способом из-за конструкции API Ampache. Вы можете создать столько паролей, сколько необходимо, и отказаться от них в любое время.",
+"Here you can generate passwords to use with the Ampache API, because they can't be stored in a really secure way due to the design of the Ampache API. You can generate as many passwords as you want and revoke them anytime." => "Здесь вы можете генерировать пароли для использования API Ampache, так как они не могут быть сохранены действительно безопасным способом из-за особенностей API Ampache. Вы можете создать столько паролей, сколько необходимо, и отказаться от них в любое время.",
 "Invalid path" => "Некорректный путь",
-"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Следует помнить, что API Ampache является демо-версией и поэтому неустойчиво. Мы будем признательны, если вы поделитесь опытом работы с этой функцией в соответствующем <a href=\"https://github.com/owncloud/music/issues/60\">разделе</a>. Я также хотел бы создать список клиентов для тестирования. Спасибо",
+"Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Следует помнить, что API Ampache является предварительной и поэтому неустойчивой реализацией. Не стесняйтесь делиться опытом работы с этой функцией в соответствующем <a href=\"https://github.com/owncloud/music/issues/60\">разделе</a>. Я также хотел бы создать список клиентов для тестирования. Спасибо",
 "Music" => "Музыка",
 "Next" => "Следующий",
 "Path to your music collection" => "Путь до вашей музыкальной коллекции",
@@ -17,12 +17,12 @@ $TRANSLATIONS = array(
 "Repeat" => "Повтор",
 "Revoke API password" => "Отозвать пароль для API",
 "Shuffle" => "Перемешать",
-"Some not playable tracks were skipped." => "Некоторые непроигрываемые композиции были пропущены.",
+"Some not playable tracks were skipped." => "Некоторые не проигрываемые композиции были пропущены.",
 "This setting specifies the folder which will be scanned for music." => "Эта настройка определяет каталог, в котором будет проведено сканирование музыки.",
 "Tracks" => "Композиции",
 "Unknown album" => "Неизвестный альбом",
 "Unknown artist" => "Неизвестный исполнитель",
 "Use this address to browse your music collection from any Ampache compatible player." => "Используйте этот адрес, чтобы просмотреть вашу музыкальную коллекцию с любого плеера совместимого с Ampache.",
-"Use your username and following password to connect to this Ampache instance:" => "Используйте свой логин и следующий ниже пароль для подключения к данному экземпляру Ampache:"
+"Use your username and following password to connect to this Ampache instance:" => "Используйте свой логин и пароль ниже для подключения к данному экземпляру Ampache:"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";

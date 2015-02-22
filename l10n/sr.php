0cb5508563dd6d7483dec273a7bef9dee271888f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-22 01:29:42 -0500

    [tx-robot] updated from transifex

--- l10n/sr.php
+++ l10n/sr.php
@@ -1,11 +1,25 @@
 <?php
 $TRANSLATIONS = array(
+"Albums" => "Албуми",
+"Artists" => "Извођачи",
 "Description" => "Опис",
+"Description (e.g. App name)" => "Опис (нпр. назив апликације)",
+"Generate API password" => "Генериши лозинку АПИ-ја",
+"Invalid path" => "Неисправна путања",
 "Music" => "Музика",
 "Next" => "Следећа",
+"Path to your music collection" => "Путања до ваше музичке колекције",
 "Pause" => "Паузирај",
 "Play" => "Пусти",
 "Previous" => "Претходна",
-"Repeat" => "Понављај"
+"Repeat" => "Понављај",
+"Shuffle" => "Измешај",
+"Some not playable tracks were skipped." => "Прескочене су нумере које се не могу пустити.",
+"This setting specifies the folder which will be scanned for music." => "Ова поставка наводи фасциклу у којој ће бити тражена музика.",
+"Tracks" => "Нумере",
+"Unknown album" => "Непознат албум",
+"Unknown artist" => "Непознат извођач",
+"Use this address to browse your music collection from any Ampache compatible player." => "Користи ову адресу за прегледање ваше музичке колекције из било ког Ампаш компатибилног плејера.",
+"Use your username and following password to connect to this Ampache instance:" => "Користите ваше корисничко име и следећу лозинку за повезивање на овај Ампаш:"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";

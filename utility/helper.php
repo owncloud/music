e23b747c6da367f40d5af3e168e9a507d8012b72
Jörn Friedrich Dreyer <jfd@butonic.de>
2015-04-21 15:30:25 +0200

    use temp table for dumb mysql

--- utility/helper.php
+++ utility/helper.php
@@ -29,11 +29,35 @@ class Helper {
 	 */
 	public function cleanUp() {
 		$sqls = array(
-			'UPDATE `*PREFIX*music_albums` SET `cover_file_id` = NULL WHERE `cover_file_id` IS NOT NULL AND `cover_file_id` IN (SELECT `cover_file_id` FROM `*PREFIX*music_albums` LEFT JOIN `*PREFIX*filecache` ON `cover_file_id`=`fileid` WHERE `fileid` IS NULL);',
-			'DELETE FROM `*PREFIX*music_tracks` WHERE `file_id` IN (SELECT `file_id` FROM `*PREFIX*music_tracks` LEFT JOIN `*PREFIX*filecache` ON `file_id`=`fileid` WHERE `fileid` IS NULL);',
-			'DELETE FROM `*PREFIX*music_albums` WHERE `id` NOT IN (SELECT `album_id` FROM `*PREFIX*music_tracks` GROUP BY `album_id`);',
-			'DELETE FROM `*PREFIX*music_album_artists` WHERE `album_id` NOT IN (SELECT `id` FROM `*PREFIX*music_albums` GROUP BY `id`);',
-			'DELETE FROM `*PREFIX*music_artists` WHERE `id` NOT IN (SELECT `artist_id` FROM `*PREFIX*music_album_artists` GROUP BY `artist_id`);'
+			'UPDATE `*PREFIX*music_albums` SET `cover_file_id` = NULL
+				WHERE `cover_file_id` IS NOT NULL AND `cover_file_id` IN (
+					SELECT `cover_file_id` FROM (
+						SELECT `cover_file_id` FROM `*PREFIX*music_albums`
+						LEFT JOIN `*PREFIX*filecache`
+							ON `cover_file_id`=`fileid`
+						WHERE `fileid` IS NULL
+					) mysqlhack
+				);',
+			'DELETE FROM `*PREFIX*music_tracks` WHERE `file_id` IN (
+				SELECT `file_id` FROM (
+					SELECT `file_id` FROM `*PREFIX*music_tracks`
+					LEFT JOIN `*PREFIX*filecache`
+						ON `file_id`=`fileid`
+					WHERE `fileid` IS NULL
+					) mysqlhack
+				);',
+			'DELETE FROM `*PREFIX*music_albums` WHERE `id` NOT IN (
+					SELECT `album_id` FROM `*PREFIX*music_tracks`
+					GROUP BY `album_id`
+				);',
+			'DELETE FROM `*PREFIX*music_album_artists` WHERE `album_id` NOT IN (
+					SELECT `id` FROM `*PREFIX*music_albums`
+					GROUP BY `id`
+				);',
+			'DELETE FROM `*PREFIX*music_artists` WHERE `id` NOT IN (
+					SELECT `artist_id` FROM `*PREFIX*music_album_artists`
+					GROUP BY `artist_id`
+				);'
 		);
 
 		foreach ($sqls as $sql) {

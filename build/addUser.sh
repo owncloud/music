a492d96a655df49931316a25e312af97d6dadd68
Morris Jobke <hey@morrisjobke.de>
2015-07-01 09:02:23 +0200

    also test for core stable7+

---
+++ build/addUser.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# ownCloud music
+#
+# @author Morris Jobke
+# @copyright 2015 Morris Jobke <hey@morrisjobke.de>
+#
+
+OWNCLOUD=$1
+USER=$2
+PASSWORD=$3
+
+if [[ "$OWNCLOUD" == "master" ]];
+then
+    OC_PASS=$PASSWORD ./occ user:add $USER --password-from-env
+    exit $?
+fi
+
+if [[ "$OWNCLOUD" == "stable8" ]];
+then
+    curl -X POST -u admin:admin -d "userid=$USER&password=$PASSWORD" http://localhost:8888/ocs/v1.php/cloud/users
+    exit $?
+fi
+
+# patch AJAX call
+sed -i "s/OCP\\\\JSON::callCheck/#OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
+
+curl -X POST -u admin:admin -d "username=$USER&password=$PASSWORD" http://localhost:8888/index.php/settings/ajax/createuser.php
+
+# reset change
+sed -i "s/#OCP\\\\JSON::callCheck/OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
+
+exit 0

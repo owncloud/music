ae0aa5141077b2e931d73380fa23be6370786f0e
Morris Jobke <hey@morrisjobke.de>
2015-07-09 10:59:06 +0200

    add stable8.1 compliance to addUser.sh

--- build/addUser.sh
+++ build/addUser.sh
@@ -10,24 +10,24 @@ OWNCLOUD=$1
 USER=$2
 PASSWORD=$3
 
-if [[ "$OWNCLOUD" == "master" ]];
-then
-    OC_PASS=$PASSWORD ./occ user:add $USER --password-from-env
-    exit $?
-fi
-
 if [[ "$OWNCLOUD" == "stable8" ]];
 then
     curl -X POST -u admin:admin -d "userid=$USER&password=$PASSWORD" http://localhost:8888/ocs/v1.php/cloud/users
     exit $?
 fi
 
-# patch AJAX call
-sed -i "s/OCP\\\\JSON::callCheck/#OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
+if [[ "$OWNCLOUD" == "stable7" ]];
+then
+    # patch AJAX call
+    sed -i "s/OCP\\\\JSON::callCheck/#OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
 
-curl -X POST -u admin:admin -d "username=$USER&password=$PASSWORD" http://localhost:8888/index.php/settings/ajax/createuser.php
+    curl -X POST -u admin:admin -d "username=$USER&password=$PASSWORD" http://localhost:8888/index.php/settings/ajax/createuser.php
 
-# reset change
-sed -i "s/#OCP\\\\JSON::callCheck/OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
+    # reset change
+    sed -i "s/#OCP\\\\JSON::callCheck/OCP\\\\JSON::callCheck/g" settings/ajax/createuser.php
 
-exit 0
+    exit 0
+fi
+
+OC_PASS=$PASSWORD ./occ user:add $USER --password-from-env
+exit $?

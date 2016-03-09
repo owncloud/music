26d3abf45563aea5de53e9b0b8ed14e91d6fd606
Lukas Reschke <lukas@owncloud.com>
2016-03-09 09:35:08 +0100

    Add CSRF token
    Fixes https://github.com/owncloud/music/issues/474

--- js/public/app.js
+++ js/public/app.js
@@ -344,7 +344,7 @@ angular.module('Music').controller('PlayerController',
 			if(mimeType=='audio/flac' || mimeType=='audio/mpeg' || mimeType=='audio/ogg') {
 				return {
 					'type': mimeType,
-					'url': track.files[mimeType]
+					'url': track.files[mimeType] + '?requesttoken=' + encodeURIComponent(OC.requestToken)
 				};
 			}
 		}

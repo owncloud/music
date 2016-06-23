1e5d60d6ce8bdba545b5174db936f2e496ba97f3
Thomas Pulzer <t.pulzer@kniel.de>
2016-06-23 11:58:19 +0200

    Changed the db update procedure.

--- appinfo/update.php
+++ appinfo/update.php
@@ -52,6 +52,8 @@ if (version_compare($installedVersion, '0.3.12', '<')) {
 		'DELETE FROM `*PREFIX*music_tracks`;',
 		'DELETE FROM `*PREFIX*music_ampache_sessions`;',
 	);
-	$query = \OCP\DB::prepare($sql);
-	$query->execute();
+	foreach ($sqls as $sql) {
+		$query = \OCP\DB::prepare($sql);
+		$query->execute();
+	}
 }

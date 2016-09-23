c0aeea5e7cd503d95b834b0c69850c56b2ea137c
Pauli Järvinen <pauli.jarvinen@takoru.fi>
2016-09-23 22:00:16 +0300

    Replaced public/app.js with the generated file
    - The intention has always been to generate public/app.js bundle from the module-specific .js files, but lately the bundle has been maintained manually
    - As a major difference, the generated app.js contains also the contents of playerwrapper.js. Hence, there is no need to load it separately in main.php.

--- js/public/app.js
+++ js/public/app.js
@@ -506,13 +506,12 @@ angular.module('Music').controller('PlaylistController',
 	$scope.playlists = playlists;
 
 }]);
-
 angular.module('Music').directive('albumart', ['$http', '$queueFactory', function($http, $queueFactory) {
 	// Calling $http.get immediately for all album cover images would be bad idea because it would
 	// block the playback until all the covers are loaded. Hence, we use queue which allows 5 simultaneous
 	// HTTP GET queries. This is faster than running them only one at a time, but still enables starting
 	// the playback with rather short delay.
-	var httpQueue = $queueFactory(5)
+	var httpQueue = $queueFactory(5);
 
 	function setCoverImage(element, imageUrl) {
 		// remove placeholder stuff
@@ -572,6 +571,7 @@ angular.module('Music').directive('albumart', ['$http', '$queueFactory', functio
 		attrs.$observe('cover', onCoverChanged);
 	};
 }]);
+
 
 angular.module('Music').directive('resize', ['$window', '$rootScope', function($window, $rootScope) {
 	return function(scope, element, attrs, ctrl) {
@@ -670,6 +670,164 @@ angular.module('Music').filter('playTime', function() {
 		return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 	};
 });
+var PlayerWrapper = function() {
+	this.underlyingPlayer = 'aurora';
+	this.aurora = {};
+	this.sm2 = {};
+	this.duration = 0;
+	this.volume = 100;
+
+	return this;
+};
+
+PlayerWrapper.prototype = _.extend({}, OC.Backbone.Events);
+
+PlayerWrapper.prototype.play = function() {
+	switch(this.underlyingPlayer) {
+		case 'sm2':
+			this.sm2.play('ownCloudSound');
+			break;
+		case 'aurora':
+			this.aurora.play();
+			break;
+	}
+};
+
+PlayerWrapper.prototype.stop = function() {
+	switch(this.underlyingPlayer) {
+		case 'sm2':
+			this.sm2.stop('ownCloudSound');
+			this.sm2.destroySound('ownCloudSound');
+			break;
+		case 'aurora':
+			if(this.aurora.asset !== undefined) {
+				// check if player's constructor has been called,
+				// if so, stop() will be available
+				this.aurora.stop();
+			}
+			break;
+	}
+};
+
+PlayerWrapper.prototype.togglePlayback = function() {
+	switch(this.underlyingPlayer) {
+		case 'sm2':
+			this.sm2.togglePause('ownCloudSound');
+			break;
+		case 'aurora':
+			this.aurora.togglePlayback();
+			break;
+	}
+};
+
+PlayerWrapper.prototype.seekingSupported = function() {
+	// Seeking is not implemented in aurora/flac.js and does not work on all
+	// files with aurora/mp3.js. Hence, we disable seeking with aurora.
+	return this.underlyingPlayer == 'sm2';
+};
+
+PlayerWrapper.prototype.seek = function(percentage) {
+	if (this.seekingSupported()) {
+		console.log('seek to '+percentage);
+		switch(this.underlyingPlayer) {
+			case 'sm2':
+				this.sm2.setPosition('ownCloudSound', percentage * this.duration);
+				break;
+			case 'aurora':
+				this.aurora.seek(percentage * this.duration);
+				break;
+		}
+	}
+	else {
+		console.log('seeking is not supported for this file');
+	}
+};
+
+PlayerWrapper.prototype.setVolume = function(percentage) {
+	this.volume = percentage;
+
+	switch(this.underlyingPlayer) {
+		case 'sm2':
+			this.sm2.setVolume('ownCloudSound', this.volume);
+			break;
+		case 'aurora':
+			this.aurora.volume = this.volume;
+			break;
+	}
+};
+
+PlayerWrapper.prototype.fromURL = function(typeAndURL) {
+	var self = this;
+	var url = typeAndURL.url;
+	var type = typeAndURL.type;
+
+	if (soundManager.canPlayURL(url)) {
+		this.underlyingPlayer = 'sm2';
+	} else {
+		this.underlyingPlayer = 'aurora';
+	}
+	console.log('Using ' + this.underlyingPlayer + ' for type ' + type + ' URL ' + url);
+
+	switch(this.underlyingPlayer) {
+		case 'sm2':
+			this.sm2 = soundManager.setup({
+				html5PollingInterval: 200
+			});
+			this.sm2.html5Only = true;
+			this.sm2.createSound({
+				id: 'ownCloudSound',
+				url: url,
+				whileplaying: function() {
+					self.trigger('progress', this.position);
+				},
+				whileloading: function() {
+					self.duration = this.durationEstimate;
+					self.trigger('duration', this.durationEstimate);
+					// The buffer may contain holes after seeking but just ignore those.
+					// Show the buffering status according the last buffered position.
+					var bufCount = this.buffered.length;
+					var bufEnd = (bufCount > 0) ? this.buffered[bufCount-1].end : 0;
+					self.trigger('buffer', bufEnd / this.durationEstimate * 100);
+				},
+				onfinish: function() {
+					self.trigger('end');
+				},
+				onload: function(success) {
+					if (success) {
+						self.trigger('ready');
+					} else {
+						console.log('SM2: sound load error');
+					}
+				}
+			});
+			break;
+		case 'aurora':
+			this.aurora = AV.Player.fromURL(url);
+			this.aurora.asset.source.chunkSize=524288;
+
+			this.aurora.on('buffer', function(percent) {
+				self.trigger('buffer', percent);
+			});
+			this.aurora.on('progress', function(currentTime) {
+				self.trigger('progress', currentTime);
+			});
+			this.aurora.on('ready', function() {
+				self.trigger('ready');
+			});
+			this.aurora.on('end', function() {
+				self.trigger('end');
+			});
+			this.aurora.on('duration', function(msecs) {
+				self.duration = msecs;
+				self.trigger('duration', msecs);
+			});
+			break;
+	}
+
+	// Set the current volume to the newly created player instance
+	this.setVolume(this.volume);
+};
+
 angular.module('Music').service('playlistService', ['$rootScope', function($rootScope) {
 	var playlist = null;
 	var currentTrackId = null;
@@ -745,7 +903,6 @@ angular.module('Music').service('playlistService', ['$rootScope', function($root
 }]);
 
 angular.module('Music').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
 	gettextCatalog.setStrings('ach', {});
 	gettextCatalog.setStrings('ady', {});
 	gettextCatalog.setStrings('af', {});

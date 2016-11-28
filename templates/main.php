d3df0a5a45d786d4cc0fa36477f4c6e47b6d191a
Pauli Järvinen <pauli.jarvinen@takoru.fi>
2016-11-28 21:43:29 +0200

    Moved alphabet-navigation from main.php to overview.php to hide it from the playlist view
    - The alpahbet navigation was not usable and made no sense in the playlist view

--- templates/main.php
+++ templates/main.php
@@ -130,14 +130,6 @@ if($version[0] < 8 || $version[0] === 8 && $version[1] < 2) {
 			<div id="app-view" ng-view ng-class="{started: started, 'icon-loading': loading}">
 			</div>
 
-			<div ng-show="artists" class="alphabet-navigation" ng-class="{started: started}" resize>
-				<a du-smooth-scroll="{{ letter }}" offset="{{ scrollOffset() }}"
-					ng-repeat="letter in letters" 
-					ng-class="{available: letterAvailable[letter], filler: ($index % 2) == 1}">
-					<span class="letter-content">{{ letter }}</span>
-				</a>
-			</div>
-
 		</div>
 
 	</div>
--- templates/partials/overview.php
+++ templates/partials/overview.php
@@ -56,3 +56,11 @@
 		</ul>
 	</div>
 </div>
+
+<div ng-show="artists" class="alphabet-navigation" ng-class="{started: started}" resize>
+	<a du-smooth-scroll="{{ letter }}" offset="{{ scrollOffset() }}"
+		ng-repeat="letter in letters" 
+		ng-class="{available: letterAvailable[letter], filler: ($index % 2) == 1}">
+		<span class="letter-content">{{ letter }}</span>
+	</a>
+</div>

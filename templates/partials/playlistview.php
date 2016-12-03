cdb3f2d59454cbfcfec82a22ed38614295cf762d
Pauli Järvinen <pauli.jarvinen@takoru.fi>
2016-12-03 18:59:26 +0200

    Show removal button instead of the letter 'X' at the end of playlist track item
    - The button is shown only on hover
    - Also, allow playlist track items to wrap to multiple lines, ensuring that the whole track title and the removal button can be always shown

--- templates/partials/playlistview.php
+++ templates/partials/playlistview.php
@@ -1,12 +1,15 @@
 <div class="playlist-area">
 	<h1 ng-click="playAll(currentPlaylist.id)">{{currentPlaylist.name}}</h1>
 	<ul class="track-list">
-		<li ng-click="playTrack(song)" ng-repeat="song in currentPlaylist.trackIds">
-			<img class="play svg" alt="{{ 'Play' | translate }}" src="<?php p(OCP\image_path('music', 'play-big.svg')) ?>"
-				ng-class="{playing: currentTrack.id == song.id}" />
-			<span class="muted">{{ $index + 1 }}.</span>
-			{{ song.artistName }} - {{song.title}}
-			<a class="action" ng-click="removeTrack(song)">x</a>
+		<li ng-repeat="song in currentPlaylist.trackIds">
+			<div ng-click="playTrack(song)">
+				<img class="play svg" alt="{{ 'Play' | translate }}" src="<?php p(OCP\image_path('music', 'play-big.svg')) ?>"
+					ng-class="{playing: currentTrack.id == song.id}" />
+				<span class="muted">{{ $index + 1 }}.</span>
+				<div>{{ song.artistName }} - {{song.title}}</div>
+			</div>
+			<button class="svg action icon-close" ng-click="removeTrack(song)"
+				alt="{{ 'Remove' | translate }}" title="{{ 'Remove track from playlist' | translate }}" />
 		</li>
 	</ul>
 

fb76c9e4db58b0479addf566f70b019144c9e9f1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-27 01:29:46 +0200

    [tx-robot] updated from transifex

--- l10n/he.php
+++ l10n/he.php
@@ -1,7 +1,10 @@
 <?php
 $TRANSLATIONS = array(
+"+ New Playlist" => "+ רשימת השמעה חדשה",
 "Albums" => "אלבומים",
+"All tracks" => "כל רצועות המוזיקה",
 "Artists" => "אומנים",
+"Click here to start the scan" => "לחיצה כאן להתחלת הסריקה",
 "Description" => "תיאור",
 "Description (e.g. App name)" => "תיאור (לדוגמא שם אפליקציה)",
 "Generate API password" => "יצירת סיסמאות API",
@@ -9,20 +12,28 @@ $TRANSLATIONS = array(
 "Invalid path" => "נתיב לא חוקי",
 "Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "יש לזכור שה- Ampache API הוא רק קדימון והוא אינו יציב. ניתן לדווח את ההתרשמות מתכונה זו בתכתובת <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. כמו כן נשמח לקבל רשימת לקוחות לבדיקה איתם. תודה",
 "Music" => "מוזיקה",
+"New music available" => "קיימת מוזיקה חדשה",
+"New music available. Click here to reload the music library." => "קיימת מוזיקה חדשה. יש ללחוץ כאן לטעינה מחדש של ספריית המוזיקה.",
 "Next" => "הבא",
+"No music found" => "לא נמצאה מוזיקה",
 "Path to your music collection" => "נתיב לאוסף המוזיקה שלך",
 "Pause" => "השהה",
 "Play" => "נגן",
 "Previous" => "קודם",
 "Repeat" => "חזרה",
 "Revoke API password" => "ביטול סיסמת API",
+"Scanning music …" => "סורק מוזיקה...",
 "Shuffle" => "ערבב",
 "Some not playable tracks were skipped." => "מספר קטעי מוסיקה לא תקינים דולגו",
 "This setting specifies the folder which will be scanned for music." => "ההגדרות של התיקייה עליה תבוצע סריקת המוזיקה",
 "Tracks" => "קטעי מוסיקה",
 "Unknown album" => "אלבום לא ידוע",
 "Unknown artist" => "אמן לא ידוע",
+"Upload music in the files app to listen to it here" => "יש להעלות מוזיקה ביישום הקבצים להשמעה שלה כאן",
 "Use this address to browse your music collection from any Ampache compatible player." => "ניתן להשתמש בכתובת זו לעיון בספריית המוזיקה שלך מכל נגן התומך באפצ'י.",
-"Use your username and following password to connect to this Ampache instance:" => "השתמשו בשם המשתמש שלכם ובסיסמא הבאה לחיבור למריץ אפצ'י זה:"
+"Use your username and following password to connect to this Ampache instance:" => "השתמשו בשם המשתמש שלכם ובסיסמא הבאה לחיבור למריץ אפצ'י זה:",
+"Volume" => "עוצמת שמע",
+"tracks" => "רצועות מוזיקה",
+"{{ scanningScanned }} of {{ scanningTotal }}" => "{{ scanningScanned }} מתוך {{ scanningTotal }}"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/sl.php
+++ l10n/sl.php
@@ -1,7 +1,9 @@
 <?php
 $TRANSLATIONS = array(
+"+ New Playlist" => "+ Nov seznam predvajanja",
 "Albums" => "Albumi",
 "Artists" => "Izvajalci",
+"Click here to start the scan" => "Kliknite za začetek preiskovanja za nove predmete",
 "Description" => "Opis",
 "Description (e.g. App name)" => "Opis (na primer ime programa)",
 "Generate API password" => "Ustvari geslo API",
@@ -9,13 +11,17 @@ $TRANSLATIONS = array(
 "Invalid path" => "Neveljavna pot",
 "Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Imejte v mislih, da je Ampache API namenjen le predogledu in ni povsem stabilna programska oprema. Vaših odzivov in izkušenj o uporabi bomo zelo veseli. Objavite jih prek <a href=\"https://github.com/owncloud/music/issues/60\">spletnega obrazca</a>. Priporočljivo je dodati tudi seznam odjemalcev. Za sodelovanje se vam vnaprej najlepše zahvaljujemo.",
 "Music" => "Glasba",
+"New music available" => "Na voljo je nova glasba",
+"New music available. Click here to reload the music library." => "Na voljo je nova glasba. Kliknite za ponovno nalaganje glasbene zbirke.",
 "Next" => "Naslednja",
+"No music found" => "Ni zaznanih glasbenih datotek",
 "Path to your music collection" => "Pot do zbirke glasbe",
 "Pause" => "Premor",
 "Play" => "Predvajaj",
 "Previous" => "Predhodna",
 "Repeat" => "Ponovi",
 "Revoke API password" => "Razveljavi geslo API",
+"Scanning music …" => "Poteka preiskovanje glasbe ...",
 "Shuffle" => "Premešaj",
 "Some not playable tracks were skipped." => "Nekateri posnetki, ki jih ni mogoče predvajati, so bili preskočeni.",
 "This setting specifies the folder which will be scanned for music." => "Nastavitev določa mapo, ki bo preiskana za glasbo.",
@@ -23,6 +29,7 @@ $TRANSLATIONS = array(
 "Unknown album" => "Neznan album",
 "Unknown artist" => "Neznan izvajalec",
 "Use this address to browse your music collection from any Ampache compatible player." => "Uporabite ta naslov za brskanje po zbirki glasbe preko kateregakoli predvajalnika, ki podpira sistem Ampache.",
-"Use your username and following password to connect to this Ampache instance:" => "Uporabite uporabniško ime in navedeno geslo za povezavo z Ampache:"
+"Use your username and following password to connect to this Ampache instance:" => "Uporabite uporabniško ime in navedeno geslo za povezavo z Ampache:",
+"Volume" => "Glasnost"
 );
 $PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";

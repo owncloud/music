6fd442047c61b98efb0fc1449d4b58d02cf155af
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-23 01:40:07 +0200

    [tx-robot] updated from transifex

--- l10n/pl.php
+++ l10n/pl.php
@@ -1,7 +1,10 @@
 <?php
 $TRANSLATIONS = array(
+"+ New Playlist" => "+ Nowa Lista odtwarzania",
 "Albums" => "Albumy",
+"All tracks" => "Wszystkie ściezki",
 "Artists" => "Artyści",
+"Click here to start the scan" => "Kliknij tutaj aby rozpocząć skanowanie",
 "Description" => "Opis",
 "Description (e.g. App name)" => "Opis (np. Nazwa aplikacji)",
 "Generate API password" => "Wygeneruj hasło API",
@@ -9,13 +12,17 @@ $TRANSLATIONS = array(
 "Invalid path" => "niewłaściwa ścieżka",
 "Keep in mind, that the Ampache API is just a preview and is unstable. Feel free to report your experience with this feature in the corresponding <a href=\"https://github.com/owncloud/music/issues/60\">issue</a>. I would also like to have a list of clients to test with. Thanks" => "Miej na uwadze, że API Ampache jest tylko poglądowe i niestabilne. Możesz swobodnie raportować swoje doświadczenia z tą funkcją w odpowiednim <a href=\"https://github.com/owncloud/music/issues/60\">dokumencie</a>. Chciałbym mieć również listę klientów z którymi będę przeprowadzać testy. Dzięki",
 "Music" => "Muzyka",
+"New music available" => "Dostępne są nowe utwory",
+"New music available. Click here to reload the music library." => "Dostępne są nowe utwory. Kliknij tutaj aby załadować ponownie bibliotekę muzyki.",
 "Next" => "Następny",
+"No music found" => "Nie znaleziono muzyki",
 "Path to your music collection" => "Ścieżka do Twojej kolekcji muzyki",
 "Pause" => "Wstrzymaj",
 "Play" => "Odtwarzaj",
 "Previous" => "Poprzedni",
 "Repeat" => "Powtarzaj",
 "Revoke API password" => "Odwołaj hasło API",
+"Scanning music …" => "Skanowanie muzyki ...",
 "Shuffle" => "Losowo",
 "Some not playable tracks were skipped." => "Niektóre nieodtwarzalne ścieżki zostały pominięte.",
 "This setting specifies the folder which will be scanned for music." => "To ustawienie określa folder, który będzie skanowany pod kątem muzyki.",
@@ -23,6 +30,9 @@ $TRANSLATIONS = array(
 "Unknown album" => "Nieznany album",
 "Unknown artist" => "Nieznany artysta",
 "Use this address to browse your music collection from any Ampache compatible player." => "Użyj tego adresu aby przeglądać swoją kolekcję muzyczną na dowolnym odtwarzaczu kompatybilnym z Ampache.",
-"Use your username and following password to connect to this Ampache instance:" => "Użyj nazwy użytkownika i następującego hasła do połączenia do tej instancji Ampache:"
+"Use your username and following password to connect to this Ampache instance:" => "Użyj nazwy użytkownika i następującego hasła do połączenia do tej instancji Ampache:",
+"Volume" => "Głośność",
+"tracks" => "ścieżki",
+"{{ scanningScanned }} of {{ scanningTotal }}" => "{{ scanningScanned }} z {{ scanningTotal }}"
 );
 $PLURAL_FORMS = "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>=14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);";

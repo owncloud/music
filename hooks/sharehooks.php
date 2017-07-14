b8132d62c17637a159c6cfeea8f62c40d4180640
Pauli Järvinen <pauli.jarvinen@gmail.com>
2017-07-14 23:50:27 +0300

    Better handling for group shares
    - The logic is still not perfect but at least it prevents leaving any dangling references:
       * No automatic db update happens when audio file is shared to group, manual rescan is needed
       * Removing a group share removes the track(s) from everyone's DBs (including the file owner!). Manual rescan restores the track(s) for those users who still have access to the file(s).
       * "Unshare by self" on a group share removes the track(s) only from the subjet user

--- hooks/sharehooks.php
+++ hooks/sharehooks.php
@@ -16,27 +16,38 @@ use \OCA\Music\App\Music;
 
 class ShareHooks {
 
+	static private function removeSharedItem($app, $itemType, $nodeId, $owner, $removeFromUser) {
+		$scanner = $app->getContainer()->query('Scanner');
+
+		if ($itemType === 'folder') {
+			$ownerHome = $scanner->resolveUserFolder($owner);
+			$nodes = $ownerHome->getById($nodeId);
+			if (count($nodes) > 0) {
+				$sharedFolder = $nodes[0];
+				$scanner->deleteFolder($sharedFolder, $removeFromUser);
+			}
+		}
+		else if ($itemType === 'file') {
+			$scanner->delete($nodeId, $removeFromUser);
+		}
+	}
+
 	/**
 	 * Invoke auto update of music database after item gets unshared
 	 * @param array $params contains the params of the removed share
 	 */
 	static public function itemUnshared($params) {
-		$app = new Music();
+		// react only on user and group shares
+		$shareType = $params['shareType'];
+		if ($shareType == \OCP\Share::SHARE_TYPE_USER || $shareType == \OCP\Share::SHARE_TYPE_GROUP) {
+			// When a group share is removed, delete the track(s) from all users. This is suboptimal
+			// because the file gets removed also from the file owner and possible unrelated sharees,
+			// but at least no-one is left with dangling references to unavailable tracks.
+			// The users having still access to the files are prompted to rescan the next time they
+			// open the Music app and the database gets fixed.
+			$removeFromUser = ($shareType == \OCP\Share::SHARE_TYPE_GROUP) ? null : $params['shareWith'];
 
-		$scanner = $app->getContainer()->query('Scanner');
-		$sharedNodeId = $params['itemSource'];
-		$shareWithUser = $params['shareWith'];
-
-		if ($params['itemType'] === 'folder') {
-			$ownerHome = $scanner->resolveUserFolder($params['uidOwner']);
-			$nodes = $ownerHome->getById($sharedNodeId);
-			if (count($nodes) > 0) {
-				$sharedFolder = $nodes[0];
-				$scanner->deleteFolder($sharedFolder, $shareWithUser);
-			}
-		}
-		else if ($params['itemType'] === 'file') {
-			$scanner->delete((int)$sharedNodeId, $shareWithUser);
+			self::removeSharedItem(new Music(), $params['itemType'], $params['itemSource'], $params['uidOwner'], $removeFromUser);
 		}
 	}
 
@@ -54,7 +65,13 @@ class ShareHooks {
 			&& array_key_exists('shareWith', $params)
 			&& array_key_exists('itemType', $params)
 			&& array_key_exists('uidOwner', $params)) {
-			self::itemUnshared($params);
+
+			// The share recipient may be an individual user or a group, but the item is always removed from
+			// the current user alone.
+			$app = new Music();
+			$removeFromUser = $app->getContainer()->query('UserId');
+
+			self::removeSharedItem($app, $params['itemType'], $params['itemSource'], $params['uidOwner'], $removeFromUser);
 		}
 	}
 
@@ -63,11 +80,11 @@ class ShareHooks {
 	 * @param array $params contains the params of the added share
 	 */
 	static public function itemShared($params) {
-		if ($params['itemType'] === 'folder') {
-			// Do not auto-update database when a folder is shared. The folder might contain
-			// thousands of audio files, and indexing them could take minutes or hours. The sharee
-			// user will be prompted to update database the next time she opens the Music app.
-		} else if ($params['itemType'] === 'file') {
+		// Do not auto-update database when a folder is shared. The folder might contain
+		// thousands of audio files, and indexing them could take minutes or hours. The sharee
+		// user will be prompted to update database the next time she opens the Music app.
+		// Similarly, do not auto-update on group shares.
+		if ($params['itemType'] === 'file' && $params['shareType'] == \OCP\Share::SHARE_TYPE_USER) {
 			$app = new Music();
 			$container = $app->getContainer();
 			$scanner = $container->query('Scanner');

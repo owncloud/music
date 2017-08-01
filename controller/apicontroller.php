owncloud/music

commit 7d139a628430f552c1a8e264cb3caa89aa2a394f

Cache cover image data in DB, embed the image data into collection.json if it's already cached
- Previously, each cover image was loaded separately by the browser. Firefox was able to cache the results, but Chrome was not. Hence, it took quite some time on Chrome before all cover images became visible as each PHP query has a static overhead which may be hundreds of milliseconds.
- With this commit, the number of PHP queries is dramatically reduced by embedding the cover image data directly to the collection.json. This is done only for cover images which have been previously cached, and hence it doesn't take many seconds of additional time. For cover images not already cached, the loading happens the same way as before.
- The trade-off is that the collection.json grows in size, so the client has to load more data before anything can be rendered. This may be significant on slow connections, but with a decent bandwidth it's a non-issue.
- Also, removed some IE8-compatibility code from directives/albumart.js. The UI has never worked well on IE8 (e.g. playing music has never worked) and ownCloud 9 has already dropped support for IE8 and IE9.

controller/apicontroller.php
@@ -131,7 +131,8 @@ class ApiController extends Controller {
 		$allAlbumsByIdAsArr = array();
 		foreach ($allAlbums as &$album) {
 			$allAlbumsByIdAsObj[$album->getId()] = $album;
-			$allAlbumsByIdAsArr[$album->getId()] = $album->toCollection($this->urlGenerator, $this->l10n);
+			$allAlbumsByIdAsArr[$album->getId()] = $album->toCollection($this->urlGenerator, $this->l10n,
+					$this->coverHelper->getCoverFromCache($album->getId(), $this->userId));
 		}
 
 		/** @var Track[] $allTracks */

utility/coverhelper.php
@@ -13,10 +13,12 @@
 namespace OCA\Music\Utility;
 
 use \OCA\Music\AppFramework\Core\Logger;
-
 use \OCA\Music\BusinessLayer\AlbumBusinessLayer;
+use \OCA\Music\Db\Cache;
 
 use \OCP\Files\Folder;
+
+use \Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 /**
  * utility to get cover image for album
@@ -24,15 +26,18 @@ use \OCP\Files\Folder;
 class CoverHelper {
 
 	private $albumBusinessLayer;
-	private $scanner;
+	private $extractor;
+	private $cache;
 	private $logger;
 
 	public function __construct(
 			AlbumBusinessLayer $albumBusinessLayer,
-			Scanner $scanner,
+			Extractor $extractor,
+			Cache $cache,
 			Logger $logger) {
 		$this->albumBusinessLayer = $albumBusinessLayer;
-		$this->scanner = $scanner;
+		$this->extractor = $extractor;
+		$this->cache = $cache;
 		$this->logger = $logger;
 	}
 
@@ -40,35 +45,102 @@ class CoverHelper {
 	 * get cover image of the album
 	 *
 	 * @param int $albumId
-	 * @return array|null dictionary with keys 'mimetype' and 'content'
+	 * @param string $userId
+	 * @param Folder $rootFolder
+	 * @return array|null Image data in format accepted by \OCA\Music\Http\FileResponse
 	 */
 	public function getCover($albumId, $userId, $rootFolder) {
-		$response = NULL;
-		$album = $this->albumBusinessLayer->find($albumId, $userId);
-
-		$nodes = $rootFolder->getById($album->getCoverFileId());
-		if (count($nodes) > 0) {
-			// get the first valid node (there shouldn't be more than one node anyway)
-			$node = $nodes[0];
-			$mime = $node->getMimeType();
-
-			if (0 === strpos($mime, 'audio')) { // embedded cover image
-				$cover = $this->scanner->parseEmbeddedCoverArt($node);
-
-				if ($cover !== NULL) {
-					$response = ['mimetype' => $cover["image_mime"], 'content' => $cover["data"]];
-				}
+		$response = $this->getCoverFromCache($albumId, $userId);
+		if ($response === null) {
+			$response = $this->readCover($albumId, $userId, $rootFolder);
+			if ($response !== null) {
+				$this->addCoverToCache($albumId, $userId, $response);
 			}
-			else { // separate image file
-				$response = ['mimetype' => $mime, 'content' => $node->getContent()];
-			}
-		}
-
-		if ($response === NULL) {
-			$this->logger->log(
-					"Requested cover not found for album $albumId, ".
-					"coverFileId={$album->getCoverFileId()}", 'error');
 		}
 		return $response;
 	}
+
+	/**
+	 * get cover image of the album if it is cached
+	 * 
+	 * @param int $albumId
+	 * @param string $userId
+	 * @return array|null Image data in format accepted by \OCA\Music\Http\FileResponse
+	 */
+	public function getCoverFromCache($albumId, $userId) {
+		$cached = $this->cache->get($userId, 'cover_' . $albumId);
+		if ($cached !== null) {
+			$delimPos = strpos($cached, '|');
+			return [
+				'mimetype' => substr($cached, 0, $delimPos),
+				'content' => substr($cached, $delimPos + 1)
+			];
+		}
+		return null;
+	}
+
+	/**
+	 * Cache the given cover image data
+	 * @param int $albumId
+	 * @param string $userId
+	 * @param array $coverData
+	 */
+	public function addCoverToCache($albumId, $userId, $coverData) {
+		try {
+			// support both the format generated by GetID3 and the format used in \OCA\Music\Http\FileResponse
+			$mime = array_key_exists('mimetype', $coverData) ? $coverData['mimetype'] : $coverData['image_mime'];
+			$content = array_key_exists('content', $coverData) ? $coverData['content'] : $coverData['data'];
+
+			if ($mime && $content) {
+				$this->cache->add($userId, 'cover_' . $albumId, $mime . '|' . $content);
+				$this->cache->remove($userId, 'collection');
+			}
+		}
+		catch (UniqueConstraintViolationException $ex) {
+			$this->logger->log("Tried to cache cover of album $albumId which is already cached. Ignoring.", 'warn');
+		}
+	}
+
+	/**
+	 * Remove cover image from cache if it is there. Silently do nothing if there
+	 * is no cached cover.
+	 * @param int $albumId
+	 * @param string $userId
+	 */
+	public function removeCoverFromCache($albumId, $userId) {
+		$this->cache->remove($userId, 'cover_' . $albumId);
+	}
+
+	private function readCover($albumId, $userId, $rootFolder) {
+		$response = null;
+		$album = $this->albumBusinessLayer->find($albumId, $userId);
+		$coverId = $album->getCoverFileId();
+
+		if ($coverId > 0) {
+			$nodes = $rootFolder->getById($coverId);
+			if (count($nodes) > 0) {
+				// get the first valid node (there shouldn't be more than one node anyway)
+				$node = $nodes[0];
+				$mime = $node->getMimeType();
+
+				if (0 === strpos($mime, 'audio')) { // embedded cover image
+					$cover = $this->extractor->parseEmbeddedCoverArt($node);
+
+					if ($cover !== null) {
+						$response = ['mimetype' => $cover['image_mime'], 'content' => $cover['data']];
+					}
+				}
+				else { // separate image file
+					$response = ['mimetype' => $mime, 'content' => $node->getContent()];
+				}
+			}
+
+			if ($response === null) {
+				$this->logger->log("Requested cover not found for album $albumId, coverId=$coverId", 'error');
+			}
+		}
+
+		return $response;
+	}
+
 }

app/music.php
@@ -272,7 +272,8 @@ class Music extends App {
 		$container->registerService('CoverHelper', function($c) {
 			return new CoverHelper(
 				$c->query('AlbumBusinessLayer'),
-				$c->query('Scanner'),
+				$c->query('ExtractorGetID3'),
+				$c->query('Cache'),
 				$c->query('Logger')
 			);
 		});
@@ -297,6 +298,7 @@ class Music extends App {
 				$c->query('TrackBusinessLayer'),
 				$c->query('PlaylistBusinessLayer'),
 				$c->query('Cache'),
+				$c->query('CoverHelper'),
 				$c->query('Logger'),
 				$c->query('Db'),
 				$c->query('Config'),

appinfo/database.xml
@@ -386,9 +386,20 @@
 			</field>
 			<field>
 				<name>data</name>
-				<type>clob</type>
+				<type>blob</type>
 				<notnull>false</notnull>
 			</field>
+
+			<index>
+				<name>music_cache_index</name>
+				<unique>true</unique>
+				<field>
+					<name>user_id</name>
+				</field>
+				<field>
+					<name>key</name>
+				</field>
+			</index>
 		</declaration>
 	</table>
 </database>

appinfo/info.xml
@@ -11,7 +11,7 @@
 	</description>
 	<licence>AGPL</licence>
 	<author>Morris Jobke, Jan-Christoph Borchardt (design)</author>
-	<version>0.4.2-alpha.1</version>
+	<version>0.4.2-alpha.2</version>
 	<category>multimedia</category>
 	<screenshot>https://user-images.githubusercontent.com/8565946/28543434-6eb12de0-70c8-11e7-966e-65f4c3a90531.png</screenshot>
 	<website>https://github.com/owncloud/music</website>

db/album.php
@@ -142,41 +142,72 @@ class Album extends Entity {
 	}
 
 	/**
-	 * Creates object used for collection API (array with name, year, disk, cover URL and ID)
-	 * @param  IURLGenerator $urlGenerator URLGenerator
-	 * @param  object $l10n L10n handler
-	 * @return array collection API object
+	 * Return the cover URL to be used in the Shiva API
+	 * @param IURLGenerator $urlGenerator
+	 * @return string|null
 	 */
-	public function toCollection(IURLGenerator $urlGenerator, $l10n) {
+	public function coverToAPI(IURLGenerator $urlGenerator) {
 		$coverUrl = null;
 		if ($this->getCoverFileId() > 0) {
 			$coverUrl = $urlGenerator->linkToRoute('music.api.cover',
-				array('albumIdOrSlug' => $this->getId()));
+					array('albumIdOrSlug' => $this->getId()));
 		}
+		return $coverUrl;
+	}
 
+	/**
+	 * If the cover image is already cached, the collection contains the image data encoded as base64.
+	 * Otherwise the collection contains URL of the image.
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  array $cachedCoverData Cached cover image data if available
+	 * $return string|null
+	 */
+	public function coverToCollection(IURLGenerator $urlGenerator, $cachedCoverData) {
+		if ($this->getCoverFileId() > 0) {
+			if ($cachedCoverData !== null) {
+				return 'data:' . $cachedCoverData['mimetype'] . ';base64,' .
+						base64_encode($cachedCoverData['content']);
+			} else {
+				return $this->coverToAPI($urlGenerator);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Creates object used for collection API (array with name, year, disk, cover URL and ID)
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  object $l10n Localization handler
+	 * @param  array $cachedCoverData Cached cover image data if available
+	 * @return array collection API object
+	 */
+	public function toCollection(IURLGenerator $urlGenerator, $l10n, $cachedCoverData) {
 		return array(
-			'name' => $this->getNameString($l10n),
-			'year' => $this->getYearRange(),
-			'disk' => $this->getDisk(),
-			'cover' => $coverUrl,
-			'id' => $this->getId(),
+			'name'  => $this->getNameString($l10n),
+			'year'  => $this->getYearRange(),
+			'disk'  => $this->getDisk(),
+			'cover' => $this->coverToCollection($urlGenerator, $cachedCoverData),
+			'id'    => $this->getId(),
 		);
 	}
 
 	/**
 	 * Creates object used by the shiva API (array with name, year, disk, cover URL, ID, slug, URI and artists Array)
-	 * @param  IURLGenerator $urlGenerator URLGenerator
-	 * @param  object $l10n L10n handler
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  object $l10n Localization handler
 	 * @return array shiva API object
 	 */
 	public function toAPI(IURLGenerator $urlGenerator, $l10n) {
-		$collection = $this->toCollection($urlGenerator, $l10n);
-		$collection["uri"] = $this->getUri($urlGenerator);
-		$collection["slug"] = $this->getid() . '-' .$this->slugify('name');
-		$collection["albumArtistId"] = $this->getAlbumArtistId();
-		$collection["artists"] = $this->getArtists($urlGenerator);
-		$collection["year"] = $this->yearToAPI();
-
-		return $collection;
+		return array(
+			'name'          => $this->getNameString($l10n),
+			'year'          => $this->yearToAPI(),
+			'disk'          => $this->getDisk(),
+			'cover'         => $this->coverToAPI($urlGenerator),
+			'id'            => $this->getId(),
+			'uri'           => $this->getUri($urlGenerator),
+			'slug'          => $this->getid() . '-' .$this->slugify('name'),
+			'albumArtistId' => $this->getAlbumArtistId(),
+			'artists'       => $this->getArtists($urlGenerator)
+		);
 	}
 }

js/app/directives/albumart.js
@@ -26,8 +26,6 @@ angular.module('Music').directive('albumart', [function() {
 		element.html('');
 		element.css('background-color', '');
 		// add background image
-		element.css('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
-		element.css('-ms-filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
 		element.css('background-image', 'url(' + imageUrl + ')');
 	}
 

js/public/app.js
@@ -994,8 +994,6 @@ angular.module('Music').directive('albumart', [function() {
 		element.html('');
 		element.css('background-color', '');
 		// add background image
-		element.css('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
-		element.css('-ms-filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
 		element.css('background-image', 'url(' + imageUrl + ')');
 	}
 

commit 15657abf66d3e87c1c64d0c644934629a705f924

Fix large cover images breaking the loading of Music app
- Embedding too large image (or many large images) broke the loading of collection.json. Apparently, the connection between web server and SQL server timed out (?).
- Now the embedding logic is as follows:
   1) Only images of size <= 100 KB are cached
   2) Cached images are embedded in collection.json as long as their accumulated size doesn't exceed 3 MB. Once the limit is met, the rest of the images are transferred as links.
- This issue was regression from 7d139a628430f55
- Refs #570

controller/apicontroller.php
@@ -116,23 +116,47 @@ class ApiController extends Controller {
 		return $response;
 	}
 
+	/**
+	 * Small cached images are embedded directly to the collection to limit the number of PHP queries.
+	 * However, the total size of all embedded covers is limited to avoid the size of the collection
+	 * from getting out of control with large music libraries.
+	 * @param int $albumId
+	 * @return string|null
+	 */
+	private function coverToEmbed($albumId) {
+		$cover = $this->coverHelper->getCoverFromCache($albumId, $this->userId, true);
+		if ($cover != null) {
+			$size = strlen($cover['content']);
+			if ($size + $this->embeddedCoversTotalSize > self::EMBEDDED_COVERS_MAX_TOTAL_SIZE) {
+				$cover = null;
+			} else {
+				$this->embeddedCoversTotalSize += $size;
+			}
+		}
+		return $cover;
+	}
+	const EMBEDDED_COVERS_MAX_TOTAL_SIZE = 3145728; // 3 MB
+	private $embeddedCoversTotalSize = 0;
+
 	private function buildCollectionJson() {
 		/** @var Artist[] $allArtists */
 		$allArtists = $this->artistBusinessLayer->findAll($this->userId);
 		$allArtistsByIdAsObj = array();
 		$allArtistsByIdAsArr = array();
 		foreach ($allArtists as &$artist) {
-			$allArtistsByIdAsObj[$artist->getId()] = $artist;
-			$allArtistsByIdAsArr[$artist->getId()] = $artist->toCollection($this->l10n);
+			$artistId = $artist->getId();
+			$allArtistsByIdAsObj[$artistId] = $artist;
+			$allArtistsByIdAsArr[$artistId] = $artist->toCollection($this->l10n);
 		}
-		
+
 		$allAlbums = $this->albumBusinessLayer->findAll($this->userId);
 		$allAlbumsByIdAsObj = array();
 		$allAlbumsByIdAsArr = array();
 		foreach ($allAlbums as &$album) {
-			$allAlbumsByIdAsObj[$album->getId()] = $album;
-			$allAlbumsByIdAsArr[$album->getId()] = $album->toCollection($this->urlGenerator, $this->l10n,
-					$this->coverHelper->getCoverFromCache($album->getId(), $this->userId, true));
+			$albumId = $album->getId();
+			$allAlbumsByIdAsObj[$albumId] = $album;
+			$allAlbumsByIdAsArr[$albumId] = $album->toCollection(
+					$this->urlGenerator, $this->l10n, $this->coverToEmbed($albumId));
 		}
 
 		/** @var Track[] $allTracks */

utility/coverhelper.php
@@ -30,6 +30,8 @@ class CoverHelper {
 	private $cache;
 	private $logger;
 
+	const MAX_SIZE_TO_CACHE = 102400;
+
 	public function __construct(
 			AlbumBusinessLayer $albumBusinessLayer,
 			Extractor $extractor,
@@ -89,18 +91,24 @@ class CoverHelper {
 	 * @param array $coverData
 	 */
 	public function addCoverToCache($albumId, $userId, $coverData) {
-		try {
-			// support both the format generated by GetID3 and the format used in \OCA\Music\Http\FileResponse
-			$mime = array_key_exists('mimetype', $coverData) ? $coverData['mimetype'] : $coverData['image_mime'];
-			$content = array_key_exists('content', $coverData) ? $coverData['content'] : $coverData['data'];
+		// support both the format generated by GetID3 and the format used in \OCA\Music\Http\FileResponse
+		$mime = array_key_exists('mimetype', $coverData) ? $coverData['mimetype'] : $coverData['image_mime'];
+		$content = array_key_exists('content', $coverData) ? $coverData['content'] : $coverData['data'];
 
-			if ($mime && $content) {
-				$this->cache->add($userId, 'cover_' . $albumId, $mime . '|' . base64_encode($content));
-				$this->cache->remove($userId, 'collection');
+		if ($mime && $content) {
+			$size = strlen($content);
+			if ($size < self::MAX_SIZE_TO_CACHE) {
+				try {
+					$this->cache->add($userId, 'cover_' . $albumId, $mime . '|' . base64_encode($content));
+					$this->cache->remove($userId, 'collection');
+				}
+				catch (UniqueConstraintViolationException $ex) {
+					$this->logger->log("Tried to cache cover of album $albumId which is already cached. Ignoring.", 'warn');
+				}
 			}
-		}
-		catch (UniqueConstraintViolationException $ex) {
-			$this->logger->log("Tried to cache cover of album $albumId which is already cached. Ignoring.", 'warn');
+			else {
+				$this->logger->log("Cover image of album $albumId is large ($size B), skip caching", 'debug');
+			}
 		}
 	}
 
owncloud/music

commit 7d139a628430f552c1a8e264cb3caa89aa2a394f

Cache cover image data in DB, embed the image data into collection.json if it's already cached
- Previously, each cover image was loaded separately by the browser. Firefox was able to cache the results, but Chrome was not. Hence, it took quite some time on Chrome before all cover images became visible as each PHP query has a static overhead which may be hundreds of milliseconds.
- With this commit, the number of PHP queries is dramatically reduced by embedding the cover image data directly to the collection.json. This is done only for cover images which have been previously cached, and hence it doesn't take many seconds of additional time. For cover images not already cached, the loading happens the same way as before.
- The trade-off is that the collection.json grows in size, so the client has to load more data before anything can be rendered. This may be significant on slow connections, but with a decent bandwidth it's a non-issue.
- Also, removed some IE8-compatibility code from directives/albumart.js. The UI has never worked well on IE8 (e.g. playing music has never worked) and ownCloud 9 has already dropped support for IE8 and IE9.

utility/extractorgetid3.php
@@ -39,7 +39,7 @@ class ExtractorGetID3 implements Extractor {
 	/**
 	 * get metadata info for a media file
 	 *
-	 * @param \OCP\Files\Node $file the file
+	 * @param \OCP\Files\File $file the file
 	 * @return array extracted data
 	 */
 	public function extract($file) {
@@ -59,4 +59,36 @@ class ExtractorGetID3 implements Extractor {
 
 		return $metadata;
 	}
+
+	/**
+	 * extract embedded cover art image from media file
+	 * 
+	 * @param \OCP\Files\File $file the media file
+	 * @return array with keys 'mimetype' and 'content'
+	 */
+	public function parseEmbeddedCoverArt($file){
+		$fileInfo = $this->extract($file);
+		return self::getTag($fileInfo, 'picture');
+	}
+
+	public static function getTag($fileInfo, $tag) {
+		if(array_key_exists('comments', $fileInfo)) {
+			$comments = $fileInfo['comments'];
+			if(array_key_exists($tag, $comments)) {
+				return $comments[$tag][0];
+			}
+		}
+		return null;
+	}
+
+	public static function getFirstOfTags($fileInfo, array $tags, $defaultValue = null) {
+		foreach ($tags as $tag) {
+			$value = self::getTag($fileInfo, $tag);
+			if ($value !== null || $value !== '') {
+				return $value;
+			}
+		}
+		return $defaultValue;
+	}
+
 }

app/music.php
@@ -272,7 +272,8 @@ class Music extends App {
 		$container->registerService('CoverHelper', function($c) {
 			return new CoverHelper(
 				$c->query('AlbumBusinessLayer'),
-				$c->query('Scanner'),
+				$c->query('ExtractorGetID3'),
+				$c->query('Cache'),
 				$c->query('Logger')
 			);
 		});
@@ -297,6 +298,7 @@ class Music extends App {
 				$c->query('TrackBusinessLayer'),
 				$c->query('PlaylistBusinessLayer'),
 				$c->query('Cache'),
+				$c->query('CoverHelper'),
 				$c->query('Logger'),
 				$c->query('Db'),
 				$c->query('Config'),

appinfo/database.xml
@@ -386,9 +386,20 @@
 			</field>
 			<field>
 				<name>data</name>
-				<type>clob</type>
+				<type>blob</type>
 				<notnull>false</notnull>
 			</field>
+
+			<index>
+				<name>music_cache_index</name>
+				<unique>true</unique>
+				<field>
+					<name>user_id</name>
+				</field>
+				<field>
+					<name>key</name>
+				</field>
+			</index>
 		</declaration>
 	</table>
 </database>

appinfo/info.xml
@@ -11,7 +11,7 @@
 	</description>
 	<licence>AGPL</licence>
 	<author>Morris Jobke, Jan-Christoph Borchardt (design)</author>
-	<version>0.4.2-alpha.1</version>
+	<version>0.4.2-alpha.2</version>
 	<category>multimedia</category>
 	<screenshot>https://user-images.githubusercontent.com/8565946/28543434-6eb12de0-70c8-11e7-966e-65f4c3a90531.png</screenshot>
 	<website>https://github.com/owncloud/music</website>

controller/apicontroller.php
@@ -131,7 +131,8 @@ class ApiController extends Controller {
 		$allAlbumsByIdAsArr = array();
 		foreach ($allAlbums as &$album) {
 			$allAlbumsByIdAsObj[$album->getId()] = $album;
-			$allAlbumsByIdAsArr[$album->getId()] = $album->toCollection($this->urlGenerator, $this->l10n);
+			$allAlbumsByIdAsArr[$album->getId()] = $album->toCollection($this->urlGenerator, $this->l10n,
+					$this->coverHelper->getCoverFromCache($album->getId(), $this->userId));
 		}
 
 		/** @var Track[] $allTracks */

db/album.php
@@ -142,41 +142,72 @@ class Album extends Entity {
 	}
 
 	/**
-	 * Creates object used for collection API (array with name, year, disk, cover URL and ID)
-	 * @param  IURLGenerator $urlGenerator URLGenerator
-	 * @param  object $l10n L10n handler
-	 * @return array collection API object
+	 * Return the cover URL to be used in the Shiva API
+	 * @param IURLGenerator $urlGenerator
+	 * @return string|null
 	 */
-	public function toCollection(IURLGenerator $urlGenerator, $l10n) {
+	public function coverToAPI(IURLGenerator $urlGenerator) {
 		$coverUrl = null;
 		if ($this->getCoverFileId() > 0) {
 			$coverUrl = $urlGenerator->linkToRoute('music.api.cover',
-				array('albumIdOrSlug' => $this->getId()));
+					array('albumIdOrSlug' => $this->getId()));
 		}
+		return $coverUrl;
+	}
 
+	/**
+	 * If the cover image is already cached, the collection contains the image data encoded as base64.
+	 * Otherwise the collection contains URL of the image.
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  array $cachedCoverData Cached cover image data if available
+	 * $return string|null
+	 */
+	public function coverToCollection(IURLGenerator $urlGenerator, $cachedCoverData) {
+		if ($this->getCoverFileId() > 0) {
+			if ($cachedCoverData !== null) {
+				return 'data:' . $cachedCoverData['mimetype'] . ';base64,' .
+						base64_encode($cachedCoverData['content']);
+			} else {
+				return $this->coverToAPI($urlGenerator);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Creates object used for collection API (array with name, year, disk, cover URL and ID)
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  object $l10n Localization handler
+	 * @param  array $cachedCoverData Cached cover image data if available
+	 * @return array collection API object
+	 */
+	public function toCollection(IURLGenerator $urlGenerator, $l10n, $cachedCoverData) {
 		return array(
-			'name' => $this->getNameString($l10n),
-			'year' => $this->getYearRange(),
-			'disk' => $this->getDisk(),
-			'cover' => $coverUrl,
-			'id' => $this->getId(),
+			'name'  => $this->getNameString($l10n),
+			'year'  => $this->getYearRange(),
+			'disk'  => $this->getDisk(),
+			'cover' => $this->coverToCollection($urlGenerator, $cachedCoverData),
+			'id'    => $this->getId(),
 		);
 	}
 
 	/**
 	 * Creates object used by the shiva API (array with name, year, disk, cover URL, ID, slug, URI and artists Array)
-	 * @param  IURLGenerator $urlGenerator URLGenerator
-	 * @param  object $l10n L10n handler
+	 * @param  IURLGenerator $urlGenerator URL Generator
+	 * @param  object $l10n Localization handler
 	 * @return array shiva API object
 	 */
 	public function toAPI(IURLGenerator $urlGenerator, $l10n) {
-		$collection = $this->toCollection($urlGenerator, $l10n);
-		$collection["uri"] = $this->getUri($urlGenerator);
-		$collection["slug"] = $this->getid() . '-' .$this->slugify('name');
-		$collection["albumArtistId"] = $this->getAlbumArtistId();
-		$collection["artists"] = $this->getArtists($urlGenerator);
-		$collection["year"] = $this->yearToAPI();
-
-		return $collection;
+		return array(
+			'name'          => $this->getNameString($l10n),
+			'year'          => $this->yearToAPI(),
+			'disk'          => $this->getDisk(),
+			'cover'         => $this->coverToAPI($urlGenerator),
+			'id'            => $this->getId(),
+			'uri'           => $this->getUri($urlGenerator),
+			'slug'          => $this->getid() . '-' .$this->slugify('name'),
+			'albumArtistId' => $this->getAlbumArtistId(),
+			'artists'       => $this->getArtists($urlGenerator)
+		);
 	}
 }

js/app/directives/albumart.js
@@ -26,8 +26,6 @@ angular.module('Music').directive('albumart', [function() {
 		element.html('');
 		element.css('background-color', '');
 		// add background image
-		element.css('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
-		element.css('-ms-filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
 		element.css('background-image', 'url(' + imageUrl + ')');
 	}
 

js/public/app.js
@@ -994,8 +994,6 @@ angular.module('Music').directive('albumart', [function() {
 		element.html('');
 		element.css('background-color', '');
 		// add background image
-		element.css('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
-		element.css('-ms-filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imageUrl + "', sizingMethod='scale')");
 		element.css('background-image', 'url(' + imageUrl + ')');
 	}
 

commit 19f573c3f5aff3adf1a5e3946ba1bf1df51acd0c

Fixed stupid metadata parsing error
- Tags with multiple possible names were not parsed properly
- Regression from 7d139a628430f55

utility/extractorgetid3.php
@@ -84,7 +84,7 @@ class ExtractorGetID3 implements Extractor {
 	public static function getFirstOfTags($fileInfo, array $tags, $defaultValue = null) {
 		foreach ($tags as $tag) {
 			$value = self::getTag($fileInfo, $tag);
-			if ($value !== null || $value !== '') {
+			if ($value !== null && $value !== '') {
 				return $value;
 			}
 		}
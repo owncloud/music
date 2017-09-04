9083f368c0da59bfefc3f8c3c43ef640d13cb70a
Pauli Järvinen <pauli.jarvinen@gmail.com>
2017-09-04 21:11:41 +0300

    Document some of the design choices for the embedded music player using the PHP comments

--- appinfo/app.php
+++ appinfo/app.php
@@ -7,7 +7,9 @@
  * later. See the COPYING file.
  *
  * @author Morris Jobke <hey@morrisjobke.de>
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
+ * @copyright Pauli Järvinen 2017
  */
 
 namespace OCA\Music\App;
@@ -52,25 +54,36 @@ $c->query('ShareHooks')->register();
 \OCP\App::registerPersonal($appName, 'settings/user');
 
 /**
- * load styles and scripts
+ * Load embedded music player for Files and Sharing apps
+ * 
+ * The nice way to do this would be
+ * \OC::$server->getEventDispatcher()->addListener('OCA\Files::loadAdditionalScripts', $loadEmbeddedMusicPlayer);
+ * \OC::$server->getEventDispatcher()->addListener('OCA\Files_Sharing::loadAdditionalScripts', $loadEmbeddedMusicPlayer);
+ * ... but this doesn't work for shared files on ownCloud 9.0, at least. Hence, we load the scripts
+ * directly if the requested URL seems to be for Files or Sharing.
+ * 
+ * Furthermore, it would be sensible to load majority of the needed scripts within the main js file (files-music-player)
+ * with OC.addScript() only when the player is actually used. However, this doesn't seem to work on Nextcloud 12.0.0,
+ * probably because of https://github.com/nextcloud/server/issues/5314.
  */
+$loadEmbeddedMusicPlayer = function() use ($appName) {
+	\OCP\Util::addScript($appName, 'vendor/soundmanager/script/soundmanager2-jsmin');
+	\OCP\Util::addScript($appName, 'vendor/aurora/aurora-bundle.min');
+	\OCP\Util::addScript($appName, 'vendor/javascript-detect-element-resize/jquery.resize');
+	\OCP\Util::addScript($appName, 'vendor/jquery-initialize/jquery.initialize.min');
+	\OCP\Util::addScript($appName, 'vendor/js-cookie/src/js.cookie');
+	\OCP\Util::addScript($appName, 'app/playerwrapper');
+	\OCP\Util::addScript($appName, 'public/files-music-player');
 
-// Load embedded player for Files and Sharing apps
+	\OCP\Util::addStyle($appName, 'files-music-player');
+};
+
 $request = \OC::$server->getRequest();
-
 if (isset($request->server['REQUEST_URI'])) {
 	$url = $request->server['REQUEST_URI'];
 	$isFilesUrl = preg_match('%/apps/files(/.*)?%', $url);
 	$isShareUrl = preg_match('%/s/.+%', $url) && !preg_match('%/apps/.*%', $url);
 	if ($isFilesUrl || $isShareUrl) {
-		\OCP\Util::addScript($appName, 'vendor/soundmanager/script/soundmanager2-jsmin');
-		\OCP\Util::addScript($appName, 'vendor/aurora/aurora-bundle.min');
-		\OCP\Util::addScript($appName, 'vendor/javascript-detect-element-resize/jquery.resize');
-		\OCP\Util::addScript($appName, 'vendor/jquery-initialize/jquery.initialize.min');
-		\OCP\Util::addScript($appName, 'vendor/js-cookie/src/js.cookie');
-		\OCP\Util::addScript($appName, 'app/playerwrapper');
-		\OCP\Util::addScript($appName, 'public/files-music-player');
-
-		\OCP\Util::addStyle($appName, 'files-music-player');
+		$loadEmbeddedMusicPlayer();
 	}
 }

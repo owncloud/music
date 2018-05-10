77bed49a0fe25880258f6b61b5a7fc4eb0bb2767
Pauli Järvinen <pauli.jarvinen@gmail.com>
2018-05-10 20:16:44 +0300

    Ignore events from new and updated files if the user has not yet scanned the music library
    Do not automatically add the newly uploaded or modified files to the
    music database if the user has not yet scanned the music library. This
    is specifically to prevent problems when getting events from any
    default audio files on a newly created user, as described in #638.

--- hooks/filehooks.php
+++ hooks/filehooks.php
@@ -20,7 +20,7 @@ class FileHooks {
 
 	private $filesystemRoot;
 
-	public function __construct($filesystemRoot){
+	public function __construct($filesystemRoot) {
 		$this->filesystemRoot = $filesystemRoot;
 	}
 
@@ -28,7 +28,7 @@ class FileHooks {
 	 * Invoke auto update of music database after file or folder deletion
 	 * @param \OCP\Files\Node $node pointing to the file or folder
 	 */
-	public static function deleted($node){
+	public static function deleted($node) {
 		$app = new Music();
 		$container = $app->getContainer();
 		$scanner = $container->query('Scanner');
@@ -44,7 +44,7 @@ class FileHooks {
 	 * Invoke auto update of music database after file update or file creation
 	 * @param \OCP\Files\Node $node pointing to the file
 	 */
-	public static function updated($node){
+	public static function updated($node) {
 		// At least on Nextcloud 13, it sometimes happens that this hook is triggered
 		// when the core creates some temporary file and trying to access the provided
 		// node throws an exception, probably because the temp file is already removed
@@ -67,7 +67,7 @@ class FileHooks {
 		}
 	}
 
-	private static function handleUpdated($node, $app){
+	private static function handleUpdated($node, $app) {
 		// we are interested only about updates on files, not on folders
 		if ($node->getType() == FileInfo::TYPE_FILE) {
 			$container = $app->getContainer();
@@ -87,14 +87,26 @@ class FileHooks {
 				}
 			}
 
-			if (!empty($userId) && !empty($userFolder)) {
+			// Ignore event if we got not user or folder or the user has not yet scanned the music
+			// collection. The last condition is especially to prevent problems when creating new user
+			// and the default file set contains one or more audio files (see the discussion in #638).
+			if (!empty($userId) && !empty($userFolder) && self::userHasMusicLib($userId, $container)) {
 				$scanner->update($node, $userId, $userFolder);
 			}
 		}
 	}
 
-	public static function preRenamed($source, $target)
-	{
+	/**
+	 * Check if user has any scanned tracks in his/her music library
+	 * @param string $userId
+	 * @param IAppContainer $container
+	 */
+	private static function userHasMusicLib($userId, $container) {
+		$trackBusinessLayer = $container->query('TrackBusinessLayer');
+		return 0 < $trackBusinessLayer->count($userId);
+	}
+
+	public static function preRenamed($source, $target) {
 		// We are interested only if the path of the folder of the file changes:
 		// that could move a music file out of the scanned folder or remove a
 		// cover image file from album folder.
@@ -105,8 +117,7 @@ class FileHooks {
 		}
 	}
 
-	public static function postRenamed($source, $target)
-	{
+	public static function postRenamed($source, $target) {
 		// Renaming/moving file may
 		// a) move it into the folder scanned by Music app
 		// b) have influence on the display name of the track

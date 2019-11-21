owncloud/music

commit c98fe53e6a022652c7d831cc55bf9538707ad36c

Subsonic: Add the API method getLyrics

controller/subsoniccontroller.php
@@ -21,7 +21,6 @@ use \OCP\Files\IRootFolder;
 use \OCP\IRequest;
 use \OCP\IURLGenerator;
 
-use \OCA\Music\AppFramework\BusinessLayer\BusinessLayerException;
 use \OCA\Music\AppFramework\Core\Logger;
 use \OCA\Music\AppFramework\Utility\MethodAnnotationReader;
 
@@ -43,6 +42,7 @@ use \OCA\Music\Http\XMLResponse;
 use \OCA\Music\Middleware\SubsonicException;
 
 use \OCA\Music\Utility\CoverHelper;
+use \OCA\Music\Utility\DetailsHelper;
 use \OCA\Music\Utility\UserMusicFolder;
 use \OCA\Music\Utility\Util;
 
@@ -59,6 +59,7 @@ class SubsonicController extends Controller {
 	private $userMusicFolder;
 	private $l10n;
 	private $coverHelper;
+	private $detailsHelper;
 	private $logger;
 	private $userId;
 	private $format;
@@ -76,6 +77,7 @@ class SubsonicController extends Controller {
 								IRootFolder $rootFolder,
 								UserMusicFolder $userMusicFolder,
 								CoverHelper $coverHelper,
+								DetailsHelper $detailsHelper,
 								Logger $logger) {
 		parent::__construct($appname, $request);
 
@@ -89,6 +91,7 @@ class SubsonicController extends Controller {
 		$this->rootFolder = $rootFolder;
 		$this->userMusicFolder = $userMusicFolder;
 		$this->coverHelper = $coverHelper;
+		$this->detailsHelper = $detailsHelper;
 		$this->logger = $logger;
 	}
 
@@ -321,6 +324,31 @@ class SubsonicController extends Controller {
 	/**
 	 * @SubsonicAPI
 	 */
+	private function getLyrics() {
+		$artistPar = $this->request->getParam('artist');
+		$titlePar = $this->request->getParam('title');
+
+		$track = $this->trackBusinessLayer->findByNameAndArtistName($titlePar, $artistPar, $this->userId);
+
+		if ($track === null) {
+			return $this->subsonicResponse(['lyrics' => []]);
+		}
+		else {
+			$artist = $this->artistBusinessLayer->find($track->getArtistId(), $this->userId);
+			$rootFolder = $this->userMusicFolder->getFolder($this->userId);
+			$lyrics = $this->detailsHelper->getLyrics($track->getFileId(), $rootFolder);
+
+			return $this->subsonicResponse(['lyrics' => [
+					'artist' => $artist->getNameString($this->l10n),
+					'title' => $track->getTitle(),
+					'value' => $lyrics
+			]]);
+		}
+	}
+
+	/**
+	 * @SubsonicAPI
+	 */
 	private function stream() {
 		// We don't support transcaoding, so 'stream' and 'download' act identically
 		return $this->download();
@@ -349,11 +377,18 @@ class SubsonicController extends Controller {
 	private function search2() {
 		$results = $this->doSearch();
 
-		return $this->subsonicResponse(['searchResult2' => [
-			'artist' => \array_map([$this, 'artistToApi'], $results['artists']),
-			'album' => \array_map([$this, 'albumToOldApi'], $results['albums']),
-			'song' => \array_map([$this, 'trackToApi'], $results['tracks'])
-		]]);
+		$formattedResults = [];
+		if (!empty($results['artists'])) {
+			$formattedResults['artist'] =  \array_map([$this, 'artistToApi'], $results['artists']);
+		}
+		if (!empty($results['albums'])) {
+			$formattedResults['album'] = \array_map([$this, 'albumToOldApi'], $results['albums']);
+		}
+		if (!empty($results['tracks'])) {
+			$formattedResults['song'] = \array_map([$this, 'trackToApi'], $results['tracks']);
+		}
+
+		return $this->subsonicResponse(['searchResult2' => $formattedResults]);
 	}
 
 	/**
@@ -362,11 +397,18 @@ class SubsonicController extends Controller {
 	private function search3() {
 		$results = $this->doSearch();
 
-		return $this->subsonicResponse(['searchResult3' => [
-			'artist' => \array_map([$this, 'artistToApi'], $results['artists']),
-			'album' => \array_map([$this, 'albumToNewApi'], $results['albums']),
-			'song' => \array_map([$this, 'trackToApi'], $results['tracks'])
-		]]);
+		$formattedResults = [];
+		if (!empty($results['artists'])) {
+			$formattedResults['artist'] =  \array_map([$this, 'artistToApi'], $results['artists']);
+		}
+		if (!empty($results['albums'])) {
+			$formattedResults['album'] = \array_map([$this, 'albumToNewApi'], $results['albums']);
+		}
+		if (!empty($results['tracks'])) {
+			$formattedResults['song'] = \array_map([$this, 'trackToApi'], $results['tracks']);
+		}
+
+		return $this->subsonicResponse(['searchResult3' => $formattedResults]);
 	}
 
 	/**

http/xmlresponse.php
@@ -50,26 +50,27 @@ class XMLResponse extends Response {
 		return $xmlTree->asXML();
 	}
 
-	private static function addChildElement($parentNode, $key, $value, $forceElement=false) {
+	private static function addChildElement($parentNode, $key, $value, $allowAttribute=true) {
 		if (\is_bool($value)) {
 			$value = $value ? 'true' : 'false';
 		}
 
 		if (\is_string($value) || \is_numeric($value)) {
-			if ($forceElement) {
-				$parentNode->addChild($key, $value);
-			} else {
+			if ($key == 'value') { // special key mapping to the element contents
+				dom_import_simplexml($parentNode)->nodeValue = $value;
+			} elseif ($allowAttribute) {
 				$parentNode->addAttribute($key, $value);
+			} else {
+				$parentNode->addChild($key, $value);
 			}
 		}
 		elseif (\is_array($value)) {
 			if (self::arrayIsIndexed($value)) {
 				foreach ($value as $child) {
-					self::addChildElement($parentNode, $key, $child, true);
+					self::addChildElement($parentNode, $key, $child, /*allowAttribute=*/false);
 				}
 			}
-			else {
-				// associative array
+			else { // associative array
 				$element = $parentNode->addChild($key);
 				foreach ($value as $childKey => $childValue) {
 					self::addChildElement($element, $childKey, $childValue);
@@ -86,10 +87,11 @@ class XMLResponse extends Response {
 
 	/**
 	 * Array is considered to be "indexed" if its first element has numerical key.
+	 * Empty array is not considered "indexed".
 	 * @param array $array
 	 */
 	private static function arrayIsIndexed(array $array) {
 		reset($array);
-		return empty($array) || \is_int(key($array));
+		return !empty($array) && \is_int(key($array));
 	}
 }

app/music.php
@@ -175,8 +175,9 @@ class Music extends App {
 				$c->query('RootFolder'),
 				$c->query('UserMusicFolder'),
 				$c->query('CoverHelper'),
+				$c->query('DetailsHelper'),
 				$c->query('Logger')
-				);
+			);
 		});
 
 		/**

businesslayer/trackbusinesslayer.php
@@ -23,6 +23,7 @@ use \OCA\Music\Db\Track;
 use \OCA\Music\Utility\Util;
 
 use \OCP\AppFramework\Db\DoesNotExistException;
+use \OCP\AppFramework\Db\MultipleObjectsReturnedException;
 
 class TrackBusinessLayer extends BusinessLayer {
 	private $logger;
@@ -70,6 +71,25 @@ class TrackBusinessLayer extends BusinessLayer {
 	 */
 	public function findAllByNameRecursive($name, $userId) {
 		return $this->mapper->findAllByNameRecursive($name, $userId);
+	}
+
+	/**
+	 * Returns track specified by name and/or artist name
+	 * @param string|null $name the name of the track
+	 * @param string|null $artistName the name of the artist
+	 * @param string $userId the name of the user
+	 * @return \OCA\Music\Db\Track|null Mathing track if the criteria uniquely defines one
+	 */
+	public function findByNameAndArtistName($name, $artistName, $userId) {
+		try {
+			return $this->mapper->findByNameAndArtistName($name, $artistName, $userId);
+		} catch (DoesNotExistException $e) {
+			$this->logger->log("No track found with title '$name' and artist '$artistName'", 'debug');
+			return null;
+		} catch (MultipleObjectsReturnedException $e) {
+			$this->logger->log("More than on track found with title '$name' and artist '$artistName'", 'debug');
+			return null;
+		}
 	}
 
 	/**

db/trackmapper.php
@@ -236,6 +236,31 @@ class TrackMapper extends BaseMapper {
 	}
 
 	/**
+	 * Returns track specified by name and/or artist name
+	 * @param string|null $name the name of the track
+	 * @param string|null $artistName the name of the artist
+	 * @param string $userId the name of the user
+	 * @return \OCA\Music\Db\Track|null Mathing track if the criteria uniquely defines one
+	 */
+	public function findByNameAndArtistName($name, $artistName, $userId) {
+		$sqlConditions = '';
+		$params = [$userId];
+
+		if (!empty($name)) {
+			$sqlConditions .= 'AND LOWER(`track`.`title`) LIKE LOWER(?) ';
+			$params[] = $name;
+		}
+
+		if (!empty($artistName)) {
+			$sqlConditions .= 'AND `track`.`artist_id` IN (SELECT `id` FROM `*PREFIX*music_artists` WHERE LOWER(`name`) LIKE LOWER(?))';
+			$params[] = $artistName;
+		}
+
+		$sql = $this->makeSelectQuery($sqlConditions);
+		return $this->findEntity($sql, $params);
+	}
+
+	/**
 	 * Finds all track IDs of the user along with the parent folder ID of each track
 	 * @param string $userId
 	 * @return array where keys are folder IDs and values are arrays of track IDs

utility/detailshelper.php
@@ -7,7 +7,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2018
+ * @copyright Pauli Järvinen 2018, 2019
  */
 
 namespace OCA\Music\Utility;
@@ -77,6 +77,21 @@ class DetailsHelper {
 	}
 
 	/**
+	 * @param integer $fileId
+	 * @param Folder $userFolder
+	 * $return string|null
+	 */
+	public function getLyrics($fileId, $userFolder) {
+		$lyrics = null;
+		$fileNodes = $userFolder->getById($fileId);
+		if (\count($fileNodes) > 0) {
+			$data = $this->extractor->extract($fileNodes[0]);
+			$lyrics = ExtractorGetID3::getFirstOfTags($data, ['unsynchronised_lyric', 'unsynced lyrics', 'LYRICS']);
+		}
+		return $lyrics;
+	}
+
+	/**
 	 * Base64 encode the picture binary data and wrap it so that it can be directly used as
 	 * src of an HTML img element.
 	 * @param string $pic
@@ -95,7 +110,7 @@ class DetailsHelper {
 	 * @param $item
 	 */
 	private static function sanitizeString(&$item){
-		if(is_string($item)) {
+		if (is_string($item)) {
 			$item = \mb_convert_encoding($item, 'UTF-8', 'UTF-8');
 		}
 	}

utility/extractorgetid3.php
@@ -84,22 +84,27 @@ class ExtractorGetID3 implements Extractor {
 	}
 
 	public static function getTag($fileInfo, $tag, $binaryValued = false) {
+		$value = null;
+
 		if (\array_key_exists('comments', $fileInfo)) {
 			$comments = $fileInfo['comments'];
 			if (\array_key_exists($tag, $comments)) {
 				$value = $comments[$tag][0];
-				if (!$binaryValued) {
-					// Ensure that the tag contains only valid utf-8 characters.
-					// Illegal characters may result, if the file metadata has a mismatch
-					// between claimed and actual encoding. Invalid characters could break
-					// the database update.
-					\mb_substitute_character(0xFFFD); // Use the Unicode REPLACEMENT CHARACTER (U+FFFD)
-					$value = \mb_convert_encoding($value, 'UTF-8', 'UTF-8');
-				}
-				return $value;
+			}
+			elseif (\array_key_exists('text', $comments) && \array_key_exists($tag, $comments['text'])) {
+				$value = $comments['text'][$tag];
+			}
+
+			if ($value !== null && !$binaryValued) {
+				// Ensure that the tag contains only valid utf-8 characters.
+				// Illegal characters may result, if the file metadata has a mismatch
+				// between claimed and actual encoding. Invalid characters could break
+				// the database update.
+				\mb_substitute_character(0xFFFD); // Use the Unicode REPLACEMENT CHARACTER (U+FFFD)
+				$value = \mb_convert_encoding($value, 'UTF-8', 'UTF-8');
 			}
 		}
-		return null;
+		return $value;
 	}
 
 	public static function getFirstOfTags($fileInfo, array $tags, $defaultValue = null) {

commit 9de74759e9b61e6ffb4db4080d14319c39d127c4

Subsonic: Fix some issues introduced by the commit c98fe53e6
The commit c98fe53e6 "Subsonic: Add the API method getLyrics" caused
some unintended side effects, when it slightly changed the formatting
of XML in XMLResponse class. Empty XML elements were being created
where they were not supposed to be, like in getVideos and getStarred
methods, as well as when fetching the contents of an empty folder.
This, in turn, caused crashes both on Subsonic Android app and DSub.

Hence, the problematic change to XMLResponse was reverted, and the
intentional empty XML element needed in getLyrics is now handled a bit
differently. This new method is also better when using the JSON
formatted results, as the empty element is now shown as "{}" instead of
"[]". This matches the behavior of the "real" Subsonic server.

controller/subsoniccontroller.php
@@ -328,7 +328,7 @@ class SubsonicController extends Controller {
 		$track = $this->trackBusinessLayer->findByNameAndArtistName($titlePar, $artistPar, $this->userId);
 
 		if ($track === null) {
-			return $this->subsonicResponse(['lyrics' => []]);
+			return $this->subsonicResponse(['lyrics' => new \stdClass]);
 		}
 		else {
 			$artist = $this->artistBusinessLayer->find($track->getArtistId(), $this->userId);
@@ -374,18 +374,11 @@ class SubsonicController extends Controller {
 	private function search2() {
 		$results = $this->doSearch();
 
-		$formattedResults = [];
-		if (!empty($results['artists'])) {
-			$formattedResults['artist'] =  \array_map([$this, 'artistToApi'], $results['artists']);
-		}
-		if (!empty($results['albums'])) {
-			$formattedResults['album'] = \array_map([$this, 'albumToOldApi'], $results['albums']);
-		}
-		if (!empty($results['tracks'])) {
-			$formattedResults['song'] = \array_map([$this, 'trackToApi'], $results['tracks']);
-		}
-
-		return $this->subsonicResponse(['searchResult2' => $formattedResults]);
+		return $this->subsonicResponse(['searchResult2' => [
+			'artist' => \array_map([$this, 'artistToApi'], $results['artists']),
+			'album' => \array_map([$this, 'albumToOldApi'], $results['albums']),
+			'song' => \array_map([$this, 'trackToApi'], $results['tracks'])
+		]]);
 	}
 
 	/**
@@ -394,18 +387,11 @@ class SubsonicController extends Controller {
 	private function search3() {
 		$results = $this->doSearch();
 
-		$formattedResults = [];
-		if (!empty($results['artists'])) {
-			$formattedResults['artist'] =  \array_map([$this, 'artistToApi'], $results['artists']);
-		}
-		if (!empty($results['albums'])) {
-			$formattedResults['album'] = \array_map([$this, 'albumToNewApi'], $results['albums']);
-		}
-		if (!empty($results['tracks'])) {
-			$formattedResults['song'] = \array_map([$this, 'trackToApi'], $results['tracks']);
-		}
-
-		return $this->subsonicResponse(['searchResult3' => $formattedResults]);
+		return $this->subsonicResponse(['searchResult3' => [
+			'artist' => \array_map([$this, 'artistToApi'], $results['artists']),
+			'album' => \array_map([$this, 'albumToNewApi'], $results['albums']),
+			'song' => \array_map([$this, 'trackToApi'], $results['tracks'])
+		]]);
 	}
 
 	/**

http/xmlresponse.php
@@ -80,6 +80,11 @@ class XMLResponse extends Response {
 				}
 			}
 		}
+		elseif ($value instanceof \stdClass) {
+			// empty element
+			$element = $this->doc->createElement($key);
+			$parentNode->appendChild($element);
+		}
 		elseif ($value === null) {
 			// skip
 		}
@@ -90,11 +95,11 @@ class XMLResponse extends Response {
 
 	/**
 	 * Array is considered to be "indexed" if its first element has numerical key.
-	 * Empty array is not considered "indexed".
+	 * Empty array is considered to be "indexed".
 	 * @param array $array
 	 */
 	private static function arrayIsIndexed(array $array) {
 		reset($array);
-		return !empty($array) && \is_int(key($array));
+		return empty($array) || \is_int(key($array));
 	}
 }
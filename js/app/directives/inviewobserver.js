owncloud/music

commit d8f86c7f7944ccead40816bfde128d4672c70035

Replace IntersectionObserver with custom InViewObserver
The javascript built-in IntersectionObserver turned out to consume lots
of CPU time when there is a huge amount of observed elements. What's
even worse, the CPU time was consumed even on idle on Firefox. On
Chrome, the CPU time consumption happened only during scrolling.

The IntersectionObserver was now replaced with our custom directive
InViewObserver, which is based on `scroll` and `resize` events. It gets
the job done, too, and uses much less CPU time. On idle, it uses no CPU
at all. Furthermore, there's still room for optimizations in
InViewObserver.

As an added benefit, our InViewObserver may be used also on IE and
other older browsers. Now, even IE11 remains somewhat usable with
library of 60k trakcs (!).

refs #728

js/app/directives/inviewobserver.js
@@ -1,0 +1,94 @@
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright 2019 Pauli Järvinen
+ *
+ */
+
+/**
+ * This directive observes whether the host element is within the view-port or not.
+ * When the status changes to either direction, it notifies the registered listeners.
+ */
+angular.module('Music').directive('inViewObserver', ['$rootScope', '$timeout', function($rootScope, $timeout) {
+
+	var instances = []; // in creation order i.e. top-most first
+
+	// Drop all instances when view switching begins
+	$rootScope.$on('deactivateView', function() {
+		instances = [];
+	});
+
+	document.getElementById("app-content").addEventListener('scroll', onScroll);
+	$rootScope.$on('resize', onScroll);
+
+	function onScroll() {
+		[].forEach.call(instances, function(inst) {
+			var elem = inst.element;
+			var nowInViewPort = elemInViewPort(elem);
+			var wasInViewPort = inst.inViewPort;
+
+			if (inst.promise) {
+				if (!nowInViewPort) {
+					// element left the viewport before it had been set up, cancel the pending notify
+					$timeout.cancel(inst.promise);
+					inst.promise = null;
+				}
+			}
+			else if (!wasInViewPort && nowInViewPort) {
+				// element entered the viewport, notify the listeners with small delay
+				inst.promise = $timeout(onEnterView, 100, true, inst);
+			}
+			else if (wasInViewPort && !nowInViewPort) {
+				// element left the viewport after it had been notified that it has entered the view
+				onLeaveView(inst);
+			}
+		});
+	}
+
+	function elemInViewPort(elem) {
+		var appView = document.getElementById('app-view');
+		var header = document.getElementById('header');
+		var viewPortTop = header.offsetHeight;
+		var viewPortBottom = header.offsetHeight + appView.offsetHeight;
+
+		var rect = elem.getBoundingClientRect();
+		return rect.bottom >= viewPortTop && rect.top <= viewPortBottom;
+	}
+
+	function onEnterView(inst) {
+		inst.promise = null;
+		inst.inViewPort = true;
+		inst.listeners.forEach(function(listener) {
+			listener.onEnterView();
+		});
+	}
+
+	function onLeaveView(inst) {
+		inst.inViewPort = false;
+		inst.listeners.forEach(function(listener) {
+			listener.onLeaveView();
+		});
+	}
+
+	return {
+		scope: {},
+		controller: function($scope, $element) {
+			this.inViewPort = false;
+			this.element = $element[0];
+			this.listeners = [];
+
+			/**
+			 * Listener must have two function-type properties: onEnterView and onLeaveView
+			 */
+			this.registerListener = function(listener) {
+				this.listeners.push(listener);
+			};
+
+			instances.push(this);
+		}
+	};
+}]);

js/app/directives/listheading.js
@@ -22,52 +22,15 @@ function ($rootScope, $timeout, gettextCatalog) {
 	var playText = gettextCatalog.getString('Play');
 	var playIconSrc = OC.imagePath('music','play-big.svg');
 
-	var observer = null;
-	var instances = null;
-
-	// Lazy loading requires support for IntersectionObserver and WeakMap. This is not
-	// available on IE and other ancient browsers.
-	if (typeof IntersectionObserver !== 'undefined' && typeof WeakMap !== 'undefined') {
-		var onVisibilityChange = function(changes) {
-			changes.forEach(function(change) {
-				var tgt = change.target;
-				var data = instances.get(tgt);
-
-				if (change.intersectionRatio > 0) {
-					// element entered the viewport, setup the layout with small delay
-					data.promise = $timeout(setup, 50, true, tgt, data);
-				}
-				else if (data.promise) {
-					// element left the viewport before it had been set up, cancel the pending setup
-					$timeout.cancel(data.promise);
-					data.promise = null;
-				}
-				else {
-					// element left the viewport after it had been set up, replace it with placeholder
-					tearDown(tgt, data);
-					setupPlaceholder(tgt, data);
-				}
-			});
-		};
-		var observerOptions = {
-			root: document.getElementById("app-content"),
-			rootMargin: '500px'
-		};
-		observer = new IntersectionObserver(onVisibilityChange, observerOptions);
-		instances = new WeakMap();
-	}
-
 	/**
 	 * Set up the contents and the listeners for a given heading element
 	 */
-	function setup(htmlElem, data) {
-		data.promise = null;
-
+	function setup(data) {
 		/**
 		 * Remove any placeholder and add the nested <li> elements for each shown track.
 		 */
-		removeChildNodes(htmlElem);
-		htmlElem.appendChild(render());
+		removeChildNodes(data.element);
+		data.element.appendChild(render());
 
 		/**
 		 * Create the contained HTML elements
@@ -101,7 +64,7 @@ function ($rootScope, $timeout, gettextCatalog) {
 			return outerSpan;
 		}
 
-		var ngElem = $(htmlElem);
+		var ngElem = $(data.element);
 
 		/**
 		 * Click handler
@@ -129,36 +92,25 @@ function ($rootScope, $timeout, gettextCatalog) {
 			$rootScope.$broadcast('ANGULAR_DRAG_START', e, 'defaultchannel', transferDataObject);
 		});
 
-		ngElem.on('dragend', 'span', function (e) {
+		ngElem.on('dragend', 'span', function(e) {
 			$rootScope.$broadcast('ANGULAR_DRAG_END', e, 'defaultchannel');
 		});
 
 		data.scope.$on('$destroy', function() {
-			tearDown(htmlElem, data);
-			if (observer !== null) {
-				observer.unobserve(htmlElem);
-				instances.delete(htmlElem);
-			}
+			tearDown(data);
 		});
 	}
 
-	/**
-	 * Tear down a given <ul> element, removing all child nodes and unsubscribing any listeners
-	 */
-	function tearDown(htmlElem, data) {
-		data.hiddenTracksRendered = false;
-		$(htmlElem).off();
+	function tearDown(data) {
+		$(data.element).off();
 		[].forEach.call(data.listeners, function (el) {
 			el();
 		});
-		removeChildNodes(htmlElem);
+		removeChildNodes(data.element);
 	}
 
-	/**
-	 * Setup a placeholder
-	 */
-	function setupPlaceholder(htmlElem, data) {
-		htmlElem.innerHTML = data.heading;
+	function setupPlaceholder(data) {
+		data.element.innerHTML = data.heading;
 	}
 
 	/**
@@ -172,35 +124,43 @@ function ($rootScope, $timeout, gettextCatalog) {
 
 	return {
 		restrict: 'E',
-		link: function (scope, element, attrs) {
-			var data = {
-				heading: scope.$eval(attrs.heading),
-				headingExt: scope.$eval(attrs.headingExt),
-				tooltip: scope.$eval(attrs.tooltip),
-				showPlayIcon: scope.$eval(attrs.showPlayIcon),
-				model: scope.$eval(attrs.model),
-				onClick: scope.$eval(attrs.onClick),
-				getDraggable: scope.$eval(attrs.getDraggable),
-				listeners: [],
-				scope: scope
-			};
-
+		require: '^inViewObserver',
+		compile: function(element, attrs) {
 			// Replace the <list-heading> element with <h?> element of desired size
 			var hElem = document.createElement('h' + (attrs.level || '1'));
 			element.replaceWith(hElem);
 
-			// On ancient browsers, build the heading fully at once
-			if (observer === null) {
-				setup(hElem, data);
-			}
-			// On modern browsers, populate the heading first with a placeholder.
-			// The placeholder is replaced with the actual content once the element
-			// enters the viewport (with some margins).
-			else {
-				setupPlaceholder(hElem, data);
-				instances.set(hElem, data);
-				observer.observe(hElem);
-			}
+			return {
+				post: function(scope, element, attrs, controller) {
+					var data = {
+						heading: scope.$eval(attrs.heading),
+						headingExt: scope.$eval(attrs.headingExt),
+						tooltip: scope.$eval(attrs.tooltip),
+						showPlayIcon: scope.$eval(attrs.showPlayIcon),
+						model: scope.$eval(attrs.model),
+						onClick: scope.$eval(attrs.onClick),
+						getDraggable: scope.$eval(attrs.getDraggable),
+						listeners: [],
+						element: element[0],
+						scope: scope
+					};
+
+					// Populate the heading first with a placeholder.
+					// The placeholder is replaced with the actual content once the element
+					// enters the viewport (with some margins).
+					setupPlaceholder(data);
+
+					controller.registerListener({
+						onEnterView: function() {
+							setup(data);
+						},
+						onLeaveView: function() {
+							tearDown(data);
+							setupPlaceholder(data);
+						}
+					});
+				}
+			};
 		}
 	};
 }]);

js/app/directives/tracklist.js
@@ -38,50 +38,15 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 		return gettextCatalog.getString('Show all {{ count }} songs …', { count: count });
 	};
 
-	var observer = null;
-	var instances = null;
-
-	// Lazy loading requires support for IntersectionObserver and WeakMap. This is not
-	// available on IE and other ancient browsers.
-	if (typeof IntersectionObserver !== 'undefined' && typeof WeakMap !== 'undefined') {
-		var onVisibilityChange = function(changes) {
-			changes.forEach(function(change) {
-				var tgt = change.target;
-				var data = instances.get(tgt);
-
-				if (change.intersectionRatio > 0) {
-					// element entered the viewport, setup the layout with small delay
-					data.promise = $timeout(setup, 50, true, tgt, data);
-				}
-				else if (data.promise) {
-					// element left the viewport before it had been set up, cancel the pending setup
-					$timeout.cancel(data.promise);
-					data.promise = null;
-				}
-				else {
-					// element left the viewport after it had been set up, replace it with placeholder
-					var height = calculateContentsHeight(tgt);
-					tearDown(tgt, data);
-					setupPlaceholder(tgt, height);
-				}
-			});
-		};
-		var observerOptions = {
-			root: document.getElementById("app-content"),
-			rootMargin: '1000px'
-		};
-		observer = new IntersectionObserver(onVisibilityChange, observerOptions);
-		instances = new WeakMap();
-	}
 
 	/**
 	 * Set up the track items and the listeners for a given <ul> element
 	 */
-	function setup(htmlElem, data) {
-		data.promise = null;
-
+	function setup(data) {
 		data.listeners.push(data.scope.$watch('currentTrack', updateClasses));
 		data.listeners.push($rootScope.$watch('playing', updateClasses));
+
+		var htmlElem = data.element[0];
 
 		/**
 		 * Remove any placeholder and add the nested <li> elements for each shown track.
@@ -202,12 +167,10 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 			}
 		}
 
-		var ngElem = $(htmlElem);
-
 		/**
 		 * Click handler for list items
 		 */
-		ngElem.on('click', 'li', function(event) {
+		data.element.on('click', 'li', function(event) {
 			var trackId = trackIdFromElementId(this.id);
 			if (trackId) {
 				if (event.target.className == 'icon-details') {
@@ -222,14 +185,14 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 					renderHiddenTracks();
 				}
 				data.expanded = !data.expanded;
-				ngElem.toggleClass('collapsed');
+				data.element.toggleClass('collapsed');
 			}
 		});
 
 		/**
 		 * Drag&Drop compatibility
 		 */
-		ngElem.on('dragstart', 'li', function(e) {
+		data.element.on('dragstart', 'li', function(e) {
 			if (e.originalEvent) {
 				e.dataTransfer = e.originalEvent.dataTransfer;
 			}
@@ -246,46 +209,42 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 			$rootScope.$broadcast('ANGULAR_DRAG_START', e, 'defaultchannel', transferDataObject);
 		});
 
-		ngElem.on('dragend', 'li', function (e) {
+		data.element.on('dragend', 'li', function (e) {
 			$rootScope.$broadcast('ANGULAR_DRAG_END', e, 'defaultchannel');
 		});
 
 		data.scope.$on('$destroy', function() {
-			tearDown(htmlElem, data);
-			if (observer !== null) {
-				observer.unobserve(htmlElem);
-				instances.delete(htmlElem);
-			}
+			tearDown(data);
 		});
 	}
 
 	/**
 	 * Tear down a given <ul> element, removing all child nodes and unsubscribing any listeners
 	 */
-	function tearDown(htmlElem, data) {
+	function tearDown(data) {
 		data.hiddenTracksRendered = false;
-		$(htmlElem).off();
-		[].forEach.call(data.listeners, function (el) {
-			el();
+		data.element.off();
+		[].forEach.call(data.listeners, function(lstnr) {
+			lstnr();
 		});
-		removeChildNodes(htmlElem);
+		removeChildNodes(data.element[0]);
 	}
 
 	/**
 	 * Setup a placeholder list item within the given <ul> element using the given height
 	 */
-	function setupPlaceholder(htmlElem, height) {
+	function setupPlaceholder(data, height) {
 		placeholder = document.createElement('li');
 		placeholder.style.height = height + 'px';
-		htmlElem.appendChild(placeholder);
+		data.element[0].appendChild(placeholder);
 	}
 
 	/**
 	 * Calculate total height of all the child nodes of the given <ul> element
 	 */
-	function calculateContentsHeight(htmlElem) {
+	function calculateContentsHeight(data) {
 		totalHeight = 0;
-		$(htmlElem).children().each(function() {
+		data.element.children().each(function() {
 			totalHeight += $(this).outerHeight(true);
 		});
 		return totalHeight;
@@ -310,6 +269,7 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 
 	return {
 		restrict: 'E',
+		require: '^inViewObserver',
 		compile: function(element, attrs) {
 			// Replace the <tack-list> element wiht <ul> element
 			var listContainer = document.createElement('ul');
@@ -317,7 +277,7 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 			element.replaceWith(listContainer);
 
 			return {
-				post: function(scope, element, attrs) {
+				post: function(scope, element, attrs, controller) {
 					var data = {
 						expanded: false,
 						hiddenTracksRendered: false,
@@ -328,21 +288,25 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 						getDraggable: scope.$eval(attrs.getDraggable),
 						collapseLimit: attrs.collapseLimit || 999999,
 						listeners: [],
-						scope: scope
+						scope: scope,
+						element: element
 					};
 
-					// On ancient browsers, build the list contents fully at once
-					if (observer === null) {
-						setup(element[0], data);
-					}
-					// On modern browsers, populate the list first with a placeholder.
+					// Populate the list first with a placeholder.
 					// The placeholder is replaced with the actual content once the element
 					// enters the viewport (with some margins).
-					else {
-						setupPlaceholder(element[0], estimateContentsHeight(data));
-						instances.set(element[0], data);
-						observer.observe(element[0]);
-					}
+					setupPlaceholder(data, estimateContentsHeight(data));
+
+					controller.registerListener({
+						onEnterView: function() {
+							setup(data);
+						},
+						onLeaveView: function() {
+							var height = calculateContentsHeight(data);
+							tearDown(data);
+							setupPlaceholder(data, height);
+						}
+					});
 				}
 			};
 		}

js/app/directives/albumart.js
@@ -11,21 +11,14 @@
  *
  */
 
-angular.module('Music').directive('albumart', ['albumartLazyLoader', function(albumartLazyLoader) {
+angular.module('Music').directive('albumart', [function() {
 
-	function setCoverImage(element, imageUrl, disableLazyLoad) {
+	function setCoverImage(element, imageUrl) {
 		// remove placeholder stuff
 		element.html('');
 		element.css('background-color', '');
 
-		// on ancient browsers, load all the cover images immediately
-		if (!albumartLazyLoader.supported() || disableLazyLoad) {
-			element.css('background-image', 'url(' + imageUrl + ')');
-		}
-		// on modern browsers, load cover images only once they enter the viewport
-		else {
-			albumartLazyLoader.register(element);
-		}
+		element.css('background-image', 'url(' + imageUrl + ')');
 	}
 
 	function setPlaceholder(element, text) {
@@ -44,24 +37,45 @@ angular.module('Music').directive('albumart', ['albumartLazyLoader', function(al
 		}
 	}
 
-	return function(scope, element, attrs, ctrl) {
+	return {
+		require: '?^inViewObserver',
+		link: function(scope, element, attrs, ctrl) {
+			/**
+			 * This directive can be used for two different purposes in two different
+			 * contexts:
+			 * 1. Within a scroling container, in which case there should be inViewObserver
+			 *    directive as ancestor of this directive. In this case, inViewObserver is 
+			 *    paased to here in the argument `ctrl`.
+			 * 2. Within the player control pane, in which case there's no ancestor inViewObserver
+			 *    and `ctrl` is null. In this case, the directive observes any changes on the
+			 *    related attributes of the element.
+			 */
 
-		var onCoverChanged = function() {
-			if (attrs.cover) {
-				setCoverImage(element, attrs.cover, attrs.noLazyLoad !== undefined);
-			} else {
-				setPlaceholder(element, attrs.albumart);
+			var onCoverChanged = function() {
+				if (attrs.cover) {
+					setCoverImage(element, attrs.cover);
+				} else {
+					setPlaceholder(element, attrs.albumart);
+				}
+			};
+
+			var onAlbumartChanged = function() {
+				if (!attrs.cover) {
+					setPlaceholder(element, attrs.albumart);
+				}
+			};
+
+			if (ctrl) {
+				ctrl.registerListener({
+					onEnterView: onCoverChanged,
+					onLeaveView: function() { /* nothing to do */ }
+				});
 			}
-		};
-
-		var onAlbumartChanged = function() {
-			if (!attrs.cover) {
-				setPlaceholder(element, attrs.albumart);
+			else {
+				attrs.$observe('albumart', onAlbumartChanged);
+				attrs.$observe('cover', onCoverChanged);
 			}
-		};
-
-		attrs.$observe('albumart', onAlbumartChanged);
-		attrs.$observe('cover', onCoverChanged);
+		}
 	};
 }]);
 

js/app/services/albumartlazyloader.js
@@ -1,68 +1,0 @@
-/**
- * ownCloud - Music app
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright 2019 Pauli Järvinen
- *
- */
-
-/**
- * Lazy-loader for albumart. This service has an intimate relation to the albumart directive.
- */
-angular.module('Music').service('albumartLazyLoader', ['$timeout', function($timeout) {
-
-	var observer = null;
-	var imagesToBeLoaded = null;
-
-	if (typeof IntersectionObserver !== 'undefined'
-		&& typeof WeakMap !== 'undefined')
-	{
-		imagesToBeLoaded = new WeakMap();
-
-		var loadAlbumart = function(tgtElem) {
-			var tgt = $(tgtElem);
-			tgt.css('background-image', 'url(' + tgt.attr('cover') + ')');
-			observer.unobserve(tgtElem);
-		};
-
-		var onVisibilityChange = function(changes) {
-			changes.forEach(function(change) {
-				var tgt = change.target;
-				var promise;
-				if (change.intersectionRatio > 0) {
-					// Load the albumart with a small delay after it enters the viewport with margins.
-					// This is to avoid loading images which just pass through the viewport during
-					// fast scrolling.
-					promise = $timeout(loadAlbumart, 100, true, tgt);
-					imagesToBeLoaded.set(tgt, promise);
-				} else {
-					// Cancel any pending load for albumart exiting the viewport with margins
-					promise = imagesToBeLoaded.get(tgt);
-					if (promise !== undefined) {
-						$timeout.cancel(promise);
-						imagesToBeLoaded.delete(tgt);
-					}
-				}
-			});
-		};
-		var observerOptions = {
-			root: document.getElementById("app-content"),
-			rootMargin: '1000px'
-		};
-
-		observer = new IntersectionObserver(onVisibilityChange, observerOptions);
-	}
-
-	return {
-		supported: function() {
-			return (observer !== null);
-		},
-		register: function(element) {
-			var img = angular.element(element)[0];
-			observer.observe(img);
-		}
-	};
-}]);

templates/partials/albumsview.php
@@ -1,5 +1,7 @@
 <div class="view-container" id="albums" ng-show="!loading && !loadingCollection">
-	<div class="artist-area" id="artist-{{ ::artist.id }}" ng-repeat="artist in artists | limitTo: incrementalLoadLimit">
+	<div class="artist-area" id="artist-{{ ::artist.id }}" in-view-observer
+		ng-repeat="artist in artists | limitTo: incrementalLoadLimit" 
+	>
 		<list-heading 
 			level="1"
 			heading="artist.name"

templates/partials/controls.php
@@ -11,7 +11,7 @@
 	</div>
 
 	<div ng-show="currentAlbum" ng-click="scrollToCurrentTrack()"
-		class="albumart clickable" cover="{{ currentAlbum.cover }}" no-lazy-load
+		class="albumart clickable" cover="{{ currentAlbum.cover }}"
 		albumart="{{ currentAlbum.name }}" title="{{ currentAlbum.name }}" ></div>
 
 	<div class="song-info clickable" ng-click="scrollToCurrentTrack()">

templates/partials/foldersview.php
@@ -1,5 +1,7 @@
 <div class="view-container" id="folders-area" ng-show="!loading && !loadingCollection">
-	<div class="playlist-area" id="folder-{{ ::folder.id }}" ng-repeat="folder in folders | limitTo: incrementalLoadLimit">
+	<div class="playlist-area" id="folder-{{ ::folder.id }}" in-view-observer
+		ng-repeat="folder in folders | limitTo: incrementalLoadLimit"
+	>
 		<list-heading 
 				level="1"
 				heading="folder.name || '(root folder)' | translate"

commit b0490d701c2967f141501431b5446838d95c6fe3

Fix several bugs introduced when InViewObserver was adopted
The commit d8f86c7f79 caused some bugs which have now been fixed:

- The "All tracks" view did not show any tracks

- The tracks were not visible in the "Folders" view in case there were
   so few tracks that no scroll bar appeared

- The tracks and album covers could be left undrawn if they appeared
   on the screen as a result of collapsing a track list above. They were
   shown only after the screen was scrolled a bit.

js/app/directives/inviewobserver.js
@@ -22,8 +22,9 @@ angular.module('Music').directive('inViewObserver', ['$rootScope', '$timeout', f
 		instances = [];
 	});
 
-	document.getElementById("app-content").addEventListener('scroll', onScroll);
+	document.getElementById('app-content').addEventListener('scroll', onScroll);
 	$rootScope.$on('resize', onScroll);
+	$rootScope.$on('trackListCollapsed', onScroll);
 
 	function onScroll() {
 		_(instances).each(function(inst) {
@@ -33,7 +34,7 @@ angular.module('Music').directive('inViewObserver', ['$rootScope', '$timeout', f
 
 			if (inst.promise) {
 				if (!nowInViewPort) {
-					// element left the viewport before it had been set up, cancel the pending notify
+					// element left the viewport before onEnterView was called, cancel the pending call
 					$timeout.cancel(inst.promise);
 					inst.promise = null;
 				}
@@ -83,6 +84,14 @@ angular.module('Music').directive('inViewObserver', ['$rootScope', '$timeout', f
 			};
 
 			instances.push(this);
+		},
+		link: function(scope) {
+			// The visibilites should be evaluated after all the ng-repeated instances
+			// have been linked. There may be no actual `scroll` or `resize` events after
+			// page load, in case there's so few items that no scrollbar appears.
+			if (scope.$parent.$last && !$rootScope.loading) {
+				$timeout(onScroll);
+			}
 		}
 	};
 }]);

js/app/directives/listheading.js
@@ -125,10 +125,10 @@ function ($rootScope, $timeout, gettextCatalog) {
 	return {
 		restrict: 'E',
 		require: '^inViewObserver',
-		compile: function(element, attrs) {
+		compile: function(tmplElement, tmplAttrs) {
 			// Replace the <list-heading> element with <h?> element of desired size
-			var hElem = document.createElement('h' + (attrs.level || '1'));
-			element.replaceWith(hElem);
+			var hElem = document.createElement('h' + (tmplAttrs.level || '1'));
+			tmplElement.replaceWith(hElem);
 
 			return {
 				post: function(scope, element, attrs, controller) {

js/app/directives/tracklist.js
@@ -269,46 +269,45 @@ function ($rootScope, $interpolate, $timeout, gettextCatalog) {
 
 	return {
 		restrict: 'E',
-		require: '^inViewObserver',
-		compile: function(element, attrs) {
-			// Replace the <tack-list> element wiht <ul> element
-			var listContainer = document.createElement('ul');
-			listContainer.className = 'track-list collapsed';
-			element.replaceWith(listContainer);
-
-			return {
-				post: function(scope, element, attrs, controller) {
-					var data = {
-						expanded: false,
-						hiddenTracksRendered: false,
-						tracks: scope.$eval(attrs.tracks),
-						getTrackData: scope.$eval(attrs.getTrackData),
-						playTrack: scope.$eval(attrs.playTrack),
-						showTrackDetails: scope.$eval(attrs.showTrackDetails),
-						getDraggable: scope.$eval(attrs.getDraggable),
-						collapseLimit: attrs.collapseLimit || 999999,
-						listeners: [],
-						scope: scope,
-						element: element
-					};
-
-					// Populate the list first with a placeholder.
-					// The placeholder is replaced with the actual content once the element
-					// enters the viewport (with some margins).
-					setupPlaceholder(data, estimateContentsHeight(data));
-
-					controller.registerListener({
-						onEnterView: function() {
-							setup(data);
-						},
-						onLeaveView: function() {
-							var height = calculateContentsHeight(data);
-							tearDown(data);
-							setupPlaceholder(data, height);
-						}
-					});
-				}
+		template: '<ul class="track-list collapsed"></ul>',
+		replace: true,
+		require: '?^inViewObserver',
+		link: function(scope, element, attrs, controller) {
+			var data = {
+				expanded: false,
+				hiddenTracksRendered: false,
+				tracks: scope.$eval(attrs.tracks),
+				getTrackData: scope.$eval(attrs.getTrackData),
+				playTrack: scope.$eval(attrs.playTrack),
+				showTrackDetails: scope.$eval(attrs.showTrackDetails),
+				getDraggable: scope.$eval(attrs.getDraggable),
+				collapseLimit: attrs.collapseLimit || 999999,
+				listeners: [],
+				scope: scope,
+				element: element
 			};
+
+			// In case this directive has inViewObserver as ancestor, populate it first
+			// with a placeholder. The placeholder is replaced with the actual content
+			// once the element enters the viewport (with some margins).
+			if (controller) {
+				setupPlaceholder(data, estimateContentsHeight(data));
+
+				controller.registerListener({
+					onEnterView: function() {
+						setup(data);
+					},
+					onLeaveView: function() {
+						var height = calculateContentsHeight(data);
+						tearDown(data);
+						setupPlaceholder(data, height);
+					}
+				});
+			}
+			// Otherwise, populate immediately with the actual content
+			else {
+				setup(data);
+			}
 		}
 	};
 }]);
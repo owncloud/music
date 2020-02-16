owncloud/music

commit 89e5b38c3e8281c9bf928c724f9deec2fccb05dc

Refactor libraryService to enable more efficient access to parent entities
When the collection is fetched, the libraryService now sets up parent
references to each entity so that it is easy to navigate from track to album
and from album to artist.

Similarly, when foldres are fetched, the parent references are set to allow
navigating from a track to its containing folder.

js/app/controllers/albumsviewcontroller.js
@@ -77,12 +77,10 @@ angular.module('Music').controller('AlbumsViewController', [
 			}
 			else {
 				var currentListId = playlistService.getCurrentPlaylistId();
-				var album = libraryService.findAlbumOfTrack(track.id);
-				var artist = libraryService.findArtistOfAlbum(album.id);
 
 				// start playing the album/artist from this track if the clicked track belongs
 				// to album/artist which is the current play scope
-				if (currentListId === 'album-' + album.id || currentListId === 'artist-' + artist.id) {
+				if (currentListId === 'album-' + track.album.id || currentListId === 'artist-' + track.album.artist.id) {
 					playPlaylistFromTrack(currentListId, playlistService.getCurrentPlaylist(), track);
 				}
 				// on any other track, start playing the collection from this track
@@ -191,9 +189,9 @@ angular.module('Music').controller('AlbumsViewController', [
 		});
 
 		function scrollToAlbumOfTrack(trackId, animationTime /* optional */) {
-			var album = libraryService.findAlbumOfTrack(trackId);
-			if (album) {
-				$scope.$parent.scrollToItem('album-' + album.id, animationTime);
+			var track = libraryService.getTrack(trackId);
+			if (track) {
+				$scope.$parent.scrollToItem('album-' + track.album.id, animationTime);
 			}
 		}
 

js/app/controllers/foldersviewcontroller.js
@@ -50,7 +50,7 @@ angular.module('Music').controller('FoldersViewController', [
 			// on any other list item, start playing the folder from this item
 			else {
 				var currentListId = playlistService.getCurrentPlaylistId();
-				var folder = libraryService.findFolderOfTrack(trackId);
+				var folder = libraryService.getTrack(trackId).folder;
 
 				// start playing the folder from this track if the clicked track belongs
 				// to folder which is the current play scope
@@ -126,7 +126,7 @@ angular.module('Music').controller('FoldersViewController', [
 				// element instead
 				var trackElem = $('#' + elementId);
 				if (trackElem.length === 0 || !trackElem.is(':visible')) {
-					var folder = libraryService.findFolderOfTrack(trackId); 
+					var folder = libraryService.getTrack(trackId).folder; 
 					elementId = 'folder-' + folder.id;
 				}
 				$scope.$parent.scrollToItem(elementId);

js/app/controllers/playercontroller.js
@@ -104,7 +104,7 @@ function ($scope, $rootScope, playlistService, libraryService,
 		if (track !== null) {
 			// switch initial state
 			$rootScope.started = true;
-			$scope.currentAlbum = libraryService.findAlbumOfTrack(track.id);
+			$scope.currentAlbum = track.album;
 			$scope.setLoading(true);
 			playTrack(track);
 		} else {

js/app/controllers/searchcontroller.js
@@ -142,7 +142,7 @@ function ($scope, $rootScope, libraryService, alphabetIndexingService, $timeout,
 		// mark track matches
 		_(trackResults.result).each(function(track) {
 			$('#track-' + track.id).addClass('matched');
-			$('#album-' + track.albumId).addClass('matched').parent().addClass('matched');
+			$('#album-' + track.album.id).addClass('matched').parent().addClass('matched');
 		});
 
 		// mark album matches
@@ -177,7 +177,7 @@ function ($scope, $rootScope, libraryService, alphabetIndexingService, $timeout,
 		// mark track matches
 		_(trackResults.result).each(function(track) {
 			$('#track-' + track.id).addClass('matched');
-			$('#folder-' + track.folderId).addClass('matched');
+			$('#folder-' + track.folder.id).addClass('matched');
 		});
 
 		// mark folder matches

js/app/services/libraryservice.js
@@ -54,12 +54,19 @@ angular.module('Music').service('libraryService', ['$rootScope', function($rootS
 		return tracks;
 	}
 
-	function sortCollection(collection) {
+	/**
+	 * Sort the passed in collection alphabetically, and set up parent references
+	 */
+	function transformCollection(collection) {
 		sortByTextField(collection, 'name');
 		_.forEach(collection, function(artist) {
 			artist.albums = sortByYearNameAndDisc(artist.albums);
 			_.forEach(artist.albums, function(album) {
+				album.artist = artist;
 				album.tracks = sortByNumberAndTitle(album.tracks);
+				_.forEach(album.tracks, function(track) {
+					track.album = album;
+				});
 			});
 		});
 		return collection;
@@ -92,13 +99,6 @@ angular.module('Music').service('libraryService', ['$rootScope', function($rootS
 	}
 
 	function createTrackContainers() {
-		// add parent album ID to each track
-		_.forEach(albums, function(album) {
-			_.forEach(album.tracks, function(track) {
-				track.albumId = album.id;
-			});
-		});
-
 		// album order "playlist"
 		var tracks = _.flatten(_.pluck(albums, 'tracks'));
 		tracksInAlbumOrder = _.map(tracks, playlistEntry);
@@ -167,7 +167,7 @@ angular.module('Music').service('libraryService', ['$rootScope', function($rootS
 
 	return {
 		setCollection: function(collection) {
-			artists = sortCollection(collection);
+			artists = transformCollection(collection);
 			albums = _.flatten(_.pluck(artists, 'albums'));
 			createTrackContainers();
 		},
@@ -186,7 +186,7 @@ angular.module('Music').service('libraryService', ['$rootScope', function($rootS
 					sortByPlaylistEntryField(folder.tracks, 'artistName');
 
 					_.forEach(folder.tracks, function(trackEntry) {
-						trackEntry.track.folderId = folder.id;
+						trackEntry.track.folder = folder;
 					});
 				});
 				tracksInFolderOrder = _.flatten(_.pluck(folders, 'tracks'));
@@ -248,21 +248,6 @@ angular.module('Music').service('libraryService', ['$rootScope', function($rootS
 		},
 		getAllFolders: function() {
 			return folders;
-		},
-		findAlbumOfTrack: function(trackId) {
-			return _.find(albums, function(album) {
-				return _.findWhere(album.tracks, {id : Number(trackId)});
-			});
-		},
-		findArtistOfAlbum: function(albumId) {
-			return _.find(artists, function(artist) {
-				return _.findWhere(artist.albums, {id : Number(albumId)});
-			});
-		},
-		findFolderOfTrack: function(trackId) {
-			return _.find(folders, function(folder) {
-				return _.find(folder.tracks, function(i) { return i.track.id == Number(trackId); });
-			});
 		},
 		findTracksByArtist: function(artistId) {
 			return _.filter(tracksIndex, {artistId: Number(artistId)});

commit ab43932c2816a76bf6905d97502035ec7debe782

Fix the drag-and-drop support broken by the commit 89e5b38c3
The ang-drag-drop used to create the drag-and-drop support internally
converts the dragged entities to json. Having circular references in the
dragged objects broke this logic.

To fix the issue, the draggables now contain only entity IDs instead of
containing the actual objects. The IDs are converted to entities using the
libraryService when needed to evaluate the drop effect.

js/app/controllers/albumsviewcontroller.js
@@ -7,7 +7,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2014
- * @copyright Pauli Järvinen 2017 - 2019
+ * @copyright Pauli Järvinen 2017 - 2020
  */
 
 angular.module('Music').controller('AlbumsViewController', [
@@ -124,7 +124,7 @@ angular.module('Music').controller('AlbumsViewController', [
 
 		function getDraggable(type, draggedElement) {
 			var draggable = {};
-			draggable[type] = draggedElement;
+			draggable[type] = draggedElement.id;
 			return draggable;
 		}
 

js/app/controllers/alltracksviewcontroller.js
@@ -82,7 +82,7 @@ angular.module('Music').controller('AllTracksViewController', [
 		};
 
 		$scope.getDraggable = function(trackId) {
-			return { track: libraryService.getTrack(trackId) };
+			return { track: trackId };
 		};
 
 		function findTrackFromBucket(bucket, trackId) {

js/app/controllers/foldersviewcontroller.js
@@ -5,7 +5,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2019
+ * @copyright Pauli Järvinen 2019, 2020
  */
 
 
@@ -87,18 +87,18 @@ angular.module('Music').controller('FoldersViewController', [
 			};
 		};
 
-		function getDraggable(type, draggedElement) {
+		function getDraggable(type, draggedElementId) {
 			var draggable = {};
-			draggable[type] = draggedElement;
+			draggable[type] = draggedElementId;
 			return draggable;
 		}
 
 		$scope.getTrackDraggable = function(trackId) {
-			return getDraggable('track', libraryService.getTrack(trackId));
+			return getDraggable('track', trackId);
 		};
 
 		$scope.getFolderDraggable = function(folder) {
-			return getDraggable('folder', folder);
+			return getDraggable('folder', folder.id);
 		};
 
 		/**

js/app/controllers/navigationcontroller.js
@@ -111,23 +111,23 @@ angular.module('Music').controller('NavigationController', [
 		};
 
 		// Add track to the playlist
-		$scope.addTrack = function(playlist, song) {
-			addTracks(playlist, [song.id]);
+		$scope.addTrack = function(playlist, songId) {
+			addTracks(playlist, [songId]);
 		};
 
 		// Add all tracks on an album to the playlist
-		$scope.addAlbum = function(playlist, album) {
-			addTracks(playlist, trackIdsFromAlbum(album));
+		$scope.addAlbum = function(playlist, albumId) {
+			addTracks(playlist, trackIdsFromAlbum(albumId));
 		};
 
 		// Add all tracks on all albums by an artist to the playlist
-		$scope.addArtist = function(playlist, artist) {
-			addTracks(playlist, trackIdsFromArtist(artist));
+		$scope.addArtist = function(playlist, artistId) {
+			addTracks(playlist, trackIdsFromArtist(artistId));
 		};
 
 		// Add all tracks in a folder to the playlist
-		$scope.addFolder = function(playlist, folder) {
-			addTracks(playlist, trackIdsFromFolder(folder));
+		$scope.addFolder = function(playlist, folderId) {
+			addTracks(playlist, trackIdsFromFolder(folderId));
 		};
 
 		// Navigate to a view selected from the navigation bar
@@ -167,15 +167,18 @@ angular.module('Music').controller('NavigationController', [
 			return $rootScope.currentView != '#/playlist/' + playlist.id;
 		};
 
-		function trackIdsFromAlbum(album) {
+		function trackIdsFromAlbum(albumId) {
+			var album = libraryService.getAlbum(albumId);
 			return _.pluck(album.tracks, 'id');
 		}
 
-		function trackIdsFromArtist(artist) {
-			return _.flatten(_.map(artist.albums, trackIdsFromAlbum));
+		function trackIdsFromArtist(artistId) {
+			var artist = libraryService.getArtist(artistId);
+			return _.flatten(_.map(_.pluck(artist.albums, 'id'), trackIdsFromAlbum));
 		}
 
-		function trackIdsFromFolder(folder) {
+		function trackIdsFromFolder(folderId) {
+			var folder = libraryService.getFolder(folderId);
 			return _.pluck(_.pluck(folder.tracks, 'track'), 'id');
 		}
 

js/app/controllers/playlistviewcontroller.js
@@ -7,7 +7,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013
- * @copyright Pauli Järvinen 2017, 2018
+ * @copyright Pauli Järvinen 2017 - 2020
  */
 
 
@@ -82,7 +82,7 @@ angular.module('Music').controller('PlaylistViewController', [
 		$scope.getDraggable = function(index) {
 			$scope.draggedIndex = index;
 			return {
-				track: $scope.tracks[index].track,
+				track: $scope.tracks[index].track.id,
 				srcIndex: index
 			};
 		};
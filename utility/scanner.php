d0e40846b6e8dc4a4b40256819c765f84245599b
Pauli Järvinen <pauli.jarvinen@gmail.com>
2020-03-22 15:00:36 +0200

    Fix embedded player not showing metadata for the unscanned files
    This was broken on version 0.13.0 by the commit 5ea5d1b49.

--- utility/scanner.php
+++ utility/scanner.php
@@ -522,7 +522,7 @@ class Scanner extends PublicEmitter {
 			if ($cover != null) {
 				$cover = [
 					'mimetype' => $cover['image_mime'],
-					'content' => $this->coverHelper->scaleDownIfLarge($cover['data'], 200)
+					'content' => $this->coverHelper->scaleDownAndCrop($cover['data'], 100)
 				];
 			}
 			return [

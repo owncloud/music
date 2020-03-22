owncloud/music

commit 4a468d32725214f44db66e0928401471ae32899a

Do not treat each disc of a multi-disc album as a separate album
Previously, each disc of a multi-disc album was indexed and shown as if
it was its own album. Each Album entity in the database carried a disc
number, and the entity could have exactly one of those.

Now this was changed so that the discs are not stored as separate Album
entities. Instead, tracks of each disc are stored in the same Album
entity, and each Track entity carries also the disc number.

On the web UI, the multi-disc albums are now shown as one album, and
the disc number is shown in front of the track number. E.g.
1-1. First track name
1-2. Second track name
1-3. Third track name
2-1. First track of the second disc
2-2. Second track of the second disc
...
One single-disc albums, the disc number is not visible on the UI.

In the Ampache, Subsonic, and Shiva APIs, the disc numbers are not
used. The Ampache and Subsonic APIs would offer some mechanisms to
handle the multi-disc albums, but because of the lack of the client
support, there's no point in trying to use those. Instead, we fabricate
the track number given in the APIs so that it includes also the disc
number information: the number used in the API is
(100 * disc_number + track_number).
The fabricated number is used only for multi-disc albums.

On software update, a migration script is run. It moves the disc
numbers from the Album entities to the Track entities, and combines the
Album entities of the multi-disc Albums.

refs #680

migration/disknumbermigration.php
@@ -1,0 +1,181 @@
+<?php
+
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright Pauli Järvinen 2020
+ */
+
+namespace OCA\Music\Migration;
+
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+
+class DiskNumberMigration implements IRepairStep {
+
+	/** @var IDBConnection */
+	private $db;
+
+	/** @var IConfig */
+	private $config;
+
+	/** @var int[] */
+	private $obsoleteAlbums;
+
+	public function __construct(IDBConnection $connection, IConfig $config) {
+		$this->db = $connection;
+		$this->config = $config;
+		$this->obsoleteAlbums = [];
+	}
+
+	public function getName() {
+		return 'Combine multi-disk albums and store disk numbers per track';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function run(IOutput $output) {
+		$installedVersion = $this->config->getAppValue('music', 'installed_version');
+
+		if (\version_compare($installedVersion, '0.13.0', '<=')) {
+			$n = $this->copyDiskNumberToTracks();
+			$output->info("$n tracks were updated with a disk number");
+
+			$n = $this->combineMultiDiskAlbums();
+			$output->info("$n tracks were assinged to new albums when combining multi-disk albums");
+
+			$n = $this->removeObsoleteAlbums();
+			$output->info("$n obsolete album entries were removed from the database");
+
+			$n = $this->reEvaluateAlbumHashes();
+			$output->info("$n albums were updated with new hashes");
+
+			$n = $this->removeDiskNumbersFromAlbums();
+			$output->info("obsolete disk number field was nullified in $n albums");
+		}
+	}
+
+	/**
+	 * Copy disk numbers from the albums table to the tracks table
+	 */
+	private function copyDiskNumberToTracks() {
+		$sql = 'UPDATE `*PREFIX*music_tracks` '. 
+				'SET `disk` = (SELECT `disk` '. 
+				'              FROM `*PREFIX*music_albums` '.
+				'              WHERE `*PREFIX*music_tracks`.`album_id` = `*PREFIX*music_albums`.`id`) '.
+				'WHERE `disk` IS NULL';
+		return $this->db->executeUpdate($sql);
+	}
+
+	/**
+	 * Move all tracks belonging to separate disks of the same album title to the
+	 * album entity matching the first of those disks. The album entities matching
+	 * the rest of the disks become obsolete.
+	 */
+	private function combineMultiDiskAlbums() {
+		$sql = 'SELECT `id`, `user_id`, `album_artist_id`, `name` '.
+				'FROM `*PREFIX*music_albums` '.
+				'ORDER BY `user_id`, `album_artist_id`, `name`';
+
+		$rows = $this->db->executeQuery($sql)->fetchAll();
+
+		$affectedTracks = 0;
+		$prevId = null;
+		$prevUser = null;
+		$prevArtist = null;
+		$prevName = null;
+		foreach ($rows as $row) {
+			$id = $row['id'];
+			$user = $row['user_id'];
+			$artist = $row['album_artist_id'];
+			$name = \mb_strtolower($row['name']);
+
+			if ($user === $prevUser && $artist === $prevArtist && $name === $prevName) {
+				// another disk of the same album => merge
+				$affectedTracks += $this->moveTracksBetweenAlbums($id, $prevId);
+				$this->obsoleteAlbums[] = $id;
+			}
+			else {
+				$prevId = $id;
+				$prevUser = $user;
+				$prevArtist = $artist;
+				$prevName = $name;
+			}
+		}
+
+		return $affectedTracks;
+	}
+
+	/**
+	 * Move all tracks from the source album entity to the destination album entity
+	 * @param int $sourceAlbum ID
+	 * @param int $destinationAlbum ID
+	 */
+	private function moveTracksBetweenAlbums($sourceAlbum, $destinationAlbum) {
+		$sql = 'UPDATE `*PREFIX*music_tracks` '.
+				'SET `album_id` = ? '.
+				'WHERE `album_id` = ?';
+		return $this->db->executeUpdate($sql, [$destinationAlbum, $sourceAlbum]);
+	}
+
+	/**
+	 * Delete from the albums table those rows which were made obsolete by the previous steps
+	 */
+	private function removeObsoleteAlbums() {
+		$sql = 'DELETE FROM `*PREFIX*music_albums` '.
+				'WHERE `id` IN '. $this->questionMarks(count($this->obsoleteAlbums));
+
+		return $this->db->executeUpdate($sql, $this->obsoleteAlbums);
+	}
+
+	/**
+	 * Recalculate the hashes for all albums in the table. The disk number is no longer part
+	 * of the calculation schema.
+	 */
+	private function reEvaluateAlbumHashes() {
+		$sql = 'SELECT `id`, `name`, `album_artist_id` '.
+				'FROM `*PREFIX*music_albums`';
+		$rows = $this->db->executeQuery($sql)->fetchAll();
+
+		$affectedRows = 0;
+		foreach ($rows as $row) {
+			$lowerName = \mb_strtolower($row['name']);
+			$artist = $row['album_artist_id'];
+			$hash = \hash('md5', "$lowerName|$artist");
+
+			$affectedRows += $this->db->executeUpdate(
+				'UPDATE `*PREFIX*music_albums` SET `hash` = ? WHERE `id` = ?',
+				[$hash, $row['id']]
+			);
+		}
+
+		return $affectedRows;
+	}
+
+	/**
+	 * Set all disk numbers stored in the albums table as NULL.
+	 */
+	private function removeDiskNumbersFromAlbums() {
+		$sql = 'UPDATE `*PREFIX*music_albums` SET `disk` = NULL';
+		return $this->db->executeUpdate($sql);
+	}
+
+	/**
+	 * helper creating a string like '(?,?,?)' with the specified number of elements
+	 * @param int $count
+	 */
+	private function questionMarks($count) {
+		$questionMarks = [];
+		for ($i = 0; $i < $count; $i++) {
+			$questionMarks[] = '?';
+		}
+		return '(' . \implode(',', $questionMarks) . ')';
+	}
+}

appinfo/database.xml
@@ -133,6 +133,7 @@
 				<type>integer</type>
 				<notnull>false</notnull>
 				<unsigned>true</unsigned>
+				<comments>deprecated</comments>
 			</field>
 			<field>
 				<name>mbid_group</name>
@@ -203,6 +204,12 @@
 			</field>
 			<field>
 				<name>number</name>
+				<type>integer</type>
+				<notnull>false</notnull>
+				<unsigned>true</unsigned>
+			</field>
+			<field>
+				<name>disk</name>
 				<type>integer</type>
 				<notnull>false</notnull>
 				<unsigned>true</unsigned>

appinfo/info.xml
@@ -10,7 +10,7 @@ In the library view, you can play tracks by album or artist, create custom playl
 Limitations: The app can handle libraries of more than 50´000 tracks on modern desktop browsers, but the user experience is not optimized for such collections. Libraries significantly larger than this may fail to function altogether. The playback from Files works regardless of the collection size.</description>
 	<licence>AGPL</licence>
 	<author>Pauli Järvinen, Morris Jobke (original author), Jan-Christoph Borchardt (original design)</author>
-	<version>0.13.0</version>
+	<version>0.13.1-alpha1</version>
 	<category>multimedia</category>
 	<screenshot>https://user-images.githubusercontent.com/8565946/43827475-94d34d7c-9b02-11e8-8bf9-9f3d91aa5dab.png</screenshot>
 	<screenshot>https://user-images.githubusercontent.com/8565946/43827480-978865f2-9b02-11e8-97fd-b46d3e171fd7.png</screenshot>
@@ -35,6 +35,7 @@ Limitations: The app can handle libraries of more than 50´000 tracks on modern 
 		</pre-migration>
 		<post-migration>
 			<step>OCA\Music\Migration\BogusTrackRemoval</step>
+			<step>OCA\Music\Migration\DiskNumberMigration</step>
 		</post-migration>
 	</repair-steps>
 </info>

businesslayer/albumbusinesslayer.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2016 - 2019
+ * @copyright Pauli Järvinen 2016 - 2020
  */
 
 namespace OCA\Music\BusinessLayer;
@@ -40,7 +40,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 */
 	public function find($albumId, $userId) {
 		$album = parent::find($albumId, $userId);
-		return $this->injectArtistsAndYears([$album], $userId)[0];
+		return $this->injectExtraFields([$album], $userId)[0];
 	}
 
 	/**
@@ -53,7 +53,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 */
 	public function findAll($userId, $sortBy=SortBy::None, $limit=null, $offset=null) {
 		$albums = parent::findAll($userId, $sortBy, $limit, $offset);
-		return $this->injectArtistsAndYears($albums, $userId, true);
+		return $this->injectExtraFields($albums, $userId, true);
 	}
 
 	/**
@@ -63,7 +63,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 */
 	public function findAllByArtist($artistId, $userId) {
 		$albums = $this->mapper->findAllByArtist($artistId, $userId);
-		return $this->injectArtistsAndYears($albums, $userId);
+		return $this->injectExtraFields($albums, $userId);
 	}
 
 	/**
@@ -73,7 +73,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 */
 	public function findAllByAlbumArtist($artistId, $userId) {
 		$albums = $this->mapper->findAllByAlbumArtist($artistId, $userId);
-		$albums = $this->injectArtistsAndYears($albums, $userId);
+		$albums = $this->injectExtraFields($albums, $userId);
 		\usort($albums, ['\OCA\Music\Db\Album', 'compareYearAndName']);
 		return $albums;
 	}
@@ -89,11 +89,11 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 */
 	public function findAllByName($name, $userId, $fuzzy = false, $limit=null, $offset=null) {
 		$albums = parent::findAllByName($name, $userId, $fuzzy, $limit, $offset);
-		return $this->injectArtistsAndYears($albums, $userId);
+		return $this->injectExtraFields($albums, $userId);
 	}
 
 	/**
-	 * Add performing artists and release years to the given album objects
+	 * Add performing artists, release years, and disk counts to the given album objects
 	 * @param Album[] $albums
 	 * @param string $userId
 	 * @param bool $allAlbums Set to true if $albums contains all albums of the user.
@@ -101,7 +101,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 *                        the database query.
 	 * @return Album[]
 	 */
-	private function injectArtistsAndYears($albums, $userId, $allAlbums = false) {
+	private function injectExtraFields($albums, $userId, $allAlbums = false) {
 		if (\count($albums) > 0) {
 			// In case we are injecting data to a lot of albums, do not limit the
 			// SQL SELECTs to only those albums. Very large amount of SQL host parameters
@@ -113,9 +113,12 @@ class AlbumBusinessLayer extends BusinessLayer {
 
 			$artists = $this->mapper->getPerformingArtistsByAlbumId($albumIds, $userId);
 			$years = $this->mapper->getYearsByAlbumId($albumIds, $userId);
+			$diskCounts = $this->mapper->getDiscCountByAlbumId($albumIds, $userId);
+
 			foreach ($albums as &$album) {
 				$albumId = $album->getId();
 				$album->setArtistIds($artists[$albumId]);
+				$album->setNumberOfDisks($diskCounts[$albumId]);
 				if (\array_key_exists($albumId, $years)) {
 					$album->setYears($years[$albumId]);
 				}
@@ -146,22 +149,20 @@ class AlbumBusinessLayer extends BusinessLayer {
 	/**
 	 * Adds an album if it does not exist already or updates an existing album
 	 * @param string $name the name of the album
-	 * @param integer $discnumber the disk number of this album's disk
 	 * @param integer $albumArtistId
 	 * @param string $userId
 	 * @return Album The added/updated album
 	 */
-	public function addOrUpdateAlbum($name, $discnumber, $albumArtistId, $userId) {
+	public function addOrUpdateAlbum($name, $albumArtistId, $userId) {
 		$album = new Album();
 		$album->setName(Util::truncate($name, 256)); // some DB setups can't truncate automatically to column max size
-		$album->setDisk($discnumber);
 		$album->setUserId($userId);
 		$album->setAlbumArtistId($albumArtistId);
 
 		// Generate hash from the set of fields forming the album identity to prevent duplicates.
 		// The uniqueness of album name is evaluated in case-insensitive manner.
-		$lowerName = \mb_strtolower($name);
-		$hash = \hash('md5', "$lowerName|$discnumber|$albumArtistId");
+		$lowerName = \mb_strtolower($album->getName());
+		$hash = \hash('md5', "$lowerName|$albumArtistId");
 		$album->setHash($hash);
 
 		return $this->mapper->insertOrUpdate($album);

businesslayer/trackbusinesslayer.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013
- * @copyright Pauli Järvinen 2016 - 2019
+ * @copyright Pauli Järvinen 2016 - 2020
  */
 
 namespace OCA\Music\BusinessLayer;
@@ -207,6 +207,7 @@ class TrackBusinessLayer extends BusinessLayer {
 	 * Adds a track if it does not exist already or updates an existing track
 	 * @param string $title the title of the track
 	 * @param int|null $number the number of the track
+	 * @param int|null $discNumber the number of the disc
 	 * @param int|null $year the year of the release
 	 * @param int $artistId the artist id of the track
 	 * @param int $albumId the album id of the track
@@ -218,11 +219,12 @@ class TrackBusinessLayer extends BusinessLayer {
 	 * @return \OCA\Music\Db\Track The added/updated track
 	 */
 	public function addOrUpdateTrack(
-			$title, $number, $year, $artistId, $albumId, $fileId,
+			$title, $number, $discNumber, $year, $artistId, $albumId, $fileId,
 			$mimetype, $userId, $length=null, $bitrate=null) {
 		$track = new Track();
 		$track->setTitle(Util::truncate($title, 256)); // some DB setups can't truncate automatically to column max size
 		$track->setNumber($number);
+		$track->setDisk($discNumber);
 		$track->setYear($year);
 		$track->setArtistId($artistId);
 		$track->setAlbumId($albumId);

controller/ampachecontroller.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2017 - 2019
+ * @copyright Pauli Järvinen 2017 - 2020
  */
 
 namespace OCA\Music\Controller;
@@ -444,7 +444,6 @@ class AmpacheController extends Controller {
 				'tracks' => [$this->trackBusinessLayer->countByAlbum($album->getId())],
 				'rating' => [0],
 				'year' => [$album->yearToAPI()],
-				'disk' => [$album->getDisk()],
 				'art' => [self::createAlbumCoverUrl($this->urlGenerator, $album, $auth)],
 				'preciserating' => [0]
 			];
@@ -491,7 +490,7 @@ class AmpacheController extends Controller {
 				],
 				'url' => [self::createAmpacheActionUrl($this->urlGenerator, 'play', $track->getId(), $auth)],
 				'time' => [$track->getLength()],
-				'track' => [$track->getNumber()],
+				'track' => [$track->getDiskAdjustedTrackNumber()],
 				'bitrate' => [$track->getBitrate()],
 				'mime' => [$track->getMimetype()],
 				'size' => [$track->getSize()],

controller/subsoniccontroller.php
@@ -260,14 +260,13 @@ class SubsonicController extends Controller {
 		$albumId = self::ripIdPrefix($id); // get rid of 'album-' prefix
 
 		$album = $this->albumBusinessLayer->find($albumId, $this->userId);
-		$albumName = $album->getNameString($this->l10n);
 		$tracks = $this->trackBusinessLayer->findAllByAlbum($albumId, $this->userId);
 
 		$albumNode = $this->albumToNewApi($album);
-		$albumNode['song'] = \array_map(function($track) use ($album, $albumName) {
-			return $this->trackToApi($track, $album, $albumName);
+		$albumNode['song'] = \array_map(function($track) use ($album) {
+			$track->setAlbum($album);
+			return $this->trackToApi($track);
 		}, $tracks);
-
 		return $this->subsonicResponse(['album' => $albumNode]);
 	}
 
@@ -723,7 +722,10 @@ class SubsonicController extends Controller {
 				'id' => $id,
 				'parent' => 'artist-' . $album->getAlbumArtistId(),
 				'name' => $albumName,
-				'child' => \array_map([$this, 'trackToApi'], $tracks)
+				'child' => \array_map(function($track) use ($album) {
+					$track->setAlbum($album);
+					return $this->trackToApi($track);
+				}, $tracks)
 			]
 		]);
 	}
@@ -815,28 +817,27 @@ class SubsonicController extends Controller {
 	 * The same API format is used both on "old" and "new" API methods. The "new" API adds some
 	 * new fields for the songs, but providing some extra fields shouldn't be a problem for the
 	 * older clients. 
-	 * @param Track $track
-	 * @param Album|null $album
-	 * @param string|null $albumName
+	 * @param Track $track The track entity must have an ablum reference. One is fetched from
+	 *                     AlbumBusinessLayer if not already present.
 	 * @return array
 	 */
-	private function trackToApi($track, $album = null, $albumName = null) {
+	private function trackToApi($track) {
 		$albumId = $track->getAlbumId();
-		if ($album == null) {
+		$album = $track->getAlbum();
+		if (empty($album)) {
 			$album = $this->albumBusinessLayer->find($albumId, $this->userId);
+			$track->setAlbum($album);
 		}
-		if (empty($albumName)) {
-			$albumName = $album->getNameString($this->l10n);
-		}
-
 		$trackArtist = $this->artistBusinessLayer->find($track->getArtistId(), $this->userId);
+
 		$result = [
 			'id' => 'track-' . $track->getId(),
 			'parent' => 'album-' . $albumId,
+			//'discNumber' => $track->getDisk(), // not supported on any of the tested clients => adjust track number instead
 			'title' => $track->getTitle(),
 			'artist' => $trackArtist->getNameString($this->l10n),
 			'isDir' => false,
-			'album' => $albumName,
+			'album' => $album->getNameString($this->l10n),
 			//'genre' => '',
 			'year' => $track->getYear(),
 			'size' => $track->getSize(),
@@ -855,8 +856,9 @@ class SubsonicController extends Controller {
 			$result['coverArt'] = $album->getId();
 		}
 
-		if ($track->getNumber() !== null) {
-			$result['track'] = $track->getNumber();
+		$trackNumber = $track->getDiskAdjustedTrackNumber();
+		if ($trackNumber !== null) {
+			$result['track'] = $trackNumber;
 		}
 
 		return $result;

db/album.php
@@ -43,19 +43,24 @@ use \OCA\Music\Utility\Util;
  * @method setAlbumArtist(Artist $albumArtist)
  * @method string getHash()
  * @method setHash(string $hash)
+ * @method int getNumberOfDisks()
+ * @method setNumberOfDisks(int $count)
  */
 class Album extends Entity {
 	public $name;
 	public $mbid;
-	public $years;
-	public $disk;
+	public $disk; // deprecated
 	public $mbidGroup;
 	public $coverFileId;
-	public $artistIds;
 	public $userId;
 	public $albumArtistId;
-	public $albumArtist;
 	public $hash;
+
+	// these don't come from the music_albums table
+	public $years;
+	public $artistIds;
+	public $numberOfDisks;
+	public $albumArtist;
 
 	public function __construct() {
 		$this->addType('disk', 'int');
@@ -173,7 +178,7 @@ class Album extends Entity {
 	}
 
 	/**
-	 * Creates object used for collection API (array with name, year, disk, cover URL and ID)
+	 * Creates object used for collection API (array with name, year, cover URL and ID)
 	 * @param  IURLGenerator $urlGenerator URL Generator
 	 * @param  object $l10n Localization handler
 	 * @param  string|null $cachedCoverHash Cached cover image hash if available
@@ -182,17 +187,17 @@ class Album extends Entity {
 	 */
 	public function toCollection(IURLGenerator $urlGenerator, $l10n, $cachedCoverHash, $tracks) {
 		return [
-			'name'   => $this->getNameString($l10n),
-			'year'   => $this->getYearRange(),
-			'disk'   => $this->getDisk(),
-			'cover'  => $this->coverToCollection($urlGenerator, $cachedCoverHash),
-			'id'     => $this->getId(),
-			'tracks' => $tracks
+			'name'      => $this->getNameString($l10n),
+			'year'      => $this->getYearRange(),
+			'cover'     => $this->coverToCollection($urlGenerator, $cachedCoverHash),
+			'id'        => $this->getId(),
+			'diskCount' => $this->getNumberOfDisks(),
+			'tracks'    => $tracks
 		];
 	}
 
 	/**
-	 * Creates object used by the shiva API (array with name, year, disk, cover URL, ID, slug, URI and artists Array)
+	 * Creates object used by the shiva API (array with name, year, cover URL, ID, slug, URI and artists Array)
 	 * @param  IURLGenerator $urlGenerator URL Generator
 	 * @param  object $l10n Localization handler
 	 * @return array shiva API object
@@ -201,7 +206,6 @@ class Album extends Entity {
 		return [
 			'name'          => $this->getNameString($l10n),
 			'year'          => $this->yearToAPI(),
-			'disk'          => $this->getDisk(),
 			'cover'         => $this->coverToAPI($urlGenerator),
 			'id'            => $this->getId(),
 			'uri'           => $this->getUri($urlGenerator),

commit 8783841bd688ec5761dc04343e5dc1f50385a996

Fix the disc number migration in case there are no obsolete albums
There was an error in the DiskNumberMigration logic, which broke the SW
update process in case when no album entities had to bee merged and
hence, the list of obsolete albums to remove was empty.

This error was introduced in the commit 4a468d327.

migration/disknumbermigration.php
@@ -129,10 +129,15 @@ class DiskNumberMigration implements IRepairStep {
 	 * Delete from the albums table those rows which were made obsolete by the previous steps
 	 */
 	private function removeObsoleteAlbums() {
-		$sql = 'DELETE FROM `*PREFIX*music_albums` '.
-				'WHERE `id` IN '. $this->questionMarks(count($this->obsoleteAlbums));
+		$count = count($this->obsoleteAlbums);
 
-		return $this->db->executeUpdate($sql, $this->obsoleteAlbums);
+		if ($count > 0) {
+			$sql = 'DELETE FROM `*PREFIX*music_albums` '.
+					'WHERE `id` IN '. $this->questionMarks($count);
+			$count = $this->db->executeUpdate($sql, $this->obsoleteAlbums);
+		}
+
+		return $count;
 	}
 
 	/**
owncloud/music

commit 4157d65ced572eaa94e91f53ea44aa31ab36806e

Fix scan state being shown incorrectly after resetting the collection

js/app/controllers/maincontroller.js
@@ -73,7 +73,7 @@ function ($rootScope, $scope, $timeout, $window, $document, ArtistFactory,
 		$scope.updateAvailable = false;
 		$rootScope.loadingCollection = true;
 
-		$scope.artists = null;
+		$scope.artists = [];
 		$rootScope.$emit('artistsUpdating');
 
 		// load the music collection
@@ -197,6 +197,13 @@ function ($rootScope, $scope, $timeout, $window, $document, ArtistFactory,
 
 	$scope.stopScanning = function() {
 		$scope.scanning = false;
+	};
+
+	$scope.resetScanned = function() {
+		$scope.toScan = false;
+		filesToScan = null;
+		filesToScanIterator = 0;
+		previouslyScannedCount = 0;
 	};
 
 	$scope.loadFoldersAndThen = function(callback) {

js/app/controllers/settingsviewcontroller.js
@@ -86,6 +86,7 @@ angular.module('Music').controller('SettingsViewController', [
 							Restangular.all('resetscanned').post().then(
 									function (data) {
 										if (data.success) {
+											parent.resetScanned();
 											parent.update();
 										}
 										$scope.resetOngoing = false;

commit 860f03044f511985974842cd75e042b61b8aad55

Fix navigation from Files search results to Music
This seems to have got broken with commit 4157d65ce already on v0.13.0
(where the search was introduced). Initializing the maincontroller
`artists` variable to [] instead of null had the unforeseen side effect
of triggering the handling of URL parameters too early, before the
collection had been loaded.

The problem was present also in other cases where the user tried to
navigate to a Music app url containing the ID of track/album/artist.

js/app/controllers/maincontroller.js
@@ -82,7 +82,7 @@ function ($rootScope, $scope, $timeout, $window, $document, ArtistFactory,
 		$scope.updateAvailable = false;
 		$rootScope.loadingCollection = true;
 
-		$scope.artists = [];
+		$scope.artists = null; // the null-value tells the views that data is not yet available
 		$rootScope.$emit('artistsUpdating');
 
 		// load the music collection
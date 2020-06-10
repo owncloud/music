owncloud/music

commit 16a2ed27838cf41989a8c52413f2568b9ec5c04e

Fix application not working using URL with no trailing slash '/'
If there was no trailing slash, the RESTAngular library assumed that
the last part in the URL 'music' was not part of the base URL. When we
then changed the base URL for RESTAngular using relative path 'api', it
ended up to be like
'<serve_domain>/nextcloud/index.php/apps/api' instead of
'<serve_domain>/nextcloud/index.php/apps/music/api'.

Fixed by setting the complete path to RESTAngular, e.g.
`/nextcloud/index.php/apps/music/api`.

refs #765

js/config/app.js
@@ -31,7 +31,7 @@ angular.module('Music', ['restangular', 'duScroll', 'gettext', 'ngRoute', 'ang-d
 			$compileProvider.debugInfoEnabled(false);
 
 			// configure RESTAngular path
-			RestangularProvider.setBaseUrl('api');
+			RestangularProvider.setBaseUrl($('base').attr('href') + 'api');
 
 			var albumsControllerConfig = {
 				controller:'AlbumsViewController',

templates/main.php
@@ -16,8 +16,10 @@
 \OCP\Util::addStyle('settings', 'settings');
 \OCP\Util::addStyle('music', 'public/app');
 
-?>
+// base URI
+\OCP\Util::addHeader('base', ['href' => \OC::$server->getURLGenerator()->linkToRoute('music.page.index')]);
 
+?>
 
 <div id="app" ng-app="Music" ng-cloak ng-init="started = false; lang = '<?php p($_['lang']) ?>'">
 

commit 1bf36a3c17260f63c5923f8305c110836a45359c

Fix an error in the browser console on NC caused by 16a2ed27
Nextcloud sets a Content-Security-Policy which bans the use of the
<base> tag. It seems that this can't even be overridden by an
application. Trying to use the <base> caused an error being printed to
the web browser console upon page load.

We were using the <base> tag only as a container to hold the base
address of the page. We were not actually relying on it doing anything,
so this caused no problems besides the error on console.

Fixed by not using the back-end utilities to generate the base address
for RESTAngular. Instead, the address is now evaluated using the
front-end utility method OC.generateUrl.

js/config/app.js
@@ -31,7 +31,7 @@ angular.module('Music', ['restangular', 'duScroll', 'gettext', 'ngRoute', 'ang-d
 			$compileProvider.debugInfoEnabled(false);
 
 			// configure RESTAngular path
-			RestangularProvider.setBaseUrl($('base').attr('href') + 'api');
+			RestangularProvider.setBaseUrl(OC.generateUrl('apps/music/api'));
 
 			var albumsControllerConfig = {
 				controller:'AlbumsViewController',

templates/main.php
@@ -16,10 +16,8 @@
 \OCP\Util::addStyle('settings', 'settings');
 \OCP\Util::addStyle('music', 'public/app');
 
-// base URI
-\OCP\Util::addHeader('base', ['href' => \OC::$server->getURLGenerator()->linkToRoute('music.page.index')]);
-
 ?>
+
 
 <div id="app" ng-app="Music" ng-cloak ng-init="started = false; lang = '<?php p($_['lang']) ?>'">
 
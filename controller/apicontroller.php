63e60ebdb331641e51f4f45727eefe52effc5fbe
Pauli Järvinen <pauli.jarvinen@gmail.com>
2020-06-30 23:20:28 +0300

    Add REST API endpoint `/api/artist/{artistIdOrSlug}/details`
    The method can be used to fetch artist info from Last.fm, provided that
    the Last.fm API key has been configured in config.php using the key
    `music.lastfm_api_key`.

--- controller/apicontroller.php
+++ controller/apicontroller.php
@@ -24,9 +24,11 @@ use \OCP\IL10N;
 use \OCP\IRequest;
 use \OCP\IURLGenerator;
 
+use \OCA\Music\AppFramework\BusinessLayer\BusinessLayerException;
 use \OCA\Music\AppFramework\Core\Logger;
 use \OCA\Music\BusinessLayer\AlbumBusinessLayer;
 use \OCA\Music\BusinessLayer\ArtistBusinessLayer;
+use \OCA\Music\BusinessLayer\GenreBusinessLayer;
 use \OCA\Music\BusinessLayer\TrackBusinessLayer;
 use \OCA\Music\Db\Album;
 use \OCA\Music\Db\Artist;
@@ -37,8 +39,8 @@ use \OCA\Music\Http\FileResponse;
 use \OCA\Music\Utility\CollectionHelper;
 use \OCA\Music\Utility\CoverHelper;
 use \OCA\Music\Utility\DetailsHelper;
+use \OCA\Music\Utility\LastfmService;
 use \OCA\Music\Utility\Scanner;
-use OCA\Music\BusinessLayer\GenreBusinessLayer;
 
 class ApiController extends Controller {
 
@@ -60,6 +62,8 @@ class ApiController extends Controller {
 	private $coverHelper;
 	/** @var DetailsHelper */
 	private $detailsHelper;
+	/** @var LastfmService */
+	private $lastfmService;
 	/** @var Maintenance */
 	private $maintenance;
 	/** @var string */
@@ -82,6 +86,7 @@ class ApiController extends Controller {
 								CollectionHelper $collectionHelper,
 								CoverHelper $coverHelper,
 								DetailsHelper $detailsHelper,
+								LastfmService $lastfmService,
 								Maintenance $maintenance,
 								$userId,
 								IL10N $l10n,
@@ -97,6 +102,7 @@ class ApiController extends Controller {
 		$this->collectionHelper = $collectionHelper;
 		$this->coverHelper = $coverHelper;
 		$this->detailsHelper = $detailsHelper;
+		$this->lastfmService = $lastfmService;
 		$this->maintenance = $maintenance;
 		$this->userId = $userId;
 		$this->urlGenerator = $urlGenerator;
@@ -444,6 +450,21 @@ class ApiController extends Controller {
 		if ($details) {
 			return new JSONResponse($details);
 		} else {
+			return new ErrorResponse(Http::STATUS_NOT_FOUND);
+		}
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+	public function artistDetails($artistIdOrSlug) {
+		try {
+			$artistId = $this->getIdFromSlug($artistIdOrSlug);
+			$info = $this->lastfmService->getArtistInfo($artistId, $this->userId);
+			return new JSONResponse($info);
+		}
+		catch (BusinessLayerException $e) {
 			return new ErrorResponse(Http::STATUS_NOT_FOUND);
 		}
 	}

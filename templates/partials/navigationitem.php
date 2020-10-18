61f054cf8536eeacb10e08a38bc0e6417d92b16d
Pauli Järvinen <pauli.jarvinen@gmail.com>
2020-10-18 18:06:54 +0300

    Fix a navigation bar layout issue visible on NC20 and OC10.4
    Nextcloud 20 and ownCloud 10.4 highlight the active view in the navigation
    pane with a background color. This made it visible that the higlighted (and
    clickable) area of the list item did not fill the whole row, but there was a
    44x margin on the right side. This was left to fit the "actions" button which
    opens the popup menu.
    
    Making the clickable and highlightable area to span the whole navigation pane
    width required quite a bit of changes on the structure of the navigation pane
    items. Most notably, the first element within the <li> element can not be an
    <a> element, because making the popup menu work correctly inside an <a>
    element seemed to be impossible. This, on the other hand, forced us to define
    many css rules which previously came from the cloud core.

--- templates/partials/navigationitem.php
+++ templates/partials/navigationitem.php
@@ -1,45 +1,46 @@
 <li class="music-navigation-item" ng-class="{active: $parent.currentView == destination, playlist: playlist}">
-	<a ng-click="$parent.navigateTo(destination)" ng-hide="playlist && $parent.showEditForm == playlist.id"
+	<div class="music-navigation-item-content" ng-click="$parent.navigateTo(destination)"
 		ng-class="{current: $parent.playingView == destination, playing: $parent.playing}" 
 	>
-		<div class="play-pause-button"
+		<div class="play-pause-button" ng-hide="playlist && $parent.showEditForm == playlist.id"
 			ng-click="$parent.togglePlay(destination, playlist); $event.stopPropagation()"
 			title="{{ (($parent.playingView == destination && $parent.playing) ? 'Pause' : 'Play list') | translate }}"
 		>
 			<div class="play-pause"></div>
 		</div>
-		<span>{{ text }}</span>
-	</a>
-	<div ng-if="playlist && $parent.showEditForm == playlist.id">
-		<div class="input-container">
-			<input type="text" class="edit-list" maxlength="256"
-				ng-enter="$parent.$parent.commitEdit(playlist)" ng-model="playlist.name"/>
+		<span ng-hide="playlist && $parent.showEditForm == playlist.id">{{ text }}</span>
+		<div ng-show="playlist && $parent.showEditForm == playlist.id">
+			<div class="input-container">
+				<input type="text" class="edit-list" maxlength="256"
+					ng-enter="$parent.$parent.commitEdit(playlist)" ng-model="playlist.name"/>
+			</div>
+			<button class="action icon-checkmark"
+				ng-class="{ disabled: playlist.name.length == 0 }" 
+				ng-click="$parent.$parent.commitEdit(playlist)"></button>
 		</div>
-		<button class="action icon-checkmark"
-			ng-class="{ disabled: playlist.name.length == 0 }" 
-			ng-click="$parent.$parent.commitEdit(playlist)"></button>
-	</div>
-	<div class="actions" title="" ng-if="playlist && $parent.showEditForm == null">
-		<span class="icon-more" ng-show="!playlist.busy" ng-click="$parent.$parent.onPlaylistMoreButton(playlist); $event.stopPropagation()"></span>
-		<span class="icon-loading-small" ng-show="playlist.busy"></span>
-		<div class="popovermenu bubble" ng-show="$parent.$parent.popupShownForPlaylist == playlist">
-			<ul>
-				<li ng-click="$parent.$parent.showDetails(playlist)">
-					<a class="icon-details"> <span translate>Details</span></a>
-				</li>
-				<li ng-click="$parent.$parent.startEdit(playlist)">
-					<a class="icon-rename"><span translate>Rename</span></a>
-				</li>
-				<li ng-click="$parent.$parent.exportToFile(playlist)">
-					<a class="icon-to-file"><span translate>Export to file</span></a>
-				</li>
-				<li ng-click="$parent.$parent.importFromFile(playlist)">
-					<a class="icon-from-file"><span translate>Import from file</span></a>
-				</li>
-				<li ng-click="$parent.$parent.remove(playlist)">
-					<a class="icon-delete"> <span translate>Delete</span></a>
-				</li>
-			</ul>
+		<div class="actions" title="" ng-show="playlist && $parent.showEditForm == null">
+			<span class="icon-more" ng-show="!playlist.busy"
+				ng-click="$parent.$parent.onPlaylistMoreButton(playlist); $event.stopPropagation()"></span>
+			<span class="icon-loading-small" ng-show="playlist.busy"></span>
+			<div class="popovermenu bubble" ng-show="$parent.$parent.popupShownForPlaylist == playlist">
+				<ul>
+					<li ng-click="$parent.$parent.showDetails(playlist)">
+						<a class="icon-details"> <span translate>Details</span></a>
+					</li>
+					<li ng-click="$parent.$parent.startEdit(playlist)">
+						<a class="icon-rename"><span translate>Rename</span></a>
+					</li>
+					<li ng-click="$parent.$parent.exportToFile(playlist)">
+						<a class="icon-to-file"><span translate>Export to file</span></a>
+					</li>
+					<li ng-click="$parent.$parent.importFromFile(playlist)">
+						<a class="icon-from-file"><span translate>Import from file</span></a>
+					</li>
+					<li ng-click="$parent.$parent.remove(playlist)">
+						<a class="icon-delete"> <span translate>Delete</span></a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </li>

owncloud/music

commit 4b1804927e57b9fd9d037f7ed18e9a962d90fdbf

Use strict typing on all PHP sources
Quite a few tweaks had to be made on the code to function properly with
the strict typing. Also, a bunch of new type declarations were added;
some but not all of these were necessary to make the logic work with
the strict typing.

There is also a good chance that some problems caused by the strict
typing were still left hiding. Unfortunately, such problems are found on
an interpreted language like PHP only upon running the problematic line
of code. Hence, we now need to take a leap of faith, and hope that we
can find any issues before making the next release.

lib/Utility/DetailsHelper.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * ownCloud - Music app

lib/Utility/LastfmService.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * ownCloud - Music app

lib/Utility/Scanner.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * ownCloud - Music app
@@ -666,9 +666,10 @@ class Scanner extends PublicEmitter {
 	}
 
 	private static function normalizeOrdinal($ordinal) {
-		// convert format '1/10' to '1'
-		$tmp = \explode('/', $ordinal);
-		$ordinal = $tmp[0];
+		if (\is_string($ordinal)) {
+			// convert format '1/10' to '1'
+			$ordinal = \explode('/', $ordinal)[0];
+		}
 
 		// check for numeric values - cast them to int and verify it's a natural number above 0
 		if (\is_numeric($ordinal) && ((int)$ordinal) > 0) {
@@ -680,7 +681,8 @@ class Scanner extends PublicEmitter {
 		return $ordinal;
 	}
 
-	private static function parseFileName($fileName) {
+	private static function parseFileName(string $fileName) : array {
+		$matches = null;
 		// If the file name starts e.g like "12. something" or "12 - something", the
 		// preceeding number is extracted as track number. Everything after the optional
 		// track number + delimiters part but before the file extension is extracted as title.
@@ -692,10 +694,12 @@ class Scanner extends PublicEmitter {
 		}
 	}
 
-	private static function normalizeYear($date) {
+	private static function normalizeYear($date) : ?int {
+		$matches = null;
+
 		if (\ctype_digit($date)) {
 			return (int)$date; // the date is a valid year as-is
-		} elseif (\preg_match('/^(\d\d\d\d)-\d\d-\d\d.*/', $date, $matches) === 1) {
+		} elseif (\is_string($date) && \preg_match('/^(\d\d\d\d)-\d\d-\d\d.*/', $date, $matches) === 1) {
 			return (int)$matches[1]; // year from ISO-formatted date yyyy-mm-dd
 		} else {
 			return null;

lib/Utility/Util.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * ownCloud - Music app
@@ -97,6 +97,16 @@ class Util {
 	}
 
 	/**
+	 * Walk through the given, potentially multi-dimensional, array and cast all leaf nodes
+	 * to integer type. The array is modified in-place.
+	 */
+	public static function intCastArrayValues(array $arr) : void {
+		\array_walk_recursive($arr, function(&$value) {
+			$value = \intval($value);
+		});
+	}
+
+	/**
 	 * Like the built-in \explode(...) function but this one can be safely called with
 	 * null string, and no warning will be emitted. Also, this returns an empty array from
 	 * null and '' inputs while the built-in alternative returns a 1-item array containing
@@ -154,8 +164,8 @@ class Util {
 	 * Multi-byte safe case-insensitive string comparison
 	 * @return int < 0 if $a is less than $b; > 0 if $a is greater than $b, and 0 if they are equal.
 	 */
-	public static function stringCaseCompare(string $a, string $b) : int {
-		return \strcmp(\mb_strtolower($a), \mb_strtolower($b));
+	public static function stringCaseCompare(?string $a, ?string $b) : int {
+		return \strcmp(\mb_strtolower($a ?? ''), \mb_strtolower($b ?? ''));
 	}
 
 	/**

commit 5d2dad373530603651eb23deb6f382b75ff71a4c

Fix opening the details pane while some of the details are missing
The strict typing enabled in 4b180492 caused the details fetching to
fail when the track in question had no "audio" info available (probably
happens only/mostly on corrupted tracks). Also, for a similar reason,
getting the Last.fm info failed for tracks with no artist info
available.

lib/Utility/DetailsHelper.php
@@ -32,21 +32,23 @@ class DetailsHelper {
 	 * @param Folder $userFolder
 	 * $return array|null
 	 */
-	public function getDetails(int $fileId, Folder $userFolder) {
-		$fileNodes = $userFolder->getById($fileId);
-		if (\count($fileNodes) > 0) {
-			$data = $this->extractor->extract($fileNodes[0]);
+	public function getDetails(int $fileId, Folder $userFolder) : ?array {
+		$file = $userFolder->getById($fileId)[0] ?? null;
+		if ($file !== null) {
+			$data = $this->extractor->extract($file);
+			$audio = $data['audio'] ?: [];
+			$comments = $data['comments'] ?: [];
 
-			// remove intermediate arrays and map null comments into an empty array
-			$data['comments'] = self::flattenComments($data['comments'] ?: []);
+			// remove intermediate arrays
+			$comments = self::flattenComments($comments);
 
 			// cleanup strings from invalid characters
-			\array_walk($data['audio'], ['self', 'sanitizeString']);
-			\array_walk($data['comments'], ['self', 'sanitizeString']);
+			\array_walk($audio, ['self', 'sanitizeString']);
+			\array_walk($comments, ['self', 'sanitizeString']);
 
 			$result = [
-				'fileinfo' => $data['audio'],
-				'tags' => $data['comments']
+				'fileinfo' => $audio,
+				'tags' => $comments
 			];
 
 			// binary data has to be encoded
@@ -78,7 +80,7 @@ class DetailsHelper {
 			$result['length'] = $data['playtime_seconds'] ?? null;
 
 			// add file path
-			$result['path'] = $userFolder->getRelativePath($fileNodes[0]->getPath());
+			$result['path'] = $userFolder->getRelativePath($file->getPath());
 
 			return $result;
 		}

lib/Utility/LastfmService.php
@@ -154,8 +154,11 @@ class LastfmService {
 			$args['api_key'] = $this->apiKey;
 			$args['format'] = 'json';
 
+			// remove args with null or empty values
+			$args = \array_filter($args, [Util::class, 'isNonEmptyString']);
+
 			// glue arg keys and values together ...
-			$args= \array_map(function ($key, $value) {
+			$args = \array_map(function ($key, $value) {
 				return $key . '=' . \urlencode($value);
 			}, \array_keys($args), $args);
 			// ... and form the final query string

lib/Utility/Scanner.php
@@ -188,29 +188,29 @@ class Scanner extends PublicEmitter {
 		$meta['albumArtist'] = ExtractorGetID3::getFirstOfTags($fileInfo, ['band', 'albumartist', 'album artist', 'album_artist']);
 
 		// use artist and albumArtist as fallbacks for each other
-		if (!self::isNonEmptyString($meta['albumArtist'])) {
+		if (!Util::isNonEmptyString($meta['albumArtist'])) {
 			$meta['albumArtist'] = $meta['artist'];
 		}
 
-		if (!self::isNonEmptyString($meta['artist'])) {
+		if (!Util::isNonEmptyString($meta['artist'])) {
 			$meta['artist'] = $meta['albumArtist'];
 		}
 
 		// set 'Unknown Artist' in case neither artist nor albumArtist was found
-		if (!self::isNonEmptyString($meta['artist'])) {
+		if (!Util::isNonEmptyString($meta['artist'])) {
 			$meta['artist'] = null;
 			$meta['albumArtist'] = null;
 		}
 
 		// title
 		$meta['title'] = ExtractorGetID3::getTag($fileInfo, 'title');
-		if (!self::isNonEmptyString($meta['title'])) {
+		if (!Util::isNonEmptyString($meta['title'])) {
 			$meta['title'] = $fieldsFromFileName['title'];
 		}
 
 		// album
 		$meta['album'] = ExtractorGetID3::getTag($fileInfo, 'album');
-		if (!self::isNonEmptyString($meta['album'])) {
+		if (!Util::isNonEmptyString($meta['album'])) {
 			// album name not set in fileinfo, use parent folder name as album name unless it is the root folder
 			$dirPath = \dirname($filePath);
 			if ($userHome->getPath() === $dirPath) {
@@ -645,10 +645,6 @@ class Scanner extends PublicEmitter {
 
 	public function resolveUserFolder(string $userId) : Folder {
 		return $this->rootFolder->getUserFolder($userId);
-	}
-
-	private static function isNonEmptyString(/*mixed*/ $item) : bool {
-		return \is_string($item) && \trim($item) !== '';
 	}
 
 	private static function normalizeOrdinal($ordinal) : ?int {

lib/Utility/Util.php
@@ -167,6 +167,13 @@ class Util {
 	public static function stringCaseCompare(?string $a, ?string $b) : int {
 		return \strcmp(\mb_strtolower($a ?? ''), \mb_strtolower($b ?? ''));
 	}
+	
+	/**
+	 * Test if $item is a string and not empty or only consisting of whitespace
+	 */
+	public static function isNonEmptyString(/*mixed*/ $item) : bool {
+		return \is_string($item) && \trim($item) !== '';
+	}
 
 	/**
 	 * Convert file size given in bytes to human-readable format
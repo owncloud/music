owncloud/music

commit 4b1804927e57b9fd9d037f7ed18e9a962d90fdbf

Use strict typing on all PHP sources
Quite a few tweaks had to be made on the code to function properly with
the strict typing. Also, a bunch of new type declarations were added;
some but not all of these were necessary to make the logic work with
the strict typing.

There is also a good chance that some problems caused by the strict
typing were still left hiding. Unfortunately, such problems are found on
an interpreted language like PHP only upon running the problematic line
of code. Hence, we now need to take a leap of faith, and hope that we
can find any issues before making the next release.

lib/Controller/PlaylistApiController.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * ownCloud - Music app

commit 46629d79863f8a83ba8d43400b84f940af6bd04e

Fix playlist modifications
The strict PHP typing added in 4b180492 caused problems for playlist
modifications as there were some inconsistencies in the use of types in
PlaylistApiController.

lib/Controller/PlaylistApiController.php
@@ -308,12 +308,12 @@ class PlaylistApiController extends Controller {
 	 * Modify playlist by calling a supplied method from PlaylistBusinessLayer
 	 * @param string $funcName  Name of a function to call from PlaylistBusinessLayer
 	 * @param array $funcParams Parameters to pass to the function 'funcName'
-	 * @return \OCP\AppFramework\Http\JSONResponse JSON representation of the modified playlist
+	 * @return JSONResponse JSON representation of the modified playlist
 	 */
-	private function modifyPlaylist(string $funcName, array $funcParams) : \OCP\AppFramework\Http\JSONResponse {
+	private function modifyPlaylist(string $funcName, array $funcParams) : JSONResponse {
 		try {
 			$playlist = \call_user_func_array([$this->playlistBusinessLayer, $funcName], $funcParams);
-			return $playlist->toAPI();
+			return new JSONResponse($playlist->toAPI());
 		} catch (BusinessLayerException $ex) {
 			return new ErrorResponse(Http::STATUS_NOT_FOUND, $ex->getMessage());
 		}
@@ -321,10 +321,10 @@ class PlaylistApiController extends Controller {
 
 	/**
 	 * Get integer array passed as parameter to the Playlist API
-	 * @param string $listAsString Comma-separated integer values in string
+	 * @param string|int $listAsString Comma-separated integer values in string, or a single integer
 	 * @return int[]
 	 */
-	private static function toIntArray(string $listAsString) : array {
-		return \array_map('intval', \explode(',', $listAsString));
+	private static function toIntArray(/*mixed*/ $listAsString) : array {
+		return \array_map('intval', \explode(',', (string)$listAsString));
 	}
 }
owncloud/music

commit fe5ad807cd7501c1bc840a34f721a83f4574be72

Add "repeat one" as a third state for the "Repeat" button
In the "repeat one" mode, the currently playing track is played again
from the beginning each time it finishes playing. However, using the
"skip next" button still jumps to the next track, and repeated song is
only once in the play history, regardless of how many times it got
played.

refs #808

js/app/controllers/playercontroller.js
@@ -23,7 +23,7 @@ function ($scope, $rootScope, playlistService, libraryService,
 	$scope.currentAlbum = null;
 	$scope.seekCursorType = 'default';
 	$scope.volume = parseInt(Cookies.get('oc_music_volume')) || 50;  // volume can be 0~100
-	$scope.repeat = Cookies.get('oc_music_repeat') == 'true';
+	$scope.repeat = Cookies.get('oc_music_repeat') || 'false';
 	$scope.shuffle = Cookies.get('oc_music_shuffle') == 'true';
 	$scope.position = {
 		bufferPercent: '0%',
@@ -57,7 +57,12 @@ function ($scope, $rootScope, playlistService, libraryService,
 		$rootScope.$emit('playerProgress', currentTime);
 	});
 	onPlayerEvent('end', function() {
-		$scope.next();
+		if ($scope.repeat === 'one') {
+			$scope.player.seek(0);
+			$scope.player.play();
+		} else {
+			$scope.next();
+		}
 	});
 	onPlayerEvent('duration', function(msecs) {
 		$scope.setTime($scope.position.current, msecs/1000);
@@ -175,9 +180,14 @@ function ($scope, $rootScope, playlistService, libraryService,
 	};
 
 	$scope.toggleRepeat = function() {
-		$scope.repeat = !$scope.repeat;
-		playlistService.setRepeat($scope.repeat);
-		Cookies.set('oc_music_repeat', $scope.repeat.toString(), { expires: 3650 });
+		var nextState = {
+			'false'	: 'true',
+			'true'	: 'one',
+			'one'	: 'false'
+		};
+		$scope.repeat = nextState[$scope.repeat];
+		playlistService.setRepeat($scope.repeat !== 'false'); // the "repeat-one" is handled internally by the PlayerController
+		Cookies.set('oc_music_repeat', $scope.repeat, { expires: 3650 });
 	};
 
 	$scope.setTime = function(position, duration) {

img/repeat-1.svg
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" version="1.1" xml:space="preserve" height="24" width="24" enable-background="new 0 0 32 32" y="0px" x="0px" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" viewBox="0 0 7.68 7.68"><path d="m2.8253 0.59188v1.0675h-1.8978c-0.2383 0.0002-0.43103 0.1929-0.43125 0.4312v4.1975c0.00022 0.2383 0.19295 0.4317 0.43125 0.4319h5.8244c0.2383-0.0002 0.4316-0.1936 0.4318-0.4319v-4.1975c-0.0002-0.2383-0.1935-0.431-0.4318-0.4312h-1.8406v0.86312h1.4094v3.3344h-4.9612v-3.3344h1.4659v1.0744l1.5025-1.5025z"/><text style="word-spacing:0px;letter-spacing:0px" xml:space="preserve" font-size="2.4px" y="4.4257627" x="2.9288135" font-family="Arial" line-height="125%" fill="#000000"><tspan y="4.4257627" x="2.9288135"/></text>
+<text style="word-spacing:0px;letter-spacing:0px" xml:space="preserve" font-size="2.4px" y="5.4521246" x="2.9967499" font-family="Sans" line-height="125%" fill="#000000"><tspan font-size="3.2px" font-family="Gadugi" y="5.4521246" x="2.9967499" font-weight="bold">1</tspan></text>
+</svg>

templates/partials/controls.php
@@ -35,7 +35,8 @@
 	<img id="shuffle" class="control toggle small svg" alt="{{ 'Shuffle' | translate }}" title="{{ 'Shuffle' | translate }}"
 		src="<?php HtmlUtil::printSvgPath('shuffle') ?>" ng-class="{active: shuffle}" ng-click="toggleShuffle()" />
 	<img id="repeat" class="control toggle small svg" alt="{{ 'Repeat' | translate }}" title="{{ 'Repeat' | translate }}"
-		src="<?php HtmlUtil::printSvgPath('repeat') ?>" ng-class="{active: repeat}" ng-click="toggleRepeat()" />
+		src="{{ repeat=='one' ? '<?php HtmlUtil::printSvgPath('repeat-1') ?>' : '<?php HtmlUtil::printSvgPath('repeat') ?>' }}"
+		ng-class="{active: repeat != 'false' }" ng-click="toggleRepeat()" />
 	<div class="volume-control" title="{{ 'Volume' | translate }} {{volume}} %">
 		<img id="volume-icon" class="control small svg" alt="{{ 'Volume' | translate }}" ng-show="volume > 0"
 			src="<?php HtmlUtil::printSvgPath('sound') ?>" />

commit d958c0e47656433c7edbe728d2a714ecacf7ab64

FIx a bug in the repeat button introduced by fe5ad807c
The repeat mode was not set correctly for the playlistService by default upon
the page load; the correct state was set only upon toggling the mode with the
controls bar button.

js/app/controllers/playercontroller.js
@@ -30,7 +30,7 @@ function ($scope, $rootScope, playlistService, Audio, Restangular, gettextCatalo
 		total: 0
 	};
 
-	playlistService.setRepeat($scope.repeat);
+	playlistService.setRepeat($scope.repeat !== 'false'); // the "repeat-one" is handled internally by the PlayerController
 	playlistService.setShuffle($scope.shuffle);
 
 	// Player events may fire synchronously or asynchronously. Utilize $timeout
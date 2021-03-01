owncloud/music

commit ab088210ad332aa36848a77a9120d08beb9979ae

Support for Nextcloud 21
NC21 wraps all the DBAL exceptions to its own generic exception type. This
makes it a bit trickier to react to unique constraint violations. To not have
to write the cumbersome try-catch-if-else blocks everywhere, both the native
DBAL UniqueConstraintViolationExceptions and the wrapped NC21 exceptions are
now caught as early as possible, and mapped to our own exception type.

refs #830

lib/Migration/DiskNumberMigration.php
@@ -175,7 +175,7 @@ class DiskNumberMigration implements IRepairStep {
 						'UPDATE `*PREFIX*music_albums` SET `hash` = ? WHERE `id` = ?',
 						[$hash, $row['id']]
 				);
-			} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException $e) {
+			} catch (\OCA\Music\AppFramework\Db\UniqueConstraintViolationException | \OCP\DB\Exception $e) {
 				$this->mergeFailureAlbums[] = $row['id'];
 			}
 		}

.travis.yml
@@ -26,6 +26,7 @@ matrix:
     - env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable18
     - env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable19
     - env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable20
+    - env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable21
     - env: DB=mysql CLOUD=nextcloud CORE_BRANCH=master
 
     # PostgreSQL
@@ -38,6 +39,8 @@ matrix:
       env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable15
     - php: 7.4
       env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable18
+    - php: 8.0
+      env: DB=mysql CLOUD=nextcloud CORE_BRANCH=stable21
   allow_failures:
     - env: DB=mysql CLOUD=owncloud CORE_BRANCH=stable10
     - env: DB=mysql CLOUD=owncloud CORE_BRANCH=master

appinfo/info.xml
@@ -28,7 +28,7 @@ Limitations: The app can handle libraries of more than 50´000 tracks on modern 
 	<dependencies>
 		<php min-version="7.1" max-version="7.4"/>
 		<owncloud min-version="10" max-version="10" />
-		<nextcloud min-version="13" max-version="20" />
+		<nextcloud min-version="13" max-version="21" />
 	</dependencies>
 	<types>
 		<!-- update metadata cache when create/update/delete a file -->

lib/AppFramework/Db/UniqueConstraintViolationException.php
@@ -1,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright Pauli Järvinen 2021
+ */
+
+namespace OCA\Music\AppFramework\Db;
+
+/**
+ * Own exception type for the Music app to be used on DB unique constraint violations.
+ * The exceptions used for this by the core vary by the version of Nexcloud or ownCloud used.
+ * Mapping these all to a common type enables unified handling.
+ */
+class UniqueConstraintViolationException extends \Exception {
+
+	/**
+	 * Constructor
+	 * @param string $msg the error message
+	 */
+	public function __construct(string $message = "", int $code = 0, \Throwable $previous = null) {
+		parent::__construct($message, $code, $previous);
+	}
+}

lib/Db/BaseMapper.php
@@ -18,7 +18,7 @@ use \OCP\AppFramework\Db\Mapper;
 use \OCP\AppFramework\Db\MultipleObjectsReturnedException;
 use \OCP\IDBConnection;
 
-use \Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use \OCA\Music\AppFramework\Db\UniqueConstraintViolationException;
 
 /**
  * Common base class for data access classes of the Music app
@@ -188,7 +188,19 @@ abstract class BaseMapper extends Mapper {
 		$nowStr = $now->format(self::SQL_DATE_FORMAT);
 		$entity->setCreated($nowStr);
 		$entity->setUpdated($nowStr);
-		return parent::insert($entity);
+
+		try {
+			return parent::insert($entity);
+		} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException $e) {
+			throw new UniqueConstraintViolationException($e->getMessage(), $e->getCode(), $e);
+		} catch (\OCP\DB\Exception $e) {
+			// Nextcloud 21
+			if ($e->getReason() == \OCP\DB\Exception::REASON_UNIQUE_CONSTRAINT_VIOLATION) {
+				throw new UniqueConstraintViolationException($e->getMessage(), $e->getCode(), $e);
+			} else {
+				throw $e;
+			}
+		}
 	}
 
 	/**

lib/Db/Cache.php
@@ -7,14 +7,14 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2017 - 2020
+ * @copyright Pauli Järvinen 2017 - 2021
  */
 
 namespace OCA\Music\Db;
 
 use OCP\IDBConnection;
 
-use \Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use \OCA\Music\AppFramework\Db\UniqueConstraintViolationException;
 
 class Cache {
 	private $db;
@@ -31,7 +31,7 @@ class Cache {
 	public function add(string $userId, string $key, string $data) : void {
 		$sql = 'INSERT INTO `*PREFIX*music_cache`
 				(`user_id`, `key`, `data`) VALUES (?, ?, ?)';
-		$this->db->executeUpdate($sql, [$userId, $key, $data]);
+		$this->executeUpdate($sql, [$userId, $key, $data]);
 	}
 
 	/**
@@ -42,7 +42,7 @@ class Cache {
 	public function update(string $userId, string $key, string $data) : void {
 		$sql = 'UPDATE `*PREFIX*music_cache` SET `data` = ?
 				WHERE `user_id` = ? AND `key` = ?';
-		$this->db->executeUpdate($sql, [$data, $userId, $key]);
+		$this->executeUpdate($sql, [$data, $userId, $key]);
 	}
 
 	/**
@@ -79,7 +79,7 @@ class Cache {
 			$sql .= ' WHERE `key` = ?';
 			$params[] = $key;
 		}
-		$this->db->executeUpdate($sql, $params);
+		$this->executeUpdate($sql, $params);
 	}
 
 	/**
@@ -131,5 +131,20 @@ class Cache {
 		$result->closeCursor();
 		
 		return \count($rows) ? $rows[0]['user_id'] : null;
+	}
+
+	private function executeUpdate(string $sql, array $params) {
+		try {
+			return $this->db->executeUpdate($sql, $params);
+		} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException $e) {
+			throw new UniqueConstraintViolationException($e->getMessage(), $e->getCode(), $e);
+		} catch (\OCP\DB\Exception $e) {
+			// Nextcloud 21
+			if ($e->getReason() == \OCP\DB\Exception::REASON_UNIQUE_CONSTRAINT_VIOLATION) {
+				throw new UniqueConstraintViolationException($e->getMessage(), $e->getCode(), $e);
+			} else {
+				throw $e;
+			}
+		}
 	}
 }

lib/Utility/CollectionHelper.php
@@ -7,18 +7,17 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2018
+ * @copyright Pauli Järvinen 2018 - 2021
  */
 
 namespace OCA\Music\Utility;
 
 use \OCA\Music\AppFramework\Core\Logger;
+use \OCA\Music\AppFramework\Db\UniqueConstraintViolationException;
 use \OCA\Music\BusinessLayer\Library;
 use \OCA\Music\Db\Cache;
 
 use \OCP\ICache;
-
-use \Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 /**
  * Utility to build and cache the monolithic json data describing the whole music library.

lib/Utility/CoverHelper.php
@@ -7,12 +7,13 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2017 - 2020
+ * @copyright Pauli Järvinen 2017 - 2021
  */
 
 namespace OCA\Music\Utility;
 
 use \OCA\Music\AppFramework\Core\Logger;
+use \OCA\Music\AppFramework\Db\UniqueConstraintViolationException;
 use \OCA\Music\Db\Album;
 use \OCA\Music\Db\Artist;
 use \OCA\Music\Db\Cache;
@@ -21,8 +22,6 @@ use \OCP\Files\Folder;
 use \OCP\Files\File;
 
 use \OCP\IConfig;
-
-use \Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 /**
  * utility to get cover image for album

commit 4952988fe97312be69d1268e5c3e03888b8ad120

Fix a bug introduced by ab088210 on the disk number migration
Changing the exception type had been accidentally applied also on one
occasion where it shouldn't have been done. Because of this, updating
from Music version <0.13.0 to 1.0.3 may lead to corrupted database on
some cases. To recover, the music collection has to be wiped clean and
rescanned.

Hopefully, not many users are still using the over a year old versions
<0.13.0. And if they are, then hopefully they don't update  now to
1.0.3 (without first installing some version in between).

lib/Migration/DiskNumberMigration.php
@@ -175,7 +175,7 @@ class DiskNumberMigration implements IRepairStep {
 						'UPDATE `*PREFIX*music_albums` SET `hash` = ? WHERE `id` = ?',
 						[$hash, $row['id']]
 				);
-			} catch (\OCA\Music\AppFramework\Db\UniqueConstraintViolationException | \OCP\DB\Exception $e) {
+			} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException | \OCP\DB\Exception $e) {
 				$this->mergeFailureAlbums[] = $row['id'];
 			}
 		}
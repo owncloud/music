d6354d02ea766e8efea4ff13935e67cbedf60e2c
Pauli Järvinen <pauli.jarvinen@gmail.com>
2021-05-23 13:14:32 +0300

    Move the deployment package logic from Makefile to its own script file
    The logic was starting to be a bit too complicated to be comfortably inlined
    within the Makefile.

---
+++ build/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# ownCloud Music
+#
+# @author Pauli Järvinen
+# @copyright 2021 Pauli Järvinen <pauli.jarvinen@gmail.com>
+#
+
+# Create the base package from the files stored in git
+cd ..
+git archive HEAD --format=zip --prefix=music/ > music.zip
+
+# Add the generated webpack files to the previously created package
+cd ..
+zip -g music/music.zip music/dist/*.js
+zip -g music/music.zip music/dist/*.css
+zip -g music/music.zip music/dist/*.json
+zip -g music/music.zip music/dist/img/**
+
+# Remove the front-end source files from the package as those are not needed to run the app
+zip -d music/music.zip "music/build/*"
+zip -d music/music.zip "music/css/*.css"
+zip -d music/music.zip "music/css/*/"
+zip -d music/music.zip "music/img/*.svg"
+zip -d music/music.zip "music/img/*/*"
+zip -d music/music.zip "music/js/*.js*"
+zip -d music/music.zip "music/js/*/*"
+zip -d music/music.zip "music/l10n/*/*"
+zip -d music/music.zip "music/tests/*"
+
+# Fork the package to own versions for Nextcloud and ownCloud.
+# Different mechanism is used on each cloud to define the database schema.
+cp music/music.zip music/music-nc.zip
+mv music/music.zip music/music-oc.zip
+zip -d music/music-nc.zip "music/appinfo/database.xml"
+zip -d music/music-oc.zip "music/lib/Migration/Version*Date*.php"

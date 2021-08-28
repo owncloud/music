owncloud/music

commit 797a01ee90e56b131d9e01e71ca7c4f100806eab

Initial support for playing external streams from playlist in Files
The code isn't super-neat but the playback of many streams found from
www.radio-browser.info actually works pretty nicely. HLS-type streams
cannot be played for now on desktop browsers, but on mobile browsers,
even that works (Android and iOS have native support for such streams;
for desktop browsers, we would need to add some javascript library like
HLS.js).

It was also noted that previously the Music app was modifying the
content-security-policy headers globally within the Nextcloud/ownCloud
server. Now this is limited to parts of the cloud where our code is
actually used: within the Music, Files, and Share apps.

refs #784

js/shared/playerwrapper.js
@@ -21,6 +21,7 @@ OCA.Music.PlayerWrapper = function() {
 	var m_volume = 100;
 	var m_playing = false;
 	var m_url = null;
+	var m_streamingExtUrl = false;
 	var m_self = this;
 
 	_.extend(this, OC.Backbone.Events);
@@ -162,7 +163,9 @@ OCA.Music.PlayerWrapper = function() {
 	this.seekingSupported = function() {
 		// Seeking is not implemented in aurora/flac.js and does not work on all
 		// files with aurora/mp3.js. Hence, we disable seeking with aurora.
-		return (m_underlyingPlayer == 'html5');
+		// Also, seeking requires that we know a valid duration for the file/stream;
+		// this is not always the case with external streams.
+		return (m_underlyingPlayer == 'html5' && $.isNumeric(m_duration) && m_duration > 0);
 	};
 
 	this.seekMsecs = function(msecs) {
@@ -234,8 +237,9 @@ OCA.Music.PlayerWrapper = function() {
 		this.trigger('loading');
 
 		m_url = url;
+		m_streamingExtUrl = (mime === null); // null-mime is used to mark an external stream URL
 
-		if (canPlayWithHtml5(mime)) {
+		if (m_streamingExtUrl || canPlayWithHtml5(mime)) {
 			m_underlyingPlayer = 'html5';
 		} else {
 			m_underlyingPlayer = 'aurora';

appinfo/app.php
@@ -33,13 +33,6 @@ $appName = $c->query('AppName');
 });
 
 /**
- * Set default content security policy to allow loading media from data URL.
- */
-$policy = new \OCP\AppFramework\Http\ContentSecurityPolicy();
-$policy->addAllowedMediaDomain('data:');
-\OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
-
-/**
  * register regular task
  */
 \OC::$server->getJobList()->add('OC\BackgroundJob\Legacy\RegularJob', ['OCA\Music\Backgroundjob\CleanUp', 'run']);
@@ -60,6 +53,17 @@ $c->getServer()->getSearch()->registerProvider(
 );
 
 /**
+ * Set default content security policy to allow loading media from data URL or any http(s) URL.
+ */
+function allowMediaSourcesInCsp() {
+	$policy = new \OCP\AppFramework\Http\ContentSecurityPolicy();
+	$policy->addAllowedMediaDomain('data:');
+	$policy->addAllowedMediaDomain('http://*:*');
+	$policy->addAllowedMediaDomain('https://*:*');
+	\OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
+}
+
+/**
  * Load embedded music player for Files and Sharing apps
  *
  * The nice way to do this would be
@@ -68,10 +72,10 @@ $c->getServer()->getSearch()->registerProvider(
  * ... but this doesn't work for shared files on ownCloud 10.0, at least. Hence, we load the scripts
  * directly if the requested URL seems to be for Files or Sharing.
  */
-$loadEmbeddedMusicPlayer = function () {
+function loadEmbeddedMusicPlayer() {
 	\OCA\Music\Utility\HtmlUtil::addWebpackScript('files_music_player');
 	\OCA\Music\Utility\HtmlUtil::addWebpackStyle('files_music_player');
-};
+}
 
 $request = \OC::$server->getRequest();
 if (isset($request->server['REQUEST_URI'])) {
@@ -81,7 +85,13 @@ if (isset($request->server['REQUEST_URI'])) {
 	$isShareUrl = \preg_match('%/s/.+%', $url)
 		&& !\preg_match('%/apps/.*%', $url)
 		&& !\preg_match('%.*/authenticate%', $url);
+	$isMusicUrl = \preg_match('%/apps/music(/.*)?%', $url);
+
 	if ($isFilesUrl || $isShareUrl) {
-		$loadEmbeddedMusicPlayer();
+		allowMediaSourcesInCsp();
+		loadEmbeddedMusicPlayer();
+	}
+	elseif ($isMusicUrl) {
+		allowMediaSourcesInCsp();
 	}
 }

controller/playlistapicontroller.php
@@ -285,17 +285,26 @@ class PlaylistApiController extends Controller {
 			$libFileIds = $this->trackBusinessLayer->findAllFileIds($this->userId);
 			$libFileIds = \array_flip($libFileIds);
 
+			$bogusUrlId = -1;
+
 			// compose the final result
-			$result['files'] = \array_map(function($fileAndCaption) use ($libFileIds) {
-				$file = $fileAndCaption['file'];
-				return [
-					'id' => $file->getId(),
-					'name' => $file->getName(),
-					'path' => $this->userFolder->getRelativePath($file->getParent()->getPath()),
-					'mimetype' => $file->getMimeType(),
-					'caption' => $fileAndCaption['caption'],
-					'in_library' => isset($libFileIds[$file->getId()])
-				];
+			$result['files'] = \array_map(function($fileInfo) use ($libFileIds, &$bogusUrlId) {
+				if (isset($fileInfo['url'])) {
+					$fileInfo['id'] = $bogusUrlId--;
+					$fileInfo['mimetype'] = null;
+					return $fileInfo;
+				}
+				else {
+					$file = $fileInfo['file'];
+					return [
+						'id' => $file->getId(),
+						'name' => $file->getName(),
+						'path' => $this->userFolder->getRelativePath($file->getParent()->getPath()),
+						'mimetype' => $file->getMimeType(),
+						'caption' => $fileInfo['caption'],
+						'in_library' => isset($libFileIds[$file->getId()])
+					];
+				}
 			}, $result['files']);
 			return new JSONResponse($result);
 		}

js/embedded/embeddedplayer.js
@@ -231,9 +231,11 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 			var ratio = 0;
 			if (songLength_s === 0) {
 				text.text(t('music', 'Loading…'));
-			} else {
+			} else if ($.isNumeric(songLength_s)) {
 				text.text(formatTime(playTime_s) + '/' + formatTime(songLength_s));
 				ratio = playTime_s / songLength_s;
+			} else {
+				text.text(formatTime(playTime_s));
 			}
 			playBar.css('width', 100 * ratio + '%');
 		}
@@ -252,9 +254,7 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 			setCursorType('default');
 		});
 		player.on('ready', function() {
-			if (player.seekingSupported()) {
-				setCursorType('pointer');
-			}
+			// nothing to do
 		});
 		player.on('buffer', function(percent) {
 			bufferBar.css('width', Math.round(percent) + '%');
@@ -266,6 +266,9 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 		player.on('duration', function(msecs) {
 			songLength_s = Math.round(msecs/1000);
 			updateProgress();
+			if (player.seekingSupported()) {
+				setCursorType('pointer');
+			}
 		});
 		player.on('play', function() {
 			playButton.css('display', 'none');
@@ -436,7 +439,7 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 		});
 	}
 
-	function playUrl(url, mime, tempTitle, nextStep) {
+	function playUrl(url, mime, tempTitle, nextStep /*optional*/) {
 		pause();
 
 		// Set placeholders for track info fields, proper data is filled once received
@@ -457,7 +460,9 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 			playDelayTimer = null;
 			player.fromURL(url, mime);
 			play();
-			nextStep();
+			if (nextStep) {
+				nextStep();
+			}
 
 			// 'Previous' button is enalbed regardless of the playlist size if seeking is
 			// supported for the file being played 
@@ -575,6 +580,15 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 			} else {
 				loadFileInfo(fileId, fallbackTitle);
 			}
+		});
+	};
+
+	this.playExtUrl = function(url, caption) {
+		playUrl(url, null, '');
+		updateMetadata({
+			title: caption,
+			artist: '',
+			cover: null
 		});
 	};
 

js/embedded/main.js
@@ -134,13 +134,17 @@ function initEmbeddedPlayer() {
 			if (!mPlayingListFile) {
 				mCurrentFile = file;
 			}
-			mPlayer.playFile(
-				urlForFile(file),
-				file.mimetype,
-				file.id,
-				file.name,
-				mShareToken
-			);
+			if ('url' in file) {
+				mPlayer.playExtUrl(file.url, file.caption);
+			} else {
+				mPlayer.playFile(
+					urlForFile(file),
+					file.mimetype,
+					file.id,
+					file.name,
+					mShareToken
+				);
+			}
 			mPlayer.setPlaylistIndex(mPlaylist.currentIndex(), mPlaylist.length());
 			if (OCA.Music.playlistTabView) {
 				// this will also take care of clearing any existing focus if the mCurrentFile is not a playlist

js/embedded/playlist.js
@@ -26,7 +26,8 @@ OCA.Music.Playlist = function() {
 
 	this.init = function(folderFiles, supportedMimes, firstFileId) {
 		mFiles = _.filter(folderFiles, function(file) {
-			return _.includes(supportedMimes, file.mimetype);
+			// external URLs do not have a valid MIME type set, attempt to play them regardless
+			return file.mimetype === null || _.includes(supportedMimes, file.mimetype);
 		});
 		mCurrentIndex = _.findIndex(mFiles, function(file) {
 			// types int/string depend on the cloud version, don't use ===

utility/playlistfileservice.php
@@ -113,7 +113,7 @@ class PlaylistFileService {
 	 * @throws \UnexpectedValueException if the $filePath points to a file of unsupported type
 	 */
 	public function importFromFile($id, $userId, $userFolder, $filePath) {
-		$parsed = $this->doParseFile($userFolder->get($filePath), $userFolder);
+		$parsed = $this->doParseFile($userFolder->get($filePath), $userFolder, /*allowUrls=*/false);
 		$trackFilesAndCaptions = $parsed['files'];
 		$invalidPaths = $parsed['invalid_paths'];
 
@@ -152,13 +152,13 @@ class PlaylistFileService {
 	public function parseFile($fileId, $baseFolder) {
 		$nodes = $baseFolder->getById($fileId);
 		if (\count($nodes) > 0) {
-			return $this->doParseFile($nodes[0], $baseFolder);
+			return $this->doParseFile($nodes[0], $baseFolder, /*allowUrls=*/true);
 		} else {
 			throw new \OCP\Files\NotFoundException();
 		}
 	}
 
-	private function doParseFile(File $file, $baseFolder) {
+	private function doParseFile(File $file, $baseFolder, $allowUrls) {
 		$mime = $file->getMimeType();
 
 		if ($mime == 'audio/mpegurl') {
@@ -175,14 +175,29 @@ class PlaylistFileService {
 		$cwd = $baseFolder->getRelativePath($file->getParent()->getPath());
 
 		foreach ($entries as $entry) {
-			$path = Util::resolveRelativePath($cwd, $entry['path']);
-			try {
-				$trackFiles[] = [
-					'file' => $baseFolder->get($path),
-					'caption' => $entry['caption']
-				];
-			} catch (\OCP\Files\NotFoundException $ex) {
-				$invalidPaths[] = $path;
+			$path = $entry['path'];
+
+			if (Util::startsWith($path, 'http')) {
+				if ($allowUrls) {
+					$trackFiles[] = [
+						'url' => $path,
+						'caption' => $entry['caption']
+					];
+				} else {
+					$invalidPaths[] = $path;
+				}
+			}
+			else {
+				$path = Util::resolveRelativePath($cwd, $path);
+
+				try {
+					$trackFiles[] = [
+						'file' => $baseFolder->get($path),
+						'caption' => $entry['caption']
+					];
+				} catch (\OCP\Files\NotFoundException $ex) {
+					$invalidPaths[] = $path;
+				}
 			}
 		}
 

commit f746271c2e341e013b792d2561419da5d7f5ab67

Fix continuing playback from the same offset on navigation from Files to Music
When clicking the song title in the "lite" player within Files, the playback
was supposed to continue from the same time offset within the Music app
(since v0.16.0). However, this had been broken by the commit 797a01ee
included in v1.0.0, and the playback always started again from the beginning
of the track.

js/shared/playerwrapper.js
@@ -170,8 +170,11 @@ OCA.Music.PlayerWrapper = function() {
 		// Seeking is not implemented in aurora/flac.js and does not work on all
 		// files with aurora/mp3.js. Hence, we disable seeking with aurora.
 		// Also, seeking requires that we know a valid duration for the file/stream;
-		// this is not always the case with external streams.
-		return (m_underlyingPlayer == 'html5' && $.isNumeric(m_duration) && m_duration > 0);
+		// this is not always the case with external streams. On the other hand, when
+		// playing a normal local file, the seeking may be requested before we have fetched
+		// the duration and that is fine.
+		var validDuration = $.isNumeric(m_duration) && m_duration > 0;
+		return (m_underlyingPlayer == 'html5' && (!m_streamingExtUrl || validDuration));
 	};
 
 	this.seekMsecs = function(msecs) {
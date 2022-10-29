owncloud/music

commit eea2d31fa2239c5c5e7228f1694b6d9901e81e8f

Invert also the play/pause icon in the play context menu on dark theme
Also, modified the inverting logic so that it can be used also on items
containing a text label which should not get inverted. With this, we could
apply the inversion via css filter on more places and could get rid of the
dedicated inverted variants of some icons.

css/music-dark-theme.css
@@ -9,12 +9,9 @@
  */
  
 .dark-theme .svg,
+.dark-theme .svg > span, /* label within the icon item is inverted again, effectively restoring the default color */
 .dark-theme :not(.svg) > .play-pause {
 	filter: invert(100%);
-}
-
-.dark-theme .icon-sort-by-alpha {
-	background-image: url(../img/sort-by-alpha-white.svg);
 }
 
 .dark-theme #app-sidebar .close {
@@ -40,12 +37,4 @@
 
 .dark-theme .icon-reload {
 	background-image: url(../img/reload-inverted.svg);
-}
-
-.dark-theme .icon-stop {
-	background-image: url(../img/stop-white.svg);
-}
-
-.dark-theme .icon-time {
-	background-image: url(../img/access-time-white.svg)
 }

templates/partials/controls.php
@@ -10,7 +10,7 @@
 			ng-on-long-press="playbackBtnLongPress($event)"
 			data-long-press-delay="500" 
 		>
-			<div id="stop-button" ng-click="stop()" class="control icon-stop"
+			<div id="stop-button" ng-click="stop()" class="control icon-stop svg"
 				ng-show="shiftHeldDown" alt="{{ 'Stop' | translate }}">
 			</div>
 			<div id="play-pause-button" ng-click="togglePlayback()" class="control svg"
@@ -20,20 +20,20 @@
 			<div id="play-pause-menu" class="popovermenu bubble" ng-show="playPauseContextMenuVisible">
 				<ul>
 					<li ng-show="!shiftHeldDown" ng-click="stop()">
-						<a class="icon-stop"><span translate>Stop</span></a>
+						<a class="icon-stop svg"><span translate>Stop</span></a>
 					</li>
 					<li ng-show="shiftHeldDown" ng-click="togglePlayback()">
-						<a ng-class="playing ? 'icon-pause-big' : 'icon-play-big'">
+						<a ng-class="playing ? 'icon-pause-big' : 'icon-play-big'" class="svg">
 							<span>{{ (playing ? 'Pause' : 'Play') | translate }}</span>
 						</a>
 					</li>
 					<li ng-click="$event.stopPropagation()" id="playback-rate-control">
-						<a class="icon-time" ng-click="stepPlaybackRate(null, false, true)"
+						<a class="icon-time svg" ng-click="stepPlaybackRate(null, false, true)"
 							ng-on-contextmenu="stepPlaybackRate($event, true, true)"
 							ng-on-long-press="stepPlaybackRate($event, true, true)"
 							data-long-press-delay="500"
 						>
-							<span translate>Playback rate</span>: {{ playbackRate | number : 2 }}
+							<span translate>Playback rate</span><span>: {{ playbackRate | number : 2 }}</span>
 						</a>
 						<input type="range" min="0.5" max="3.0" step="0.05" ng-model="playbackRate"/>
 					</li>

templates/partials/navigationitem.php
@@ -45,7 +45,7 @@
 						<a class="icon-to-file"><span translate>Export to file</span></a>
 					</li>
 					<li ng-click="subMenuShown = !subMenuShown; $event.stopPropagation()">
-						<a class="icon-sort-by-alpha"><span translate>Sort …</span></a>
+						<a class="icon-sort-by-alpha svg"><span translate>Sort …</span></a>
 						<div class="popovermenu bubble submenu" ng-show="subMenuShown">
 							<ul>
 								<li ng-click="$parent.$parent.sortPlaylist(playlist, 'track')">

css/music-navigation.css
@@ -152,6 +152,10 @@
 	height: 100%;
 }
 
+#app-navigation .item-with-actions .actions .popovermenu a {
+	box-shadow: unset; /* older NC versions mistakenly apply also here the blue edge meant for navi pane items */
+}
+
 #app-navigation button.disabled {
 	opacity: .5;
 	pointer-events: none;

img/access-time-white.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>

img/sort-by-alpha-white.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px"><path d="M0 0h24v24H0V0zm0 0h24v24H0V0zm.75.75h22.5v22.5H.75z" fill="none"/><path d="M14.94 4.66h-4.72l2.36-2.36zm-4.69 14.71h4.66l-2.33 2.33zM6.1 6.27L1.6 17.73h1.84l.92-2.45h5.11l.92 2.45h1.84L7.74 6.27H6.1zm-1.13 7.37l1.94-5.18 1.94 5.18H4.97zm10.76 2.5h6.12v1.59h-8.53v-1.29l5.92-8.56h-5.88v-1.6h8.3v1.26l-5.93 8.6z"/></svg>

img/stop-white.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 16 16" width="16px" fill="#ffffff"><path d="M1 1h14v14H1z"/></svg>

commit 1055fea6baefcb47938f8257c9f021af6358ed79

Fix also context menu highlight getting inverted on dark theme on NC25
- This bug was introduced by the commit eea2d31f

- To mitigate the problem, all context menu icons now live in their own span
  elements instead of defining the icons on the parent <a> elements. Some
  adjustments to css were needed to make this work also on ownCloud.

css/music-actions-menu.css
@@ -26,12 +26,18 @@
 }
 
 .app-music #app .popovermenu ul li a {
-	padding-left: 32px;
+	padding-left: 0;
 	padding-right: 12px;
 	background-position: 10px center;
 	background-size: 16px;
 	width: 100%;
 	opacity: .7;
+}
+
+.app-music #app .popovermenu ul li a .icon {
+	padding: 22px 0 22px 44px;
+	vertical-align: bottom;
+	background-size: inherit;
 }
 
 .app-music #app .popovermenu ul a:hover {

css/music-dark-theme.css
@@ -9,7 +9,6 @@
  */
  
 .dark-theme .svg,
-.dark-theme .svg > span, /* label within the icon item is inverted again, effectively restoring the default color */
 .dark-theme :not(.svg) > .play-pause {
 	filter: invert(100%);
 }

js/app/directives/listheading.js
@@ -5,7 +5,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright 2019 - 2021 Pauli Järvinen
+ * @copyright 2019 - 2022 Pauli Järvinen
  *
  */
 
@@ -106,13 +106,15 @@ function ($rootScope, gettextCatalog) {
 			for (var action of actions) {
 				var listitem = document.createElement('li');
 				var link = document.createElement('a');
-				link.className = 'icon-' + action.icon;
-				var span = document.createElement('span');
-				span.innerText = gettextCatalog.getString(action.text);
+				var icon = document.createElement('span');
+				icon.className = 'icon icon-' + action.icon;
+				var text = document.createElement('span');
+				text.innerText = gettextCatalog.getString(action.text);
 				// Note: l10n-extract cannot find localised string defined like above.
 				// Ensure that the same string can be extracted from somewhere else.
 				$(listitem).data('callback', action.callback);
-				link.appendChild(span);
+				link.appendChild(icon);
+				link.appendChild(text);
 				listitem.appendChild(link);
 				list.appendChild(listitem);
 			}

templates/partials/controls.php
@@ -20,23 +20,24 @@
 			<div id="play-pause-menu" class="popovermenu bubble" ng-show="playPauseContextMenuVisible">
 				<ul>
 					<li ng-show="!shiftHeldDown" ng-click="stop()">
-						<a class="icon-stop svg"><span translate>Stop</span></a>
+						<a><span class="icon-stop icon svg"></span><span translate>Stop</span></a></a>
 					</li>
 					<li ng-show="shiftHeldDown" ng-click="togglePlayback()">
-						<a ng-class="playing ? 'icon-pause-big' : 'icon-play-big'" class="svg">
+						<a>
+							<span ng-class="playing ? 'icon-pause-big' : 'icon-play-big'" class="icon svg"></span>
 							<span>{{ (playing ? 'Pause' : 'Play') | translate }}</span>
 						</a>
 					</li>
 					<li id="context-menu-skip-prev" ng-click="prev()">
-						<a class="icon-skip-prev svg"><span translate>Previous</span></a>
+						<a><span class="icon-skip-prev icon svg"></span><span translate>Previous</span></a>
 					</li>
 					<li ng-click="$event.stopPropagation()" id="playback-rate-control">
-						<a class="icon-time svg" ng-click="stepPlaybackRate(null, false, true)"
+						<a ng-click="stepPlaybackRate(null, false, true)"
 							ng-on-contextmenu="stepPlaybackRate($event, true, true)"
 							ng-on-long-press="stepPlaybackRate($event, true, true)"
 							data-long-press-delay="500"
 						>
-							<span translate>Playback rate</span><span>: {{ playbackRate | number : 2 }}</span>
+							<span class="icon-time icon svg"></span><span translate>Playback rate</span><span>: {{ playbackRate | number : 2 }}</span>
 						</a>
 						<input type="range" min="0.5" max="3.0" step="0.05" ng-model="playbackRate"/>
 					</li>

templates/partials/navigationitem.php
@@ -33,19 +33,19 @@
 			<div class="popovermenu bubble" ng-show="$parent.$parent.popupShownForPlaylist == playlist">
 				<ul>
 					<li ng-click="$parent.$parent.showDetails(playlist)">
-						<a class="icon-details"><span translate>Details</span></a>
+						<a><span class="icon-details icon"></span><span translate>Details</span></a>
 					</li>
 					<li ng-click="$parent.$parent.startEdit(playlist)" class="app-navigation-noclose">
-						<a class="icon-rename"><span translate>Rename</span></a>
+						<a><span class="icon-rename icon"></span><span translate>Rename</span></a>
 					</li>
 					<li ng-click="$parent.$parent.importFromFile(playlist)">
-						<a class="icon-from-file svg"><span translate>Import from file</span></a>
+						<a><span class="icon-from-file icon svg"></span><span translate>Import from file</span></a>
 					</li>
 					<li ng-click="$parent.$parent.exportToFile(playlist)">
-						<a class="icon-to-file svg"><span translate>Export to file</span></a>
+						<a><span class="icon-to-file icon svg"></span><span translate>Export to file</span></a>
 					</li>
 					<li ng-click="subMenuShown = !subMenuShown; $event.stopPropagation()">
-						<a class="icon-sort-by-alpha svg"><span translate>Sort …</span></a>
+						<a><span class="icon-sort-by-alpha icon svg"></span><span translate>Sort …</span></a>
 						<div class="popovermenu bubble submenu" ng-show="subMenuShown">
 							<ul>
 								<li ng-click="$parent.$parent.sortPlaylist(playlist, 'track')">
@@ -61,10 +61,10 @@
 						</div>
 					</li>
 					<li ng-click="$parent.$parent.removeDuplicates(playlist)">
-						<a class="icon-close"><span translate>Remove duplicates</span></a>
+						<a><span class="icon-close icon"></span><span translate>Remove duplicates</span></a>
 					</li>
 					<li ng-click="$parent.$parent.remove(playlist)">
-						<a class="icon-delete"><span translate>Delete</span></a>
+						<a><span class="icon-delete icon"></span><span translate>Delete</span></a>
 					</li>
 				</ul>
 			</div>
@@ -76,16 +76,16 @@
 			<div class="popovermenu bubble" ng-show="$parent.popupShownForPlaylist == 'radio'">
 				<ul>
 					<li ng-click="$parent.showRadioHint()">
-						<a class="icon-details"><span translate>Getting started</span></a>
+						<a><span class="icon-details icon"></span><span translate>Getting started</span></a>
 					</li>
 					<li ng-click="$parent.importFromFileToRadio()">
-						<a class="icon-from-file svg"><span translate>Import from file</span></a>
+						<a><span class="icon-from-file icon svg"></span><span translate>Import from file</span></a>
 					</li>
 					<li ng-click="$parent.exportRadioToFile()">
-						<a class="icon-to-file svg"><span translate>Export to file</span></a>
+						<a><span class="icon-to-file icon svg"></span><span translate>Export to file</span></a>
 					</li>
 					<li ng-click="$parent.addRadio()">
-						<a class="icon-add"><span translate>Add manually</span></a>
+						<a><span class="icon-add icon"></span><span translate>Add manually</span></a>
 					</li>
 				</ul>
 			</div>
@@ -97,10 +97,10 @@
 			<div class="popovermenu bubble" ng-show="$parent.popupShownForPlaylist == 'podcasts'">
 				<ul>
 					<li ng-click="$parent.addPodcast()">
-						<a class="icon-add"><span translate>Add from RSS feed</span></a>
+						<a><span class="icon-add icon"></span><span translate>Add from RSS feed</span></a>
 					</li>
 					<li ng-click="$parent.reloadPodcasts($event)" ng-class="{ disabled: !$parent.anyPodcastChannels() }">
-						<a class="icon-reload"><span translate>Reload channels</span></a>
+						<a><span class="icon-reload icon"></span><span translate>Reload channels</span></a>
 					</li>
 				</ul>
 			</div>
@@ -111,10 +111,10 @@
 			<div class="popovermenu bubble" ng-show="$parent.popupShownForPlaylist == 'albums'">
 				<ul>
 					<li ng-click="$parent.toggleAlbumsCompactLayout(false)">
-						<a ng-class="$parent.albumsCompactLayout ? 'icon-radio-button' : 'icon-radio-button-checked'"><span translate>Normal layout</span></a>
+						<a><span class="icon" ng-class="$parent.albumsCompactLayout ? 'icon-radio-button' : 'icon-radio-button-checked'"></span><span translate>Normal layout</span></a>
 					</li>
 					<li ng-click="$parent.toggleAlbumsCompactLayout(true)">
-						<a ng-class="$parent.albumsCompactLayout ? 'icon-radio-button-checked' : 'icon-radio-button'"><span translate>Compact layout</span></a>
+						<a><span class="icon" ng-class="$parent.albumsCompactLayout ? 'icon-radio-button-checked' : 'icon-radio-button'"></span><span translate>Compact layout</span></a>
 					</li>
 				</ul>
 			</div>
@@ -125,10 +125,10 @@
 			<div class="popovermenu bubble" ng-show="$parent.popupShownForPlaylist == 'folders'">
 				<ul>
 					<li ng-click="$parent.toggleFoldersFlatLayout(false)">
-						<a ng-class="$parent.foldersFlatLayout ? 'icon-radio-button' : 'icon-radio-button-checked'"><span translate>Tree layout</span></a>
+						<a><span class="icon" ng-class="$parent.foldersFlatLayout ? 'icon-radio-button' : 'icon-radio-button-checked'"></span><span translate>Tree layout</span></a>
 					</li>
 					<li ng-click="$parent.toggleFoldersFlatLayout(true)">
-						<a ng-class="$parent.foldersFlatLayout ? 'icon-radio-button-checked' : 'icon-radio-button'"><span translate>Flat layout</span></a>
+						<a><span class="icon" ng-class="$parent.foldersFlatLayout ? 'icon-radio-button-checked' : 'icon-radio-button'"></span><span translate>Flat layout</span></a>
 					</li>
 				</ul>
 			</div>
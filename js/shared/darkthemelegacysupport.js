owncloud/music

commit 9c96724f3d3128b21ec653a180b061549992c949

"Polyfill" the var(--background-invert-if-dark) on older NC versions
Fix the dark theme icon color inverting on on NC < 25 after the previous
commit. We now set the CSS variable --background-invert-if-dark on our own if
it has not been set by the cloud core.

In this legacy support, we now identify the dark theme by analyzing the
background color directly. It has been a longstanding issue that the API
previously used to detect the selected theme often returned wrong values after
changing the theme, until a forced reload of the web page was conducted.

js/shared/darkthemelegacysupport.js
@@ -1,0 +1,40 @@
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright Pauli Järvinen 2022
+ */
+
+OCA.Music = OCA.Music || {};
+
+/** @namespace */
+OCA.Music.DarkThemeLegacySupport = {
+	applyOnElement: function(element) {
+		if (getComputedStyle(element).getPropertyValue('--background-invert-if-dark') == '') {
+			// The property is not available => Nextcloud < 25 or ownCloud.
+			
+			// There doesn't seem to be a reliable method to find out the active dark theme on NC in all cases.
+			// Both accessing OCA.Accessibility.theme and looking at the <body> class 'dark' will often give
+			// wrong (cached?) results after changing the theme, until a forced page reload is made.
+
+			// Workaround: Try to figure out on our own if the background color is indeed dark. This should work
+			// also with any dark third party themes (e.g. Breeze Dark).
+			const rgb = getComputedStyle(document.getElementById('app-content')).backgroundColor;
+			const m = rgb.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+			if (m) {
+				const [r, g, b] = [m[1], m[2], m[3]];
+				// Analyze perceived brightness, based on https://stackoverflow.com/a/12043228
+				const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+				if (luma < 128) { //dark
+					element.style.setProperty('--background-invert-if-dark', 'invert(100%)');
+				} else { // light
+					element.style.setProperty('--background-invert-if-dark', 'no');
+				}
+			}
+		}
+	}
+};

js/app/controllers/maincontroller.js
@@ -19,6 +19,9 @@ function ($rootScope, $scope, $timeout, $window, $document, ArtistFactory,
 	// retrieve language from backend - is set in ng-app HTML element
 	gettextCatalog.currentLanguage = $rootScope.lang;
 
+	// setup dark theme support for Nextcloud versions older than 25
+	OCA.Music.DarkThemeLegacySupport.applyOnElement(document.getElementById('app'));
+
 	$rootScope.playing = false;
 	$rootScope.playingView = null;
 	$scope.currentTrack = null;

js/embedded/embeddedplayer.js
@@ -525,6 +525,9 @@ OCA.Music.EmbeddedPlayer = function(onClose, onNext, onPrev, onMenuOpen, onShowL
 
 		parentContainer.append(musicControls);
 
+		// setup dark theme support for Nextcloud versions older than 25
+		OCA.Music.DarkThemeLegacySupport.applyOnElement(musicControls[0]);
+
 		// Resize music controls bar to fit the scroll bar when window size changes or details pane opens/closes.
 		// Also the internal layout of the bar is responsive to the available width.
 		var resizeControls = function() {

js/embedded/playlisttabview.js
@@ -20,10 +20,6 @@ OCA.Music.initPlaylistTabView = function(playlistMimes) {
 				var container = this.$el;
 				container.empty(); // erase any previous content
 
-				if (OCA.Music.Utils.darkThemeActive()) {
-					container.addClass('dark-theme');
-				}
-
 				var fileInfo = this.getFileInfo();
 
 				if (fileInfo) {

js/shared/utils.js
@@ -13,19 +13,6 @@ OCA.Music = OCA.Music || {};
 /** @namespace */
 OCA.Music.Utils = {
 
-	internal: {
-		// dark theme information was in the Accessibility app up until NC24 
-		themeInAccessiblity: function() {
-			return Object.prototype.hasOwnProperty.call(OCA, 'Accessibility')
-				&& Object.prototype.hasOwnProperty.call(OCA.Accessibility, 'theme');
-		},
-		// dark theme information was moved to the Theming app in NC25
-		themeInTheming: function() {
-			return Object.prototype.hasOwnProperty.call(OCA, 'Theming')
-				&& Object.prototype.hasOwnProperty.call(OCA.Theming, 'enabledThemes');
-		}
-	},
-
 	/**
 	 * Originally in ownCloud and in Nextcloud up to version 13, the #app-content element acted as the main scroll container.
 	 * Nextcloud 14 changed this so that the document became the main scrollable container, and this needed some adjustments
@@ -42,35 +29,6 @@ OCA.Music.Utils = {
 	 */
 	isLegacyLayout: function() {
 		return $('#content-wrapper').length > 0;
-	},
-
-	/**
-	 * Newer versions of Nextcloud come with a "dark theme" which may be activated
-	 * from the accessibility settings. Test if the theme is active.
-	 * Note: This may not be able to return the correct state during the application initialization,
-	 * in case the OCA.Accessibility has not got initialized yet. The function themeInfoAvailable may
-	 * be used to check if the information is available.
-	 */
-	darkThemeActive: function() {
-		if (OCA.Music.Utils.internal.themeInAccessiblity()) {
-			// The name of the theme was originally 'themedark' but changed to simply 'dark' in NC18.
-			return (OCA.Accessibility.theme == 'themedark' || OCA.Accessibility.theme == 'dark');
-		} else if (OCA.Music.Utils.internal.themeInTheming()) {
-			return OCA.Theming.enabledThemes.includes('dark') 
-				|| OCA.Theming.enabledThemes.includes('dark-highcontrast')
-				|| (OCA.Theming.enabledThemes.includes('default') && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-		} else {
-			return false;
-		}
-	},
-
-	/**
-	 * Check if theme info is currently available. It may be unavailable for two reasons:
-	 * 1) it has not been loaded yet
-	 * 2) it is not supported by the cloud
-	 */
-	themeInfoAvailable: function() {
-		return OCA.Music.Utils.internal.themeInAccessiblity() || OCA.Music.Utils.internal.themeInTheming();
 	},
 
 	/**

commit 7a01fe3129e98e10588eb1da27631ad8912902b5

Fix embedded player on single share broken after 9c96724f
The embedded player didn't work on individually shared audio files after the
commit 9c96724f. This affected only ownCloud as the Music app is not used on
those shares on Nextcloud at all. The problem was that those share pages don't
have the #app-content element but that was not null-checked.

Obviously, the dark-theme logic does not work now for the individual share page
but that shouldn't be an issue because:
a) There's no built-in dark theme on ownCloud
b) The individual share page on OC doesn't seem to be built so that it easily
    could have any non-default background color.

js/shared/darkthemelegacysupport.js
@@ -22,17 +22,20 @@ OCA.Music.DarkThemeLegacySupport = {
 
 			// Workaround: Try to figure out on our own if the background color is indeed dark. This should work
 			// also with any dark third party themes (e.g. Breeze Dark).
-			const rgb = getComputedStyle(document.getElementById('app-content')).backgroundColor;
-			const m = rgb.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
-			if (m) {
-				const [r, g, b] = [m[1], m[2], m[3]];
-				// Analyze perceived brightness, based on https://stackoverflow.com/a/12043228
-				const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-
-				if (luma < 128) { //dark
-					element.style.setProperty('--background-invert-if-dark', 'invert(100%)');
-				} else { // light
-					element.style.setProperty('--background-invert-if-dark', 'no');
+			const appContent = document.getElementById('app-content');
+			if (appContent) {
+				const rgb = getComputedStyle(appContent)?.backgroundColor;
+				const m = rgb?.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+				if (m) {
+					const [r, g, b] = [m[1], m[2], m[3]];
+					// Analyze perceived brightness, based on https://stackoverflow.com/a/12043228
+					const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+	
+					if (luma < 128) { //dark
+						element.style.setProperty('--background-invert-if-dark', 'invert(100%)');
+					} else { // light
+						element.style.setProperty('--background-invert-if-dark', 'no');
+					}
 				}
 			}
 		}
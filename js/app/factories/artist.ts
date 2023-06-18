eb046dd3d86c9222021c85bb8eecbf4500f167fd
Pauli Järvinen <pauli.jarvinen@gmail.com>
2023-06-18 20:21:54 +0300

    JS: Use TypeScript for all the factories and filters

--- js/app/factories/artist.ts
+++ js/app/factories/artist.ts
@@ -7,14 +7,18 @@
  * @author Morris Jobke <morris.jobke@gmail.com>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright 2013 Morris Jobke
- * @copyright 2018 - 2022 Pauli Järvinen
+ * @copyright 2018 - 2023 Pauli Järvinen
  *
  */
 
-angular.module('Music').factory('ArtistFactory', ['Restangular', '$rootScope', function (Restangular, $rootScope) {
+import * as angular from "angular";
+import { MusicRootScope } from "app/config/musicrootscope";
+import { IService } from "restangular";
+
+angular.module('Music').factory('ArtistFactory', ['Restangular', '$rootScope', function (Restangular : IService, $rootScope : MusicRootScope) {
 	return {
-		getArtists: function() {
-			return Restangular.all('prepare_collection').post().then(function(reply) {
+		getArtists() {
+			return Restangular.all('prepare_collection').post({}).then(function(reply) {
 				$rootScope.$emit('newCoverArtToken', reply.cover_token);
 				$rootScope.$emit('updateIgnoredArticles', reply.ignored_articles);
 				return Restangular.all('collection').getList({hash: reply.hash});
--- js/app/factories/audio.ts
+++ js/app/factories/audio.ts
@@ -7,7 +7,7 @@
  * @author Morris Jobke <morris.jobke@gmail.com>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013
- * @copyrigth Pauli Järvinen 2017 - 2022
+ * @copyrigth Pauli Järvinen 2017 - 2023
  */
 
 angular.module('Music').factory('Audio', [function () {

6465c41715c00ea5ff5b9db141969a5e45e5eb4a
Pauli Järvinen <pauli.jarvinen@gmail.com>
2023-07-14 16:05:07 +0300

    Rename smartlist filter "Play rate" to "Play history"
    Also, added some missing `translate` tags to the template.

--- templates/partials/sidebar/smartlistfilters.php
+++ templates/partials/sidebar/smartlistfilters.php
@@ -3,36 +3,36 @@
 	<h1 translate>Smart playlist filters</h1>
 
 	<div>
-		<label for="size">List size</label>
+		<label for="size" translate>List size</label>
 		<input type="number" name="size" id="filters-size" ng-model="listSize"/>
 	</div>
 
 	<div>
-		<label for="from-year">From year</label>
+		<label for="from-year" translate>From year</label>
 		<input type="number" name="from-year" ng-model="fromYear"/>
 	</div>
 
 	<div>
-		<label for="to-year">To year</label>
+		<label for="to-year" translate>To year</label>
 		<input type="number" name="to-year" ng-model="toYear"/>
 	</div>
 
 	<div>
-		<label for="genres">Genres</label>
+		<label for="genres" translate>Genres</label>
 		<select name="genres" id="filter-genres" multiple data-placeholder=" " ng-model="genres">
 			<option ng-repeat="genre in allGenres" value="{{ genre.id }}">{{ genre.name }}</option>
 		</select>
 	</div>
 
 	<div>
-		<label for="artists">Artists</label>
+		<label for="artists" translate>Artists</label>
 		<select name="artists" id="filter-artists" multiple data-placeholder=" " ng-model="artists">
 			<option ng-repeat="artist in allArtists" value="{{ artist.id }}">{{ artist.name }}</option>
 		</select>
 	</div>
 
 	<div title="{{ 'Note that this selection makes any difference only when the library has more than requested number of matches' | translate }}">
-		<label for="play-rate">Play rate</label>
+		<label for="play-rate" translate>Play history</label>
 		<select name="play-rate" id="filter-play-rate" ng-model="playRate">
 			<option value=""></option>
 			<option value="recently" translate>Recently played</option>

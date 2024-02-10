178b7d067b61a186091965939a346e1d7e5dd8bb
Pauli Järvinen <pauli.jarvinen@gmail.com>
2024-02-10 13:56:28 +0200

    Some layout improvements on the advanced search view

--- templates/partials/views/advancedsearchview.php
+++ templates/partials/views/advancedsearchview.php
@@ -1,27 +1,32 @@
-<div class="view-container " id="advanced-search-area">
+<div class="view-container playlist-area" id="advanced-search-area">
 	<h1 translate>Advanced search</h1>
 
-	<table id="adv-search-rules" class="grid">
-		<tr class="adv-search-rule-row" ng-repeat="rule in searchRules">
-			<td><select ng-model="rule.rule"><option ng-repeat="ruleType in searchRuleTypes" value="{{ ruleType.key }}">{{ ruleType.name }}</option></select></td>
-			<td><select ng-model="rule.operator"><option ng-repeat="ruleOp in operatorsForRule(rule.rule)" value="{{ ruleOp.key }}">{{ ruleOp.name }}</option></select></td>
-			<td><input type="text" ng-model="rule.input"/></td>
-			<td><a class="icon icon-close" ng-click="removeSearchRule($index)"></a></td>
-		</tr>
-		<tr class="add-row clickable" ng-click="addSearchRule()">
-			<td><a class="icon icon-add"></a></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
-	</table>
-	<button ng-click="search()" translate>Search</button><span style="color:red" ng-show="errorDescription" translate>{{ errorDescription }}</span>
+	<div id="adv-search-controls">
+		<table id="adv-search-rules">
+			<tr class="adv-search-rule-row" ng-repeat="rule in searchRules">
+				<td><select ng-model="rule.rule"><option ng-repeat="ruleType in searchRuleTypes" value="{{ ruleType.key }}">{{ ruleType.name }}</option></select></td>
+				<td><select ng-model="rule.operator"><option ng-repeat="ruleOp in operatorsForRule(rule.rule)" value="{{ ruleOp.key }}">{{ ruleOp.name }}</option></select></td>
+				<td><input type="text" ng-model="rule.input"/></td>
+				<td><a class="icon icon-close" ng-click="removeSearchRule($index)"></a></td>
+			</tr>
+			<tr class="add-row clickable" ng-click="addSearchRule()">
+				<td><a class="icon icon-add"></a></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+		<button ng-click="search()" translate>Search</button><span style="color:red" ng-show="errorDescription" translate>{{ errorDescription }}</span>
+	</div>
 
-	<div class="playlist-area" ng-if="resultList.tracks">
-        <h2 class="clickable" ng-click="onHeaderClick()"
-            translate translate-n="resultList.tracks.length" translate-plural="{{ resultList.tracks.length }} results">
-            1 result
-        </h2>
+	<div ng-if="resultList.tracks" class="flat-list-view">
+		<h2>
+			<span ng-class="{ clickable: resultList.tracks.length }" ng-click="onHeaderClick()">
+				<span translate translate-n="resultList.tracks.length" translate-plural="{{ resultList.tracks.length }} results">1 result</span>
+				<img ng-if="resultList.tracks.length" class="play svg" alt="{{ 'Play' | translate }}"
+					src="<?php \OCA\Music\Utility\HtmlUtil::printSvgPath('play-big') ?>"/>
+			</span>
+		</h2>
 		<track-list
 			tracks="resultList.tracks"
 			get-track-data="getTrackData"

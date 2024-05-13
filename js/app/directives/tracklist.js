owncloud/music

commit a44d9804a9ac9f704df677e0e47c657a88585454

Use same kind of padding for the Details icon in all list views
The vertical padding of the Details (and Remove) button of the playlist
view was increased in the commit 0225f76da to make the button easier to
click. Now the same style is applied also to all the other list views
using the "cover art and two lines" layout.

js/app/directives/tracklist.js
@@ -178,7 +178,7 @@ function ($rootScope, $interpolate, gettextCatalog, albumartService) {
 
 			if (data.showTrackDetails) {
 				let detailsButton = document.createElement('button');
-				detailsButton.className = 'icon-details';
+				detailsButton.className = 'icon-details action';
 				detailsButton.title = detailsText;
 				listItem.appendChild(detailsButton);
 			}

commit 9ea9f7d3262e3206379e665198cad8dabd9da95d

Fix tracklist details button broken by a44d9804

js/app/directives/tracklist.js
@@ -235,7 +235,7 @@ function ($rootScope, $interpolate, gettextCatalog, albumartService) {
 		data.element.on('click', 'li', function(event) {
 			let trackId = trackIdFromElementId(this.id);
 			if (trackId) {
-				if (event.target.className == 'icon-details') {
+				if (event.target.className.includes('icon-details')) {
 					data.showTrackDetails(trackId);
 				} else if ($(event.target).closest('.item-info').length > 0) {
 					// target is .item-info or any of its descendants
5e930137d8e3ce05e757e507026a3379cdba39fc
Pauli Järvinen <pauli.jarvinen@gmail.com>
2024-06-19 23:43:34 +0300

    Polyfill String.includes for IE
    This fixes once again our (partial) support for Internet Explorer. It's
    partial as there are some graphical glitches, and what's worse, the context
    menu buttons don't show up in the navigation pane. The rest of the
    functionality should now mostly work again.

--- js/index.app.js
+++ js/index.app.js
@@ -17,6 +17,7 @@ function requireAll(context) {
 
 /* Polyfills for IE compatibility */
 require('node_modules/core-js/features/array/includes');
+require('node_modules/core-js/features/string/includes');
 require('node_modules/core-js/features/string/replace-all');
 require('node_modules/core-js/features/string/starts-with');
 require('node_modules/core-js/features/string/ends-with');

5ca940d0a0d046e670cb23be1286b2739f3ffaff
Pauli Järvinen <pauli.jarvinen@gmail.com>
2024-08-19 23:03:21 +0300

    Implement the angular playlistService using a class instead of a function

--- js/app/services/playlistservice.ts
+++ js/app/services/playlistservice.ts
@@ -13,32 +13,37 @@
 import * as ng from "angular";
 import * as _ from "lodash";
 
-interface PlaylistEntry {
+interface PlayQueueEntry {
 };
 
-ng.module('Music').service('playlistService', [function() {
-	let playlist : PlaylistEntry[]|null = null;
-	let playlistId : string|null = null;
-	let playOrder : number[] = [];
-	let playOrderIter = -1;
-	let startFromIndex : number|null = null;
-	let shuffle = false;
-	let repeat = false;
-	let prevShuffleState = false;
-	let eventDispatcher = _.clone(OC.Backbone.Events);
+class PlayQueueService {
+	#playlist : PlayQueueEntry[]|null = null;
+	#playlistId : string|null = null;
+	#playOrder : number[] = [];
+	#playOrderIter = -1;
+	#startFromIndex : number|null = null;
+	#shuffle = false;
+	#repeat = false;
+	#prevShuffleState = false;
+	#eventDispatcher = _.clone(OC.Backbone.Events);
 
-	function shuffledIndices() : number[] {
-		let indices = _.range(playlist.length);
+	/* -------------------------------
+	 * PRIVATE METHODS
+	 * -------------------------------
+	 */
+
+	#shuffledIndices() : number[] {
+		let indices = _.range(this.#playlist.length);
 		return _.shuffle(indices);
 	}
 
-	function shuffledIndicesExcluding(toExclude : number) : number[] {
-		let indices = _.range(playlist.length);
+	#shuffledIndicesExcluding(toExclude : number) : number[] {
+		let indices = _.range(this.#playlist.length);
 		indices.splice(toExclude, 1);
 		return _.shuffle(indices);
 	}
 
-	function wrapIndexToStart<Type>(list : Type[], index : number) : Type[] {
+	#wrapIndexToStart<Type>(list : Type[], index : number) : Type[] {
 		if (index > 0) {
 			// slice array in two parts and interchange them
 			let begin = list.slice(0, index);
@@ -48,176 +53,195 @@ ng.module('Music').service('playlistService', [function() {
 		return list;
 	}
 
-	function enqueueIndices() : void {
+	#enqueueIndices() : void {
 		let nextIndices : number[] = null;
 
-		if (shuffle) {
-			if (startFromIndex !== null) {
-				nextIndices = [startFromIndex].concat(shuffledIndicesExcluding(startFromIndex));
+		if (this.#shuffle) {
+			if (this.#startFromIndex !== null) {
+				nextIndices = [this.#startFromIndex].concat(this.#shuffledIndicesExcluding(this.#startFromIndex));
 			} else {
-				nextIndices = shuffledIndices();
+				nextIndices = this.#shuffledIndices();
 			}
 			// if the next index ended up to be tha same as the pervious one, flip
 			// it to the end of the order
-			if (playlist.length > 1 && _.last(playOrder) == _.first(nextIndices)) {
-				nextIndices = wrapIndexToStart(nextIndices, 1);
+			if (this.#playlist.length > 1 && _.last(this.#playOrder) == _.first(nextIndices)) {
+				nextIndices = this.#wrapIndexToStart(nextIndices, 1);
 			}
 		}
 		else {
-			nextIndices = _.range(playlist.length);
-			if (startFromIndex !== null) {
-				nextIndices = wrapIndexToStart(nextIndices, startFromIndex);
+			nextIndices = _.range(this.#playlist.length);
+			if (this.#startFromIndex !== null) {
+				nextIndices = this.#wrapIndexToStart(nextIndices, this.#startFromIndex);
 			}
 		}
 
-		playOrder = playOrder.concat(nextIndices);
-		prevShuffleState = shuffle;
+		this.#playOrder = this.#playOrder.concat(nextIndices);
+		this.#prevShuffleState = this.#shuffle;
 	}
 
 	// drop the planned play order but preserve the history
-	function dropFuturePlayOrder() : void {
-		playOrder = _.take(playOrder, playOrderIter + 1);
+	#dropFuturePlayOrder() : void {
+		this.#playOrder = _.take(this.#playOrder, this.#playOrderIter + 1);
 	}
 
-	function insertMany(hostArray : number[], targetIndex : number, insertedItems : number[]) : void {
+	#insertMany(hostArray : number[], targetIndex : number, insertedItems : number[]) : void {
 		hostArray.splice.apply(hostArray, [targetIndex, 0].concat(insertedItems));
 	}
 
-	return {
-		setShuffle(state : boolean) : void {
-			shuffle = state;
-		},
-		setRepeat(state : boolean) : void {
-			repeat = state;
-		},
-		getCurrentIndex() : number|null {
-			return (playOrderIter >= 0) ? playOrder[playOrderIter] : null;
-		},
-		getCurrentPlaylistId() : string|null {
-			return playlistId;
-		},
-		getCurrentPlaylist() : PlaylistEntry[]|null {
-			return playlist;
-		},
-		jumpToPrevTrack() : PlaylistEntry|null {
-			if (playlist && playOrderIter > 0) {
-				--playOrderIter;
-				let track = playlist[this.getCurrentIndex()];
-				this.publish('trackChanged', track);
-				return track;
-			}
-			return null;
-		},
-		jumpToNextTrack() : PlaylistEntry|null {
-			if (playlist === null || playOrder === null) {
-				return null;
-			}
+	/* -------------------------------
+	 * PUBLIC METHODS
+	 * -------------------------------
+	 */
 
-			// check if shuffle state has changed after the play order was last updated
-			if (shuffle != prevShuffleState) {
-				dropFuturePlayOrder();
-				startFromIndex = playOrder[playOrderIter];
-				playOrder = _.initial(playOrder); // drop also current index as it will be readded on next step
-				enqueueIndices();
-			}
+	setShuffle(state : boolean) : void {
+		this.#shuffle = state;
+	}
 
-			++playOrderIter;
+	setRepeat(state : boolean) : void {
+		this.#repeat = state;
+	}
 
-			// check if we have run to the end of the enqueued tracks
-			if (playOrderIter >= playOrder.length) {
-				if (repeat) { // start another round
-					enqueueIndices();
-				} else { // we are done
-					this.clearPlaylist();
-					return null;
-				}
-			}
+	getCurrentIndex() : number|null {
+		return (this.#playOrderIter >= 0) ? this.#playOrder[this.#playOrderIter] : null;
+	}
 
-			let track = playlist[this.getCurrentIndex()];
+	getCurrentPlaylistId() : string|null {
+		return this.#playlistId;
+	}
+
+	getCurrentPlaylist() : PlayQueueEntry[]|null {
+		return this.#playlist;
+	}
+
+	jumpToPrevTrack() : PlayQueueEntry|null {
+		if (this.#playlist && this.#playOrderIter > 0) {
+			--this.#playOrderIter;
+			let track = this.#playlist[this.getCurrentIndex()];
 			this.publish('trackChanged', track);
 			return track;
-		},
-		peekNextTrack() : PlaylistEntry|null {
-			// The next track may be peeked only when there are forthcoming tracks already enqueued, not when jumping
-			// to the next track would start a new round in the Repeat mode
-			if (playlist === null || playOrder === null || playOrderIter < 0 || playOrderIter >= playOrder.length - 1) {
-				return null;
-			} else {
-				return playlist[playOrder[playOrderIter + 1]];
-			}
-		},
-		setPlaylist(listId : string, pl : PlaylistEntry[], startIndex : number|null = null) : void {
-			playlist = pl.slice(); // copy
-			startFromIndex = startIndex;
-			if (listId === playlistId) {
-				// preserve the history if list wasn't actually changed
-				dropFuturePlayOrder();
-			} else {
-				// drop the history if list changed
-				playOrder = [];
-				playOrderIter = -1; // jumpToNextTrack will move this to first valid index
-				playlistId = listId;
-				this.publish('playlistChanged', playlistId);
-			}
-			enqueueIndices();
-		},
-		clearPlaylist() : void {
-			playOrderIter = -1;
-			playlist = null;
-			playlistId = null;
-			this.publish('playlistEnded');
-		},
-		onPlaylistModified(pl : PlaylistEntry[], currentIndex : number) : void {
-			let currentTrack = playlist[this.getCurrentIndex()];
-			// check if the track being played is still available in the list
-			if (pl[currentIndex] === currentTrack) {
-				// re-init the play-order, erasing any history data
-				playlist = pl.slice(); // copy
-				startFromIndex = currentIndex;
-				playOrder = [];
-				enqueueIndices();
-				playOrderIter = 0;
-			}
-			// if not, then we no longer have a valid list position
-			else {
-				playlist = null;
-				playlistId = null;
-				playOrder = null;
-				playOrderIter = -1;
-			}
-			this.publish('trackChanged', currentTrack);
-		},
-		onTracksAdded(newTracks : PlaylistEntry[]) : void {
-			let prevListSize = playlist.length;
-			playlist = playlist.concat(newTracks);
-			let newIndices = _.range(prevListSize, playlist.length);
-			if (prevShuffleState) {
-				// Shuffle the new tracks with the remaining tracks on the list
-				let remaining = _.drop(playOrder, playOrderIter+1);
-				remaining = _.shuffle(remaining.concat(newIndices));
-				playOrder = _.take(playOrder, playOrderIter+1).concat(remaining);
-			}
-			else {
-				// Try to find the next position of the previously last track of the list,
-				// and insert the new tracks in play order after that. If the index is not
-				// found, then we have already wrapped over the last track and the new tracks
-				// do not need to be added.
-				let insertPos = _.indexOf(playOrder, prevListSize-1, playOrderIter);
-				if (insertPos >= 0) {
-					++insertPos;
-					insertMany(playOrder, insertPos, newIndices);
-				}
-			}
-		},
-		publish(name : string, ...args : any[]) : void {
-			eventDispatcher.trigger(name, ...args);
-		},
-		subscribe(name : string, listener : (...args: any[]) => any, context : any) : void {
-			// the context must be supplied if there is ever a need to unsubscribe
-			eventDispatcher.on(name, listener, context);
-		},
-		unsubscribeAll(context : any) {
-			eventDispatcher.off(null, null, context);
 		}
-	};
-}]);
+		return null;
+	}
+
+	jumpToNextTrack() : PlayQueueEntry|null {
+		if (this.#playlist === null || this.#playOrder === null) {
+			return null;
+		}
+
+		// check if shuffle state has changed after the play order was last updated
+		if (this.#shuffle != this.#prevShuffleState) {
+			this.#dropFuturePlayOrder();
+			this.#startFromIndex = this.#playOrder[this.#playOrderIter];
+			this.#playOrder = _.initial(this.#playOrder); // drop also current index as it will be re-added on next step
+			this.#enqueueIndices();
+		}
+
+		++this.#playOrderIter;
+
+		// check if we have run to the end of the enqueued tracks
+		if (this.#playOrderIter >= this.#playOrder.length) {
+			if (this.#repeat) { // start another round
+				this.#enqueueIndices();
+			} else { // we are done
+				this.clearPlaylist();
+				return null;
+			}
+		}
+
+		let track = this.#playlist[this.getCurrentIndex()];
+		this.publish('trackChanged', track);
+		return track;
+	}
+
+	peekNextTrack() : PlayQueueEntry|null {
+		// The next track may be peeked only when there are forthcoming tracks already enqueued, not when jumping
+		// to the next track would start a new round in the Repeat mode
+		if (this.#playlist === null || this.#playOrder === null || this.#playOrderIter < 0 || this.#playOrderIter >= this.#playOrder.length - 1) {
+			return null;
+		} else {
+			return this.#playlist[this.#playOrder[this.#playOrderIter + 1]];
+		}
+	}
+
+	setPlaylist(listId : string, pl : PlayQueueEntry[], startIndex : number|null = null) : void {
+		this.#playlist = pl.slice(); // copy
+		this.#startFromIndex = startIndex;
+		if (listId === this.#playlistId) {
+			// preserve the history if list wasn't actually changed
+			this.#dropFuturePlayOrder();
+		} else {
+			// drop the history if list changed
+			this.#playOrder = [];
+			this.#playOrderIter = -1; // jumpToNextTrack will move this to first valid index
+			this.#playlistId = listId;
+			this.publish('playlistChanged', this.#playlistId);
+		}
+		this.#enqueueIndices();
+	}
+
+	clearPlaylist() : void {
+		this.#playOrderIter = -1;
+		this.#playlist = null;
+		this.#playlistId = null;
+		this.publish('playlistEnded');
+	}
+
+	onPlaylistModified(pl : PlayQueueEntry[], currentIndex : number) : void {
+		let currentTrack = this.#playlist[this.getCurrentIndex()];
+		// check if the track being played is still available in the list
+		if (pl[currentIndex] === currentTrack) {
+			// re-init the play-order, erasing any history data
+			this.#playlist = pl.slice(); // copy
+			this.#startFromIndex = currentIndex;
+			this.#playOrder = [];
+			this.#enqueueIndices();
+			this.#playOrderIter = 0;
+		}
+		// if not, then we no longer have a valid list position
+		else {
+			this.#playlist = null;
+			this.#playlistId = null;
+			this.#playOrder = null;
+			this.#playOrderIter = -1;
+		}
+		this.publish('trackChanged', currentTrack);
+	}
+
+	onTracksAdded(newTracks : PlayQueueEntry[]) : void {
+		let prevListSize = this.#playlist.length;
+		this.#playlist = this.#playlist.concat(newTracks);
+		let newIndices = _.range(prevListSize, this.#playlist.length);
+		if (this.#prevShuffleState) {
+			// Shuffle the new tracks with the remaining tracks on the list
+			let remaining = _.drop(this.#playOrder, this.#playOrderIter+1);
+			remaining = _.shuffle(remaining.concat(newIndices));
+			this.#playOrder = _.take(this.#playOrder, this.#playOrderIter+1).concat(remaining);
+		}
+		else {
+			// Try to find the next position of the previously last track of the list,
+			// and insert the new tracks in play order after that. If the index is not
+			// found, then we have already wrapped over the last track and the new tracks
+			// do not need to be added.
+			let insertPos = _.indexOf(this.#playOrder, prevListSize-1, this.#playOrderIter);
+			if (insertPos >= 0) {
+				++insertPos;
+				this.#insertMany(this.#playOrder, insertPos, newIndices);
+			}
+		}
+	}
+
+	publish(name : string, ...args : any[]) : void {
+		this.#eventDispatcher.trigger(name, ...args);
+	}
+
+	subscribe(name : string, listener : (...args: any[]) => any, context : any) : void {
+		// the context must be supplied if there is ever a need to unsubscribe
+		this.#eventDispatcher.on(name, listener, context);
+	}
+
+	unsubscribeAll(context : any) {
+		this.#eventDispatcher.off(null, null, context);
+	}
+}
+
+ng.module('Music').service('playlistService', [PlayQueueService]);

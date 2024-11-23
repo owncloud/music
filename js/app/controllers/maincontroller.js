owncloud/music

commit 9c43c9cbdce5111d8ea7c97b2f21996f3d75028a

Keep old track IDs when moving files within the library
Our previous FileHooks implementation was such that moving a scanned audio
file from on folder within the library to another folder within the library
caused the track object matching the said file to be deleted and the
recreated. Hence, the track got a new ID and it got removed from any
playlists where it belonged to.

Now we handle file moving separately and differently than a file deletion
followed by file recreation, and the track gets updated "in place", without
changing the ID or removing it from existing playlists.

If, on the other hand, an entire folder full of scanned audio files gets
moved, then we do one of two things:
1) In case there's 30 or less audio files contained, we handle each file
separately the same way as if those 30 files were moved individually
2) In case there are more than 30 audio files contained, we remove them all
from the library and the user needs to rescan the files.

The second case is suboptimal but we can't handle any number of files
one-by-one during the moving because that could slow down the moving
operation too much. In the best case this would annoy the user and in the
worst case it would cause a time-out and break the entire operation.

This is a partial fix for https://github.com/owncloud/music/issues/1173.

lib/Utility/Scanner.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2016 - 2023
+ * @copyright Pauli Järvinen 2016 - 2024
  */
 
 namespace OCA\Music\Utility;
@@ -83,25 +83,71 @@ class Scanner extends PublicEmitter {
 	 * Gets called by 'post_write' (file creation, file update) and 'post_share' hooks
 	 */
 	public function update(File $file, string $userId, string $filePath) : void {
-		// debug logging
-		$this->logger->log("update - $filePath", 'debug');
+		$mimetype = $file->getMimeType();
+		$this->logger->log("update - $filePath - $mimetype", 'debug');
 
-		// skip files that aren't inside the user specified path
 		if (!$this->librarySettings->pathBelongsToMusicLibrary($filePath, $userId)) {
 			$this->logger->log("skipped - file is outside of specified music folder", 'debug');
-			return;
 		}
-
-		$mimetype = $file->getMimeType();
-
-		// debug logging
-		$this->logger->log("update - mimetype $mimetype", 'debug');
-
-		if (Util::startsWith($mimetype, 'image')) {
+		elseif (Util::startsWith($mimetype, 'image')) {
 			$this->updateImage($file, $userId);
-		} elseif (Util::startsWith($mimetype, 'audio') && !self::isPlaylistMime($mimetype)) {
+		}
+		elseif (Util::startsWith($mimetype, 'audio') && !self::isPlaylistMime($mimetype)) {
 			$libraryRoot = $this->librarySettings->getFolder($userId);
 			$this->updateAudio($file, $userId, $libraryRoot, $filePath, $mimetype, /*partOfScan=*/false);
+		}
+	}
+
+	public function fileMoved(File $file, string $userId) : void {
+		$mimetype = $file->getMimeType();
+		$this->logger->log('fileMoved - '. $file->getPath() . " - $mimetype", 'debug');
+
+		if (Util::startsWith($mimetype, 'image')) {
+			// we don't need to track the identity of images and moving a file can be handled as it was 
+			// a file deletion followed by a file addition
+			$this->deleteImage([$file->getId()], [$userId]);
+			$this->updateImage($file, $userId);
+		}
+		elseif (Util::startsWith($mimetype, 'audio') && !self::isPlaylistMime($mimetype)) {
+			if ($this->librarySettings->pathBelongsToMusicLibrary($file->getPath(), $userId)) {
+				// In the new path, the file (now or still) belongs to the library. Even if it was already in the lib,
+				// the new path may have an influence on the album or artist name (in case of incomplete metadata).
+				$libraryRoot = $this->librarySettings->getFolder($userId);
+				$this->updateAudio($file, $userId, $libraryRoot, $file->getPath(), $mimetype, /*partOfScan=*/false);
+			} else {
+				// In the new path, the file doesn't (still or any longer) belong to the library. Remove it if
+				// it happened to be in the library.
+				$this->deleteAudio([$file->getId()], [$userId]);
+			}
+		}
+	}
+
+	public function folderMoved(Folder $folder, string $userId) : void {
+		$this->logger->log('folderMoved - '. $folder->getPath(), 'debug');
+
+		$audioFiles = $folder->searchByMime('audio');
+
+		if (\count($audioFiles) > 0) {
+			if ($this->librarySettings->pathBelongsToMusicLibrary($folder->getPath(), $userId)) {
+				// The new path of the folder belongs to the library but this doesn't necessarily mean
+				// that all the file paths below belong to the library, because of the path exclusions.
+				// Each file needs to be checked and updated separately but this may take too much time
+				// if there is extensive number of files.
+				if (\count($audioFiles) <= 30) {
+					foreach ($audioFiles as $file) {
+						$this->fileMoved($file, $userId);
+					}
+				} else {
+					// Remove the scanned files to get them rescanned when the Music app is opened.
+					// TODO: Better handling e.g. by marking the files as dirty.
+					$this->deleteAudio(Util::extractIds($audioFiles), [$userId]);	
+				}
+			}
+			else {
+				// The new path of the folder doesn't belong to the library so neither does any of the
+				// contained files. Remove audio files from the lib if found.
+				$this->deleteAudio(Util::extractIds($audioFiles), [$userId]);
+			}
 		}
 	}
 
@@ -172,7 +218,6 @@ class Scanner extends PublicEmitter {
 			$this->cache->remove($userId, 'collection');
 		}
 
-		// debug logging
 		$this->logger->log('imported entities - ' .
 				"artist: $artistId, albumArtist: $albumArtistId, album: $albumId, track: {$track->getId()}",
 				'debug');

lib/Hooks/FileHooks.php
@@ -31,9 +31,8 @@ class FileHooks {
 	 * Invoke auto update of music database after file or folder deletion
 	 * @param Node $node pointing to the file or folder
 	 */
-	public static function deleted(Node $node) {
-		$app = \OC::$server->query(Application::class);
-		$container = $app->getContainer();
+	public static function deleted(Node $node) : void {
+		$container = self::getContainer();
 		$scanner = $container->query('Scanner');
 
 		if ($node->getType() == FileInfo::TYPE_FILE) {
@@ -47,7 +46,7 @@ class FileHooks {
 	 * Invoke auto update of music database after file update or file creation
 	 * @param Node $node pointing to the file
 	 */
-	public static function updated(Node $node) {
+	public static function updated(Node $node) : void {
 		// At least on Nextcloud 13, it sometimes happens that this hook is triggered
 		// when the core creates some temporary file and trying to access the provided
 		// node throws an exception, probably because the temp file is already removed
@@ -56,8 +55,7 @@ class FileHooks {
 		// File::fopen, this hook gets triggered immediately after the opening succeeds,
 		// before anything is actually written and while the file is *exclusively locked
 		// because of the write mode*. See #638.
-		$app = \OC::$server->query(Application::class);
-		$container = $app->getContainer();
+		$container = self::getContainer();
 		try {
 			self::handleUpdated($node, $container);
 		} catch (\OCP\Files\NotFoundException $e) {
@@ -69,18 +67,11 @@ class FileHooks {
 		}
 	}
 
-	private static function handleUpdated(Node $node, IAppContainer $container) {
+	private static function handleUpdated(Node $node, IAppContainer $container) : void {
 		// we are interested only about updates on files, not on folders
 		if ($node->getType() == FileInfo::TYPE_FILE) {
 			$scanner = $container->query('Scanner');
-			$userId = $container->query('UserId');
-
-			// When a file is uploaded to a folder shared by link, we end up here without current user.
-			// In that case, fall back to using file owner
-			if (empty($userId)) {
-				$owner = $node->getOwner();
-				$userId = $owner ? $owner->getUID() : null; // @phpstan-ignore-line At least some versions of NC may violate their PhpDoc and return null owner
-			}
+			$userId = self::getUser($node, $container);
 
 			// Ignore event if we got no user or folder or the user has not yet scanned the music
 			// collection. The last condition is especially to prevent problems when creating new user
@@ -91,39 +82,75 @@ class FileHooks {
 		}
 	}
 
+	private static function moved(Node $node) : void {
+		$container = self::getContainer();
+		try {
+			self::handleMoved($node, $container);
+		} catch (\OCP\Files\NotFoundException $e) {
+			$logger = $container->query('Logger');
+			$logger->log('FileHooks::moved triggered for a non-existing file', 'warn');
+		} catch (\OCP\Lock\LockedException $e) {
+			$logger = $container->query('Logger');
+			$logger->log('FileHooks::moved triggered for a locked file ' . $node->getName(), 'warn');
+		}
+	}
+
+	private static function handleMoved(Node $node, IAppContainer $container) : void {
+		$scanner = $container->query('Scanner');
+		$userId = self::getUser($node, $container);
+
+		if (!empty($userId) && self::userHasMusicLib($userId, $container)) {
+			if ($node->getType() == FileInfo::TYPE_FILE) {
+				$scanner->fileMoved($node, $userId);
+			} else {
+				$scanner->folderMoved($node, $userId);
+			}
+		}
+	}
+
+	private static function getUser(Node $node, IAppContainer $container) : ?string {
+		$userId = $container->query('UserId');
+
+		// When a file is uploaded to a folder shared by link, we end up here without current user.
+		// In that case, fall back to using file owner
+		if (empty($userId)) {
+			// At least some versions of NC may violate their PhpDoc and return null owner, hence we need to aid PHPStan a bit about the type.
+			/** @var \OCP\IUser|null $owner */
+			$owner = $node->getOwner();
+			$userId = $owner ? $owner->getUID() : null;
+		}
+
+		return $userId;
+	}
+
+	private static function getContainer() : IAppContainer {
+		$app = \OC::$server->query(Application::class);
+		return $app->getContainer();
+	}
+
 	/**
 	 * Check if user has any scanned tracks in his/her music library
 	 * @param string $userId
 	 * @param IAppContainer $container
 	 */
-	private static function userHasMusicLib(string $userId, IAppContainer $container) {
+	private static function userHasMusicLib(string $userId, IAppContainer $container) : bool {
 		$trackBusinessLayer = $container->query('TrackBusinessLayer');
 		return 0 < $trackBusinessLayer->count($userId);
 	}
 
-	public static function preRenamed(Node $source, Node $target) {
-		// We are interested only if the path of the folder of the file changes:
-		// that could move a music file out of the scanned folder or remove a
-		// cover image file from album folder.
-		if ($source->getParent()->getId() != $target->getParent()->getId()) {
-			self::deleted($source);
-			// $target here doesn't point to an existing file, hence we need also
-			// the postRenamed hook
-		}
-	}
+	public static function postRenamed(Node $source, Node $target) : void {
+		// Beware: the $source describes the past state of the file and some of its functions will throw upon calling
 
-	public static function postRenamed(Node $source, Node $target) {
-		// Renaming/moving file may
-		// a) move it into the folder scanned by Music app
-		// b) have influence on the display name of the track
-		// Both of these cases can be handled like file addition/modification.
-		self::updated($target);
+		if ($source->getParent()->getId() != $target->getParent()->getId()) {
+			self::moved($target);
+		} else {
+			self::updated($target);
+		}
 	}
 
 	public function register() {
 		$this->filesystemRoot->listen('\OC\Files', 'postWrite', [__CLASS__, 'updated']);
 		$this->filesystemRoot->listen('\OC\Files', 'preDelete', [__CLASS__, 'deleted']);
-		$this->filesystemRoot->listen('\OC\Files', 'preRename', [__CLASS__, 'preRenamed']);
 		$this->filesystemRoot->listen('\OC\Files', 'postRename', [__CLASS__, 'postRenamed']);
 	}
 }

commit ce9a6615474f696969a015bdf79eccf8e71d0999

Prompt to rescan "dirty" files when Music app loaded
- If the user's library contains any files potentially needing rescanning,
  this is now prompted to the user upon app loading, similarly as any new
  files to scan

- The rescanning of modified files, both with the new prompt and using
  `occ music:scan --rescan-modified`, can now also remove files from the
  library if the modified file turns out to be within an excluded folder

- When moving folder within the music library, and that folder contains more
  than 15 audio files, those files are now marked as "dirty" to trigger the
  mechanism described above. Previously, up to 30 files were rescanned
  immediately and over 30 files were removed from the library, which was
  sub-optimal.

- This completes the fix for https://github.com/owncloud/music/issues/1173
  which was started with the commit 9c43c9cbd. This also provides a partial
  fix for the old issue https://github.com/owncloud/music/issues/706.

appinfo/database.xml
@@ -312,6 +312,13 @@
 				<type>timestamp</type>
 				<notnull>false</notnull>
 			</field>
+			<field>
+				<name>dirty</name>
+				<type>integer</type>
+				<length>1</integer>
+				<notnull>true</notnull>
+				<default>0</default>
+			</field>
 
 			<index>
 				<name>music_tracks_artist_id_idx</name>

js/app/controllers/maincontroller.js
@@ -176,7 +176,7 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 			// check the availability of unscanned files after the collection has been loaded,
 			// unless we are already in the middle of scanning (and intermediate results were just loaded)
 			if (!$scope.scanning) {
-				$scope.updateFilesToScan();
+				updateFilesToScan();
 			}
 		},
 		function(response) { // error handling
@@ -228,22 +228,18 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 	$scope.updateRadio();
 	$scope.updatePodcasts();
 
-	let FILES_TO_SCAN_PER_STEP = 10;
+	const FILES_TO_SCAN_PER_STEP = 10;
 	let filesToScan = null;
-	let filesToScanIterator = 0;
-	let previouslyScannedCount = 0;
+	$scope.unscannedFiles = null;
+	$scope.dirtyFiles = null;
 
-	$scope.updateFilesToScan = function() {
+	function updateFilesToScan() {
 		$scope.checkingUnscanned = true;
 		Restangular.one('scanstate').get().then(function(state) {
 			$scope.checkingUnscanned = false;
-			previouslyScannedCount = state.scannedCount;
-			filesToScan = state.unscannedFiles;
-			filesToScanIterator = 0;
-			$scope.toScan = (filesToScan.length > 0);
-			$scope.scanningScanned = previouslyScannedCount;
-			$scope.scanningTotal = previouslyScannedCount + filesToScan.length;
-			$scope.noMusicAvailable = ($scope.scanningTotal === 0);
+			$scope.unscannedFiles = state.unscannedFiles;
+			$scope.dirtyFiles = state.dirtyFiles;
+			$scope.noMusicAvailable = (state.scannedCount + state.unscannedFiles.length === 0);
 		},
 		function(error) {
 			$scope.checkingUnscanned = false;
@@ -251,11 +247,11 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 					gettextCatalog.getString('Failed to check for new audio files (error {{ code }}); check the server logs for details', {code: error.status})
 			);
 		});
-	};
+	}
 
 	function processNextScanStep() {
-		let sliceEnd = filesToScanIterator + FILES_TO_SCAN_PER_STEP;
-		let filesForStep = filesToScan.slice(filesToScanIterator, sliceEnd);
+		let sliceEnd = $scope.scanningScanned + FILES_TO_SCAN_PER_STEP;
+		let filesForStep = filesToScan.slice($scope.scanningScanned, sliceEnd);
 		let params = {
 				files: filesForStep.join(','),
 				finalize: sliceEnd >= filesToScan.length
@@ -264,15 +260,13 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 			// Ignore the results if scanning has been cancelled while we
 			// were waiting for the result.
 			if ($scope.scanning) {
-				filesToScanIterator = sliceEnd;
+				$scope.scanningScanned = sliceEnd;
 
 				if (result.filesScanned || result.albumCoversUpdated) {
 					$scope.updateAvailable = true;
 				}
 
-				$scope.scanningScanned = previouslyScannedCount + filesToScanIterator;
-
-				if (filesToScanIterator < filesToScan.length) {
+				if ($scope.scanningScanned < filesToScan.length) {
 					processNextScanStep();
 				} else {
 					$scope.scanning = false;
@@ -289,15 +283,16 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 		});
 	}
 
-	$scope.startScanning = function(fileIds = null) {
-		if (fileIds) {
-			filesToScan = fileIds;
-			previouslyScannedCount = 0;
-			$scope.scanningScanned = 0;
-			$scope.scanningTotal = fileIds.length;
-		}
+	$scope.startScanning = function(fileIds) {
+		filesToScan = fileIds;
+		$scope.scanningScanned = 0;
+		$scope.scanningTotal = filesToScan.length;
 
-		$scope.toScan = false;
+		if (fileIds == $scope.unscannedFiles) {
+			$scope.unscannedFiles = null;
+		} else if (fileIds == $scope.dirtyFiles) {
+			$scope.dirtyFiles = null;
+		}
 		$scope.scanning = true;
 		processNextScanStep();
 	};
@@ -307,10 +302,9 @@ function ($rootScope, $scope, $timeout, $window, ArtistFactory,
 	};
 
 	$scope.resetScanned = function() {
-		$scope.toScan = false;
+		$scope.unscannedFiles = null;
+		$scope.dirtyFiles = null;
 		filesToScan = null;
-		filesToScanIterator = 0;
-		previouslyScannedCount = 0;
 		// Genre and artist IDs have got invalidated while resetting the library, drop any related filters
 		if ($scope.smartListParams !== null) {
 			$scope.smartListParams.genres = [];

lib/BusinessLayer/TrackBusinessLayer.php
@@ -118,15 +118,17 @@ class TrackBusinessLayer extends BusinessLayer {
 	}
 
 	/**
-	 * Returns all tracks where the 'modified' time in the file system (actually in the cloud's file cache)
-	 * is later than the 'updated' field of the entity in the database.
+	 * Returns all tracks of the user which should be rescanned to ensure that the library details are up-to-date.
+	 * The track may be considered "dirty" for on of two reasons:
+	 * - its 'modified' time in the file system (actually in the cloud's file cache) is later than the 'updated' field of the entity in the database
+	 * - it has been specifically marked as dirty, maybe in response to being moved to another directory
 	 * @return Track[]
 	 */
 	public function findAllDirty(string $userId) : array {
 		$tracks = $this->findAll($userId);
 		return \array_filter($tracks, function (Track $track) {
 			$dbModTime = new \DateTime($track->getUpdated());
-			return ($dbModTime->getTimestamp() < $track->getFileModTime());
+			return ($track->getDirty() || $dbModTime->getTimestamp() < $track->getFileModTime());
 		});
 	}
 
@@ -363,11 +365,12 @@ class TrackBusinessLayer extends BusinessLayer {
 		$track->setUserId($userId);
 		$track->setLength($length);
 		$track->setBitrate($bitrate);
+		$track->setDirty(0);
 		return $this->mapper->insertOrUpdate($track);
 	}
 
 	/**
-	 * Deletes a track
+	 * Deletes tracks
 	 * @param int[] $fileIds file IDs of the tracks to delete
 	 * @param string[]|null $userIds the target users; if omitted, the tracks matching the
 	 *                      $fileIds are deleted from all users
@@ -435,5 +438,20 @@ class TrackBusinessLayer extends BusinessLayer {
 		}
 
 		return $result;
+	}
+
+	/**
+	 * Marks tracks as dirty, ultimately requesting the user to rescan them
+	 * @param int[] $fileIds file IDs of the tracks to mark as dirty
+	 * @param string[]|null $userIds the target users; if omitted, the tracks matching the
+	 *                      $fileIds are marked for all users
+	 */
+	public function markTracksDirty(array $fileIds, ?array $userIds=null) : void {
+		// be prepared for huge number of file IDs
+		$chunkMaxSize = self::MAX_SQL_ARGS - \count($userIds ?? []);
+		$idChunks = \array_chunk($fileIds, $chunkMaxSize);
+		foreach ($idChunks as $idChunk) {
+			$this->mapper->markTracksDirty($idChunk, $userIds);
+		}
 	}
 }

lib/Command/Scan.php
@@ -76,6 +76,7 @@ class Scan extends BaseCommand {
 	protected function doExecute(InputInterface $input, OutputInterface $output, array $users) : void {
 		if (!$input->getOption('debug')) {
 			$this->scanner->listen(Scanner::class, 'update', fn($path) => $output->writeln("Scanning <info>$path</info>"));
+			$this->scanner->listen(Scanner::class, 'exclude', fn($path) => $output->writeln("!! Removing <info>$path</info>"));
 		}
 
 		if ($input->getOption('rescan') && $input->getOption('rescan-modified')) {

lib/Controller/ApiController.php
@@ -196,6 +196,7 @@ class ApiController extends Controller {
 	public function getScanState() {
 		return new JSONResponse([
 			'unscannedFiles' => $this->scanner->getUnscannedMusicFileIds($this->userId),
+			'dirtyFiles' => $this->scanner->getDirtyMusicFileIds($this->userId),
 			'scannedCount' => $this->trackBusinessLayer->count($this->userId)
 		]);
 	}

lib/Db/Track.php
@@ -51,6 +51,8 @@ use OCP\IURLGenerator;
  * @method void setPlayCount(int $count)
  * @method ?string getLastPlayed()
  * @method void setLastPlayed(?string $timestamp)
+ * @method int getDirty()
+ * @method void setDirty(int $dirty)
  *
  * @method string getFilename()
  * @method int getSize()
@@ -77,6 +79,7 @@ class Track extends Entity {
 	public $genreId;
 	public $playCount;
 	public $lastPlayed;
+	public $dirty;
 
 	// not from the music_tracks table but still part of the standard content of this entity:
 	public $filename;

lib/Db/TrackMapper.php
@@ -120,16 +120,12 @@ class TrackMapper extends BaseMapper {
 	}
 
 	/**
-	 * @param string $userId
 	 * @return int[]
 	 */
 	public function findAllFileIds(string $userId) : array {
 		$sql = 'SELECT `file_id` FROM `*PREFIX*music_tracks` WHERE `user_id` = ?';
 		$result = $this->execute($sql, [$userId]);
-
-		return \array_map(function ($i) {
-			return (int)$i['file_id'];
-		}, $result->fetchAll());
+		return $result->fetchAll(\PDO::FETCH_COLUMN);
 	}
 
 	/**
@@ -469,6 +465,28 @@ class TrackMapper extends BaseMapper {
 		$params = [$timeOfPlay->format(BaseMapper::SQL_DATE_FORMAT), $userId, $trackId];
 		$result = $this->execute($sql, $params);
 		return ($result->rowCount() > 0);
+	}
+
+	/**
+	 * Marks tracks as dirty, ultimately requesting the user to rescan them
+	 * @param int[] $fileIds file IDs of the tracks to mark as dirty
+	 * @param string[]|null $userIds the target users; if omitted, the tracks matching the
+	 *                      $fileIds are marked for all users
+	 * @return int number of rows affected
+	 */
+	public function markTracksDirty(array $fileIds, ?array $userIds=null) : int {
+		$sql = 'UPDATE `*PREFIX*music_tracks`
+				SET `dirty` = 1
+				WHERE `file_id` IN ' . $this->questionMarks(\count($fileIds));
+		$params = $fileIds;
+
+		if (!empty($userIds)) {
+			$sql .= ' AND `user_id` IN ' . $this->questionMarks(\count($userIds));
+			$params = \array_merge($params, $userIds);
+		}
+
+		$result = $this->execute($sql, $params);
+		return $result->rowCount();
 	}
 
 	/**

lib/Migration/Version020100Date20241114220300.php
@@ -1,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Music\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Migrate the DB schema to Music v2.1.0 level from the v1.9.1 level
+ */
+class Version020100Date20241114220300 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		$this->addDirtyFieldToTrack($schema);
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * Add the new field 'dirty' to the table 'music_tracks'
+	 */
+	private function addDirtyFieldToTrack(ISchemaWrapper $schema) {
+		$table = $schema->getTable('music_tracks');
+		$this->setColumn($table, 'dirty', 'smallint', ['notnull' => true, 'default' => 0]);
+	}
+
+	private function setColumn($table, string $name, string $type, array $args) : void {
+		if (!$table->hasColumn($name)) {
+			$table->addColumn($name, $type, $args);
+		}
+	}
+}

lib/Utility/Scanner.php
@@ -131,17 +131,16 @@ class Scanner extends PublicEmitter {
 			if ($this->librarySettings->pathBelongsToMusicLibrary($folder->getPath(), $userId)) {
 				// The new path of the folder belongs to the library but this doesn't necessarily mean
 				// that all the file paths below belong to the library, because of the path exclusions.
-				// Each file needs to be checked and updated separately but this may take too much time
-				// if there is extensive number of files.
-				if (\count($audioFiles) <= 30) {
+				// Each file needs to be checked and updated separately.
+				if (\count($audioFiles) <= 15) {
 					foreach ($audioFiles as $file) {
 						\assert($file instanceof File); // a clue for PHPStan
 						$this->fileMoved($file, $userId);
 					}
 				} else {
-					// Remove the scanned files to get them rescanned when the Music app is opened.
-					// TODO: Better handling e.g. by marking the files as dirty.
-					$this->deleteAudio(Util::extractIds($audioFiles), [$userId]);	
+					// There are too many files to handle them now as we don't want to delay the move operation
+					// too much. The user will be prompted to rescan the files upon opening the Music app.
+					$this->trackBusinessLayer->markTracksDirty(Util::extractIds($audioFiles), [$userId]);
 				}
 			}
 			else {
@@ -562,6 +561,10 @@ class Scanner extends PublicEmitter {
 		$count = 0;
 		foreach ($fileIds as $fileId) {
 			$file = $libraryRoot->getById($fileId)[0] ?? null;
+			if ($file != null && !$this->librarySettings->pathBelongsToMusicLibrary($file->getPath(), $userId)) {
+				$this->emit(self::class, 'exclude', [$file->getPath()]);
+				$file = null;
+			}
 			if ($file instanceof File) {
 				$memBefore = $debugOutput ? \memory_get_usage(true) : 0;
 				$this->updateAudio($file, $userId, $libraryRoot, $file->getPath(), $file->getMimetype(), /*partOfScan=*/true);
@@ -575,7 +578,8 @@ class Scanner extends PublicEmitter {
 				}
 				$count++;
 			} else {
-				$this->logger->log("File with id $fileId not found for user $userId", 'warn');
+				$this->logger->log("File with id $fileId not found for user $userId, removing it from the library if present", 'info');
+				$this->deleteAudio([$fileId], [$userId]);
 			}
 		}
 

templates/main.php
@@ -48,11 +48,19 @@ HtmlUtil::addWebpackStyle('app');
 				</div>
 			</div>
 
-			<div id="toScan" class="emptycontent clickable" ng-show="toScan && viewingLibrary()" ng-click="startScanning()">
+			<div id="toScan" class="emptycontent clickable" ng-show="!scanning && unscannedFiles.length && viewingLibrary()" ng-click="startScanning(unscannedFiles)">
 				<div class="icon-audio svg"></div>
 				<div>
 					<h2 translate>New music available</h2>
 					<p translate>Click here to start the scan</p>
+				</div>
+			</div>
+
+			<div id="toRescan" class="emptycontent clickable" ng-show="!scanning && !unscannedFiles.length && dirtyFiles.length && viewingLibrary()" ng-click="startScanning(dirtyFiles)">
+				<div class="icon-audio svg"></div>
+				<div>
+					<h2 translate>Some of the previously scanned files may have changed</h2>
+					<p translate>Click here to rescan these files</p>
 				</div>
 			</div>
 
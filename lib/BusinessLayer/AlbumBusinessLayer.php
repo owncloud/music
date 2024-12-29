owncloud/music

commit aebccd309b38dc7ddc0ef59fac95244ef7c29259

Add new mode "Podcasts" to the dashboard widget
To make this work, we had to add also the field `art` to the Ampache
results of the type `podcast_episode`. Also `has_art` was added on the
same go to better match the latest API specification.

lib/Controller/AmpacheController.php
@@ -2067,8 +2067,12 @@ class AmpacheController extends Controller {
 	 * @param PodcastEpisode[] $episodes
 	 */
 	private function renderPodcastEpisodes(array $episodes) : array {
+		$createImageUrl = function(PodcastEpisode $episode) : string {
+			return $this->createAmpacheActionUrl('get_art', $episode->getChannelId(), 'podcast');
+		};
+
 		return [
-			'podcast_episode' => Util::arrayMapMethod($episodes, 'toAmpacheApi')
+			'podcast_episode' => Util::arrayMapMethod($episodes, 'toAmpacheApi', [$createImageUrl])
 		];
 	}
 

lib/Db/PodcastEpisode.php
@@ -112,7 +112,8 @@ class PodcastEpisode extends Entity {
 		];
 	}
 
-	public function toAmpacheApi() : array {
+	public function toAmpacheApi(callable $createImageUrl) : array {
+		$imageUrl = $createImageUrl($this);
 		return [
 			'id' => (string)$this->getId(),
 			'name' => $this->getTitle(),
@@ -131,6 +132,8 @@ class PodcastEpisode extends Entity {
 			'size' => $this->getSize(),
 			'mime' => $this->getMimetype(),
 			'url' => $this->getStreamUrl(),
+			'art' => $imageUrl,
+			'has_art' => !empty($imageUrl),
 			'flag' => !empty($this->getStarred()),
 			'rating' => $this->getRating() ?? 0,
 			'preciserating' => $this->getRating() ?? 0,

js/dashboard/musicwidget.ts
@@ -49,6 +49,7 @@ export class MusicWidget {
 			{ id: 'genres',			name: t('music', 'Genres') },
 			{ id: 'all_tracks',		name: t('music', 'All tracks') },
 			{ id: 'playlists',		name: t('music', 'Playlists') },
+			{ id: 'podcasts',		name: t('music', 'Podcasts') },
 		];
 		const placeholder = t('music', 'Select mode');
 		this.#modeSelect = createSelect(types, placeholder).appendTo(this.#selectContainer).on('change', () => this.#onModeSelect());
@@ -73,9 +74,14 @@ export class MusicWidget {
 			if (track !== null) {
 				const $albumArt = this.#controls.find('.albumart');
 				this.#loadBackgroundImage($albumArt, track.art);
-				$albumArt.prop('title', `${track.name} (${track.artist.name})`);
+				if ('artist' in track) {
+					$albumArt.prop('title', `${track.name} (${track.artist.name})`);
+					this.#player.fromUrl(track.url, track.stream_mime);
+				} else {
+					$albumArt.prop('title', track.name);
+					this.#player.fromExtUrl(track.url, false);
+				}
 
-				this.#player.fromUrl(track.url, track.stream_mime);
 				this.#player.play();
 			}
 		}, 300);
@@ -146,6 +152,9 @@ export class MusicWidget {
 				break;
 			case 'playlists':
 				this.#showPlaylists();
+				break;
+			case 'podcasts':
+				this.#showPodcasts();
 				break;
 			default:
 				console.error('unexpected mode selection:', this.#modeSelect.val());
@@ -246,6 +255,26 @@ export class MusicWidget {
 		});
 	}
 
+	#showPodcasts() : void {
+		ampacheApiAction('list', { type: 'podcast' }, (result: any) => {
+			this.#parent1Select = createSelect(
+				result.list,
+				t('music', 'Select channel')
+			).appendTo(this.#selectContainer);
+
+			this.#parent1Select.on('change', () => {
+				this.#trackList?.remove();
+				const channelId = this.#parent1Select.val() as string;
+
+				this.#trackListContainer.addClass('icon-loading');
+				ampacheApiAction('podcast_episodes', {filter: channelId}, (result: any) => {
+					this.#listTracks('podcast-' + channelId, result.podcast_episode, channelId);
+					this.#trackListContainer.removeClass('icon-loading');
+				});
+			});
+		});
+	}
+
 	#ampacheLoadAndShowTracks(action: string, args: JQuery.PlainObject, listId: string, parentId: string|null) {
 		this.#trackListContainer.addClass('icon-loading');
 		ampacheApiAction(action, args, (result: any) => {
@@ -324,7 +353,7 @@ function createTrackList(tracks: any[], parentId: string|null) : JQuery<HTMLULis
 	$(tracks).each(function(index: number) {
 		let liContent = this.name;
 		let tooltip = this.name;
-		if (this.artist.id != parentId) {
+		if ('artist' in this && this.artist.id != parentId) {
 			liContent += ` <span class="dimmed">(${this.artist.name})</span>`;
 			tooltip += ` (${this.artist.name})`
 		}

commit cc4d5a293ed401bdf8f33b52515987ce09639dbc

Refactoring + bug fix for Ampache action `podcasts` with `include=episodes`
- Replace everywhere our own helper Util::arrayMapMethod with the use of
  \array_map with an arrow function. The syntax is about as long but using the
  built-in language syntax constructs allows VSCode and PHPStan to follow the
  logic, enabling them to flag any errors. This is made evident by the next
  point.

- There had been a bug in the Ampache API since the commit aebccd30 which
  caused an unhandled exception if the action `podcast` or `podcasts` was
  called with the argument `include=episodes` This was because PodcastChannel
  class called PodcastEpisode::toAmpacheApi() without passing the required
  parameters. No analyzer could pick this up because the call was made
  dynamically using Util::arrayMapMethod.

lib/BusinessLayer/AlbumBusinessLayer.php
@@ -350,7 +350,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 * @param int|null $coverFileId the file id of the cover image
 	 * @param int $albumId the id of the album to be modified
 	 */
-	public function setCover(?int $coverFileId, int $albumId) {
+	public function setCover(?int $coverFileId, int $albumId) : void {
 		$this->mapper->setCover($coverFileId, $albumId);
 	}
 
@@ -408,7 +408,7 @@ class AlbumBusinessLayer extends BusinessLayer {
 	 * Given an array of Track objects, inject the corresponding Album object to each of them
 	 * @param Track[] $tracks (in|out)
 	 */
-	public function injectAlbumsToTracks(array &$tracks, string $userId) {
+	public function injectAlbumsToTracks(array &$tracks, string $userId) : void {
 		$albumIds = [];
 
 		// get unique album IDs

lib/Controller/AmpacheController.php
@@ -2128,8 +2128,10 @@ class AmpacheController extends ApiController {
 		$includeArtists = ($this->jsonMode && $this->apiMajorVersion() > 5);
 
 		return [
-			'song' => Util::arrayMapMethod($tracks, 'toAmpacheApi', 
-				[$this->l10n, $createPlayUrl, $createImageUrl, $renderRef, $genreKey, $includeArtists])
+			'song' => \array_map(
+				fn($t) => $t->toAmpacheApi($this->l10n, $createPlayUrl, $createImageUrl, $renderRef, $genreKey, $includeArtists),
+				$tracks
+			)
 		];
 	}
 
@@ -2146,7 +2148,7 @@ class AmpacheController extends ApiController {
 		};
 
 		$result = [
-			'playlist' => Util::arrayMapMethod($playlists, 'toAmpacheApi', [$createImageUrl, $includeTracks])
+			'playlist' => \array_map(fn($p) => $p->toAmpacheApi($createImageUrl, $includeTracks), $playlists)
 		];
 
 		// annoyingly, the structure of the included tracks is quite different in JSON compared to XML
@@ -2164,7 +2166,7 @@ class AmpacheController extends ApiController {
 	 */
 	private function renderPodcastChannels(array $channels) : array {
 		return [
-			'podcast' => Util::arrayMapMethod($channels, 'toAmpacheApi')
+			'podcast' => \array_map(fn($c) => $c->toAmpacheApi(), $channels)
 		];
 	}
 
@@ -2173,10 +2175,10 @@ class AmpacheController extends ApiController {
 	 */
 	private function renderPodcastEpisodes(array $episodes) : array {
 		return [
-			'podcast_episode' => Util::arrayMapMethod($episodes, 'toAmpacheApi', [
+			'podcast_episode' => \array_map(fn($e) => $e->toAmpacheApi(
 				fn($episode) => $this->createAmpacheActionUrl('get_art', $episode->getChannelId(), 'podcast'),
 				fn($episode) => $this->createAmpacheActionUrl('stream', $episode->getId(), 'podcast_episode')
-			])
+			), $episodes)
 		];
 	}
 
@@ -2187,7 +2189,7 @@ class AmpacheController extends ApiController {
 		$createImageUrl = fn(RadioStation $station) => $this->createAmpacheActionUrl('get_art', $station->getId(), 'live_stream');
 
 		return [
-			'live_stream' => Util::arrayMapMethod($stations, 'toAmpacheApi', [$createImageUrl])
+			'live_stream' => \array_map(fn($s) => $s->toAmpacheApi($createImageUrl), $stations)
 		];
 	}
 
@@ -2196,7 +2198,7 @@ class AmpacheController extends ApiController {
 	 */
 	private function renderTags(array $genres) : array {
 		return [
-			'tag' => Util::arrayMapMethod($genres, 'toAmpacheApi', [$this->l10n])
+			'tag' => \array_map(fn($g) => $g->toAmpacheApi($this->l10n), $genres)
 		];
 	}
 
@@ -2205,7 +2207,7 @@ class AmpacheController extends ApiController {
 	 */
 	private function renderGenres(array $genres) : array {
 		return [
-			'genre' => Util::arrayMapMethod($genres, 'toAmpacheApi', [$this->l10n])
+			'genre' => \array_map(fn($g) => $g->toAmpacheApi($this->l10n), $genres)
 		];
 	}
 
@@ -2224,7 +2226,7 @@ class AmpacheController extends ApiController {
 		}
 
 		return [
-			'bookmark' => Util::arrayMapMethod($bookmarks, 'toAmpacheApi', [$renderEntry])
+			'bookmark' => \array_map(fn($b) => $b->toAmpacheApi($renderEntry), $bookmarks)
 		];
 	}
 

lib/Controller/PlaylistApiController.php
@@ -87,7 +87,7 @@ class PlaylistApiController extends Controller {
 	 */
 	public function getAll() {
 		$playlists = $this->playlistBusinessLayer->findAll($this->userId);
-		return Util::arrayMapMethod($playlists, 'toAPI', [$this->urlGenerator]);
+		return \array_map(fn($p) => $p->toAPI($this->urlGenerator), $playlists);
 	}
 
 	/**
@@ -152,7 +152,7 @@ class PlaylistApiController extends Controller {
 
 		$result = $playlist->toAPI($this->urlGenerator);
 		unset($result['trackIds']);
-		$result['tracks'] = Util::arrayMapMethod($tracks, 'toAPI', [$this->urlGenerator]);
+		$result['tracks'] = \array_map(fn($t) => $t->toAPI($this->urlGenerator), $tracks);
 
 		return $result;
 	}

lib/Controller/PodcastApiController.php
@@ -57,7 +57,7 @@ class PodcastApiController extends Controller {
 	 */
 	public function getAll() {
 		$channels = $this->podcastService->getAllChannels($this->userId, /*$includeEpisodes=*/ true);
-		return Util::arrayMapMethod($channels, 'toApi', ['urlGenerator' => $this->urlGenerator]);
+		return \array_map(fn($c) => $c->toApi($this->urlGenerator), $channels);
 	}
 
 	/**

lib/Controller/RadioApiController.php
@@ -76,7 +76,7 @@ class RadioApiController extends Controller {
 	 */
 	public function getAll() {
 		$stations = $this->businessLayer->findAll($this->userId);
-		return Util::arrayMapMethod($stations, 'toApi');
+		return \array_map(fn($s) => $s->toApi(), $stations);
 	}
 
 	/**
@@ -202,7 +202,7 @@ class RadioApiController extends Controller {
 		}
 		try {
 			$result = $this->playlistFileService->importRadioStationsFromFile($this->userId, $this->userFolder, $filePath);
-			$result['stations'] = Util::arrayMapMethod($result['stations'], 'toApi');
+			$result['stations'] = \array_map(fn($s) => $s->toApi(), $result['stations']);
 			return $result;
 		} catch (\OCP\Files\NotFoundException $ex) {
 			return new ErrorResponse(Http::STATUS_NOT_FOUND, 'playlist file not found');

lib/Controller/SubsonicController.php
@@ -599,7 +599,7 @@ class SubsonicController extends Controller {
 		}
 
 		return $this->subsonicResponse(['playlists' =>
-			['playlist' => Util::arrayMapMethod($playlists, 'toSubsonicApi')]
+			['playlist' => \array_map(fn($p) => $p->toSubsonicApi(), $playlists)]
 		]);
 	}
 
@@ -925,7 +925,7 @@ class SubsonicController extends Controller {
 
 		return $this->subsonicResponse([
 			'podcasts' => [
-				'channel' => Util::arrayMapMethod($channels, 'toSubsonicApi')
+				'channel' => \array_map(fn($c) => $c->toSubsonicApi(), $channels)
 			]
 		]);
 	}
@@ -938,7 +938,7 @@ class SubsonicController extends Controller {
 
 		return $this->subsonicResponse([
 			'newestPodcasts' => [
-				'episode' => Util::arrayMapMethod($episodes, 'toSubsonicApi')
+				'episode' => \array_map(fn($e) => $e->toSubsonicApi(), $episodes)
 			]
 		]);
 	}
@@ -1338,7 +1338,7 @@ class SubsonicController extends Controller {
 			'directory' => [
 				'id' => $id,
 				'name' => $channel->getTitle(),
-				'child' => Util::arrayMapMethod($channel->getEpisodes() ?? [], 'toSubsonicApi')
+				'child' => \array_map(fn($e) => $e->toSubsonicApi(), $channel->getEpisodes() ?? [])
 			]
 		]);
 	}
@@ -1432,7 +1432,7 @@ class SubsonicController extends Controller {
 	 */
 	private function tracksToApi(array $tracks) : array {
 		$this->albumBusinessLayer->injectAlbumsToTracks($tracks, $this->user());
-		return Util::arrayMapMethod($tracks, 'toSubsonicApi', [$this->l10n, $this->ignoredArticles]);
+		return \array_map(fn($t) => $t->toSubsonicApi($this->l10n, $this->ignoredArticles), $tracks);
 	}
 
 	/**

lib/Db/PodcastChannel.php
@@ -97,7 +97,7 @@ class PodcastChannel extends Entity {
 		];
 
 		if ($this->episodes !== null) {
-			$result['episodes'] = Util::arrayMapMethod($this->episodes, 'toApi', ['urlGenerator' => $urlGenerator]);
+			$result['episodes'] = \array_map(fn($e) => $e->toApi($urlGenerator), $this->episodes);
 		}
 
 		return $result;
@@ -141,7 +141,8 @@ class PodcastChannel extends Entity {
 		];
 
 		if ($this->episodes !== null) {
-			$result['podcast_episode'] = Util::arrayMapMethod($this->episodes, 'toAmpacheApi');
+			$createImageUrl = fn($e) => $this->getImageUrl();
+			$result['podcast_episode'] = \array_map(fn($e) => $e->toAmpacheApi($createImageUrl, null), $this->episodes);
 		}
 
 		return $result;
@@ -160,7 +161,7 @@ class PodcastChannel extends Entity {
 		];
 
 		if ($this->episodes !== null) {
-			$result['episode'] = Util::arrayMapMethod($this->episodes, 'toSubsonicApi');
+			$result['episode'] = \array_map(fn($e) => $e->toSubsonicApi(), $this->episodes);
 		}
 
 		return $result;

lib/Db/PodcastEpisode.php
@@ -113,7 +113,7 @@ class PodcastEpisode extends Entity {
 		];
 	}
 
-	public function toAmpacheApi(callable $createImageUrl, callable $createStreamUrl) : array {
+	public function toAmpacheApi(callable $createImageUrl, ?callable $createStreamUrl) : array {
 		$imageUrl = $createImageUrl($this);
 		return [
 			'id' => (string)$this->getId(),
@@ -132,7 +132,7 @@ class PodcastEpisode extends Entity {
 			'time' => $this->getDuration(),
 			'size' => $this->getSize(),
 			'mime' => $this->getMimetype(),
-			'url' => $createStreamUrl($this),
+			'url' => $createStreamUrl ? $createStreamUrl($this) : $this->getStreamUrl(),
 			'art' => $imageUrl,
 			'has_art' => !empty($imageUrl),
 			'flag' => !empty($this->getStarred()),

lib/Db/Track.php
@@ -265,6 +265,8 @@ class Track extends Entity {
 	 * The same API format is used both on "old" and "new" API methods. The "new" API adds some
 	 * new fields for the songs, but providing some extra fields shouldn't be a problem for the
 	 * older clients. The $track entity must have the Album reference injected prior to calling this.
+	 * 
+	 * @param string[] $ignoredArticles
 	 */
 	public function toSubsonicApi(IL10N $l10n, array $ignoredArticles) : array {
 		$albumId = $this->getAlbumId();

lib/Utility/Scanner.php
@@ -460,7 +460,7 @@ class Scanner extends PublicEmitter {
 		return $this->trackBusinessLayer->findAllFileIds($userId);
 	}
 
-	private function getMusicFolder(string $userId, ?string $path) {
+	private function getMusicFolder(string $userId, ?string $path) : Folder {
 		$folder = $this->librarySettings->getFolder($userId);
 
 		if (!empty($path)) {
@@ -532,7 +532,7 @@ class Scanner extends PublicEmitter {
 	 */
 	public function getDirtyMusicFileIds(string $userId, ?string $path = null) : array {
 		$tracks = $this->trackBusinessLayer->findAllDirty($userId);
-		$fileIds = Util::arrayMapMethod($tracks, 'getFileId');
+		$fileIds = \array_map(fn($t) => $t->getFileId(), $tracks);
 
 		// filter by path if given
 		if (!empty($path)) {

lib/Utility/Util.php
@@ -24,19 +24,11 @@ class Util {
 	const SINT32_MIN = -self::SINT32_MAX - 1;
 
 	/**
-	 * Map the given array by calling a named member function for each of the array elements
-	 */
-	public static function arrayMapMethod(array $arr, string $methodName, array $methodArgs=[]) : array {
-		$func = fn($obj) => \call_user_func_array([$obj, $methodName], $methodArgs);
-		return \array_map($func, $arr);
-	}
-
-	/**
 	 * Extract ID of each array element by calling getId and return
 	 * the IDs as an array
 	 */
 	public static function extractIds(array $arr) : array {
-		return self::arrayMapMethod($arr, 'getId');
+		return \array_map(fn($i) => $i->getId(), $arr);
 	}
 
 	/**
@@ -44,7 +36,7 @@ class Util {
 	 * the IDs as an array
 	 */
 	public static function extractUserIds(array $arr) : array {
-		return self::arrayMapMethod($arr, 'getUserId');
+		return \array_map(fn($i) => $i->getUserId(), $arr);
 	}
 
 	/**
owncloud/music

commit dce4bea89cd12edd894ce5ffd9885bcad699225b

Subsonic: Use the Ampache image route for `artistImageUrl`
The property `artistImageUrl` used to point to the route `getCoverArt` of
the Subsonic API but now we point it to the Ampache image route instead. This
has two benefits:

- We don't need to include the user api password into the response

- This works also when the user has been authenticated using the argument
  `apiKey` instead of `u` and `p`

lib/Db/AmpacheUserMapper.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2018 - 2024
+ * @copyright Pauli Järvinen 2018 - 2025
  */
 
 namespace OCA\Music\Db;
@@ -57,10 +57,10 @@ class AmpacheUserMapper {
 
 	/**
 	 * @param string $hash Password hash
-	 * @return ?string User ID matching the $hash or null if not found
+	 * @return ?array like ['key_id' => int, 'user_id' => string] or null if not found
 	 */
-	public function getUserByPasswordHash(string $hash) : ?string {
-		$sql = 'SELECT `user_id` FROM `*PREFIX*music_ampache_users` WHERE `hash` = ?';
+	public function getUserByPasswordHash(string $hash) : ?array {
+		$sql = 'SELECT `id`, `user_id` FROM `*PREFIX*music_ampache_users` WHERE `hash` = ?';
 		$params = [$hash];
 		$result = $this->db->executeQuery($sql, $params);
 		$row = $result->fetch();
@@ -69,7 +69,10 @@ class AmpacheUserMapper {
 			return null;
 		}
 
-		return $row['user_id'];
+		return [
+			'key_id' => $row['id'],
+			'user_id' => $row['user_id']
+		];
 	}
 
 	/**
@@ -114,7 +117,7 @@ class AmpacheUserMapper {
 	}
 
 	/**
-	 * @return ?int ID of the added key on null on failure (which is highly unexpected)
+	 * @return ?int ID of the added key or null on failure (which is highly unexpected)
 	 */
 	public function addUserKey(string $userId, string $hash, ?string $description) : ?int {
 		$sql = 'INSERT INTO `*PREFIX*music_ampache_users`

lib/AppInfo/Application.php
@@ -368,6 +368,7 @@ class Application extends ApplicationBase {
 				$c->query('DetailsHelper'),
 				$c->query('LastfmService'),
 				$c->query('PodcastService'),
+				$c->query('AmpacheImageService'),
 				$c->query('Random'),
 				$c->query('Logger')
 			);

lib/Controller/AmpacheImageController.php
@@ -7,7 +7,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2023, 2024
+ * @copyright Pauli Järvinen 2023 - 2025
  */
 
 namespace OCA\Music\Controller;
@@ -65,7 +65,7 @@ class AmpacheImageController extends Controller {
 	 * @NoSameSiteCookieRequired
 	 * @CORS
 	 */
-	public function image(?string $token, ?string $object_id, string $object_type='album') : Response {
+	public function image(?string $token, ?string $object_id, string $object_type='album', ?int $size=null) : Response {
 		if ($token === null) {
 			// Workaround for Ample client which uses this kind of call to get the placeholder graphics
 			return new FileResponse(PlaceholderImage::generateForResponse('?', $object_type, 200));
@@ -78,18 +78,18 @@ class AmpacheImageController extends Controller {
 
 		$businessLayer = $this->getBusinessLayer($object_type);
 		if ($businessLayer === null) {
-			return new ErrorResponse(Http::STATUS_NOT_FOUND, "invalid object_type $object_type");	
+			return new ErrorResponse(Http::STATUS_NOT_FOUND, "invalid object_type $object_type");
 		}
 
 		try {
 			$entity = $businessLayer->find((int)$object_id, $userId);
 		} catch (BusinessLayerException $e) {
-			return new ErrorResponse(Http::STATUS_NOT_FOUND, "$object_type $object_id not found");	
+			return new ErrorResponse(Http::STATUS_NOT_FOUND, "$object_type $object_id not found");
 		}
 
-		$coverImage = $this->coverHelper->getCover($entity, $userId, $this->librarySettings->getFolder($userId));
+		$coverImage = $this->coverHelper->getCover($entity, $userId, $this->librarySettings->getFolder($userId), $size);
 		if ($coverImage === null) {
-			return new ErrorResponse(Http::STATUS_NOT_FOUND, "$object_type $object_id has no cover image");		
+			return new ErrorResponse(Http::STATUS_NOT_FOUND, "$object_type $object_id has no cover image");
 		}
 
 		return new FileResponse($coverImage);

lib/Controller/SubsonicController.php
@@ -7,7 +7,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2019 - 2024
+ * @copyright Pauli Järvinen 2019 - 2025
  */
 
 namespace OCA\Music\Controller;
@@ -54,6 +54,7 @@ use OCA\Music\Http\XmlResponse;
 
 use OCA\Music\Middleware\SubsonicException;
 
+use OCA\Music\Utility\AmpacheImageService;
 use OCA\Music\Utility\AppInfo;
 use OCA\Music\Utility\CoverHelper;
 use OCA\Music\Utility\DetailsHelper;
@@ -85,9 +86,11 @@ class SubsonicController extends Controller {
 	private DetailsHelper $detailsHelper;
 	private LastfmService $lastfmService;
 	private PodcastService $podcastService;
+	private AmpacheImageService $imageService;
 	private Random $random;
 	private Logger $logger;
 	private ?string $userId;
+	private ?int $keyId;
 	private array $ignoredArticles;
 	private string $format;
 	private ?string $callback;
@@ -111,6 +114,7 @@ class SubsonicController extends Controller {
 								DetailsHelper $detailsHelper,
 								LastfmService $lastfmService,
 								PodcastService $podcastService,
+								AmpacheImageService $imageService,
 								Random $random,
 								Logger $logger) {
 		parent::__construct($appname, $request);
@@ -132,9 +136,11 @@ class SubsonicController extends Controller {
 		$this->detailsHelper = $detailsHelper;
 		$this->lastfmService = $lastfmService;
 		$this->podcastService = $podcastService;
+		$this->imageService = $imageService;
 		$this->random = $random;
 		$this->logger = $logger;
 		$this->userId = null;
+		$this->keyId = null;
 		$this->ignoredArticles = [];
 		$this->format = 'xml'; // default, should be immediately overridden by SubsonicMiddleware
 	}
@@ -151,10 +157,10 @@ class SubsonicController extends Controller {
 
 	/**
 	 * Called by the middleware once the user credentials have been checked
-	 * @param string $userId
 	 */
-	public function setAuthenticatedUser(string $userId) {
+	public function setAuthenticatedUser(string $userId, int $keyId) {
 		$this->userId = $userId;
+		$this->keyId = $keyId;
 		$this->ignoredArticles = $this->librarySettings->getIgnoredArticles($userId);
 	}
 
@@ -1364,7 +1370,7 @@ class SubsonicController extends Controller {
 
 		if (!empty($artist->getCoverFileId())) {
 			$result['coverArt'] = $result['id'];
-			$result['artistImageUrl'] = $this->artistImageUrl($result['id']);
+			$result['artistImageUrl'] = $this->artistImageUrl($id);
 		}
 
 		return $result;
@@ -1541,7 +1547,7 @@ class SubsonicController extends Controller {
 
 			$artist = $this->artistBusinessLayer->find($artistId, $userId);
 			if ($artist->getCoverFileId() !== null) {
-				$content['largeImageUrl'] = [$this->artistImageUrl('artist-' . $artistId)];
+				$content['largeImageUrl'] = [$this->artistImageUrl($artistId)];
 			}
 		}
 
@@ -1736,11 +1742,10 @@ class SubsonicController extends Controller {
 		return \count($genreArr) ? $genreArr[0] : null;
 	}
 
-	private function artistImageUrl(string $id) : string {
-		$par = $this->request->getParams();
-		return $this->urlGenerator->linkToRouteAbsolute('music.subsonic.handleRequest', ['method' => 'getCoverArt'])
-			. "?u={$par['u']}&p={$par['p']}&v={$par['v']}&c={$par['c']}&id=$id&size=" . CoverHelper::DO_NOT_CROP_OR_SCALE;
-		// Note: Using DO_NOT_CROP_OR_SCALE (-1) as size is our proprietary extension and not part of the Subsonic API
+	private function artistImageUrl(int $id) : string {
+		$token = $this->imageService->getToken('artist', $id, $this->keyId);
+		return $this->urlGenerator->linkToRouteAbsolute('music.ampacheImage.image',
+			['object_type' => 'artist', 'object_id' => $id, 'token' => $token, 'size' => CoverHelper::DO_NOT_CROP_OR_SCALE]);
 	}
 
 	/**

lib/Middleware/SubsonicMiddleware.php
@@ -7,7 +7,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2019 - 2024
+ * @copyright Pauli Järvinen 2019 - 2025
  */
 
 namespace OCA\Music\Middleware;
@@ -98,9 +98,9 @@ class SubsonicMiddleware extends Middleware {
 			throw new SubsonicException('Multiple conflicting authentication mechanisms provided', 43);
 		}
 		else if ($apiKey !== null) {
-			$user = $this->userForPass($apiKey);
-			if ($user !== null) {
-				$controller->setAuthenticatedUser($user);
+			$credentials = $this->userAndKeyIdForPass($apiKey);
+			if ($credentials !== null) {
+				$controller->setAuthenticatedUser($credentials['user_id'], $credentials['key_id']);
 			} else {
 				throw new SubsonicException('Invalid API key', 44);
 			}
@@ -116,9 +116,9 @@ class SubsonicMiddleware extends Middleware {
 				$pass = \hex2bin(\substr($pass, \strlen('enc:')));
 			}
 
-			$user = $this->userMapper->getProperUserId($user);
-			if ($user !== null && $this->userForPass($pass) == $user) {
-				$controller->setAuthenticatedUser($user);
+			$credentials = $this->userAndKeyIdForPass($pass);
+			if (Util::stringCaseCompare($user, $credentials['user_id']) === 0) {
+				$controller->setAuthenticatedUser($credentials['user_id'], $credentials['key_id']);
 			} else {
 				throw new SubsonicException('Wrong username or password', 40);
 			}
@@ -131,9 +131,9 @@ class SubsonicMiddleware extends Middleware {
 
 	/**
 	 * @param string $pass Password aka API key
-	 * @return string User ID or null if the $pass was not valid
+	 * @return ?array like ['key_id' => int, 'user_id' => string] or null if $pass was not valid
 	 */
-	private function userForPass(string $pass) : ?string {
+	private function userAndKeyIdForPass(string $pass) : ?array {
 		$hash = \hash('sha256', $pass);
 		return $this->userMapper->getUserByPasswordHash($hash);
 	}

commit 25ce8e2bc650f550aff6d24045b5adcc2ae3ec6c

Fix Subsonic API not working on PHP versions 7.4 and 8.0
The database integration seems to work a bit differently on PHP 8.0 and older
compared to 8.1 and newer: When calling
\Doctrine\DBAL\Driver\Statement::fetch, the newer PHP versions seem to cast
the integer-typed DB column values automatically to ints while older versions
return them as strings. As we later assume the values to be ints, an explicit
cast was added.

The type mismatch prevented entirely the use of the Subsonic API on PHP 8.0
and older. This bug has got introduced by the commit dce4bea8 first released
in Music v2.1.3.

refs https://github.com/owncloud/music/issues/1218

lib/Db/AmpacheUserMapper.php
@@ -70,7 +70,7 @@ class AmpacheUserMapper {
 		}
 
 		return [
-			'key_id' => $row['id'],
+			'key_id' => (int)$row['id'],
 			'user_id' => $row['user_id']
 		];
 	}
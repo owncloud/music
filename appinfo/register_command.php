3d44537a6dc0178f0702d5258a04738c958d0deb
Pauli Järvinen <pauli.jarvinen@gmail.com>
2025-07-27 23:07:02 +0300

    Autoload the dependencies when registering commands for occ

--- appinfo/register_command.php
+++ appinfo/register_command.php
@@ -14,83 +14,23 @@
  * @copyright Pauli Järvinen 2017 - 2025
  */
 
-use OCA\Music\AppInfo\Application;
-use OCA\Music\BusinessLayer\PlaylistBusinessLayer;
-use OCA\Music\BusinessLayer\PodcastChannelBusinessLayer;
-use OCA\Music\BusinessLayer\PodcastEpisodeBusinessLayer;
-use OCA\Music\Db\Cache;
-use OCA\Music\Db\Maintenance;
-use OCA\Music\Service\PlaylistFileService;
-use OCA\Music\Service\PodcastService;
-use OCA\Music\Service\Scanner;
-use OCP\Files\IMimeTypeLoader;
-use OCP\Files\IRootFolder;
-use OCP\IGroupManager;
-use OCP\IUserManager;
+namespace OCA\Music\Command;
 
+use OCA\Music\AppInfo\Application;
+
+/** @var Application $app */
 $app = \OC::$server->query(Application::class);
 $c = $app->getContainer();
 
-$application->add(new OCA\Music\Command\Scan(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(Scanner::class)
-));
-$application->add(new OCA\Music\Command\ResetDatabase(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(Maintenance::class)
-));
-$application->add(new OCA\Music\Command\ResetCache(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(Cache::class)
-));
-$application->add(new OCA\Music\Command\Cleanup(
-		$c->query(Maintenance::class)
-));
-$application->add(new OCA\Music\Command\RegisterMimeTypes(
-		$c->query(IMimeTypeLoader::class)
-));
-$application->add(new OCA\Music\Command\PodcastAdd(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(PodcastChannelBusinessLayer::class),
-		$c->query(PodcastEpisodeBusinessLayer::class)
-));
-$application->add(new OCA\Music\Command\PodcastExport(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(IRootFolder::class),
-		$c->query(PodcastService::class)
-));
-$application->add(new OCA\Music\Command\PodcastImport(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(IRootFolder::class),
-		$c->query(PodcastService::class)
-));
-$application->add(new OCA\Music\Command\PodcastReset(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(PodcastService::class)
-));
-$application->add(new OCA\Music\Command\PodcastUpdate(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(PodcastService::class)
-));
-$application->add(new OCA\Music\Command\PlaylistExport(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(IRootFolder::class),
-		$c->query(PlaylistBusinessLayer::class),
-		$c->query(PlaylistFileService::class)
-));
-$application->add(new OCA\Music\Command\PlaylistImport(
-		$c->query(IUserManager::class),
-		$c->query(IGroupManager::class),
-		$c->query(IRootFolder::class),
-		$c->query(PlaylistBusinessLayer::class),
-		$c->query(PlaylistFileService::class)
-));
+$application->add($c->query(Scan::class));
+$application->add($c->query(ResetDatabase::class));
+$application->add($c->query(ResetCache::class));
+$application->add($c->query(Cleanup::class));
+$application->add($c->query(RegisterMimeTypes::class));
+$application->add($c->query(PodcastAdd::class));
+$application->add($c->query(PodcastExport::class));
+$application->add($c->query(PodcastImport::class));
+$application->add($c->query(PodcastReset::class));
+$application->add($c->query(PodcastUpdate::class));
+$application->add($c->query(PlaylistExport::class));
+$application->add($c->query(PlaylistImport::class));

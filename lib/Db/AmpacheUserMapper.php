owncloud/music

commit 3bfbb7ecb58541b22e5f90794697a3993563bf72

Avoid "dirty table read" in AmpacheUserMapper::addUserKey
Writing to the DB and then immediately reading back the written row is a bad
practice since it may fail on very large installations with a distributed DB
server. In this particular case, we were interested only about the ID of the
added row and the DB read could be avoided altogether by using the function
IDBConnection::lastInsertId.

lib/Db/AmpacheUserMapper.php
@@ -123,19 +123,9 @@ class AmpacheUserMapper {
 		$sql = 'INSERT INTO `*PREFIX*music_ampache_users`
 				(`user_id`, `hash`, `description`) VALUES (?, ?, ?)';
 		$params = [$userId, $hash, $description];
-		$this->db->executeUpdate($sql, $params);
+		$affectedRows = $this->db->executeUpdate($sql, $params);
 
-		$sql = 'SELECT `id` FROM `*PREFIX*music_ampache_users`
-				WHERE `user_id` = ? AND `hash` = ?';
-		$params = [$userId, $hash];
-		$result = $this->db->executeQuery($sql, $params);
-		$row = $result->fetch();
-
-		if ($row === false) {
-			return null;
-		}
-
-		return (int)$row['id'];
+		return ($affectedRows > 0) ? $this->db->lastInsertId('*PREFIX*music_ampache_users') : null;
 	}
 
 	public function removeUserKey(string $userId, int $id) : void {

commit d4e59d8d21ddbd3d7d91709d7054e75925428f80

Fix Ampache API key creation showing an error on some environments
Although IDBConnection::lastInsertId should return int according to its
phpdoc, it seems to actually return a string on some environments. This was
reproduced at least on OC10.6, OC10.14, and NC20, all using sqlite3. This
doesn't seem to happen on the recent NC versions. This may be related to the
fact that https://www.php.net/manual/en/pdo.lastinsertid.php actually returns
a string.

This problem was introduced in 3bfbb7ec which has not yet been in any released
app version.

lib/Db/AmpacheUserMapper.php
@@ -125,7 +125,7 @@ class AmpacheUserMapper {
 		$params = [$userId, $hash, $description];
 		$affectedRows = $this->db->executeUpdate($sql, $params);
 
-		return ($affectedRows > 0) ? $this->db->lastInsertId('*PREFIX*music_ampache_users') : null;
+		return ($affectedRows > 0) ? (int)$this->db->lastInsertId('*PREFIX*music_ampache_users') : null;
 	}
 
 	public function removeUserKey(string $userId, int $id) : void {
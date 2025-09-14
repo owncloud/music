owncloud/music

commit 6e94d13c198e9437a27f85526053fbaad10e7fef

Add type declarations to Controller classes

lib/Controller/FavoritesController.php
@@ -7,12 +7,13 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2024
+ * @copyright Pauli Järvinen 2024, 2025
  */
 
 namespace OCA\Music\Controller;
 
 use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\IRequest;
 
@@ -23,6 +24,7 @@ use OCA\Music\BusinessLayer\PlaylistBusinessLayer;
 use OCA\Music\BusinessLayer\PodcastChannelBusinessLayer;
 use OCA\Music\BusinessLayer\PodcastEpisodeBusinessLayer;
 use OCA\Music\BusinessLayer\TrackBusinessLayer;
+use OCA\Music\Http\ErrorResponse;
 
 class FavoritesController extends Controller {
 	private AlbumBusinessLayer $albumBusinessLayer;
@@ -59,7 +61,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function favorites() {
+	public function favorites() : JSONResponse {
 		return new JSONResponse([
 			'tracks' => $this->trackBusinessLayer->findAllStarredIds($this->userId),
 			'albums' => $this->albumBusinessLayer->findAllStarredIds($this->userId),
@@ -74,7 +76,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoriteTrack(int $id, $status) {
+	public function setFavoriteTrack(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->trackBusinessLayer, $id, $status);
 	}
 
@@ -82,7 +84,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoriteAlbum(int $id, $status) {
+	public function setFavoriteAlbum(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->albumBusinessLayer, $id, $status);
 	}
 
@@ -90,7 +92,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoriteArtist(int $id, $status) {
+	public function setFavoriteArtist(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->artistBusinessLayer, $id, $status);
 	}
 
@@ -98,7 +100,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoritePlaylist(int $id, $status) {
+	public function setFavoritePlaylist(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->playlistBusinessLayer, $id, $status);
 	}
 
@@ -106,7 +108,7 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoriteChannel(int $id, $status) {
+	public function setFavoriteChannel(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->podcastChannelBusinessLayer, $id, $status);
 	}
 
@@ -114,17 +116,21 @@ class FavoritesController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function setFavoriteEpisode(int $id, $status) {
+	public function setFavoriteEpisode(int $id, ?string $status) : JSONResponse {
 		return $this->setFavorite($this->podcastEpisodeBusinessLayer, $id, $status);
 	}
 
-	private function setFavorite(BusinessLayer $businessLayer, int $id, $status) {
-		$status = \filter_var($status, FILTER_VALIDATE_BOOLEAN);
-		if ($status) {
-			$businessLayer->setStarred([$id], $this->userId);
+	private function setFavorite(BusinessLayer $businessLayer, int $id, ?string $status) : JSONResponse {
+		if ($status === null) {
+			return new ErrorResponse(Http::STATUS_BAD_REQUEST, "argument 'status' is required");
 		} else {
-			$businessLayer->unsetStarred([$id], $this->userId);
+			$status = \filter_var($status, FILTER_VALIDATE_BOOLEAN);
+			if ($status) {
+				$businessLayer->setStarred([$id], $this->userId);
+			} else {
+				$businessLayer->unsetStarred([$id], $this->userId);
+			}
+			return new JSONResponse(['favorite' => ($status != 0)]);
 		}
-		return new JSONResponse(['favorite' => ($status != 0)]);
 	}
 }

lib/Controller/CoverApiController.php
@@ -27,6 +27,7 @@ use OCA\Music\BusinessLayer\ArtistBusinessLayer;
 use OCA\Music\BusinessLayer\PodcastChannelBusinessLayer;
 use OCA\Music\Db\Album;
 use OCA\Music\Db\Artist;
+use OCA\Music\Db\Entity;
 use OCA\Music\Db\PodcastChannel;
 use OCA\Music\Http\ErrorResponse;
 use OCA\Music\Http\FileResponse;
@@ -69,7 +70,7 @@ class CoverApiController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 */
-	public function albumCover(int $albumId, $originalSize, $coverToken) {
+	public function albumCover(int $albumId, ?string $originalSize, ?string $coverToken) : Response {
 		try {
 			$userId = $this->userId ?? $this->coverService->getUserForAccessToken($coverToken);
 			$album = $this->albumBusinessLayer->find($albumId, $userId);
@@ -84,7 +85,7 @@ class CoverApiController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 */
-	public function artistCover(int $artistId, $originalSize, $coverToken) {
+	public function artistCover(int $artistId, ?string $originalSize, ?string $coverToken) : Response {
 		try {
 			$userId = $this->userId ?? $this->coverService->getUserForAccessToken($coverToken);
 			$artist = $this->artistBusinessLayer->find($artistId, $userId);
@@ -99,7 +100,7 @@ class CoverApiController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 */
-	public function podcastCover(int $channelId, $originalSize, $coverToken) {
+	public function podcastCover(int $channelId, ?string $originalSize, ?string $coverToken) : Response {
 		try {
 			$userId = $this->userId ?? $this->coverService->getUserForAccessToken($coverToken);
 			$channel = $this->podcastChannelBusinessLayer->find($channelId, $userId);
@@ -114,7 +115,7 @@ class CoverApiController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 */
-	public function cachedCover(string $hash, ?string $coverToken) {
+	public function cachedCover(string $hash, ?string $coverToken) : Response {
 		try {
 			$userId = $this->userId ?? $this->coverService->getUserForAccessToken($coverToken);
 			$coverData = $this->coverService->getCoverFromCache($hash, $userId);
@@ -135,7 +136,7 @@ class CoverApiController extends Controller {
 	/**
 	 * @param Artist|Album|PodcastChannel $entity
 	 */
-	private function cover($entity, string $userId, $originalSize) {
+	private function cover(Entity $entity, string $userId, ?string $originalSize) : Response {
 		$originalSize = \filter_var($originalSize, FILTER_VALIDATE_BOOLEAN);
 		$userFolder = $this->rootFolder->getUserFolder($userId);
 

lib/Controller/LogController.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2020 - 2024
+ * @copyright Pauli Järvinen 2020 - 2025
  */
 
 namespace OCA\Music\Controller;
@@ -34,7 +34,7 @@ class LogController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function log($message) {
+	public function log(?string $message) : JSONResponse {
 		$this->logger->log('JS: ' . $message, 'debug');
 		return new JSONResponse(['success' => true]);
 	}

lib/Controller/MusicApiController.php
@@ -18,6 +18,7 @@ use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataDisplayResponse;
 use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http\Response;
 use OCP\IRequest;
 
 use OCA\Music\AppFramework\BusinessLayer\BusinessLayerException;
@@ -81,7 +82,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function prepareCollection() {
+	public function prepareCollection() : JSONResponse {
 		$hash = $this->collectionService->getCachedJsonHash();
 		if ($hash === null) {
 			// build the collection but ignore the data for now
@@ -101,7 +102,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function collection() {
+	public function collection() : DataDisplayResponse {
 		$collectionJson = $this->collectionService->getJson();
 		$response = new DataDisplayResponse($collectionJson);
 		$response->addHeader('Content-Type', 'application/json; charset=utf-8');
@@ -122,7 +123,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function folders() {
+	public function folders() : JSONResponse {
 		$musicFolder = $this->librarySettings->getFolder($this->userId);
 		$folders = $this->trackBusinessLayer->findAllFolders($this->userId, $musicFolder);
 		return new JSONResponse($folders);
@@ -132,7 +133,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function genres() {
+	public function genres() : JSONResponse {
 		$genres = $this->genreBusinessLayer->findAllWithTrackIds($this->userId);
 		$unscanned =  $this->trackBusinessLayer->findFilesWithoutScannedGenre($this->userId);
 		return new JSONResponse([
@@ -145,7 +146,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function trackByFileId(int $fileId) {
+	public function trackByFileId(int $fileId) : JSONResponse {
 		$track = $this->trackBusinessLayer->findByFileId($fileId, $this->userId);
 		if ($track !== null) {
 			return new JSONResponse($track->toCollection());
@@ -158,7 +159,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function getScanState() {
+	public function getScanState() : JSONResponse {
 		return new JSONResponse([
 			'unscannedFiles' => $this->scanner->getUnscannedMusicFileIds($this->userId),
 			'dirtyFiles' => $this->scanner->getDirtyMusicFileIds($this->userId),
@@ -171,7 +172,7 @@ class MusicApiController extends Controller {
 	 * @NoCSRFRequired
 	 * @UseSession to keep the session reserved while execution in progress
 	 */
-	public function scan(string $files, $finalize) {
+	public function scan(string $files, ?string $finalize) : JSONResponse {
 		// extract the parameters
 		$fileIds = \array_map('intval', \explode(',', $files));
 		$finalize = \filter_var($finalize, FILTER_VALIDATE_BOOLEAN);
@@ -197,7 +198,7 @@ class MusicApiController extends Controller {
 	 * @NoCSRFRequired
 	 * @UseSession to keep the session reserved while execution in progress
 	 */
-	public function resetScanned() {
+	public function resetScanned() : JSONResponse {
 		$this->maintenance->resetLibrary($this->userId);
 		return new JSONResponse(['success' => true]);
 	}
@@ -206,7 +207,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function download(int $fileId) {
+	public function download(int $fileId) : Response {
 		$nodes = $this->scanner->resolveUserFolder($this->userId)->getById($fileId);
 		$node = $nodes[0] ?? null;
 		if ($node instanceof \OCP\Files\File) {
@@ -220,7 +221,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function filePath(int $fileId) {
+	public function filePath(int $fileId) : JSONResponse {
 		$userFolder = $this->scanner->resolveUserFolder($this->userId);
 		$nodes = $userFolder->getById($fileId);
 		if (\count($nodes) == 0) {
@@ -236,7 +237,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function fileInfo(int $fileId) {
+	public function fileInfo(int $fileId) : JSONResponse {
 		$userFolder = $this->scanner->resolveUserFolder($this->userId);
 		$info = $this->scanner->getFileInfo($fileId, $this->userId, $userFolder);
 		if ($info) {
@@ -250,7 +251,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function fileDetails(int $fileId) {
+	public function fileDetails(int $fileId) : JSONResponse {
 		$userFolder = $this->scanner->resolveUserFolder($this->userId);
 		$details = $this->detailsService->getDetails($fileId, $userFolder);
 		if ($details) {
@@ -272,7 +273,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function fileLyrics(int $fileId, ?string $format) {
+	public function fileLyrics(int $fileId, ?string $format) : Response {
 		$userFolder = $this->scanner->resolveUserFolder($this->userId);
 		if ($format == 'plaintext') {
 			$lyrics = $this->detailsService->getLyricsAsPlainText($fileId, $userFolder);
@@ -292,7 +293,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function scrobble(int $trackId) {
+	public function scrobble(int $trackId) : JSONResponse {
 		try {
 			$this->trackBusinessLayer->recordTrackPlayed($trackId, $this->userId);
 			return new JSONResponse(['success' => true]);
@@ -305,7 +306,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function albumDetails(int $albumId) {
+	public function albumDetails(int $albumId) : JSONResponse {
 		try {
 			$info = $this->lastfmService->getAlbumInfo($albumId, $this->userId);
 			return new JSONResponse($info);
@@ -318,7 +319,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function artistDetails(int $artistId) {
+	public function artistDetails(int $artistId) : JSONResponse {
 		try {
 			$info = $this->lastfmService->getArtistInfo($artistId, $this->userId);
 			return new JSONResponse($info);
@@ -331,7 +332,7 @@ class MusicApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function similarArtists(int $artistId) {
+	public function similarArtists(int $artistId) : JSONResponse {
 		try {
 			$similar = $this->lastfmService->getSimilarArtists($artistId, $this->userId, /*includeNotPresent=*/true);
 			return new JSONResponse(\array_map(fn($artist) => [

lib/Controller/PageController.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2019 - 2023
+ * @copyright Pauli Järvinen 2019 - 2025
  */
 
 namespace OCA\Music\Controller;
@@ -33,7 +33,7 @@ class PageController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function index() {
+	public function index() : TemplateResponse {
 		$userLang = $this->l10n->getLanguageCode();
 		return new TemplateResponse($this->appName, 'main', ['lang' => $userLang]);
 	}

lib/Controller/PlaylistApiController.php
@@ -17,6 +17,7 @@ namespace OCA\Music\Controller;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http\Response;
 
 use OCP\Files\Folder;
 use OCP\IConfig;
@@ -86,13 +87,12 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function getAll(string $type = 'shiva') {
+	public function getAll(string $type = 'shiva') : JSONResponse {
 		$playlists = $this->playlistBusinessLayer->findAll($this->userId);
-		if ($type === 'shiva') {
-			return \array_map(fn($p) => $p->toShivaApi($this->urlGenerator), $playlists);
-		} else {
-			return \array_map(fn($p) => $p->toApi($this->urlGenerator), $playlists);
-		}
+		$result = ($type === 'shiva')
+			? \array_map(fn($p) => $p->toShivaApi($this->urlGenerator), $playlists)
+			: \array_map(fn($p) => $p->toApi($this->urlGenerator), $playlists);
+		return new JSONResponse($result);
 	}
 
 	/**
@@ -100,9 +100,11 @@ class PlaylistApiController extends Controller {
 	 *
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * 
+	 * @param string|int|null $trackIds
 	 */
-	public function create(string $name, /*mixed*/ $trackIds, ?string $comment=null) {
-		$playlist = $this->playlistBusinessLayer->create($name, $this->userId);
+	public function create(?string $name, /*mixed*/ $trackIds, ?string $comment=null) : JSONResponse {
+		$playlist = $this->playlistBusinessLayer->create($name ?? '', $this->userId);
 
 		// add trackIds and comment to the newly created playlist if provided
 		if (!empty($trackIds)) {
@@ -113,7 +115,7 @@ class PlaylistApiController extends Controller {
 			$playlist = $this->playlistBusinessLayer->setComment($comment, $playlist->getId(), $this->userId);
 		}
 
-		return $playlist->toApi($this->urlGenerator);
+		return new JSONResponse($playlist->toApi($this->urlGenerator));
 	}
 
 	/**
@@ -123,9 +125,9 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function delete(int $id) {
+	public function delete(int $id) : JSONResponse {
 		$this->playlistBusinessLayer->delete($id, $this->userId);
-		return [];
+		return new JSONResponse([]);
 	}
 
 	/**
@@ -135,7 +137,7 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function get(int $id, string $type = 'shiva', $fulltree = 'false') {
+	public function get(int $id, string $type = 'shiva', string $fulltree = 'false') : JSONResponse {
 		try {
 			$playlist = $this->playlistBusinessLayer->find($id, $this->userId);
 
@@ -151,7 +153,7 @@ class PlaylistApiController extends Controller {
 				$result['tracks'] = $this->getTracksFulltree($playlist);
 			}
 
-			return $result;
+			return new JSONResponse($result);
 		} catch (BusinessLayerException $ex) {
 			return new ErrorResponse(Http::STATUS_NOT_FOUND, $ex->getMessage());
 		}
@@ -176,7 +178,7 @@ class PlaylistApiController extends Controller {
 	 */
 	public function generate(
 			?bool $useLatestParams, ?string $history, ?string $genres, ?string $artists,
-			?int $fromYear, ?int $toYear, ?string $favorite=null, int $size=100, string $historyStrict='false') {
+			?int $fromYear, ?int $toYear, ?string $favorite=null, int $size=100, string $historyStrict='false') : JSONResponse {
 
 		if ($useLatestParams) {
 			$history = $this->configManager->getUserValue($this->userId, $this->appName, 'smartlist_history') ?: null;
@@ -218,7 +220,7 @@ class PlaylistApiController extends Controller {
 			'size' => $size
 		];
 
-		return $result;
+		return new JSONResponse($result);
 	}
 
 	/**
@@ -228,7 +230,7 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function getCover(int $id) {
+	public function getCover(int $id) : Response {
 		try {
 			$playlist = $this->playlistBusinessLayer->find($id, $this->userId);
 			$cover = $this->coverService->getCover($playlist, $this->userId, $this->userFolder);
@@ -250,7 +252,7 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function update(int $id, ?string $name = null, ?string $comment = null, ?string $trackIds = null) {
+	public function update(int $id, ?string $name = null, ?string $comment = null, ?string $trackIds = null) : JSONResponse {
 		$result = null;
 		if ($name !== null) {
 			$result = $this->modifyPlaylist('rename', [$name, $id, $this->userId]);
@@ -258,7 +260,7 @@ class PlaylistApiController extends Controller {
 		if ($comment !== null) {
 			$result = $this->modifyPlaylist('setComment', [$comment, $id, $this->userId]);
 		}
-		if ($trackIds!== null) {
+		if ($trackIds !== null) {
 			$result = $this->modifyPlaylist('setTracks', [self::toIntArray($trackIds), $id, $this->userId]);
 		}
 		if ($result === null) {
@@ -270,25 +272,25 @@ class PlaylistApiController extends Controller {
 	/**
 	 * insert or append tracks to a playlist
 	 * @param int $id playlist ID
-	 * @param string|int $track Comma-separated list of track IDs
+	 * @param string|int|null $track Comma-separated list of track IDs
 	 * @param ?int $index Insertion position within the playlist, or null to append
 	 *
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function addTracks(int $id, $track, ?int $index = null) {
+	public function addTracks(int $id, /*mixed*/ $track, ?int $index = null) : JSONResponse {
 		return $this->modifyPlaylist('addTracks', [self::toIntArray($track), $id, $this->userId, $index]);
 	}
 
 	/**
 	 * removes tracks from a playlist
 	 * @param int $id playlist ID
-	 * @param string|int $index Comma-separated list of track indices within the playlist
+	 * @param string|int|null $index Comma-separated list of track indices within the playlist
 	 *
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function removeTracks(int $id, $index) {
+	public function removeTracks(int $id, /*mixed*/ $index) : JSONResponse {
 		return $this->modifyPlaylist('removeTracks', [self::toIntArray($index), $id, $this->userId]);
 	}
 
@@ -299,9 +301,12 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function reorder(int $id, $fromIndex, $toIndex) {
-		return $this->modifyPlaylist('moveTrack',
-				[$fromIndex, $toIndex, $id, $this->userId]);
+	public function reorder(int $id, ?int $fromIndex, ?int $toIndex) : JSONResponse {
+		if ($fromIndex === null || $toIndex === null) {
+			return new ErrorResponse(Http::STATUS_BAD_REQUEST, "Arguments 'fromIndex' and 'toIndex' are required");
+		} else {
+			return $this->modifyPlaylist('moveTrack', [$fromIndex, $toIndex, $id, $this->userId]);
+		}
 	}
 
 	/**
@@ -318,7 +323,7 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function exportToFile(int $id, string $path, ?string $filename=null, string $oncollision='abort') {
+	public function exportToFile(int $id, string $path, ?string $filename=null, string $oncollision='abort') : JSONResponse {
 		try {
 			$exportedFilePath = $this->playlistFileService->exportToFile(
 					$id, $this->userId, $this->userFolder, $path, $filename, $oncollision);
@@ -342,11 +347,11 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function importFromFile(int $id, string $filePath) {
+	public function importFromFile(int $id, string $filePath) : JSONResponse {
 		try {
 			$result = $this->playlistFileService->importFromFile($id, $this->userId, $this->userFolder, $filePath);
 			$result['playlist'] = $result['playlist']->toApi($this->urlGenerator);
-			return $result;
+			return new JSONResponse($result);
 		} catch (BusinessLayerException $ex) {
 			return new ErrorResponse(Http::STATUS_NOT_FOUND, 'playlist not found');
 		} catch (\OCP\Files\NotFoundException $ex) {
@@ -363,7 +368,7 @@ class PlaylistApiController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
-	public function parseFile(int $fileId) {
+	public function parseFile(int $fileId) : JSONResponse {
 		try {
 			$result = $this->playlistFileService->parseFile($fileId, $this->userFolder);
 

commit e731ef90580cfe27ca0c91e437f3cb9f0071dfa6

Fix favorites toggle not working in the details pane
Apparently this has been broken since 6e94d13c. A similar problem was fixed
for many other controllers in ed8f5a16 but it didn't address the issue
correctly on the FavoritesController.

refs https://github.com/owncloud/music/issues/1250

lib/Controller/FavoritesController.php
@@ -75,48 +75,54 @@ class FavoritesController extends Controller {
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoriteTrack(int $id, ?string $status) : JSONResponse {
+	public function setFavoriteTrack(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->trackBusinessLayer, $id, $status);
 	}
 
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoriteAlbum(int $id, ?string $status) : JSONResponse {
+	public function setFavoriteAlbum(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->albumBusinessLayer, $id, $status);
 	}
 
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoriteArtist(int $id, ?string $status) : JSONResponse {
+	public function setFavoriteArtist(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->artistBusinessLayer, $id, $status);
 	}
 
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoritePlaylist(int $id, ?string $status) : JSONResponse {
+	public function setFavoritePlaylist(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->playlistBusinessLayer, $id, $status);
 	}
 
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoriteChannel(int $id, ?string $status) : JSONResponse {
+	public function setFavoriteChannel(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->podcastChannelBusinessLayer, $id, $status);
 	}
 
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
+	 * @param string|int|bool|null $status
 	 */
-	public function setFavoriteEpisode(int $id, ?string $status) : JSONResponse {
+	public function setFavoriteEpisode(int $id, /*mixed*/ $status) : JSONResponse {
 		return $this->setFavorite($this->podcastEpisodeBusinessLayer, $id, $status);
 	}
 
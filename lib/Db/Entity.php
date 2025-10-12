owncloud/music

commit 3ca8b9968c07b6bbd3902ab6bd2fb6f892494535

Add property type declarations to all entity classes
Strictly types properties must always have also default values set upon
construction.

lib/Db/Entity.php
@@ -19,15 +19,15 @@ use OCP\IL10N;
  * 
  * @method string getUserId()
  * @method void setUserId(string $userId)
- * @method string getCreated()
- * @method setCreated(string $timestamp)
- * @method string getUpdated()
- * @method setUpdated(string $timestamp)
+ * @method ?string getCreated()
+ * @method setCreated(?string $timestamp)
+ * @method ?string getUpdated()
+ * @method setUpdated(?string $timestamp)
  */
 class Entity extends \OCP\AppFramework\Db\Entity {
-	public $userId;
-	public $created;
-	public $updated;
+	public string $userId = '';
+	public ?string $created = null;
+	public ?string $updated = null;
 
 	/**
 	 * All entities have a non-empty human-readable name, although the exact name of the

lib/BusinessLayer/TrackBusinessLayer.php
@@ -395,8 +395,8 @@ class TrackBusinessLayer extends BusinessLayer {
 	 * @return Track The added/updated track
 	 */
 	public function addOrUpdateTrack(
-			$title, $number, $discNumber, $year, $genreId, $artistId, $albumId,
-			$fileId, $mimetype, $userId, $length=null, $bitrate=null) {
+			string $title, ?int $number, ?int $discNumber, ?int $year, int $genreId, int $artistId, int $albumId,
+			int $fileId, string $mimetype, string $userId, ?int $length=null, ?int $bitrate=null) : Track {
 		$track = new Track();
 		$track->setTitle(StringUtil::truncate($title, 256)); // some DB setups can't truncate automatically to column max size
 		$track->setNumber($number);

lib/Db/Album.php
@@ -33,30 +33,34 @@ use OCP\IURLGenerator;
  * @method void setHash(string $hash)
  * @method ?string getStarred()
  * @method void setStarred(?string $timestamp)
- * @method ?int getRating()
- * @method setRating(?int $rating)
+ * @method int getRating()
+ * @method setRating(int $rating)
  * @method ?string getAlbumArtistName()
  */
 class Album extends Entity {
-	public $name;
-	public $mbid;
-	public $mbidGroup;
-	public $coverFileId;
-	public $albumArtistId;
-	public $hash;
-	public $starred;
-	public $rating;
-	public $albumArtistName; // not from music_albums table but still part of the standard content
-	public $disk; // deprecated
+	public ?string $name = null;
+	public ?string $mbid = null;
+	public ?string $mbidGroup = null;
+	public ?int $coverFileId = null;
+	public ?int $albumArtistId = null;
+	public string $hash = '';
+	public ?string $starred = null;
+	public int $rating = 0;
+	public ?string $albumArtistName = null; // not from music_albums table but still part of the standard content
+	public ?int $disk = 0; // deprecated
 
 	// extra fields injected separately by AlbumBusinessLayer
+	/** @var ?int[] $years */
 	private ?array $years = null;
-	private ?array $genres = null; // *partial* Genre objects, not all properties are set
+	/** @var ?Genre[] $genres - AlbumBusinessLayer injects *partial* Genre objects, not all properties are set */
+	private ?array $genres = null;
+	/** @var ?int[] $artistIds */
 	private ?array $artistIds = null;
 	private ?int $numberOfDisks = null;
 
 	// injected separately when needed
-	private $tracks;
+	/** @var ?Track[] $tracks */
+	private ?array $tracks = null;
 
 	public function __construct() {
 		$this->addType('disk', 'int');
@@ -65,26 +69,44 @@ class Album extends Entity {
 		$this->addType('rating', 'int');
 	}
 
+	/**
+	 * @return ?int[]
+	 */
 	public function getYears() : ?array {
 		return $this->years;
 	}
 
+	/**
+	 * @param ?int[] $years
+	 */
 	public function setYears(?array $years) : void {
 		$this->years = $years;
 	}
 
+	/**
+	 * @return ?Genre[]
+	 */
 	public function getGenres() : ?array {
 		return $this->genres;
 	}
 
+	/**
+	 * @param ?Genre[] $genres
+	 */
 	public function setGenres(?array $genres) : void {
 		$this->genres = $genres;
 	}
 
+	/**
+	 * @return ?int[]
+	 */
 	public function getArtistIds() : ?array {
 		return $this->artistIds;
 	}
 
+	/**
+	 * @param ?int[] $artistIds
+	 */
 	public function setArtistIds(?array $artistIds) : void {
 		$this->artistIds = $artistIds;
 	}

lib/Db/AmpacheSession.php
@@ -9,7 +9,7 @@
  * @author Morris Jobke <hey@morrisjobke.de>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
- * @copyright Pauli Järvinen 2023
+ * @copyright Pauli Järvinen 2023 - 2025
  */
 
 namespace OCA\Music\Db;
@@ -27,11 +27,11 @@ namespace OCA\Music\Db;
  * @method setAmpacheUserId(int $id)
  */
 class AmpacheSession extends \OCP\AppFramework\Db\Entity {
-	public $userId;
-	public $token;
-	public $expiry;
-	public $apiVersion;
-	public $ampacheUserId;
+	public string $userId = '';
+	public string $token = '';
+	public int $expiry = 0;
+	public ?string $apiVersion = null;
+	public int $ampacheUserId = 0;
 
 	public function __construct() {
 		$this->addType('expiry', 'int');

lib/Db/Artist.php
@@ -28,20 +28,22 @@ use OCP\IURLGenerator;
  * @method void setHash(string $hash)
  * @method ?string getStarred()
  * @method void setStarred(?string $timestamp)
- * @method ?int getRating()
- * @method setRating(?int $rating)
+ * @method int getRating()
+ * @method setRating(int $rating)
  */
 class Artist extends Entity {
-	public $name;
-	public $coverFileId;
-	public $mbid;
-	public $hash;
-	public $starred;
-	public $rating;
+	public ?string $name = null;
+	public ?int $coverFileId = null;
+	public ?string $mbid = null;
+	public string $hash = '';
+	public ?string $starred = null;
+	public int $rating = 0;
 
 	// not part of the standard content, injected separately when needed
 	private ?string $lastfmUrl = null;
+	/** @var ?Album[] $albums */
 	private ?array $albums = null;
+	/** @var ?Track[] $tracks */
 	private ?array $tracks = null;
 
 	public function __construct() {

lib/Db/Bookmark.php
@@ -9,7 +9,7 @@
  * @author Gavin E <no.emai@address.for.me>
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Gavin E 2020
- * @copyright Pauli Järvinen 2020 - 2023
+ * @copyright Pauli Järvinen 2020 - 2025
  */
 
 namespace OCA\Music\Db;
@@ -29,10 +29,10 @@ use OCP\IL10N;
  * @method void setComment(?string $comment)
  */
 class Bookmark extends Entity {
-	public $type;
-	public $entryId;
-	public $position;
-	public $comment;
+	public int $type = 0;
+	public int $entryId = 0;
+	public int $position = 0;
+	public ?string $comment = null;
 
 	public const TYPE_TRACK = 1;
 	public const TYPE_PODCAST_EPISODE = 2;

lib/Db/Genre.php
@@ -15,25 +15,26 @@ namespace OCA\Music\Db;
 use OCP\IL10N;
 
 /**
- * @method string getName()
- * @method void setName(string $name)
- * @method string getLowerName()
- * @method void setLowerName(string $lowerName)
+ * @method ?string getName()
+ * @method void setName(?string $name)
+ * @method ?string getLowerName()
+ * @method void setLowerName(?string $lowerName)
  *
  * @method int getTrackCount()
  * @method int getAlbumCount()
  * @method int getArtistCount()
  */
 class Genre extends Entity {
-	public $name;
-	public $lowerName;
+	public ?string $name = null;
+	public ?string $lowerName = null;
 	// not from the music_genres table but still part of the standard content of this entity
-	public $trackCount;
-	public $albumCount;
-	public $artistCount;
+	public int $trackCount = 0;
+	public int $albumCount = 0;
+	public int $artistCount = 0;
 
 	// not part of the standard content, injected separately when needed
-	public $trackIds;
+	/** @var ?int[] $trackIds */
+	public ?array $trackIds = null;
 
 	public function __construct() {
 		$this->addType('trackCount', 'int');

lib/Db/Playlist.php
@@ -18,27 +18,27 @@ use OCA\Music\Utility\Util;
 use OCP\IURLGenerator;
 
 /**
- * @method string getName()
- * @method setName(string $name)
- * @method string getTrackIds()
- * @method setTrackIds(string $trackIds)
- * @method string getComment()
- * @method setComment(string $comment)
+ * @method ?string getName()
+ * @method setName(?string $name)
+ * @method ?string getTrackIds()
+ * @method setTrackIds(?string $trackIds)
+ * @method ?string getComment()
+ * @method setComment(?string $comment)
  * @method ?string getStarred()
  * @method void setStarred(?string $timestamp)
- * @method ?int getRating()
- * @method setRating(?int $rating)
+ * @method int getRating()
+ * @method setRating(int $rating)
  */
 class Playlist extends Entity {
-	public $name;
-	public $trackIds;
-	public $comment;
-	public $starred;
-	public $rating;
+	public ?string $name = null;
+	public ?string $trackIds = null;
+	public ?string $comment = null;
+	public ?string $starred = null;
+	public int $rating = 0;
 
 	// injected separately when needed
-	private $duration;
-	private $readOnly;
+	private ?int $duration = null;
+	private bool $readOnly = false;
 
 	public function __construct() {
 		$this->addType('rating', 'int');
@@ -53,7 +53,7 @@ class Playlist extends Entity {
 	}
 
 	public function getReadOnly() : bool {
-		return $this->readOnly ?? false;
+		return $this->readOnly;
 	}
 
 	public function setReadOnly(bool $readOnly) : void {

commit 9c4cd10a59c2ef9f8b7c5e21c43733f9a94694a2

Fix files remaining "dirty" even after rescan when folder moved
The root cause of this was a mismatch between how the platform Entity and
Mapper classes track changed fields and what the Music app assumed and
required. This in combination with changing the `Track::dirty` default value
from (implicit) null to 0 in 3ca8b9968c0 caused the bug
https://github.com/owncloud/music/issues/1251. However, this same principal
issue may have caused also other subtle bugs in various cases not related to
the `dirty` field.

lib/Db/Entity.php
@@ -47,4 +47,27 @@ class Entity extends \OCP\AppFramework\Db\Entity {
 			return 'UNIMPLEMENTED';
 		}
 	}
+
+	/**
+	 * Override the property setter from the platform base class.
+	 *
+	 * NOTE: Type declarations should not be used on the parameters because OC and NC < 26
+	 * don't use them in the parent class. On those platforms, using the declarations in this
+	 * override method would break the PHP contravariance rules.
+	 *
+	 * @param string $name
+	 * @param mixed[] $args
+	 */
+	protected function setter($name, $args) : void {
+		parent::setter($name, $args);
+		/**
+		 * The parent implementation has such a feature that it doesn't mark a field updated
+		 * if the new value is the same as the previous value. This is problematic in case
+		 * we have created a new Entity and the new value is the same as the default value.
+		 * We may still use that new Entity with Mapper::update with the intention to change
+		 * that field on an existing row to its default value. This is what caused
+		 * https://github.com/owncloud/music/issues/1251 and probably some other subtle bugs, too.
+		 */
+		$this->markFieldUpdated($name);
+	}
 }
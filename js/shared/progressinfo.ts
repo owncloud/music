owncloud/music

commit c6612540443757038da75e02b3592c4824e3dffd

Refactoring: Utilize the shared ProgressInfo also in Music app proper
There's no reason to duplicate the progress component logic using the
AngularJS syntax. We now have a custom element directive <progress-info>
which is just a thin wrapper for the ProgressInfo class used also in the
embedded Files player and the Dashboard widget. The shared ProgressInfo
communicates directly with the PlayerWrapper and needs no other boiler-plate
in the application code.

js/shared/progressinfo.ts
@@ -5,7 +5,7 @@
  * later. See the COPYING file.
  *
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2024
+ * @copyright Pauli Järvinen 2024, 2025
  */
 
 import { PlayerWrapper } from "./playerwrapper";
@@ -31,6 +31,10 @@ export class ProgressInfo {
 
 	addToContainer(container : JQuery<HTMLElement>) : void {
 		container.append(this.#elem);
+	}
+
+	getElement() : JQuery<HTMLElement> {
+		return this.#elem;
 	}
 
 	hide() : void {

css/app/music-controls.css
@@ -131,50 +131,13 @@ body:not(.snapjs-left) .app-music.nc14to24 #controls {
 	line-height: 29px;
 }
 
-#controls .progress-info {
+#controls .music-progress-info {
 	position: absolute;
 	top: 0px;
 	left: 710px;
 	right: 165px;
 	max-width: 35%;
-	text-align: center;
 	margin: 7px auto 14px auto;
-}
-
-#controls .progress-info span {
-	line-height: 30px;
-}
-
-#controls .progress, #controls .seek-bar {
-	width: 100%;
-	height: 15px;
-	position: relative;
-	background-color: #eee;
-}
-
-#controls.started .progress, #controls.started .seek-bar, #controls.started .play-bar, #controls .buffer-bar {
-	display: block;
-}
-
-#controls .play-bar, #controls .buffer-bar {
-	position: absolute;
-	left: 0;
-	top: 0;
-	height: 15px;
-	width: 0%;
-	background-color: var(--color-primary, #1d2d44);
-}
-
-.ie #controls .play-bar, #controls .buffer-bar {
-	background-color: #1d2d44;
-}
-
-.translucent {
-	opacity: 0.75;
-}
-
-#controls .buffer-bar {
-	opacity: 0.1;
 }
 
 #shuffle {
@@ -231,27 +194,28 @@ body:not(.snapjs-left) .app-music.nc14to24 #controls {
 	right: 80px;
 }
 
-#controls.two-line .progress-info {
+#controls.two-line .music-progress-info {
 	left: 270px;
 	right: 20px;
 	top: 60px;
 	max-width: 100%;
+	height: 30px;
 }
 
-#controls.two-line .progress-info .progress-text {
+#controls.two-line .music-progress-info .progress-text {
 	position: absolute;
 	right: 0px;
 	top: -8px;
 	width: 90px;
 }
 
-#controls.two-line .progress-info .progress {
+#controls.two-line .music-progress-info .seek-bar {
 	position: absolute;
 	width: auto;
 	left: 0;
 	right: 90px;
 }
 
-#controls.two-line .progress-info {
+#controls.two-line .music-progress-info {
 	left: 212px;
 }

css/app/music-mobile.css
@@ -102,7 +102,7 @@
 .mobile #controls.two-line #repeat {
 	left: 108px;
 }
-.mobile #controls.two-line .progress-info {
+.mobile #controls.two-line .music-progress-info {
 	left: 172px;
 }
 .mobile #controls #play-pause-menu {
@@ -125,13 +125,13 @@
 .mobile.portrait #controls.two-line .song-info {
 	left: 172px;
 }
-.mobile.portrait #controls.two-line .progress-info {
+.mobile.portrait #controls.two-line .music-progress-info {
 	left: 124px;
 }
 .mobile.portrait #controls #play-pause-menu {
 	right: -54px;
 }
-.mobile.portrait #controls #play-pause-menu::after /* the carret */ {
+.mobile.portrait #controls #play-pause-menu::after /* the caret */ {
 	left: 36px;
 }
 
@@ -155,13 +155,13 @@
 }
 
 /* min-width screen may result on desktop browser when manually resizing the window; here we need to drop also essential elements */
-.mobile.portrait.min-width #controls .progress,
+.mobile.portrait.min-width #controls .music-progress-info .seek-bar,
 .mobile.portrait.min-width #controls .music-volume-control {
 	display: none;
 }
 .mobile.portrait.min-width #controls.two-line .song-info {
 	right: 10px;
 }
-.mobile.portrait.min-width #controls .progress-text {
+.mobile.portrait.min-width #controls .music-progress-info .progress-text {
 	text-align: right;
 }

css/app/music-tablet.css
@@ -9,11 +9,11 @@
  * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
  * @copyright Morris Jobke 2013, 2014
  * @copyright Jan-Christoph Borchardt 2014
- * @copyright Pauli Järvinen 2016 - 2022
+ * @copyright Pauli Järvinen 2016 - 2025
  */
 
 /* Adjust layout so that there is room for the toggle button of the collapsible navigation pane.
-   The rule has to match the one used by the core to make the pane collpsible.
+   The rule has to match the one used by the core to make the pane collapsible.
  */
 @media only screen and (max-width: 1024px) {
 	#app-view {
@@ -46,7 +46,7 @@
 	width: 200px;
 }
 
-.tablet #controls:not(.two-line) .progress-info {
+.tablet #controls:not(.two-line) .music-progress-info {
 	left: 460px;
 	max-width: 350px;
 }

js/app/controllers/playercontroller.js
@@ -25,17 +25,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 	$scope.repeat = OCA.Music.Storage.get('repeat') || 'false';
 	$scope.shuffle = (OCA.Music.Storage.get('shuffle') === 'true');
 	$scope.playbackRate = 1.0;  // rate can be 0.5~3.0
-	$scope.position = {
-		bufferPercent: 0,
-		currentPercent: 0,
-		previewPercent: 0,
-		currentPreview: null,
-		currentPreview_ts: null, // Activation time stamp (Type: Date)
-		currentPreview_tf: null, // Transient mouse movement filter (Type: Number/Timer-Handle)
-		previewVisible: () => ($scope.position.currentPreview !== null) && !$scope.position.currentPreview_tf,
-		current: 0,
-		total: 0
-	};
 	let scrobblePending = false;
 	let scheduledRadioTitleFetch = null;
 	let abortRadioTitleFetch = null;
@@ -80,8 +69,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 	}
 
 	onPlayerEvent('buffer', function (percent) {
-		$scope.setBufferPercentage(percent);
-
 		// prepare the next song once buffering this one is done (sometimes the percent never goes above something like 99.996%)
 		if (percent > 99 && $scope.currentTrack.type === 'song') {
 			let entry = playQueueService.peekNextTrack();
@@ -93,12 +80,8 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 			}
 		}
 	});
-	onPlayerEvent('ready', function () {
-		$scope.setLoading(false);
-	});
 	onPlayerEvent('progress', function (currentTime) {
 		if (!$scope.loading && $scope.currentTrack) {
-			$scope.setTime(currentTime/1000, $scope.position.total);
 			$rootScope.$emit('playerProgress', currentTime);
 
 			// Scrobble when the track has been listened for 10 seconds
@@ -107,8 +90,8 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 			}
 
 			// Gapless jump to the next track when the playback is very close to the end of a local track
-			if ($scope.position.total > 0 && $scope.currentTrack.type === 'song' && $scope.repeat !== 'one') {
-				let timeLeft = $scope.position.total*1000 - currentTime;
+			if ($scope.player.getDuration() > 0 && $scope.currentTrack.type === 'song' && $scope.repeat !== 'one') {
+				let timeLeft = $scope.player.getDuration() - currentTime;
 				if (timeLeft < GAPLESS_PLAY_OVERLAP_MS) {
 					let nextTrackId = playQueueService.peekNextTrack()?.track?.id;
 					if (nextTrackId !== null && nextTrackId !== $scope.currentTrack.id) {
@@ -117,21 +100,8 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 				}
 			}
 		}
-
-		// Show progress again instead of preview after a timeout of 2000ms
-		if ($scope.position.currentPreview_ts) {
-			let timeSincePreview = Date.now() - $scope.position.currentPreview_ts;
-			if (timeSincePreview >= 2000) {
-				$scope.seekbarLeave();
-			}
-		}
 	});
 	onPlayerEvent('end', onEnd);
-	onPlayerEvent('duration', function(msecs) {
-		$scope.setTime($scope.position.current, msecs/1000);
-		// Seeking may be possible once the duration is available
-		$scope.seekCursorType = $scope.player.seekingSupported() ? 'pointer' : 'default';
-	});
 	onPlayerEvent('error', function(url) {
 		OC.Notification.showTemporary(gettextCatalog.getString('Error playing URL:') + ' ' + url);
 		// Jump automatically to the next track unless we were playing an external stream
@@ -169,10 +139,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 		}
 		scrobblePending = false;
 	}
-
-	$scope.durationKnown = function() {
-		return $.isNumeric($scope.position.total) && $scope.position.total !== 0;
-	};
 
 	$scope.$watch('currentTrack', function(newTrack) {
 		updateWindowTitle(newTrack);
@@ -264,7 +230,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 		if (track !== null) {
 			// switch initial state
 			$rootScope.started = true;
-			$scope.setLoading(true);
 			playTrack(track, startOffset, gapless);
 		} else {
 			$scope.stop();
@@ -343,19 +308,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 		return {track: $scope.currentTrack?.id};
 	};
 
-	$scope.setLoading = function(loading) {
-		$scope.loading = loading;
-		if (loading) {
-			$scope.position.current = 0;
-			$scope.position.currentPercent = 0;
-			$scope.position.currentPreview = null;
-			$scope.position.currentPreview_ts = null;
-			$scope.position.currentPreview_tf = null;
-			$scope.position.bufferPercent = 0;
-			$scope.position.total = 0;
-		}
-	};
-
 	const notifyPlaybackRateNotAdjustable = _.debounce(
 		() => OC.Notification.showTemporary(gettextCatalog.getString('Playback speed not adjustible for the current song')),
 		1000, {leading: true, trailing: false}
@@ -390,21 +342,6 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 		$scope.repeat = nextState[$scope.repeat];
 		playQueueService.setRepeat($scope.repeat !== 'false'); // the "repeat-one" is handled internally by the PlayerController
 		OCA.Music.Storage.set('repeat', $scope.repeat);
-	};
-
-	$scope.setTime = function(position, duration) {
-		// sometimes the duration is reported slightly incorrectly and position may exceed it by a few seconds
-		if (duration > 0 && duration < position) {
-			duration = position;
-		}
-
-		$scope.position.current = position;
-		$scope.position.total = duration;
-		$scope.position.currentPercent = (duration > 0) ? position/duration*100 : 0;
-	};
-
-	$scope.setBufferPercentage = function(percent) {
-		$scope.position.bufferPercent = Math.min(100, percent);
 	};
 
 	$scope.play = function() {
@@ -515,7 +452,7 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 	$scope.prev = function() {
 		// Jump to the beginning of the current track if it has already played more than 2 secs.
 		// This is disabled for radio streams where jumping to the beginning often does not work.
-		if ($scope.position.current > 2.0 && $scope.currentTrack?.type !== 'radio') {
+		if ($scope.player.playPosition() > 2000 && $scope.currentTrack?.type !== 'radio') {
 			$scope.player.seek(0);
 		}
 		// Jump to the previous track if the current track has played only 2 secs or less
@@ -527,66 +464,14 @@ function ($scope, $rootScope, playQueueService, Audio, gettextCatalog, Restangul
 		}
 	};
 
-	// Seekbar preview mouse support
-	function updateSeekPreview(xCoordinate) {
-		let seekBar = $('.seek-bar');
-		let offsetX = xCoordinate - seekBar.offset().left;
-		offsetX = Math.min(Math.max(0, offsetX), seekBar.width());
-		let ratio = offsetX / seekBar.width();
-		let timestamp = ratio * $scope.position.total;
-
-		$scope.position.previewPercent = ratio * 100;
-		$scope.position.currentPreview = timestamp;
-	}
-
-	$scope.seekbarPreview = function($event) {
+	$scope.seekOffset = function(offset_s) {
 		if ($scope.player.seekingSupported()) {
-			updateSeekPreview($event.clientX);
-			$scope.position.currentPreview_ts = Date.now();
-		}
-	};
-
-	$scope.seekbarEnter = function() {
-		// Simple filter for transient mouse movements
-		$scope.position.currentPreview_tf = $timeout(() => $scope.position.currentPreview_tf = null, 100);
-	};
-
-	$scope.seekbarLeave = function() {
-		$scope.position.currentPreview = null;
-		$scope.position.currentPreview_ts = null;
-	};
-
-	// Seekbar preview touch support
-	$scope.seekbarTouchLeave = function($event) {
-		if ($scope.player.seekingSupported() && $event?.type === 'touchend') {
-			$scope.player.seek($scope.position.previewPercent / 100);
-			$scope.seekbarLeave();
-		}
-	};
-
-	$scope.seekbarTouchPreview = function($event) {
-		if ($scope.player.seekingSupported()) {
-			updateSeekPreview($event.targetTouches[0].clientX);
-		}
-	};
-
-	$scope.seekOffset = function(offset) {
-		if ($scope.player.seekingSupported()) {
-			const target = $scope.position.current + offset;
-			let ratio = target / $scope.position.total;
+			const target = $scope.player.playPosition() + offset_s * 1000;
+			let ratio = target / $scope.player.getDuration();
 			// Clamp between the begin and end of the track
 			ratio = Math.min(Math.max(0, ratio), 1);
 			$scope.player.seek(ratio);
 		}
-	};
-
-	// Seekbar click / tap
-	$scope.seek = function($event) {
-		let seekBar = $('.seek-bar');
-		let offsetX = $event.clientX - seekBar.offset().left;
-		let ratio = offsetX / seekBar.width();
-		$scope.player.seek(ratio);
-		$scope.seekbarLeave(); // Reset seek preview
 	};
 
 	$scope.seekBackward = $scope.player.seekBackward;

js/app/directives/progressinfo.js
@@ -1,0 +1,26 @@
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright 2025 Pauli Järvinen
+ *
+ */
+
+import { ProgressInfo } from 'shared/progressinfo';
+
+angular.module('Music').directive('progressInfo', [
+function () {
+	return {
+		restrict: 'E',
+		scope: {
+			player: '<'
+		},
+		link: function(scope, element, _attrs, _ctrl) {
+			const widget = new ProgressInfo(scope.player);
+			element.replaceWith(widget.getElement());
+		}
+	};
+}]);

templates/partials/controls.php
@@ -64,36 +64,16 @@
 		<span class="title" title="{{ primaryTitle() }}">{{ primaryTitle() }}</span><br />
 		<span class="artist" title="{{ secondaryTitle() }}">{{ secondaryTitle() }}</span>
 	</div>
-	<div ng-show="currentTrack" class="progress-info">
-		<div class="progress-text">
-			<span ng-show="!loading" class="muted" ng-style="position.previewVisible() && {'font-style': 'italic'}">
-				{{ (position.previewVisible() ? position.currentPreview : position.current) | playTime }}
-			</span>
-			<span ng-show="!loading && durationKnown()" class="muted">/ {{ position.total | playTime }}</span>
-			<span ng-show="loading" class="muted">Loading...</span>
-		</div>
-		<div class="progress">
-			<div class="seek-bar" ng-style="{'cursor': seekCursorType}"
-				ng-click="seek($event)" ng-mousemove="seekbarPreview($event)" ng-mouseenter="seekbarEnter($event)" ng-mouseleave="seekbarLeave($event)"
-				ng-on-touchmove="seekbarTouchPreview($event)" ng-on-touchend="seekbarTouchLeave($event)"
-			>
-				<div class="buffer-bar" ng-style="{'width': position.bufferPercent + '%', 'cursor': seekCursorType}"></div>
-				<div class="play-bar" ng-show="position.total"
-					ng-style="{'width': (position.previewVisible() ? min(position.currentPercent, position.previewPercent) : position.currentPercent) + '%',
-								'cursor': seekCursorType}"></div>
-				<div class="play-bar translucent" ng-show="position.total && position.previewVisible()"
-					ng-style="{'width': abs(position.currentPercent - position.previewPercent) + '%',
-								'left': min(position.currentPercent, position.previewPercent) + '%',
-								'cursor': seekCursorType}"></div>
-			</div>
-		</div>
-	</div>
+
+	<progress-info ng-show="currentTrack" player="player">
+	</progress-info>
 
 	<img id="shuffle" class="control toggle small svg" alt="{{ 'Shuffle' | translate }}" title="{{ shuffleTooltip() }}"
 		src="<?php HtmlUtil::printSvgPath('shuffle') ?>" ng-class="{active: shuffle}" ng-click="toggleShuffle()" />
 	<img id="repeat" class="control toggle small svg" alt="{{ 'Repeat' | translate }}" title="{{ repeatTooltip() }}"
 		src="{{ repeat === 'one' ? '<?php HtmlUtil::printSvgPath('repeat-1') ?>' : '<?php HtmlUtil::printSvgPath('repeat') ?>' }}"
 		ng-class="{active: repeat != 'false' }" ng-click="toggleRepeat()" />
+
 	<volume-control player="player">
 	</volume-control>
 </div>

commit 5a512713d331cec0d14f7fe0d76ce6d9b9a7306f

Fix a small layout glitch on OC caused by c661254
The ownCloud core defines some layout rules for the class "separator" which
messed up our UI a bit. Changed our own class name to "time-separator" to
avoid being affected by the core rule.

css/embedded/files-music-player.css
@@ -88,9 +88,8 @@
 	box-sizing: border-box;
 }
 
-#music-controls .separator {
+#music-controls .time-separator {
 	display: inline;
-	border: none;
 }
 
 #music-controls .control {

js/shared/progressinfo.ts
@@ -50,7 +50,7 @@ export class ProgressInfo {
 
 		let text = $('<span class="progress-text text-loaded" />');
 		$('<span class="play-time" />').appendTo(text);
-		$('<span class="separator">\xa0/\xa0</span>').appendTo(text);
+		$('<span class="time-separator">\xa0/\xa0</span>').appendTo(text);
 		$('<span class="song-length" />').appendTo(text);
 
 		let seekBar = $(document.createElement('div')).attr('class', 'seek-bar');
@@ -69,7 +69,7 @@ export class ProgressInfo {
 
 	#updateProgress() : void {
 		const text_playTime = this.#elem.find('.progress-text .play-time');
-		const text_separator = this.#elem.find('.progress-text .separator');
+		const text_separator = this.#elem.find('.progress-text .time-separator');
 		const text_songLength = this.#elem.find('.progress-text .song-length');
 		const playBar = this.#elem.find('.seek-bar .play-bar.solid');
 		const transBar = this.#elem.find('.seek-bar .play-bar.translucent');
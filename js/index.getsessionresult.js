6152db2713278f72be183047555f3ddaed1102fa
Matthew Wells <matthewpaulwells@gmail.com>
2025-11-21 23:28:46 -0500

    Add forgotten session result JS

---
+++ js/index.getsessionresult.js
@@ -0,0 +1,15 @@
+/**
+ * ownCloud - Music app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
+ * @copyright Pauli Järvinen 2020 - 2022
+ */
+
+const bc = new BroadcastChannel('scrobble-session-result');
+const appContent = document.querySelector('#app-content');
+if (appContent) {
+    bc.postMessage(Boolean(appContent.dataset.result));
+}

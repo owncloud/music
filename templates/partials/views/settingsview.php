0cfa5ae0974d20dd28e82486751852674f3df530
Matthew Wells <matthewpaulwells@gmail.com>
2025-11-22 09:29:10 -0500

    Improve instructions in configured state

--- templates/partials/views/settingsview.php
+++ templates/partials/views/settingsview.php
@@ -181,7 +181,9 @@
 	<h2 translate>Scrobbler Integration</h2>
 	<div>
 		<p ng-hide="settings.scrobbler.configured" translate>The server admin can configure your server for scrobbling. The <strong>Admin</strong> section below details how to achieve this.</p>
-		<p ng-show="settings.scrobbler.configured" translate >The server admin has configured your server for scrobbling. Click the <strong>Get Scrobble Session</strong> button to send your streams to {{settings.scrobbler.service}}</p>
+		<p ng-show="settings.scrobbler.configured" translate >The server admin has configured your server for scrobbling.</p>
+		<p ng-show="settings.scrobbler.configured && !settings.scrobbler.hasSession">Click the <strong>Get Scrobble Session</strong> button to send your streams to {{settings.scrobbler.service}}.</p>
+		<p ng-show="settings.scrobbler.configured && settings.scrobbler.hasSession">Click the <strong>Clear Session</strong> to stop scrobbling.</p>
 		<button ng-show="settings.scrobbler.configured" ng-disabled="settings.scrobbler.hasSession" ng-click="generateSession()" translate>Get Scrobble Session</button>
 		<button ng-show="settings.scrobbler.configured"  ng-disabled="!settings.scrobbler.hasSession" ng-click="clearSession()" translate>Clear Session</button>
 	</div>

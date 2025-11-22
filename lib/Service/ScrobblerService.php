f579d6e836ae2e5abcf9fa3a3a945a33c0f234bf
Matthew Wells <matthewpaulwells@gmail.com>
2025-11-22 16:45:50 -0500

    Update copyright/authorship to me!

--- lib/Service/ScrobblerService.php
+++ lib/Service/ScrobblerService.php
@@ -6,8 +6,8 @@
  * This file is licensed under the Affero General Public License version 3 or
  * later. See the COPYING file.
  *
- * @author Pauli Järvinen <pauli.jarvinen@gmail.com>
- * @copyright Pauli Järvinen 2020 - 2025
+ * @author Matthew Wells
+ * @copyright Matthew Wells 2025
  */
 
 namespace OCA\Music\Service;

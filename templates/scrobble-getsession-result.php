7027744f0c5b1c64bbbae938bce7edf25a163640
Pauli Järvinen <pauli.jarvinen@gmail.com>
2025-12-25 18:43:23 +0200

    Fix layout issues on the scrobbler session connect result page
    - On Nextcloud, the bottom of the #app-content background was cut off which
      looked ugly
    
    - On ownCloud, part or all of the text was hidden by default (depending on
      the window size) because the height of #app-content was very small
    
    - The view looks more balanced with centered text. This also works on RTL just
      as well as on LTR.

--- templates/scrobble-getsession-result.php
+++ templates/scrobble-getsession-result.php
@@ -11,18 +11,14 @@ HtmlUtil::addWebpackScript('scrobble_getsession_result');
 HtmlUtil::addWebpackStyle('app');
 
 ?>
-<div id="app-content" data-result="<?= $_['success'] ?>" data-identifier="<?= $_['identifier'] ?>">
+<div id="app-content" class="music-scrobble-getsession-result" data-result="<?= $_['success'] ?>" data-identifier="<?= $_['identifier'] ?>">
 	<div class="section">
-		<div id="app-view">
-			<div id="music-user">
-				<h2><?php HtmlUtil::p($_['headline']) ?></h2>
-				<div><?php HtmlUtil::p($_['instructions']) ?></div>
-				<?php if (!$_['success']): ?>
-				<div class="warning"><strong><?php HtmlUtil::p($_['getsession_response']) ?></strong></div>
-				<?php else: ?>
-				<div><?= $l->t('You can now close this window') ?></div>
-				<?php endif ?>
-			</div>
-		</div>
+		<h2><?php HtmlUtil::p($_['headline']) ?></h2>
+		<div><?php HtmlUtil::p($_['instructions']) ?></div>
+		<?php if (!$_['success']): ?>
+		<div class="warning"><strong><?php HtmlUtil::p($_['getsession_response']) ?></strong></div>
+		<?php else: ?>
+		<div><?= $l->t('You can now close this window') ?></div>
+		<?php endif ?>
 	</div>
 </div>

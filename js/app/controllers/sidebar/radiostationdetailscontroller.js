83c7e6a912b57625302ff243eb65ccf243e67642
Pauli Järvinen <pauli.jarvinen@gmail.com>
2025-12-25 13:26:49 +0200

    Handle race conditions on radio Last.fm data updating
    Previously, it could happen that the Last.fm data fetching got triggered
    multiple times back-to-back with different track/artist names. In this case,
    it was random, which one of the fetches finished last and was left visible on
    the UI. This could happen especially when switching back and forth between
    stations and the current song of one of the stations changed.
    
    With the changed logic, we may still sometimes fetch info for many songs
    back-to-back but only the data of the actual current track should get
    rendered. Also, we now store the fetched Last.fm data per-station,
    potentially reducing queries to the cloud server and eventually to the Last.fm
    server.

--- js/app/controllers/sidebar/radiostationdetailscontroller.js
+++ js/app/controllers/sidebar/radiostationdetailscontroller.js
@@ -65,46 +65,62 @@ angular.module('Music').controller('RadioStationDetailsController', [
 			$scope.updatedDate = OCA.Music.Utils.formatDateTime(updated);
 		});
 
-		var prevTitle = null;
 		$scope.$watch('station.metadata', function(metadata) {
-			if (metadata?.title != prevTitle) {
-				prevTitle = metadata?.title;
+			$scope.resetLastFmData();
+			$scope.lastfmCoverUrl = null;
 
-				$scope.resetLastFmData();
-				$scope.lastfmCoverUrl = null;
-				if (metadata?.title) {
-					// Split artist name and track title assuming the format "artist - track".
-					// If there are multiple instances of " - ", the first one is used as separator
-					// and the rest are assumed to belong to the track title.
-					const matches = metadata.title.match(/^(.+?) - (.+)$/);
-					if (matches === null) {
-						$scope.setLastfmPlaceholder(metadata.title, null);
-					} else {
-						updateDetails(matches[2], matches[1]);
-					}
+			if (metadata?.title == null) {
+				$scope.selectedTab = 'general';
+			} else {
+				// Split artist name and track title assuming the format "artist - track".
+				// If there are multiple instances of " - ", the first one is used as separator
+				// and the rest are assumed to belong to the track title.
+				const matches = metadata.title.match(/^(.+?) - (.+)$/);
+				if (matches === null) {
+					$scope.setLastfmPlaceholder(metadata.title, null);
 				} else {
-					$scope.selectedTab = 'general';
+					const [, artistName, songTitle] = matches;
+					$scope.setLastfmPlaceholder(songTitle, artistName);
+					if (metadata.title != $scope.station.nowPlaying?.title || $scope.station.nowPlaying?.lastfm == null) {
+						$scope.station.nowPlaying = {title: metadata?.title, lastfm: null};
+						updateDetails($scope.station, songTitle, artistName);
+					} else {
+						// previously fetched Last.fm data is still valid, show it
+						setLastfmInfo($scope.station.nowPlaying.lastfm);
+					}
 				}
 			}
 		});
 
 		// Update details from external sources like Last.fm
-		function updateDetails(songTitle, artistName) {
-			$scope.setLastfmPlaceholder(songTitle, artistName);
+		function updateDetails(station, songTitle, artistName) {
+			const titleOnFetch = station.nowPlaying.title;
 			Restangular.one('details').get({song: songTitle, artist: artistName}).then(
 				(response) => {
-					$scope.setLastfmTrackInfo(response.lastfm);
-					if (response.lastfm.track?.album?.image) {
-						// there are usually many image sizes provided but the last one should be the largest
-						const urlFromLastFm = response.lastfm.track.album.image.at(-1)['#text'];
-						// Last.fm may sometimes return empty URLs
-						if (urlFromLastFm.length > 0) {
-							$scope.lastfmCoverUrl = OC.generateUrl('apps/music/api/cover/external?url={url}', {url: urlFromLastFm});
+					// ignore the response if the current song of the station has changed in teh meantimne
+					if (station.nowPlaying.title == titleOnFetch) {
+						station.nowPlaying.lastfm = response.lastfm;
+
+						// Update the visible info only if the current station has not changed while fethcing the details
+						if ($scope.station == station) {
+							setLastfmInfo(response.lastfm);
 						}
 					}
 				},
 				(error) => console.error(error)
 			);
+		}
+
+		function setLastfmInfo(lastfmInfo) {
+			$scope.setLastfmTrackInfo(lastfmInfo);
+			if (lastfmInfo.track?.album?.image) {
+				// there are usually many image sizes provided but the last one should be the largest
+				const urlFromLastFm = lastfmInfo.track.album.image.at(-1)['#text'];
+				// Last.fm may sometimes return empty URLs
+				if (urlFromLastFm.length > 0) {
+					$scope.lastfmCoverUrl = OC.generateUrl('apps/music/api/cover/external?url={url}', {url: urlFromLastFm});
+				}
+			}
 		}
 
 		// Enter the edit mode
